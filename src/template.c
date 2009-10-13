a1124fa38e279873e75ceedc46af979e8d1710f0
Walter Bright <walter@digitalmars.com>
2009-10-13 23:20:04 +0000

    bugzilla 923

--- src/template.c
+++ src/template.c
@@ -3962,6 +3962,7 @@ Identifier *TemplateInstance::genIdent()
 	{   sinteger_t v;
 	    real_t r;
 
+	    ea = ea->optimize(WANTvalue | WANTinterpret);
 	    if (ea->op == TOKvar)
 	    {
 		sa = ((VarExp *)ea)->var;
