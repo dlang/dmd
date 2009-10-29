fac8efc2eb5a2c2bbdb6bc185c2907757185ce2b
Walter Bright <walter@digitalmars.com>
2009-10-29 05:06:58 +0000

    bugzilla 3349

--- src/typinf.c
+++ src/typinf.c
@@ -254,11 +254,7 @@ void TypeInfoSharedDeclaration::toDt(dt_t **pdt)
     //printf("TypeInfoSharedDeclaration::toDt() %s\n", toChars());
     dtxoff(pdt, Type::typeinfoshared->toVtblSymbol(), 0, TYnptr); // vtbl for TypeInfo_Shared
     dtdword(pdt, 0);			    // monitor
-    Type *tm;
-    if (tinfo->isConst())		// it was 'shared const'
-	tm = tinfo->constOf();
-    else				// it was just 'shared'
-	tm = tinfo->mutableOf();
+    Type *tm = tinfo->unSharedOf();
     tm = tm->merge();
     tm->getTypeInfo(NULL);
     dtxoff(pdt, tm->vtinfo->toSymbol(), 0, TYnptr);
