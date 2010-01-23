713fa8479f55bb194ee935bac19199a5b1b83f40
Walter Bright <walter@digitalmars.com>
2010-01-23 10:02:51 +0000

    bugzilla 3685 Regression(D1 only): DMD silently exits on valid code

--- src/template.c
+++ src/template.c
@@ -1,6 +1,6 @@
 
 // Compiler implementation of the D programming language
-// Copyright (c) 1999-2009 by Digital Mars
+// Copyright (c) 1999-2010 by Digital Mars
 // All Rights Reserved
 // written by Walter Bright
 // http://www.digitalmars.com
@@ -3497,7 +3497,8 @@ void TemplateInstance::semantic(Scope *sc)
 	error("error instantiating");
 	if (tinst)
 	{   tinst->printInstantiationTrace();
-	    fatal();
+	    if (!global.gag)
+		fatal();
 	}
 	errors = 1;
 	if (global.gag)
