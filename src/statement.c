73b4e1ec1720522d3a54d403f8f8b122c905d12b
Walter Bright <walter@digitalmars.com>
2010-02-04 10:24:18 +0000

    bugzilla 2463 No line number in 'statement is not reachable' warning

--- src/statement.c
+++ src/statement.c
@@ -1,6 +1,6 @@
 
 // Compiler implementation of the D programming language
-// Copyright (c) 1999-2009 by Digital Mars
+// Copyright (c) 1999-2010 by Digital Mars
 // All Rights Reserved
 // written by Walter Bright
 // http://www.digitalmars.com
@@ -3359,7 +3359,6 @@ Statement *ReturnStatement::semantic(Scope *sc)
 	Statement *s = new ExpStatement(loc, exp);
 	exp = NULL;
 	s = s->semantic(sc);
-	loc = 0;
 	return new CompoundStatement(loc, s, this);
     }
 
