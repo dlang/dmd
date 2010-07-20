fdc78b95ecea92b92861376211c091a238cdf381
Sean Kelly <sean@invisibleduck.org>
2010-07-20 22:22:47 +0000

    Issue 4385 - Uncaught exceptions should dump a core

--- src/rt/dmain2.d
+++ src/rt/dmain2.d
@@ -437,5 +437,7 @@ extern (C) int main(int argc, char** argv)
         _STD_critical_term();
         _STD_monitor_staticdtor();
     }
+    if (result == EXIT_FAILURE)
+        abort();
     return result;
 }
