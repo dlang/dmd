77b33e2193ac8bc884a6aeb71b1f86b97da141ae
Brad Roberts <braddr@puremagic.com>
2010-09-02 05:59:11 +0000

    reenable fail274.d since the halt it was hitting has been removed

--- test/fail_compilation/fail274.d
+++ test/fail_compilation/fail274.d
@@ -1,16 +1,15 @@
-disabled until bug 4771 is fixed
-//void main() {
-//    asm { inc [; }
-//}
-//
-//// 1144 mixin_34_A.  Segfault  D1 only
-//char[] testHelper(A ...)(){
-// 	    char[] result;
-// 	    foreach(t; a){
-// 	        result ~= "int " ~ t ~ ";\n";
-// 	    }
-// 	    return result;
-// 	} 	
-//void main(){
-// 	    mixin( testHelper!( "hello", "world" )() );
-// 	}
+void main() {
+    asm { inc [; }
+}
+
+// 1144 mixin_34_A.  Segfault  D1 only
+char[] testHelper(A ...)(){
+ 	    char[] result;
+ 	    foreach(t; a){
+ 	        result ~= "int " ~ t ~ ";\n";
+ 	    }
+ 	    return result;
+ 	} 	
+void main(){
+ 	    mixin( testHelper!( "hello", "world" )() );
+ 	}
