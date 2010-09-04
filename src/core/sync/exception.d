12b99ff8dc73ddf8ea26c8436b97ba8429f5847e
Walter Bright <walter@digitalmars.com>
2010-09-04 04:34:04 +0000

    fix ddoc comment

--- src/core/sync/exception.d
+++ src/core/sync/exception.d
@@ -1,15 +1,11 @@
 /**
- * The barrier module provides a primitive for synchronizing the progress of
- * a group of threads.
+ * Define base class for synchronization exceptions.
  *
  * Copyright: Copyright Sean Kelly 2005 - 2009.
- * License:   <a href="http://www.boost.org/LICENSE_1_0.txt">Boost License 1.0</a>.
+ * License: Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy
+ *	at <a href="http://www.boost.org/LICENSE_1_0.txt">boost.org</a>)
  * Authors:   Sean Kelly
- *
- *          Copyright Sean Kelly 2005 - 2009.
- * Distributed under the Boost Software License, Version 1.0.
- *    (See accompanying file LICENSE_1_0.txt or copy at
- *          http://www.boost.org/LICENSE_1_0.txt)
  */
 module core.sync.exception;
 
