08d56fdfa9fe073f38041b3842b20a9702a67305
Walter Bright <walter@digitalmars.com>
2010-09-11 21:05:26 +0000

    bugzilla 4571 Regression(1.062, 2.047) ICE(constfold.c) >> after error

--- src/constfold.c
+++ src/constfold.c
@@ -545,13 +545,11 @@ Expression *Shl(Type *type, Expression *e1, Expression *e2)
 }
 
 Expression *Shr(Type *type, Expression *e1, Expression *e2)
-{   Expression *e;
+{
     Loc loc = e1->loc;
-    unsigned count;
-    dinteger_t value;
 
-    value = e1->toInteger();
-    count = e2->toInteger();
+    dinteger_t value = e1->toInteger();
+    unsigned count = e2->toInteger();
     switch (e1->type->toBasetype()->ty)
     {
         case Tint8:
@@ -586,21 +584,22 @@ Expression *Shr(Type *type, Expression *e1, Expression *e2)
                 value = (d_uns64)(value) >> count;
                 break;
 
+        case Terror:
+                return e1;
+
         default:
                 assert(0);
     }
-    e = new IntegerExp(loc, value, type);
+    Expression *e = new IntegerExp(loc, value, type);
     return e;
 }
 
 Expression *Ushr(Type *type, Expression *e1, Expression *e2)
-{   Expression *e;
+{
     Loc loc = e1->loc;
-    unsigned count;
-    dinteger_t value;
 
-    value = e1->toInteger();
-    count = e2->toInteger();
+    dinteger_t value = e1->toInteger();
+    unsigned count = e2->toInteger();
     switch (e1->type->toBasetype()->ty)
     {
         case Tint8:
@@ -625,10 +624,13 @@ Expression *Ushr(Type *type, Expression *e1, Expression *e2)
                 value = (d_uns64)(value) >> count;
                 break;
 
+        case Terror:
+                return e1;
+
         default:
                 assert(0);
     }
-    e = new IntegerExp(loc, value, type);
+    Expression *e = new IntegerExp(loc, value, type);
     return e;
 }
 
@@ -833,7 +835,7 @@ Expression *Equal(enum TOK op, Type *type, Expression *e1, Expression *e2)
     {
         cmp = e1->toComplex() == e2->toComplex();
     }
-    else if (e1->type->isintegral() || e1->type->ty == Tpointer)
+    else if (e1->type->isintegral() || e1->type->toBasetype()->ty == Tpointer)
     {
         cmp = (e1->toInteger() == e2->toInteger());
     }
