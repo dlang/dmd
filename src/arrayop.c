8fb6d9a935b7b51ca6caf8e26c94125fc317b434
Walter Bright <walter@digitalmars.com>
2010-09-21 21:00:42 +0000

    bugzilla 3665 Regression(1.051, 2.036) Assignment with array slicing does not work

--- src/arrayop.c
+++ src/arrayop.c
@@ -53,8 +53,18 @@ bool isArrayOpValid(Expression *e)
             case TOKxor:
             case TOKand:
             case TOKor:
+            case TOKassign:
+            case TOKaddass:
+            case TOKminass:
+            case TOKmulass:
+            case TOKdivass:
+            case TOKmodass:
+            case TOKxorass:
+            case TOKandass:
+            case TOKorass:
 #if DMDV2
             case TOKpow:
+            case TOKpowass:
 #endif
                  return isArrayOpValid(((BinExp *)e)->e1) && isArrayOpValid(((BinExp *)e)->e2);
 
@@ -597,6 +607,19 @@ int Expression::isArrayOperand()
             case TOKxor:
             case TOKand:
             case TOKor:
+            case TOKassign:
+            case TOKaddass:
+            case TOKminass:
+            case TOKmulass:
+            case TOKdivass:
+            case TOKmodass:
+            case TOKxorass:
+            case TOKandass:
+            case TOKorass:
+#if DMDV2
+            case TOKpow:
+            case TOKpowass:
+#endif
             case TOKneg:
             case TOKtilde:
                 return 1;
