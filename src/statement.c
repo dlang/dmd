1e391e49c2d8ec6fa3aaa9d273efad201b757302
Walter Bright <walter@digitalmars.com>
2010-09-27 21:17:23 +0000

    bugzilla 4938 Regression(2.047) dmd segfault when compiling

--- src/statement.c
+++ src/statement.c
@@ -3980,7 +3980,12 @@ void Catch::semantic(Scope *sc)
         type = new TypeIdentifier(0, Id::Object);
     type = type->semantic(loc, sc);
     if (!type->toBasetype()->isClassHandle())
-        error(loc, "can only catch class objects, not '%s'", type->toChars());
+    {
+        if (type != Type::terror)
+        {   error(loc, "can only catch class objects, not '%s'", type->toChars());
+            type = Type::terror;
+        }
+    }
     else if (ident)
     {
         var = new VarDeclaration(loc, type, ident, NULL);
