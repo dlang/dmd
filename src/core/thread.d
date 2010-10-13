32b8a6a81bf58e4be659545d2f3a25438f6615ee
Walter Bright <walter@digitalmars.com>
2010-10-13 18:22:14 +0000

    add source link

--- src/core/thread.d
+++ src/core/thread.d
@@ -2,13 +2,14 @@
  * The thread module provides support for thread creation and management.
  *
  * Copyright: Copyright Sean Kelly 2005 - 2009.
- * License:   <a href="http://www.boost.org/LICENSE_1_0.txt">Boost License 1.0</a>.
- * Authors:   Sean Kelly
+ * License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+ * Authors:   Sean Kelly, Walter Bright
  *
  *          Copyright Sean Kelly 2005 - 2009.
  * Distributed under the Boost Software License, Version 1.0.
  *    (See accompanying file LICENSE_1_0.txt or copy at
  *          http://www.boost.org/LICENSE_1_0.txt)
+ * Source: $(LINK http://www.dsource.org/projects/druntime/browser/trunk/src/core/thread.d)
  */
 module core.thread;
 
