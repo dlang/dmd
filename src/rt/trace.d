996ed815a00c27b43b8d509823e9ccb700dd1b4e
Sean Kelly <sean@invisibleduck.org>
2010-11-15 21:18:32 +0000

    Changed output of log file so the aggregate results (after the '=') are demangled using core.demangle.  This previously only occurred on Windows using some DMC backend routine.

--- src/rt/trace.d
+++ src/rt/trace.d
@@ -14,16 +14,15 @@ module rt.trace;
 
 private
 {
-    import rt.util.string;
+    import core.demangle;
     import core.stdc.ctype;
     import core.stdc.stdio;
-    import core.stdc.string;
     import core.stdc.stdlib;
+    import core.stdc.string;
+    import rt.util.string;
 }
 
 extern (C):
-
-char* unmangle_ident(char*);    // from DMC++ runtime library
 
 alias long timer_t;
 
@@ -317,57 +316,31 @@ static void trace_times(Symbol* root)
     fprintf(fplog,"  Num          Tree        Func        Per\n");
     fprintf(fplog,"  Calls        Time        Time        Call\n\n");
     for (u = 0; u < nsymbols; u++)
-    {   Symbol* s = psymbols[u];
+    {   
+        Symbol* s = psymbols[u];
         timer_t tl,tr;
         timer_t fl,fr;
         timer_t pl,pr;
         timer_t percall;
+        char[8192] buf;
         SymPair* sp;
         uint calls;
         char[] id;
 
-        version (Windows)
-        {
-            char* p = (s.Sident ~ '\0').ptr;
-            p = unmangle_ident(p);
-            if (p)
-                id = p[0 .. strlen(p)];
-        }
-        if (!id)
-            id = s.Sident;
         calls = 0;
+        id = demangle(s.Sident, buf);
         for (sp = s.Sfanin; sp; sp = sp.next)
             calls += sp.count;
         if (calls == 0)
             calls = 1;
 
-version (all)
-{
         tl = (s.totaltime * 1000000) / freq;
         fl = (s.functime  * 1000000) / freq;
         percall = s.functime / calls;
         pl = (s.functime * 1000000) / calls / freq;
 
         fprintf(fplog,"%7d%12lld%12lld%12lld     %.*s\n",
-            calls,tl,fl,pl,id);
-}
-else
-{
-        tl = s.totaltime / freq;
-        tr = ((s.totaltime - tl * freq) * 10000000) / freq;
-
-        fl = s.functime  / freq;
-        fr = ((s.functime  - fl * freq) * 10000000) / freq;
-
-        percall = s.functime / calls;
-        pl = percall  / freq;
-        pr = ((percall  - pl * freq) * 10000000) / freq;
-
-        fprintf(fplog,"%7d\t%3lld.%07lld\t%3lld.%07lld\t%3lld.%07lld\t%.*s\n",
-            calls,tl,tr,fl,fr,pl,pr,id);
-}
-        if (id !is s.Sident)
-            free(id.ptr);
+                      calls,tl,fl,pl,id);
     }
 }
 
