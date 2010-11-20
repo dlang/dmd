5c304a22cd1472525eee1861213e37c1ed6fcf62
Brad Roberts <braddr@puremagic.com>
2010-11-20 07:42:41 +0000

    Fix two bad uses of printf.  Recent path changes tickled them enough to cause failures.

--- test/runnable/hello-profile.d
+++ test/runnable/hello-profile.d
@@ -16,7 +16,7 @@ void showargs(char[][] args)
     printf("hello world\n");
     printf("args.length = %d\n", args.length);
     for (int i = 0; i < args.length; i++)
-	printf("args[%d] = '%s'\n", i, cast(char *)args[i]);
+	printf("args[%d] = '%.*s'\n", i, args[i].length, args[i].ptr);
 }
 
 int main(char[][] args)
