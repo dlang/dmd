40ac99687e7c227b1ed1d18484f24f69dd57d9b9
Don Clugston <dclugston@googlemail.com>
2010-11-24 19:32:34 +0000

    Move Boost copyright declaration from ddoc to normal comment. Fixes broken ddoc output.

--- src/core/time.d
+++ src/core/time.d
@@ -5,8 +5,8 @@
  * Copyright: Copyright Sean Kelly 2010 - 2010.
  * License:   <a href="http://www.boost.org/LICENSE_1_0.txt">Boost License 1.0</a>.
  * Authors:   Sean Kelly
- *
- *          Copyright Sean Kelly 2010 - 2010.
+ */
+/*          Copyright Sean Kelly 2010 - 2010.
  * Distributed under the Boost Software License, Version 1.0.
  *    (See accompanying file LICENSE_1_0.txt or copy at
  *          http://www.boost.org/LICENSE_1_0.txt)
