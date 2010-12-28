831287435627aceb3ede569c50d039645b1037a3
Walter Bright <walter@digitalmars.com>
2010-12-28 01:59:22 +0000

    mitigation of bugzilla 4860

--- src/e2ir.c
+++ src/e2ir.c
@@ -3141,6 +3141,9 @@ elem *DelegateExp::toElem(IRState *irs)
             ep = el_una(OPind, TYnptr, ep);
             vindex = func->vtblIndex;
 
+            if ((int)vindex < 0)
+                error("Internal compiler error: malformed delegate. See Bugzilla 4860");
+
             // Build *(ep + vindex * 4)
             ep = el_bin(OPadd,TYnptr,ep,el_long(TYsize_t, vindex * PTRSIZE));
             ep = el_una(OPind,TYnptr,ep);
