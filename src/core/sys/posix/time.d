a6e25682003de68505d7fbfeed4183ae28a3328b
Brad Roberts <braddr@puremagic.com>
2011-01-08 21:20:00 +0000

    add the missing CLOCK_* enums

--- src/core/sys/posix/time.d
+++ src/core/sys/posix/time.d
@@ -120,9 +120,6 @@ int timer_settime(timer_t, int, in itimerspec*, itimerspec*);
 
 version( linux )
 {
-    enum CLOCK_PROCESS_CPUTIME_ID   = 2; // (TMR|CPT)
-    enum CLOCK_THREAD_CPUTIME_ID    = 3; // (TMR|TCT)
-
     // NOTE: See above for why this is commented out.
     //
     //struct timespec
@@ -137,8 +134,15 @@ version( linux )
         timespec it_value;
     }
 
-    enum CLOCK_REALTIME     = 0;
-    enum TIMER_ABSTIME      = 0x01;
+    enum CLOCK_REALTIME           = 0; // Identifier for system-wide realtime clock.
+    enum CLOCK_MONOTONIC          = 1; // Monotonic system-wide clock.
+    enum CLOCK_PROCESS_CPUTIME_ID = 2; // High-resolution timer from the CPU.
+    enum CLOCK_THREAD_CPUTIME_ID  = 3; // Thread-specific CPU-time clock.
+    enum CLOCK_MONOTONIC_RAW      = 4; // Monotonic system-wide clock, not adjusted for frequency scaling.
+    enum CLOCK_REALTIME_COARSE    = 5; // Identifier for system-wide realtime clock, updated only on ticks.
+    enum CLOCK_MONOTONIC_COARSE   = 6; // Monotonic system-wide clock, updated only on ticks.
+
+    enum TIMER_ABSTIME = 0x01;
 
     alias int clockid_t;
     alias int timer_t;
