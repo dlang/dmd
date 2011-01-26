fce5be52f1a7b7e4f910d8bc4ee2b632f6fc9843
Don Clugston <dclugston@googlemail.com>
2011-01-26 22:39:47 +0100

    Bug 1899 AA of fixed length arrays fails to initialize

--- src/expression.c
+++ src/expression.c
@@ -9086,10 +9086,23 @@ Expression *AssignExp::semantic(Scope *sc)
     }
 
     if (t1->ty == Tsarray && !refinit)
-    {   // Convert e1 to e1[]
-        Expression *e = new SliceExp(e1->loc, e1, NULL, NULL);
-        e1 = e->semantic(sc);
-        t1 = e1->type->toBasetype();
+    {   
+        if (e1->op == TOKindex &&
+            ((IndexExp *)e1)->e1->type->toBasetype()->ty == Taarray)
+        {
+            // Assignment to an AA of fixed-length arrays.
+            // Convert T[n][U] = T[] into T[n][U] = T[n]
+            e2 = e2->implicitCastTo(sc, e1->type);
+            if (e2->type == Type::terror)
+                return e2;
+        }
+        else
+        {
+            // Convert e1 to e1[]
+            Expression *e = new SliceExp(e1->loc, e1, NULL, NULL);
+            e1 = e->semantic(sc);
+            t1 = e1->type->toBasetype();
+        }
     }
 
     e2->rvalue();
@@ -9131,8 +9144,13 @@ Expression *AssignExp::semantic(Scope *sc)
     else if (t1->ty == Tsarray)
     {
         /* Should have already converted e1 => e1[]
+         * unless it is an AA
          */
-        assert(op == TOKconstruct);
+        if (!(e1->op == TOKindex && t2->ty == Tsarray &&
+            ((IndexExp *)e1)->e1->type->toBasetype()->ty == Taarray))
+        {
+            assert(op == TOKconstruct);
+        }
         //error("cannot assign to static array %s", e1->toChars());
     }
     else if (e1->op == TOKslice)
