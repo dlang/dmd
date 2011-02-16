bb29b7eba53cedf52c732bebd8bd11c042f89da9
Walter Bright <walter@walterbright.com>
2011-02-15 23:46:52 -0800

    fix line ending

--- src/func.c
+++ src/func.c
@@ -1,5 +1,5 @@
 // Compiler implementation of the D programming language
-// Copyright (c) 1999-2010 by Digital Mars
+// Copyright (c) 1999-2011 by Digital Mars
 // All Rights Reserved
 // written by Walter Bright
 // http://www.digitalmars.com
@@ -2996,7 +2996,8 @@ int StaticCtorDeclaration::addPostInvariant()
 void StaticCtorDeclaration::toCBuffer(OutBuffer *buf, HdrGenState *hgs)
 {
     if (hgs->hdrgen)
-    {   buf->writestring("static this();\n");
+    {   buf->writestring("static this();");
+        buf->writenl();
         return;
     }
     buf->writestring("static this()");
