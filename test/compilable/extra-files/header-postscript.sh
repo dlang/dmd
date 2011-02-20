3728868fa0a0fb00e346d55c96fe635bab79b3c4
Brad Roberts <braddr@puremagic.com>
2011-02-20 15:15:40 +0000

    Fix up to work on FreeBSD.  Disable the few tests that fail on FreeBSD right now.

--- test/compilable/extra-files/header-postscript.sh
+++ test/compilable/extra-files/header-postscript.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 grep -v "D import file generated from" ${RESULTS_DIR}/compilable/header.di > ${RESULTS_DIR}/compilable/header.di.2
 diff --strip-trailing-cr compilable/extra-files/header.di ${RESULTS_DIR}/compilable/header.di.2
