7d948b923b928447e44efcdd5395353e09fb6627
Walter Bright <walter@walterbright.com>
2011-03-02 13:11:28 -0800

    Issue 5680 - wrong calling convention on wsprintfA/W

--- src/core/sys/windows/windows.d
+++ src/core/sys/windows/windows.d
@@ -2915,8 +2915,11 @@ export UINT IsDlgButtonChecked(HWND hDlg, int nIDButton);
 
 export HWND SetFocus(HWND hWnd);
 
-export int wsprintfA(LPSTR, LPCSTR, ...);
-export int wsprintfW(LPWSTR, LPCWSTR, ...);
+extern (C)
+{
+    export int wsprintfA(LPSTR, LPCSTR, ...);
+    export int wsprintfW(LPWSTR, LPCWSTR, ...);
+}
 
 enum : uint
 {
