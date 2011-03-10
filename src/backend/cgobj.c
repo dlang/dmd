3988cc51fb0119fde15e1d188a17b4882d9c615c
Walter Bright <walter@walterbright.com>
2011-03-10 15:18:50 -0800

    remove unneeded includes

--- src/backend/cgobj.c
+++ src/backend/cgobj.c
@@ -27,8 +27,8 @@
 #include        "code.h"
 #include        "type.h"
 #include        "outbuf.h"
-#include        "oper.h"
-#include        "scope.h"
+//#include        "oper.h"
+//#include        "scope.h"
 
 #include        "md5.h"
 
