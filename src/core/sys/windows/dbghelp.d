d4b6b7150225502a2147e9e6c94d6ca8f2b6773c
Sean Kelly <sean@invisibleduck.org>
2011-03-23 14:48:55 -0700

    eliminated cyclic dependency part deux

--- src/core/sys/windows/dbghelp.d
+++ src/core/sys/windows/dbghelp.d
@@ -192,7 +192,7 @@ struct DbgHelp
     {
         if( sm_hndl != sm_hndl.init )
             return &sm_inst;
-        if( (sm_hndl = LoadLibrary( "dbghelp.dll" )) != sm_hndl.init )
+        if( (sm_hndl = LoadLibraryA( "dbghelp.dll" )) != sm_hndl.init )
         {
             sm_inst.SymInitialize            = cast(SymInitializeFunc) GetProcAddress(sm_hndl,"SymInitialize");
             sm_inst.SymCleanup               = cast(SymCleanupFunc) GetProcAddress(sm_hndl,"SymCleanup");
