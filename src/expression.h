dlang/dmd

commit 7192ddfb8bf16417882e067f1e14050caae957a9

4298 Constant array translated to unnecessary array literal creation
Uses rsinfu's patch, which works once the patch for bug 3779 is included.
I also need to make a little change to CTFE, which can no longer assume that
const variables have been expanded. This triggered one failure in Phobos --
but it was a bug:
immutable int[1] x =[7];
void main(){ int [] y = x;} // passed, now correctly generates an error

src/optimize.c
@@ -60,7 +60,7 @@ Expression *expandVar(int result, VarDeclaration *v)
         Type *tb = v->type->toBasetype();
         if (result & WANTinterpret ||
             v->storage_class & STCmanifest ||
-            (tb->ty != Tsarray && tb->ty != Tstruct)
+            v->type->toBasetype()->isscalar()
            )
         {
             if (v->init)

src/interpret.c
@@ -1287,7 +1287,6 @@ Expression *getVarExp(Loc loc, InterState *istate, Declaration *d, CtfeGoal goal
          */
         if (v->ident == Id::ctfe)
             return new IntegerExp(loc, 1, Type::tbool);
-
         if ((v->isConst() || v->isImmutable() || v->storage_class & STCmanifest) && v->init && !v->getValue())
 #else
         if (v->isConst() && v->init)
@@ -1295,6 +1294,10 @@ Expression *getVarExp(Loc loc, InterState *istate, Declaration *d, CtfeGoal goal
         {   e = v->init->toExpression();
             if (e && !e->type)
                 e->type = v->type;
+            if (e)
+                e = e->interpret(istate, ctfeNeedAnyValue);
+            if (e && e != EXP_CANT_INTERPRET)
+                v->setValueWithoutChecking(e);
         }
         else if ((v->isCTFE() || (!v->isDataseg() && istate)) && !v->getValue())
         {
@@ -1317,7 +1320,7 @@ Expression *getVarExp(Loc loc, InterState *istate, Declaration *d, CtfeGoal goal
         }
         else
         {   e = v->getValue();
-            if (!v->isCTFE() && v->isDataseg())
+            if (!e && !v->isCTFE() && v->isDataseg())
             {   error(loc, "static variable %s cannot be read at compile time", v->toChars());
                 e = EXP_CANT_INTERPRET;
             }

commit 25fb95e7c8501f200e30fe591b656845ff318a2b

Restore constant folding for .length expressions
Commit 7192ddf (fixing bug 4298) caused the loss of some array constant
folding. This caused implicit conversions to fail in a few cases, eg:
immutable table[][2] = [ ['a', 'b']];
short k = table.length; // fails because length not constant any more
This patch is an unsubtle fix: add a new optimize option, WANTexpand,
which reproduces the old behaviour. It is currently used only in array
length. (It could perhaps also be used in IndexExp).

src/expression.h
@@ -149,7 +149,10 @@ struct Expression : Object
     virtual Expression *optimize(int result);
     #define WANTflags   1
     #define WANTvalue   2
+    // A compile-time result is required. Give an error if not possible
     #define WANTinterpret 4
+    // Same as WANTvalue, but also expand variables as far as possible
+    #define WANTexpand  8
 
     virtual Expression *interpret(InterState *istate, CtfeGoal goal = ctfeNeedRvalue);
 

src/optimize.c
@@ -60,7 +60,8 @@ Expression *expandVar(int result, VarDeclaration *v)
         Type *tb = v->type->toBasetype();
         if (result & WANTinterpret ||
             v->storage_class & STCmanifest ||
-            v->type->toBasetype()->isscalar()
+            v->type->toBasetype()->isscalar() ||
+            ((result & WANTexpand) && (tb->ty != Tsarray && tb->ty != Tstruct))
            )
         {
             if (v->init)
@@ -207,7 +208,7 @@ Expression *ArrayLiteralExp::optimize(int result)
         for (size_t i = 0; i < elements->dim; i++)
         {   Expression *e = (Expression *)elements->data[i];
 
-            e = e->optimize(WANTvalue | (result & WANTinterpret));
+            e = e->optimize(WANTvalue | (result & (WANTinterpret | WANTexpand)));
             elements->data[i] = (void *)e;
         }
     }
@@ -220,11 +221,11 @@ Expression *AssocArrayLiteralExp::optimize(int result)
     for (size_t i = 0; i < keys->dim; i++)
     {   Expression *e = (Expression *)keys->data[i];
 
-        e = e->optimize(WANTvalue | (result & WANTinterpret));
+        e = e->optimize(WANTvalue | (result & (WANTinterpret | WANTexpand)));
         keys->data[i] = (void *)e;
 
         e = (Expression *)values->data[i];
-        e = e->optimize(WANTvalue | (result & WANTinterpret));
+        e = e->optimize(WANTvalue | (result & (WANTinterpret | WANTexpand)));
         values->data[i] = (void *)e;
     }
     return this;
@@ -238,7 +239,7 @@ Expression *StructLiteralExp::optimize(int result)
         {   Expression *e = (Expression *)elements->data[i];
             if (!e)
                 continue;
-            e = e->optimize(WANTvalue | (result & WANTinterpret));
+            e = e->optimize(WANTvalue | (result & (WANTinterpret | WANTexpand)));
             elements->data[i] = (void *)e;
         }
     }
@@ -865,7 +866,7 @@ Expression *ArrayLengthExp::optimize(int result)
 {   Expression *e;
 
     //printf("ArrayLengthExp::optimize(result = %d) %s\n", result, toChars());
-    e1 = e1->optimize(WANTvalue | (result & WANTinterpret));
+    e1 = e1->optimize(WANTvalue | WANTexpand | (result & WANTinterpret));
     e = this;
     if (e1->op == TOKstring || e1->op == TOKarrayliteral || e1->op == TOKassocarrayliteral)
     {
@@ -934,7 +935,8 @@ Expression *IndexExp::optimize(int result)
 {   Expression *e;
 
     //printf("IndexExp::optimize(result = %d) %s\n", result, toChars());
-    Expression *e1 = this->e1->optimize(WANTvalue | (result & WANTinterpret));
+    Expression *e1 = this->e1->optimize(
+        WANTvalue | (result & (WANTinterpret| WANTexpand)));
     e1 = fromConstInitializer(result, e1);
     if (this->e1->op == TOKvar)
     {   VarExp *ve = (VarExp *)this->e1;
@@ -961,7 +963,7 @@ Expression *SliceExp::optimize(int result)
 
     //printf("SliceExp::optimize(result = %d) %s\n", result, toChars());
     e = this;
-    e1 = e1->optimize(WANTvalue | (result & WANTinterpret));
+    e1 = e1->optimize(WANTvalue | (result & (WANTinterpret|WANTexpand)));
     if (!lwr)
     {   if (e1->op == TOKstring)
         {   // Convert slice of string literal into dynamic array