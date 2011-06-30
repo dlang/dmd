9f945ec6f3679bd47181f32ddb00e66371410bd2
k-hara <k.hara.pg@gmail.com>
2011-06-30 20:05:30 +0900

    Add test cases.

--- test/runnable/template9.d
+++ test/runnable/template9.d
@@ -149,6 +149,42 @@ void test7()
 }
 
 /**********************************/
+// 5946
+
+template TTest8()
+{
+    int call(){ return this.g(); }
+}
+class CTest8
+{
+    int f() { mixin TTest8!(); return call(); }
+    int g() { return 10; }
+}
+void test8()
+{
+    assert((new CTest8()).f() == 10);
+}
+
+/**********************************/
+// 693
+
+template TTest9(alias sym)
+{
+    int call(){ return sym.g(); }
+}
+class CTest9
+{
+    int f1() { mixin TTest9!(this); return call(); }
+    int f2() { mixin TTest9!this; return call(); }
+    int g() { return 10; }
+}
+void test9()
+{
+    assert((new CTest9()).f1() == 10);
+    assert((new CTest9()).f2() == 10);
+}
+
+/**********************************/
 
 int main()
 {
@@ -159,6 +195,8 @@ int main()
     test5();
     test6();
     test7();
+    test8();
+    test9();
 
     printf("Success\n");
     return 0;
