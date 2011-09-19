f2375d4400c63206c557259c55eaaa361f2cf4eb
Don Clugston <dclugston@googlemail.com>
2011-09-19 09:38:28 +0200

    Bug 6599: Segfault: invalid expression in initializer
    A gagging bug. The fix for bug 3426 set scope=NULL after running deferred
    semantic, but we'll need to use it again if we're inside trySemantic and
    any errors happen. I'm actually not sure if it should EVER be set to NULL
    (no tests fail if it is removed entirely).

--- src/expression.c
+++ src/expression.c
@@ -3772,7 +3772,10 @@ Expression *StructLiteralExp::semantic(Scope *sc)
                         Initializer *i2 = v->init->syntaxCopy();
                         i2 = i2->semantic(v->scope, v->type, WANTinterpret);
                         e = i2->toExpression();
-                        v->scope = NULL;
+                        // remove v->scope (see bug 3426)
+                        // but not if gagged, for we might be called again.
+                        if (!global.gag)
+                            v->scope = NULL;
                     }
                 }
             }
