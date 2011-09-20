83cafe5625ecd288b41aa6ebfca5985b7999eafd
dawg <dawg@dawgfoto.de>
2011-09-20 20:46:30 +0200

    make tests pass with dmd DEBUG builds

--- test/runnable/extra-files/statictor-postscript.sh
+++ test/runnable/extra-files/statictor-postscript.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # trim off the first line which contains the path of the file which differs between windows and non-windows
-grep -v runnable ${RESULTS_DIR}/runnable/statictor.d.out > ${RESULTS_DIR}/runnable/statictor.d.out.2
+# also trim off compiler debug message
+grep -v "runnable\|DEBUG" ${RESULTS_DIR}/runnable/statictor.d.out > ${RESULTS_DIR}/runnable/statictor.d.out.2
 
 diff --strip-trailing-cr runnable/extra-files/statictor.d.out ${RESULTS_DIR}/runnable/statictor.d.out.2
 if [ $? -ne 0 ]; then
