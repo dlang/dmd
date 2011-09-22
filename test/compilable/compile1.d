f24733aa9df5aa5dacc22e89ac59e7462fe16c15
Don Clugston <dclugston@googlemail.com>
2011-09-22 13:40:05 +0200

    Add a test file for compile-only typedef

--- test/compilable/compile1.d
+++ test/compilable/compile1.d
@@ -1,21 +1,5 @@
 // PERMUTE_ARGS:
 
-
-/**************************************************
-   Segfault.
-   http://www.digitalmars.com/d/archives/6376.html
-**************************************************/
-
-alias float[2] vector2;
-typedef vector2 point2;  // if I change this typedef to alias it works fine
-
-float distance(point2 a, point2 b)
-{
-  point2 d;
-  d[0] = b[0] - a[0]; // if I comment out this line it won't crash
-  return 0.0f;
-}
-
 /**************************************************
     5996    ICE(expression.c)
 **************************************************/
