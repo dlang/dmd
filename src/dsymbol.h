012c1f3da9195796e75ef0cdda887cf55b5c5b52
Walter Bright <walter@walterbright.com>
2011-09-23 14:26:26 -0700

    not using StringTable

--- src/dsymbol.h
+++ src/dsymbol.h
@@ -1,6 +1,6 @@
 
 // Compiler implementation of the D programming language
-// Copyright (c) 1999-2010 by Digital Mars
+// Copyright (c) 1999-2011 by Digital Mars
 // All Rights Reserved
 // written by Walter Bright
 // http://www.digitalmars.com
@@ -323,11 +323,7 @@ struct OverloadSet : Dsymbol
 
 struct DsymbolTable : Object
 {
-#if STRINGTABLE
-    StringTable *tab;
-#else
     AA *tab;
-#endif
 
     DsymbolTable();
     ~DsymbolTable();
