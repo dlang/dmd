b1f938f57929bf7bd66e4134c84e355b4349b8d9
k-hara <k.hara.pg@gmail.com>
2011-10-09 20:57:14 +0900

    Issue 3474 - Implement opDollar for struct and class indexing operations
    Applying Don's patch into git master

--- src/expression.h
+++ src/expression.h
@@ -1147,6 +1147,8 @@ struct ArrayLengthExp : UnaExp
 struct ArrayExp : UnaExp
 {
     Expressions *arguments;             // Array of Expression's
+    size_t currentDimension;            // for opDollar
+    VarDeclaration *lengthVar;
 
     ArrayExp(Loc loc, Expression *e1, Expressions *arguments);
     Expression *syntaxCopy();
