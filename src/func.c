45b2b4d0fd67e5d1b1dcb7ade97c28b2c113a64b
Walter Bright <walter@walterbright.com>
2011-10-09 11:17:52 -0700

    should still stay a warning for now

--- src/func.c
+++ src/func.c
@@ -481,7 +481,7 @@ void FuncDeclaration::semantic(Scope *sc)
 
 #if DMDV2
                 if (!isOverride())
-                    error(loc, "overrides base class function %s, but is not marked with 'override'", fdv->toPrettyChars());
+                    warning(loc, "overrides base class function %s, but is not marked with 'override'", fdv->toPrettyChars());
 #endif
 
                 if (fdv->toParent() == parent)
