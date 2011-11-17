9ec37917d3d87dc9dca7ef96ed039bf3b2447399
Don Clugston <dclugston@googlemail.com>
2011-11-17 10:31:08 +0100

    static assert CTFE failures -> supplementary error
    If CTFE fails inside a static assert, the static assert error is just giving
    context.

--- src/staticassert.c
+++ src/staticassert.c
@@ -60,8 +60,13 @@ void StaticAssert::semantic2(Scope *sc)
     sc = sc->pop();
     if (e->op == TOKerror)
         return;
+    unsigned olderrs = global.errors;
     e = e->optimize(WANTvalue | WANTinterpret);
-    if (e->isBool(FALSE))
+    if (global.errors != olderrs)
+    {
+        errorSupplemental(loc, "while evaluating: static assert(%s)", exp->toChars());
+    }
+    else if (e->isBool(FALSE))
     {
         if (msg)
         {   HdrGenState hgs;
