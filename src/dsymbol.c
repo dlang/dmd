12d62ca530f800841807f3ed8f2d56c05ca4098d
Walter Bright <walter@walterbright.com>
2011-12-30 19:34:17 -0800

    partial integration of pull 190

--- src/dsymbol.c
+++ src/dsymbol.c
@@ -779,7 +779,7 @@ Dsymbol *ScopeDsymbol::search(Loc loc, Identifier *ident, int flags)
 
         // Look in imported modules
         for (size_t i = 0; i < imports->dim; i++)
-        {   ScopeDsymbol *ss = (*imports)[i];
+        {   Dsymbol *ss = (*imports)[i];
             Dsymbol *s2;
 
             // If private import, don't search it
@@ -842,7 +842,7 @@ Dsymbol *ScopeDsymbol::search(Loc loc, Identifier *ident, int flags)
                         if (flags & 4)          // if return NULL on ambiguity
                             return NULL;
                         if (!(flags & 2))
-                            ss->multiplyDefined(loc, s, s2);
+                            ScopeDsymbol::multiplyDefined(loc, s, s2);
                         break;
                     }
                 }
@@ -869,7 +869,7 @@ Dsymbol *ScopeDsymbol::search(Loc loc, Identifier *ident, int flags)
     return s;
 }
 
-void ScopeDsymbol::importScope(ScopeDsymbol *s, enum PROT protection)
+void ScopeDsymbol::importScope(Dsymbol *s, enum PROT protection)
 {
     //printf("%s->ScopeDsymbol::importScope(%s, %d)\n", toChars(), s->toChars(), protection);
 
@@ -877,11 +877,11 @@ void ScopeDsymbol::importScope(ScopeDsymbol *s, enum PROT protection)
     if (s != this)
     {
         if (!imports)
-            imports = new ScopeDsymbols();
+            imports = new Dsymbols();
         else
         {
             for (size_t i = 0; i < imports->dim; i++)
-            {   ScopeDsymbol *ss = (*imports)[i];
+            {   Dsymbol *ss = (*imports)[i];
                 if (ss == s)                    // if already imported
                 {
                     if (protection > prots[i])
