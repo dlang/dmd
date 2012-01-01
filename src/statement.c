3e73b815eb3e24584843a9d82f3088e44c613979
k-hara <k.hara.pg@gmail.com>
2012-01-02 00:53:30 +0900

    Issue 2411 - Reference Tuple Foreach

--- src/statement.c
+++ src/statement.c
@@ -1499,7 +1499,8 @@ Statement *ForeachStatement::semantic(Scope *sc)
                 arg = arguments->tdata()[1];  // value
             }
             // Declare value
-            if (arg->storageClass & (STCout | STCref | STClazy))
+            if (arg->storageClass & (STCout | STClazy) ||
+                arg->storageClass & STCref && !te)
                 error("no storage class for value %s", arg->ident->toChars());
             Dsymbol *var;
             if (te)
@@ -1513,14 +1514,24 @@ Statement *ForeachStatement::semantic(Scope *sc)
                     s =((ScopeExp *)e)->sds;
 
                 if (s)
+                {
                     var = new AliasDeclaration(loc, arg->ident, s);
+                    if (arg->storageClass & STCref)
+                        error("symbol %s cannot be ref", s->toChars());
+                }
                 else
                 {
                     arg->type = e->type;
                     Initializer *ie = new ExpInitializer(0, e);
                     VarDeclaration *v = new VarDeclaration(loc, arg->type, arg->ident, ie);
+                    if (arg->storageClass & STCref)
+                        v->storage_class |= STCref | STCforeach;
                     if (e->isConst() || e->op == TOKstring)
-                        v->storage_class |= STCmanifest;
+                    {   if (v->storage_class & STCref)
+                            error("constant value %s cannot be ref", ie->toChars());
+                        else
+                            v->storage_class |= STCmanifest;
+                    }
                     var = v;
                 }
             }
