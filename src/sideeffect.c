b99d3cb66daf1ee7fbf25c224de0d22cb05b9949
Walter Bright <walter@walterbright.com>
2012-01-05 12:28:47 -0800

    add sideeffect.c

---
+++ src/sideeffect.c
@@ -0,0 +1,34 @@
+
+// Compiler implementation of the D programming language
+// Copyright (c) 1999-2012 by Digital Mars
+// All Rights Reserved
+// written by Walter Bright
+// http://www.digitalmars.com
+// License for redistribution is by either the Artistic License
+// in artistic.txt, or the GNU General Public License in gnu.txt.
+// See the included readme.txt for details.
+
+#include <stdio.h>
+#include <assert.h>
+
+#include "mars.h"
+#include "init.h"
+#include "expression.h"
+#include "template.h"
+#include "statement.h"
+#include "mtype.h"
+#include "utf.h"
+#include "declaration.h"
+#include "aggregate.h"
+#include "scope.h"
+#include "attrib.h"
+
+/********************************************
+ * Determine if Expression has any side effects.
+ */
+
+bool Expression::hasSideEffect()
+{
+    return checkSideEffect(2);
+}
+
