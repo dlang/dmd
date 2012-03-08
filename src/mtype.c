e46bd9c44efe1697fdd81eb4915224733f371a9e
Walter Bright <walter@walterbright.com>
2012-03-07 21:17:36 -0800

    fix Issue 6391 - Line-less error when passing the '.im' of floating pointer value by reference

--- src/mtype.c
+++ src/mtype.c
@@ -1395,7 +1395,7 @@ Expression *TypeBasic::dotExp(Scope *sc, Expression *e, Identifier *ident)
             case Timaginary64:  t = tfloat64;           goto L2;
             case Timaginary80:  t = tfloat80;           goto L2;
             L2:
-                e = new RealExp(0, 0.0, t);
+                e = new RealExp(e->loc, 0.0, t);
                 break;
 
             default:
@@ -1425,7 +1425,7 @@ Expression *TypeBasic::dotExp(Scope *sc, Expression *e, Identifier *ident)
             case Tfloat32:
             case Tfloat64:
             case Tfloat80:
-                e = new RealExp(0, 0.0, this);
+                e = new RealExp(e->loc, 0.0, this);
                 break;
 
             default:
