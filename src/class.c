1ac0b1021b77190f512399a2bde9bfac7e05f940
Walter Bright <walter@walterbright.com>
2012-03-12 20:03:28 -0700

    fix Issue 3510 - Cannot forward reference a templated type from within a template mixin

--- src/class.c
+++ src/class.c
@@ -617,7 +617,8 @@ void ClassDeclaration::semantic(Scope *sc)
          */
         if (s->isEnumDeclaration() ||
             (s->isAggregateDeclaration() && s->ident) ||
-            s->isTemplateMixin())
+            s->isTemplateMixin() ||
+            s->isAliasDeclaration())
         {
             //printf("setScope %s %s\n", s->kind(), s->toChars());
             s->setScope(sc);
