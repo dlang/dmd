6471e7fab9c71037881c24612fd49845df582aea
Matt Peterson <ricochet1k@gmail.com>
2012-03-15 22:05:06 -0600

    Move JsonOut method declarations out of the header file

--- src/json.h
+++ src/json.h
@@ -20,63 +20,7 @@
 
 void json_generate(Modules *);
 
-struct JsonOut
-{
-    OutBuffer *buf;
-    int indentLevel;
-
-    JsonOut(OutBuffer *buf) {this->buf = buf; indentLevel = 0;}
-
-    void indent();
-    void removeComma();
-    void comma();
-
-    void value(const char*);
-    void value(int);
-    void valueBool(bool);
-
-    void item(const char*);
-    void item(int);
-    void itemBool(bool);
-
-    void arrayStart();
-    void arrayEnd();
-    void objectStart();
-    void objectEnd();
-
-    void propertyStart(const char*);
-
-    void property(const char*, const char*);
-    void property(const char*, int);
-    void propertyBool(const char*, bool);
-    void propertyStorageClass(const char*, StorageClass);
-    void property(const char*, Type*);
-    void property(const char*, Parameters*);
-
-    void properties(Module*);
-    void properties(Dsymbol*);
-    void properties(Declaration*);
-    void properties(TypeSArray*);
-    void properties(TypeDArray*);
-    void properties(TypeAArray*);
-    void properties(TypePointer*);
-    void properties(TypeReference*);
-    void properties(TypeFunction*);
-    void properties(TypeDelegate*);
-    void properties(TypeQualified*);
-    void properties(TypeIdentifier*);
-    void properties(TypeInstance*);
-    void properties(TypeTypeof*);
-    void properties(TypeReturn*);
-    void properties(TypeStruct*);
-    void properties(TypeEnum*);
-    void properties(TypeTypedef*);
-    void properties(TypeClass*);
-    void properties(TypeTuple*);
-    void properties(TypeSlice*);
-    void properties(TypeNull*);
-    void properties(TypeVector*);
-};
+struct JsonOut;
 
 #endif /* DMD_JSON_H */
 
