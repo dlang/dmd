ccf26e67f764680f653ccc48e268b18cb00f81ac
k-hara <k.hara.pg@gmail.com>
2012-04-03 18:23:16 +0900

    Raising error is still needed.

--- src/module.c
+++ src/module.c
@@ -1174,7 +1174,26 @@ DsymbolTable *Package::resolve(Identifiers *packages, Dsymbol **pparent, Package
                 // It might already be a module, not a package, but that needs
                 // to be checked at a higher level, where a nice error message
                 // can be generated.
-                // dot net needs modules and packages with same name
+
+#if TARGET_NET  //dot net needs modules and packages with same name
+#else
+                if (p->isModule())
+                {   p->error("module and package have the same name");
+                    //fatal();
+                    //break;
+
+                    // Example:
+                    //   src/a.d is:
+                    //       module src; // only the package name (invalid)
+                    //   src/b.d is:
+                    //       module src.b;
+                    //   command line:
+                    //       dmd src/a.d src/b.d
+                    // adds 'src' as module name into global table, and
+                    // succeeds to compile.
+                    // This error is still needed to detect it.
+                }
+#endif
 
                 if (!((Package *)p)->symtab)
                     ((Package *)p)->symtab = new DsymbolTable();
