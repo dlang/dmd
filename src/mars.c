c009bbd1d56001402f441b727508ec993e0163a1
Walter Bright <walter@walterbright.com>
2012-04-13 18:45:18 -0700

    start 1.075

--- src/mars.c
+++ src/mars.c
@@ -94,7 +94,7 @@ Global::Global()
     "\nMSIL back-end (alpha release) by Cristian L. Vlasceanu and associates.";
 #endif
     ;
-    version = "v1.074";
+    version = "v1.075";
     global.structalign = 8;
 
     memset(&params, 0, sizeof(Param));
