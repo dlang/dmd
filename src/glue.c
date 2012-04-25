900c537038932435eaebdf7c0f80926e0bd8f2f5
Walter Bright <walter@walterbright.com>
2012-04-25 16:26:52 -0700

    fix Issue 2962 - ICE(glue.c) or bad codegen passing variable as template value parameter

--- src/glue.c
+++ src/glue.c
@@ -546,7 +546,7 @@ void FuncDeclaration::toObjFile(int multiobj)
     semanticRun = PASSobj;
 
     if (global.params.verbose)
-        printf("function  %s\n",func->toChars());
+        printf("function  %s\n",func->toPrettyChars());
 
     Symbol *s = func->toSymbol();
     func_t *f = s->Sfunc;
@@ -585,6 +585,20 @@ void FuncDeclaration::toObjFile(int multiobj)
 //      if (!(config.flags3 & CFG3pic))
 //          s->Sclass = SCstatic;
         f->Fflags3 |= Fnested;
+
+        /* The enclosing function must have its code generated first,
+         * so we know things like where its local symbols are stored.
+         */
+        FuncDeclaration *fdp = toAliasFunc()->toParent2()->isFuncDeclaration();
+        if (fdp && fdp->semanticRun == PASSsemantic3done &&
+            !fdp->isUnitTestDeclaration())
+        {
+            /* Can't do unittest's out of order, they are order dependent in that their
+             * execution is done in lexical order, and some modules (std.datetime *cough*
+             * *cough*) rely on this.
+             */
+            fdp->toObjFile(multiobj);
+        }
     }
     else
     {
