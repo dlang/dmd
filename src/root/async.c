ee14453951fe261c572875b8e332fd22977a0e5b
Iain Buclaw <ibuclaw@ubuntu.com>
2012-06-20 03:30:02 +0100

    Add license to all significant files in D frontend.

--- src/root/async.c
+++ src/root/async.c
@@ -1,4 +1,12 @@
 
+// Copyright (c) 2009-2012 by Digital Mars
+// All Rights Reserved
+// written by Walter Bright
+// http://www.digitalmars.com
+// License for redistribution is by either the Artistic License
+// in artistic.txt, or the GNU General Public License in gnu.txt.
+// See the included readme.txt for details.
+
 #define _MT 1
 
 #include <stdio.h>
