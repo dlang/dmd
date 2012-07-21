55bedf48b0df58c7c4e4aeb4c8ed9969bfab1005
k-hara <k.hara.pg@gmail.com>
2012-07-21 13:55:35 +0900

    merge D2 pull #690

--- src/init.h
+++ src/init.h
@@ -113,6 +113,7 @@ struct ArrayInitializer : Initializer
 struct ExpInitializer : Initializer
 {
     Expression *exp;
+    int expandTuples;
 
     ExpInitializer(Loc loc, Expression *exp);
     Initializer *syntaxCopy();
