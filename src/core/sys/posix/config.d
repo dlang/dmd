59c65b840d10d47d70405a7c2af9ad9d604368f3
Robert Klotzner <robert.klotzner@gmx.at>
2012-07-24 20:11:05 +0200

    Isolated fixing of stat.d.
    Avoided unrelated whitspace changes.
    Removed unit tests, as they are being replaced by a more general suite,
    according to Martin Nowak.
    
    Work in progress. Not yet tested.

--- src/core/sys/posix/config.d
+++ src/core/sys/posix/config.d
@@ -23,4 +23,82 @@ version( linux )
     enum bool  __USE_LARGEFILE64    = true;
     enum bool  __USE_FILE_OFFSET64  = __USE_LARGEFILE64;
     enum bool  __REDIRECT           = false;
+    enum bool  __USE_XOPEN2K8		= true; //#if (_POSIX_C_SOURCE - 0) >= 200809L
+    enum bool _BSD_SOURCE			= true; /// Seems to be default in gcc.
+    enum bool _SVID_SOURCE   		= true; /// Ditto
+    enum bool __USE_GNU				= true;
+    static if(_BSD_SOURCE || _SVID_SOURCE) {
+    	enum bool __USE_MISC			= true; 
+    }
+
+    // Word sizes:
+    version(X86) {
+        enum __WORDSIZE=32;
+    }
+
+    version(X86_64) {
+    	enum __WORDSIZE=64;
+    }
+
+    version(ARM) {
+    	enum __WORDSIZE=32;
+    }
+
+    version(PPC) {
+    	enum __WORDSIZE=32;
+    }
+
+    version(PPC64) {
+    	enum __WORDSIZE=64;
+    }
+
+    version(IA64) {
+    	enum __WORDSIZE=64;
+    }
+
+    version(MIPS) {
+    	enum __WORDSIZE=32;
+    }
+
+    version(MIPS6) {
+    	enum __WORDSIZE=64;
+    }
+
+    version(SPARC) {
+    	enum __WORDSIZE=32;
+    }
+
+    version(SPARC64) {
+    	enum __WORDSIZE=64;
+    }
+
+    version(S390) {
+    	enum __WORDSIZE=32;
+    }
+
+    version(S390X) {
+    	enum __WORDSIZE=64;
+    }
+
+    version(HPPA) {
+    	enum __WORDSIZE=32;
+    }
+
+    version(HPPA6) {
+    	enum __WORDSIZE=64;
+    }
+
+    version(SH) {
+    	enum __WORDSIZE=32;
+    }
+
+    version(SH64) {
+    	enum __WORDSIZE=64;
+    }
+
+    version(Alpha) {
+    	enum __WORDSIZE=64;
+    }
+
+
 }
--- src/core/sys/posix/sys/stat.d
+++ src/core/sys/posix/sys/stat.d
@@ -76,11 +76,11 @@ int    mkdir(in char*, mode_t);
 int    mkfifo(in char*, mode_t);
 int    stat(in char*, stat*);
 mode_t umask(mode_t);
-*/
+ */
 
 version( linux )
 {
-    static if( __USE_LARGEFILE64 )
+    static if(__WORDSIZE==64)
     {
         private alias uint _pad_t;
     }
@@ -92,51 +92,69 @@ version( linux )
     struct stat_t
     {
         dev_t       st_dev;
-        _pad_t      __pad1;
-      static if( __USE_FILE_OFFSET64 )
-      {
-        ino_t       __st_ino;
-      }
-      else
-      {
-        ino_t       st_ino;
-      }
-        mode_t      st_mode;
-        nlink_t     st_nlink;
+        static if(__WORDSIZE==32) {
+            _pad_t      __pad1;
+        } 
+        static if( !__USE_FILE_OFFSET64 || __WORDSIZE==64 )
+        {
+            uint       st_ino;
+        }
+        else
+        {
+            uint       __st_ino;
+        }
+        static if (__WORDSIZE==32) {
+            mode_t      st_mode;
+            nlink_t     st_nlink;
+        }
+        else {
+            nlink_t     st_nlink;
+            mode_t      st_mode;
+        }
         uid_t       st_uid;
         gid_t       st_gid;
+        static if(__WORDSIZE==64) {
+            _pad_t pad0;
+        }
         dev_t       st_rdev;
-        _pad_t      __pad2;
+        static if(__WORDSIZE==32) {
+            _pad_t      __pad2;
+        }
         off_t       st_size;
         blksize_t   st_blksize;
         blkcnt_t    st_blocks;
-      static if( false /*__USE_MISC*/ ) // true if _BSD_SOURCE || _SVID_SOURCE
-      {
-        timespec    st_atim;
-        timespec    st_mtim;
-        timespec    st_ctim;
-        alias st_atim.tv_sec st_atime;
-        alias st_mtim.tv_sec st_mtime;
-        alias st_ctim.tv_sec st_ctime;
-      }
-      else
-      {
-        time_t      st_atime;
-        c_ulong     st_atimensec;
-        time_t      st_mtime;
-        c_ulong     st_mtimensec;
-        time_t      st_ctime;
-        c_ulong     st_ctimensec;
-      }
-      static if( __USE_FILE_OFFSET64 )
-      {
-        ino_t       st_ino;
-      }
-      else
-      {
-        c_ulong     __unused4;
-        c_ulong     __unused5;
-      }
+        static if( __USE_MISC ) // true if _BSD_SOURCE || _SVID_SOURCE
+        {
+            timespec    st_atim;
+            timespec    st_mtim;
+            timespec    st_ctim;
+            alias st_atim.tv_sec st_atime;
+            alias st_mtim.tv_sec st_mtime;
+            alias st_ctim.tv_sec st_ctime;
+        }
+        else
+        {
+            time_t      st_atime;
+            c_ulong     st_atimensec;
+            time_t      st_mtime;
+            c_ulong     st_mtimensec;
+            time_t      st_ctime;
+            c_ulong     st_ctimensec;
+        }
+        static if(__WORDSIZE==64) {
+            c_long __unused[3];
+        }
+        else {
+            static if( __USE_FILE_OFFSET64 )
+            {
+                    ino_t       st_ino;
+            }
+            else
+            {
+                c_ulong     __unused4;
+                c_ulong     __unused5;
+            }
+        }
     }
 
     enum S_IRUSR    = 0x100; // octal 0400
