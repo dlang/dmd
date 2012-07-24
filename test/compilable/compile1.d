27928882cfda7eb65c89a9c59768b5456ec6b70c
Walter Bright <walter@walterbright.com>
2012-07-23 20:45:32 -0700

    add test case for bug 8163

--- test/compilable/compile1.d
+++ test/compilable/compile1.d
@@ -167,16 +167,31 @@ struct Foo1099 {
     mixin Mix1099!(2);
 }
 
-/**************************************************
-    4967, 7058
-**************************************************/
+/***************************************************/
 
-enum Bug7058 bug7058 = { 1.5f, 2};
-static assert(bug7058.z == 99);
-
-struct Bug7058
+template test8163(T...)
 {
-     float x = 0;
-     float y = 0;
-     float z = 99;
+    struct Point
+    {
+        T fields;
+    }
+
+    enum N = 2; // N>=2 triggers the bug
+    extern Point[N] bar();
+
+    void foo()
+    {
+        Point[N] _ = bar();
+    }
 }
+
+alias test8163!(long) _l;
+alias test8163!(double) _d;
+alias test8163!(float, float) _ff;
+alias test8163!(int, int) _ii;
+alias test8163!(int, float) _if;
+alias test8163!(ushort, ushort, ushort, ushort) _SSSS;
+alias test8163!(ubyte, ubyte, ubyte, ubyte, ubyte, ubyte, ubyte, ubyte) _BBBBBBBB;
+alias test8163!(ubyte, ubyte, ushort, float) _BBSf;
+
+
