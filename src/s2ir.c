3c20198bdc02b356ea30d6697e99f6e70026aa45
Walter Bright <walter@walterbright.com>
2012-08-11 00:33:28 -0700

    more Win64

--- src/s2ir.c
+++ src/s2ir.c
@@ -66,7 +66,8 @@ StructDeclaration *needsPostblit(Type *t);
 #if SEH
 void setScopeIndex(Blockx *blx, block *b, int scope_index)
 {
-    block_appendexp(b, nteh_setScopeTableIndex(blx, scope_index));
+    if (!global.params.is64bit)
+        block_appendexp(b, nteh_setScopeTableIndex(blx, scope_index));
 }
 #else
 #define setScopeIndex(blx, b, scope_index) ;
@@ -1424,7 +1425,8 @@ void TryCatchStatement::toIR(IRState *irs)
     Blockx *blx = irs->blx;
 
 #if SEH
-    nteh_declarvars(blx);
+    if (!global.params.is64bit)
+        nteh_declarvars(blx);
 #endif
 
     IRState mystate(irs, this);
@@ -1502,7 +1504,8 @@ void TryFinallyStatement::toIR(IRState *irs)
     Blockx *blx = irs->blx;
 
 #if SEH
-    nteh_declarvars(blx);
+    if (!global.params.is64bit)
+        nteh_declarvars(blx);
 #endif
 
     block *tryblock = block_goto(blx, BCgoto, NULL);
