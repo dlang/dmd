81df94dc831269a3cd1969149944f4119f14ccad
Robert Klotzner <robert.klotzner@gmx.at>
2012-08-28 21:01:54 +0200

    Followed advice of Martin Nowak and set whole module in nothrow.

--- src/core/sys/linux/sys/xattr.d
+++ src/core/sys/linux/sys/xattr.d
@@ -11,23 +11,23 @@ import core.sys.posix.sys.types;
 version (linux):
 
 extern (C):
-
+nothrow:
 enum {
 	XATTR_CREATE=1, /* set value, fail if attr already exists.  */
 	XATTR_REPLACE=2 /* set value, fail if attr does not exist.  */
 }
 
-int setxattr(in char* path, in char* name, in void* value, size_t size, int flags) nothrow;
+int setxattr(in char* path, in char* name, in void* value, size_t size, int flags);
 
-int lsetxattr(in char* path, in char* name, in void* value, size_t size, int flags) nothrow;
-int fsetxattr(int fd, in char* name, in void* value, size_t size, int flags) nothrow;
-ssize_t getxattr(in char* path, in char* name, void* value, size_t size) nothrow;
-ssize_t lgetxattr(in char* path, in char* name, void* value, size_t size) nothrow;
-ssize_t fgetxattr(int fd, in char* name, void* value, size_t size) nothrow;
-ssize_t listxattr(in char* path, char* list, size_t size) nothrow;
-ssize_t llistxattr(in char* path, char* list, size_t size) nothrow;
-ssize_t flistxattr (int __fd, char *list, size_t size) nothrow;
-int removexattr (in char *path, in char *name) nothrow;
-int lremovexattr (in char *path, in char *name) nothrow;
-int fremovexattr (int fd, in char *name) nothrow;
+int lsetxattr(in char* path, in char* name, in void* value, size_t size, int flags);
+int fsetxattr(int fd, in char* name, in void* value, size_t size, int flags);
+ssize_t getxattr(in char* path, in char* name, void* value, size_t size);
+ssize_t lgetxattr(in char* path, in char* name, void* value, size_t size);
+ssize_t fgetxattr(int fd, in char* name, void* value, size_t size);
+ssize_t listxattr(in char* path, char* list, size_t size);
+ssize_t llistxattr(in char* path, char* list, size_t size);
+ssize_t flistxattr (int __fd, char *list, size_t size);
+int removexattr (in char *path, in char *name);
+int lremovexattr (in char *path, in char *name);
+int fremovexattr (int fd, in char *name);
 
