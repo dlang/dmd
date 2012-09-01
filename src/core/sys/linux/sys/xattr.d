3350bff82a9844008f87f52cb8db5a68ab2ec1e4
Robert Klotzner <robert.klotzner@gmx.at>
2012-09-01 12:45:04 +0200

    Added namespace definitions in linux/xattr.h.

--- src/core/sys/linux/sys/xattr.d
+++ src/core/sys/linux/sys/xattr.d
@@ -16,9 +16,37 @@ extern (C):
 nothrow:
 
 enum {
-	XATTR_CREATE=1, /* set value, fail if attr already exists.  */
-	XATTR_REPLACE=2 /* set value, fail if attr does not exist.  */
+	XATTR_CREATE = 1, /* set value, fail if attr already exists.  */
+	XATTR_REPLACE = 2 /* set value, fail if attr does not exist.  */
 }
+
+enum XATTR_OS2_PREFIX = "os2.";
+enum XATTR_SECURITY_PREFIX = "security.";
+enum XATTR_SYSTEM_PREFIX = "system.";
+enum XATTR_TRUSTED_PREFIX = "trusted.";
+enum XATTR_USER_PREFIX = "user.";
+
+/* Security namespace */
+enum XATTR_SELINUX_SUFFIX = "selinux.";
+enum XATTR_NAME_SELINUX = XATTR_SECURITY_PREFIX ~ XATTR_SELINUX_SUFFIX;
+
+enum XATTR_SMACK_SUFFIX = "SMACK64";
+enum XATTR_SMACK_IPIN = "SMACK64IPIN";
+enum XATTR_SMACK_IPOUT = "SMACK64IPOUT";
+enum XATTR_SMACK_EXEC = "SMACK64EXEC";
+enum XATTR_SMACK_TRANSMUTE = "SMACK64TRANSMUTE";
+enum XATTR_SMACK_MMAP = "SMACK64MMAP";
+
+enum XATTR_NAME_SMACK = XATTR_SECURITY_PREFIX ~ XATTR_SMACK_SUFFIX;
+enum XATTR_NAME_SMACKIPIN = XATTR_SECURITY_PREFIX ~ XATTR_SMACK_IPIN;
+enum XATTR_NAME_SMACKIPOUT = XATTR_SECURITY_PREFIX ~ XATTR_SMACK_IPOUT;
+enum XATTR_NAME_SMACKEXEC = XATTR_SECURITY_PREFIX ~ XATTR_SMACK_EXEC;
+enum XATTR_NAME_SMACKTRANSMUTE = XATTR_SECURITY_PREFIX ~ XATTR_SMACK_TRANSMUTE;
+enum XATTR_NAME_SMACKMMAP = XATTR_SECURITY_PREFIX ~ XATTR_SMACK_MMAP;
+
+enum XATTR_CAPS_SUFFIX = "capability";
+enum XATTR_NAME_CAPS = XATTR_SECURITY_PREFIX ~ XATTR_CAPS_SUFFIX;
+
 
 int setxattr(in char* path, in char* name, in void* value, size_t size, int flags);
 
