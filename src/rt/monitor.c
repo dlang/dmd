09979d25777dd4f5ae7fda6b38e44f0e2117f5b9
Walter Bright <walter@walterbright.com>
2012-09-09 14:30:19 -0700

    revert stuff that inadvertently crept back in

--- src/rt/monitor.c
+++ src/rt/monitor.c
@@ -1,33 +1,21 @@
 /**
- * Contains the implementation for object monitors.
+ * Contains the Windows implementation for object monitors.
  *
- * Copyright: Copyright Digital Mars 2000 - 2010.
- * License:   <a href="http://www.boost.org/LICENSE_1_0.txt">Boost License 1.0</a>.
+ * Copyright: Copyright Digital Mars 2000 - 2012.
+ * License: Distributed under the
+ *      $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0).
+ *    (See accompanying file LICENSE)
  * Authors:   Walter Bright, Sean Kelly
+ * Source: $(DRUNTIMESRC src/rt/_monitor.c)
  */
 
-/*          Copyright Digital Mars 2000 - 2010.
- * Distributed under the Boost Software License, Version 1.0.
- *    (See accompanying file LICENSE_1_0.txt or copy at
- *          http://www.boost.org/LICENSE_1_0.txt)
- */
+#if _WIN32 /* and Windows 64 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
-#if _WIN32
-#elif linux || __APPLE__ || __FreeBSD__
-#define USE_PTHREADS    1
-#else
-#endif
-
-#if _WIN32
 #include <windows.h>
-#endif
-
-#if USE_PTHREADS
-#include <pthread.h>
-#endif
 
 #include "mars.h"
 
@@ -38,13 +26,7 @@ typedef struct Monitor
     Array  devt; // for internal monitors
     size_t refs; // reference count
 
-#if _WIN32
     CRITICAL_SECTION mon;
-#endif
-
-#if USE_PTHREADS
-    pthread_mutex_t mon;
-#endif
 } Monitor;
 
 #define MONPTR(h)       (&((Monitor *)(h)->monitor)->mon)
@@ -52,8 +34,6 @@ typedef struct Monitor
 static volatile int inited;
 
 /* =============================== Win32 ============================ */
-
-#if _WIN32
 
 static CRITICAL_SECTION _monitor_critsec;
 
@@ -129,93 +109,3 @@ void _d_monitor_unlock(Object *h)
 
 #endif
 
-/* =============================== linux ============================ */
-
-#if USE_PTHREADS
-
-#ifdef linux
-#  ifndef PTHREAD_MUTEX_RECURSIVE
-#    define PTHREAD_MUTEX_RECURSIVE PTHREAD_MUTEX_RECURSIVE_NP
-#  endif
-#endif
-
-// Includes attribute fixes from David Friedman's GDC port
-
-static pthread_mutex_t _monitor_critsec;
-static pthread_mutexattr_t _monitors_attr;
-
-void _STI_monitor_staticctor()
-{
-    if (!inited)
-    {
-        pthread_mutexattr_init(&_monitors_attr);
-        pthread_mutexattr_settype(&_monitors_attr, PTHREAD_MUTEX_RECURSIVE);
-        pthread_mutex_init(&_monitor_critsec, &_monitors_attr);
-        inited = 1;
-    }
-}
-
-void _STD_monitor_staticdtor()
-{
-    if (inited)
-    {   inited = 0;
-        pthread_mutex_destroy(&_monitor_critsec);
-        pthread_mutexattr_destroy(&_monitors_attr);
-    }
-}
-
-void _d_monitor_create(Object *h)
-{
-    /*
-     * NOTE: Assume this is only called when h->monitor is null prior to the
-     * call.  However, please note that another thread may call this function
-     * at the same time, so we can not assert this here.  Instead, try and
-     * create a lock, and if one already exists then forget about it.
-     */
-
-    //printf("+_d_monitor_create(%p)\n", h);
-    assert(h);
-    Monitor *cs = NULL;
-    pthread_mutex_lock(&_monitor_critsec);
-    if (!h->monitor)
-    {
-        cs = (Monitor *)calloc(sizeof(Monitor), 1);
-        assert(cs);
-        pthread_mutex_init(&cs->mon, & _monitors_attr);
-        h->monitor = (void *)cs;
-        cs->refs = 1;
-        cs = NULL;
-    }
-    pthread_mutex_unlock(&_monitor_critsec);
-    if (cs)
-        free(cs);
-    //printf("-_d_monitor_create(%p)\n", h);
-}
-
-void _d_monitor_destroy(Object *h)
-{
-    //printf("+_d_monitor_destroy(%p)\n", h);
-    assert(h && h->monitor && !(((Monitor*)h->monitor)->impl));
-    pthread_mutex_destroy(MONPTR(h));
-    free((void *)h->monitor);
-    h->monitor = NULL;
-    //printf("-_d_monitor_destroy(%p)\n", h);
-}
-
-void _d_monitor_lock(Object *h)
-{
-    //printf("+_d_monitor_acquire(%p)\n", h);
-    assert(h && h->monitor && !(((Monitor*)h->monitor)->impl));
-    pthread_mutex_lock(MONPTR(h));
-    //printf("-_d_monitor_acquire(%p)\n", h);
-}
-
-void _d_monitor_unlock(Object *h)
-{
-    //printf("+_d_monitor_release(%p)\n", h);
-    assert(h && h->monitor && !(((Monitor*)h->monitor)->impl));
-    pthread_mutex_unlock(MONPTR(h));
-    //printf("-_d_monitor_release(%p)\n", h);
-}
-
-#endif
