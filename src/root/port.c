74671e8108452d088076c005aeb8265a7a493835
Walter Bright <walter@walterbright.com>
2012-09-13 22:12:27 -0700

    merge D2 pull #1113

--- src/root/port.c
+++ src/root/port.c
@@ -610,7 +610,7 @@ double Port::pow(double x, double y)
 longdouble Port::fmodl(longdouble x, longdouble y)
 {
     return ::fmodl(x, y);
-}   
+}
 
 unsigned long long Port::strtoull(const char *p, char **pend, int base)
 {
