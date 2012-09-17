108abf50f04b85c4bf5a3c1d6d48300d13ec1f51
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2012-09-17 19:35:12 +0400

    Remove redundant trimming from statictor-postscript.sh

--- test/runnable/extra-files/statictor-postscript.sh
+++ test/runnable/extra-files/statictor-postscript.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-# trim off the first line which contains the path of the file which differs between windows and non-windows
-# also trim off compiler debug message
-grep -v "runnable\|DEBUG" ${RESULTS_DIR}/runnable/statictor.d.log > ${RESULTS_DIR}/runnable/statictor.d.log.2
-
-diff --strip-trailing-cr runnable/extra-files/statictor.d.out ${RESULTS_DIR}/runnable/statictor.d.log.2
+diff --strip-trailing-cr runnable/extra-files/statictor.d.out ${RESULTS_DIR}/runnable/statictor.d.out
 if [ $? -ne 0 ]; then
     exit 1;
 fi
-
-rm ${RESULTS_DIR}/runnable/statictor.d.log.2
-
