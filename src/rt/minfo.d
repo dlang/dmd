3c9cf0815c61740be80c187c8d6130bdf305d31b
Walter Bright <walter@walterbright.com>
2012-10-06 13:34:02 -0700

    Win64 module info

--- src/rt/minfo.d
+++ src/rt/minfo.d
@@ -102,18 +102,6 @@ private:
     ModuleInfo*[] _tlsctors;
 }
 
-version (Windows)
-{
-    // Windows: this gets initialized by minit.asm
-    // Posix: this gets initialized in _moduleCtor()
-    extern(C) __gshared ModuleInfo*[] _moduleinfo_array;
-    extern(C) void _minit();
-}
-version (OSX)
-{
-    extern (C) __gshared ModuleInfo*[] _moduleinfo_array;
-}
-
 __gshared ModuleGroup _moduleGroup;
 
 /********************************************
@@ -170,7 +158,25 @@ extern (C) void rt_moduleDtor()
  * Access compiler generated list of modules.
  */
 
-version (OSX) {}
+version (Win32)
+{
+    // Windows: this gets initialized by minit.asm
+    // Posix: this gets initialized in _moduleCtor()
+    extern(C) __gshared ModuleInfo*[] _moduleinfo_array;
+    extern(C) void _minit();
+}
+else version (Win64)
+{
+    extern (C)
+    {
+        extern void* _minfo_beg;
+        extern void* _minfo_end;
+    }
+}
+else version (OSX)
+{
+    extern (C) __gshared ModuleInfo*[] _moduleinfo_array;
+}
 else version (Posix)
 {
     // This linked list is created by a compiler generated function inserted
@@ -182,6 +188,10 @@ else version (Posix)
     }
 
     extern (C) __gshared ModuleReference* _Dmodule_ref;   // start of linked list
+}
+else
+{
+    static assert(0);
 }
 
 ModuleInfo*[] getModuleInfos()
@@ -228,11 +238,19 @@ body
             len++;
         }
     }
-    else version (Windows)
+    else version (Win32)
     {
         // _minit directly alters the global _moduleinfo_array
         _minit();
         result = _moduleinfo_array;
+    }
+    else version (Win64)
+    {
+        result = (cast(ModuleInfo**)&_minfo_beg)[1 .. &_minfo_end - &_minfo_end];
+    }
+    else
+    {
+        static assert(0);
     }
     return result;
 }
