7268fce0fb1c5f23e25271fc6828ecbad1330f1a
k-hara <k.hara.pg@gmail.com>
2012-10-13 18:03:29 +0900

    merge D2 pull #683

--- src/init.c
+++ src/init.c
@@ -445,6 +445,7 @@ Initializer *ArrayInitializer::semantic(Scope *sc, Type *t, NeedInterpret needIn
         return this;
     sem = 1;
     type = t;
+    Initializer *aa = NULL;
     t = t->toBasetype();
     switch (t->ty)
     {
@@ -452,6 +453,11 @@ Initializer *ArrayInitializer::semantic(Scope *sc, Type *t, NeedInterpret needIn
         case Tsarray:
         case Tarray:
             break;
+
+        case Taarray:
+            // was actually an associative array literal
+            aa = toAssocArrayInitializer();
+            return aa->semantic(sc, t, needInterpret);
 
         default:
             error(loc, "cannot use array to initialize %s", type->toChars());
@@ -564,7 +570,12 @@ Expression *ArrayInitializer::toExpression()
         for (size_t i = 0, j = 0; i < value.dim; i++, j++)
         {
             if (index[i])
-                j = index[i]->toInteger();
+            {
+                if (index[i]->op == TOKint64)
+                    j = index[i]->toInteger();
+                else
+                    goto Lno;
+            }
             if (j >= edim)
                 edim = j + 1;
         }
