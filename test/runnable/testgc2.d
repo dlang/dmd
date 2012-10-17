4a9232e0ef9ca63cc88d3009dd56f3ac993e77f9
David Nadlinger <code@klickverbot.at>
2012-10-17 23:48:57 +0200

    testgc2: Minor cleanup.
    Tabs -> spaces, removed empty version(none) block.

--- test/runnable/testgc2.d
+++ test/runnable/testgc2.d
@@ -9,16 +9,11 @@ import core.exception : OutOfMemoryError;
 
 void test1()
 {
-  version (none)
-  {
-  }
-  else
-  {
     printf("This should not take a while\n");
     try
     {
-	long[] l = new long[ptrdiff_t.max];
-	assert(0);
+        long[] l = new long[ptrdiff_t.max];
+        assert(0);
     }
     catch (OutOfMemoryError o)
     {
@@ -27,14 +22,13 @@ void test1()
     printf("This may take a while\n");
     try
     {
-	byte[] b = new byte[size_t.max / 3];
-	version (Windows)
-	    assert(0);
+        byte[] b = new byte[size_t.max / 3];
+        version (Windows)
+            assert(0);
     }
     catch (OutOfMemoryError o)
     {
     }
-  }
 }
 
 /*******************************************/
