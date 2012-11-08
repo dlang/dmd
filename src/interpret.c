bf3a4c05952e31c9a00687280c413d033b08784f
Walter Bright <walter@walterbright.com>
2012-11-07 16:34:26 -0800

    fix issue 7940 - CTFE wrong-code for opAssign and std.array.Appender

--- src/interpret.c
+++ src/interpret.c
@@ -1898,10 +1898,10 @@ Expression *ArrayLiteralExp::interpret(InterState *istate, CtfeGoal goal)
 {   Expressions *expsx = NULL;
 
 #if LOG
-    printf("ArrayLiteralExp::interpret() %s\n", toChars());
+    printf("%s ArrayLiteralExp::interpret() %s\n", loc.toChars(), toChars());
 #endif
     if (ownedByCtfe) // We've already interpreted all the elements
-        return copyLiteral(this);
+        return this;
     if (elements)
     {
         for (size_t i = 0; i < elements->dim; i++)
