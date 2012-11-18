aaee56d289cbbb5c08657b588985db7af634a7f4
Leandro Lucarella <luca@llucax.com.ar>
2012-11-18 12:32:05 +0100

    Remove old, unused deprecation code

--- src/parse.c
+++ src/parse.c
@@ -1313,8 +1313,6 @@ BaseClasses *Parser::parseBaseClasses()
                 nextToken();
                 break;
         }
-        //if (prot)
-            //deprecation("use of base class protection is deprecated");
         if (token.value == TOKidentifier || token.value == TOKdot)
         {
             BaseClass *b = new BaseClass(parseBasicType(), protection);
