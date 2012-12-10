af2c21b58e357a214da2bc1cf959c322f8ad1cd4
Walter Bright <walter@walterbright.com>
2012-12-10 03:55:47 -0800

    fix issue 9057 - Regression(Beta): Segfault or 'Non-constant expression' error with local import

---
+++ test/compilable/test9057.d
@@ -0,0 +1,16 @@
+struct Bug9057(T)
+{
+    T x;
+}
+
+void test9507() {
+    import imp9057;
+    Bug9057!(BugInt) xxx;
+}
+
+void test9507_2() {
+    import imp9057_2;
+    Bug9057!(BugInt) xxx;
+}
+
+
