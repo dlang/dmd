3f5912d00ab1a81c5c47140026a5dd483840f825
k-hara <k.hara.pg@gmail.com>
2012-12-14 00:26:08 +0900

    fix Issue 9150 - Mismatching static array length should be detected in foreach

--- test/fail_compilation/fail3703.d
+++ test/fail_compilation/fail3703.d
@@ -1,5 +1,12 @@
 // Issue 3703 - static array assignment
 
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fail3703.d(15): Error: mismatched array lengths, 2 and 1
+---
+*/
+
 void main()
 {
     int[1] a = [1];
