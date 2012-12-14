473499c0e461ccd43baad3239db14d230a7699e0
k-hara <k.hara.pg@gmail.com>
2012-12-14 16:37:36 +0900

    fix Issue 9037 - DDoc: Chokes on consecutive example sections

---
+++ test/compilable/ddoc9037.d
@@ -0,0 +1,18 @@
+// PERMUTE_ARGS:
+// REQUIRED_ARGS: -D -Ddtest_results/compilable -o-
+// POST_SCRIPT: compilable/extra-files/ddocAny-postscript.sh 9037
+
+module ddoc9037;
+
+/**
+Example:
+----
+D d = d;
+----
+----
+D d = d;
+----
+*/
+void test9037()
+{
+}
