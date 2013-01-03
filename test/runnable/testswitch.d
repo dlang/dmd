aff0bbb9dc4ab1396288e114e716ac7dc18a8894
k-hara <k.hara.pg@gmail.com>
2013-01-04 00:18:44 +0900

    detab test/runnable/testswitch.d

--- test/runnable/testswitch.d
+++ test/runnable/testswitch.d
@@ -8,28 +8,29 @@ int testswitch(string h)
 
     switch (h)
     {
-	case "abc":
-	    printf("abc\n");
-	    x = 4;
-	    break;
-	case "foo":
-	    printf("foo\n");
-	    x = 1;
-	    break;
-	case "bar":
-	    printf("bar\n");
-	    x = 2;
-	    break;
-	default:
-	    printf("default\n");
-	    x = 3;
-	    break;
+        case "abc":
+            printf("abc\n");
+            x = 4;
+            break;
+        case "foo":
+            printf("foo\n");
+            x = 1;
+            break;
+        case "bar":
+            printf("bar\n");
+            x = 2;
+            break;
+        default:
+            printf("default\n");
+            x = 3;
+            break;
     }
     return x;
 }
 
 void test1()
-{   int i;
+{
+    int i;
 
     i = testswitch("foo");
     printf("i = %d\n", i);
@@ -43,18 +44,19 @@ void test1()
 /*****************************************/
 
 void test2()
-{   int i;
+{
+    int i;
 
     switch (5)
     {
-	case 3,4,5,6:
-	    i = 20;
-	    break;
+        case 3,4,5,6:
+            i = 20;
+            break;
 
-	case 7:
-	default:
-	    assert(0);
-	    break;
+        case 7:
+        default:
+            assert(0);
+            break;
     }
     assert(i == 20);
 }
@@ -63,20 +65,21 @@ void test2()
 /*****************************************/
 
 void test3()
-{   int i;
+{
+    int i;
 
     switch (5)
     {
-	case 7:
-	    i = 6;
-	    goto default;
-	default:
-	    i = 8;
-	    break;
+        case 7:
+            i = 6;
+            goto default;
+        default:
+            i = 8;
+            break;
 
-	case 3,4,5,6:
-	    i = 20;
-	    goto default;
+        case 3,4,5,6:
+            i = 20;
+            goto default;
     }
     assert(i == 8);
 }
@@ -89,17 +92,17 @@ void test4()
 
     switch (5)
     {
-	case 3,4,5,6:
-	    i = 20;
-	    goto default;
+        case 3,4,5,6:
+            i = 20;
+            goto default;
 
-	case 7:
-	    i = 6;
-	    goto default;
+        case 7:
+            i = 6;
+            goto default;
 
-	default:
-	    i = 8;
-	    break;
+        default:
+            i = 8;
+            break;
     }
     assert(i == 8);
 }
@@ -112,16 +115,16 @@ void test5()
 
     switch (5)
     {
-	case 7:
-	    i = 6;
-	    goto case;
-	default:
-	    i = 8;
-	    break;
+        case 7:
+            i = 6;
+            goto case;
+        default:
+            i = 8;
+            break;
 
-	case 3,4,5,6:
-	    i = 20;
-	    break;
+        case 3,4,5,6:
+            i = 20;
+            break;
     }
     assert(i == 20);
 }
@@ -130,20 +133,21 @@ void test5()
 /*****************************************/
 
 void test6()
-{   int i;
+{
+    int i;
 
     switch (5)
     {
-	case 7:
-	    i = 6;
-	    goto case 4;
-	default:
-	    i = 8;
-	    break;
+        case 7:
+            i = 6;
+            goto case 4;
+        default:
+            i = 8;
+            break;
 
-	case 3,4,5,6:
-	    i = 20;
-	    break;
+        case 3,4,5,6:
+            i = 20;
+            break;
     }
     assert(i == 20);
 }
@@ -152,20 +156,21 @@ void test6()
 /*****************************************/
 
 void test7()
-{   int i;
+{
+    int i;
 
     switch (5)
     {
-	case 3,4,5,6:
-	    i = 20;
-	    break;
+        case 3,4,5,6:
+            i = 20;
+            break;
 
-	case 7:
-	    i = 6;
-	    goto case 4;
-	default:
-	    i = 8;
-	    break;
+        case 7:
+            i = 6;
+            goto case 4;
+        default:
+            i = 8;
+            break;
     }
     assert(i == 20);
 }
@@ -178,15 +183,15 @@ void test8()
     dstring str = "xyz";
     switch (str)
     {
-	case "xyz":
-	    printf("correct\n");
-	    return;
+        case "xyz":
+            printf("correct\n");
+            return;
 
-	case "abc":
-	    break;
+        case "abc":
+            break;
 
-	default:
-	    assert(0);
+        default:
+            assert(0);
     }
     assert(0);
 }
@@ -199,18 +204,18 @@ void test9()
 
     switch(i)
     {
-	case 2:
-	    return;
-	case 1:
-	    switch(i)
-	    {
-		case 1:
-		    goto case 2;
-		default:
-		    assert(0);
-	    }
-	default:
-	    assert(0);
+        case 2:
+            return;
+        case 1:
+            switch(i)
+            {
+                case 1:
+                    goto case 2;
+                default:
+                    assert(0);
+            }
+        default:
+            assert(0);
     }
     assert(0);
 }
@@ -231,8 +236,8 @@ void test10()
         case 3: ++id; goto case;
         case 2: ++id; goto case;
         case 1: ++id; goto default;
-	default:
-	    break;
+        default:
+            break;
     }
     assert(id == 8);
 }
@@ -244,11 +249,11 @@ void test11()
     long foo = 4;
     switch (foo)
     {
-	case 2: assert (false); break;
-	case 3: break;
-	case 4: break;
-	case 5: break;
-	default: assert(0);
+        case 2: assert (false); break;
+        case 3: break;
+        case 4: break;
+        case 5: break;
+        default: assert(0);
     }
 }
 
@@ -256,68 +261,68 @@ void test11()
 
 void test12()
 {
-  switch("#!")
-  {
-    case "#!": printf("----Found #!\n");    break;
-    case "\xFF\xFE"c:                       break;
-    default:
-	assert(0);
-	printf("----Found ASCII\n"); break;
-  }
+    switch("#!")
+    {
+        case "#!": printf("----Found #!\n");    break;
+        case "\xFF\xFE"c:                       break;
+        default:
+            assert(0);
+            printf("----Found ASCII\n");        break;
+    }
 }
 
 /*****************************************/
 
 void test13()
 {
-  switch("#!")
-  {
-    case "#!": printf("----Found #!\n");    break;
-    case "#\xFE"c:                       break;
-    default:
-	assert(0);
-	printf("----Found ASCII\n"); break;
-  }
+    switch("#!")
+    {
+        case "#!": printf("----Found #!\n");    break;
+        case "#\xFE"c:                          break;
+        default:
+            assert(0);
+            printf("----Found ASCII\n");        break;
+    }
 }
 
 /*****************************************/
 
 void foo14(A...)(int i)
 {
-        switch (i)
-        {
-                foreach(a; A)
-                {
-			goto case;
-                case a:
-                        printf("%d\n", a);
-                }
-		break;
-	    default:
-		assert(0);
-        }
+    switch (i)
+    {
+            foreach(a; A)
+            {
+                goto case;
+            case a:
+                printf("%d\n", a);
+            }
+            break;
+        default:
+            assert(0);
+    }
 }
 
 void bar14(A...)(int i)
 {
-        switch (i)
+    switch (i)
+    {
+        foreach(j, a; A)
         {
-                foreach(j, a; A)
-                {
-			goto case;
-                case A[j]:
-                        printf("a = %d, A[%d] = %d\n", a, j, A[j]);
-                }
-		break;
-	    default:
-		assert(0);
+            goto case;
+        case A[j]:
+            printf("a = %d, A[%d] = %d\n", a, j, A[j]);
         }
+        break;
+    default:
+        assert(0);
+    }
 }
 
 void test14()
 {
-        foo14!(1,2,3,4,5)(1); 
-        bar14!(1,2,3,4,5)(1);
+    foo14!(1,2,3,4,5)(1);
+    bar14!(1,2,3,4,5)(1);
 }
 
 /*****************************************/
@@ -331,21 +336,21 @@ int foo15(int i)
     auto y = 1;
     switch (i)
     {
-	case X15:
-	    y += 1;
-	    goto case;
-	case 3:
-	    y += 2;
-	    break;
-	case Y15:
-	    y += 20;
-	    goto case;
-	case Z15:
-	    y += 10;
-	    break;
-	default:
-	    y += 4;
-	    break;
+        case X15:
+            y += 1;
+            goto case;
+        case 3:
+            y += 2;
+            break;
+        case Y15:
+            y += 20;
+            goto case;
+        case Z15:
+            y += 10;
+            break;
+        default:
+            y += 4;
+            break;
     }
     printf("y = %d\n", y);
     return y;
@@ -382,10 +387,10 @@ void test16()
     E16 e = E16.A;
     final switch (e)
     {
-	case E16.A:
-	case E16.B:
-	case E16.C:
-	    ;
+        case E16.A:
+        case E16.B:
+        case E16.C:
+            ;
     }
 }
 
@@ -396,37 +401,37 @@ void test17()
     int i = 2;
     switch (i)
     {
-	case 1: .. case 3:
-	    i = 5;
-	    break;
-	default:
-	    assert(0);
+        case 1: .. case 3:
+            i = 5;
+            break;
+        default:
+            assert(0);
     }
     if (i != 5)
-	assert(0);
+        assert(0);
 
     switch (i)
     {
-	case 1: .. case 3:
-	    i = 4;
-	    break;
-	case 5:
-	    i = 6;
-	    break;
-	default:
-	    assert(0);
+        case 1: .. case 3:
+            i = 4;
+            break;
+        case 5:
+            i = 6;
+            break;
+        default:
+            assert(0);
     }
     if (i != 6)
-	assert(0);
+        assert(0);
 }
 
 /*****************************************/
 
 int test19()
 {
-  enum foo{ bar };
-  foo x;
-  final switch(x){ case foo.bar: return 0; }
+    enum foo{ bar };
+    foo x;
+    final switch(x){ case foo.bar: return 0; }
 }
 
 /*****************************************/
@@ -435,10 +440,10 @@ void test20()
 {
     switch(1)
     {
-	mixin("case 0:{}");
-	case 1:
-	case 2:
-	default:
+        mixin("case 0:{}");
+        case 1:
+        case 2:
+        default:
     }
 }
 
@@ -458,7 +463,7 @@ int wrongcode3139(int x)
         case -9: .. case 2: return 3;
         default:
         return 4;
-   }   
+   }
 }
 
 static assert(wrongcode3139(-5)==3);
