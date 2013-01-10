6aad61027c0ffdf8b504d14cc558d98bd62c0cdb
k-hara <k.hara.pg@gmail.com>
2013-01-10 18:45:48 +0900

    Fix test cases 4
    Fail-to-compile tests by using `deprecated` symbols use `REQUIRED_ARGS: -de`

--- test/fail_compilation/fail199.d
+++ test/fail_compilation/fail199.d
@@ -1,4 +1,12 @@
 // REQUIRED_ARGS: -de
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fail199.d(19): Deprecation: class fail199.DepClass is deprecated
+fail_compilation/fail199.d(19): Deprecation: class fail199.DepClass is deprecated
+---
+*/
+// Issue 549 - A class derived from a deprecated class is not caught
 
 import std.stdio;
 
--- test/fail_compilation/fail200.d
+++ test/fail_compilation/fail200.d
@@ -1,4 +1,12 @@
 // REQUIRED_ARGS: -de
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fail200.d(17): Deprecation: interface fail200.DepClass is deprecated
+fail_compilation/fail200.d(17): Deprecation: interface fail200.DepClass is deprecated
+---
+*/
+// Issue 549 - A class derived from a deprecated class is not caught
 
 import std.stdio;
 
