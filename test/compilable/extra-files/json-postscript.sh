f637f306a4566f1e23c1f20432a4d104a93cb1fb
Walter Bright <walter@walterbright.com>
2013-01-20 22:24:28 -0800

    json test case from pull #1179

--- test/compilable/extra-files/json-postscript.sh
+++ test/compilable/extra-files/json-postscript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-grep -v "\"file\" : " ${RESULTS_DIR}/compilable/json.out > ${RESULTS_DIR}/compilable/json.out.2
+grep -v "\"file\" : " ${RESULTS_DIR}/compilable/json.out | grep -v "\"offset\" : " > ${RESULTS_DIR}/compilable/json.out.2
 diff --strip-trailing-cr compilable/extra-files/json.out ${RESULTS_DIR}/compilable/json.out.2
 if [ $? -ne 0 ]; then
     exit 1;
