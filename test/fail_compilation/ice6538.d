38cc12d81f242eed4b1827b70a572a4ab7e52be1
k-hara <k.hara.pg@gmail.com>
2013-01-23 11:29:17 +0900

    fix Issue 9361 - Nasty bug and/or error message for template constaints using this
    Add test cases

--- test/fail_compilation/ice6538.d
+++ test/fail_compilation/ice6538.d
@@ -41,3 +41,52 @@ void test6538b()
     bar(42, 86);
 }
 
+/**************************************/
+// 9361
+
+template Sym(alias A)
+{
+    enum Sym = true;
+}
+
+/*
+TEST_OUTPUT:
+---
+fail_compilation/ice6538.d(63): Error: cannot take a not yet instantiated symbol 'this' inside template constraint
+fail_compilation/ice6538.d(69): Error: template ice6538.S.foo does not match any function template declaration. Candidates are:
+fail_compilation/ice6538.d(63):        ice6538.S.foo()() if (Sym!(this))
+fail_compilation/ice6538.d(69): Error: template ice6538.S.foo()() if (Sym!(this)) cannot deduce template function from argument types !()()
+---
+*/
+struct S
+{
+    void foo()() if (Sym!(this)) {}
+    void bar()() { static assert(Sym!(this)); }   // OK
+}
+void test9361a()
+{
+    S s;
+    s.foo();    // fail
+    s.bar();    // OK
+}
+
+/*
+TEST_OUTPUT:
+---
+fail_compilation/ice6538.d(85): Error: cannot take a not yet instantiated symbol 'super' inside template constraint
+fail_compilation/ice6538.d(90): Error: template ice6538.D.foo does not match any function template declaration. Candidates are:
+fail_compilation/ice6538.d(85):        ice6538.D.foo()() if (Sym!(super))
+fail_compilation/ice6538.d(90): Error: template ice6538.D.foo()() if (Sym!(super)) cannot deduce template function from argument types !()()
+---
+*/
+class C {}
+class D : C
+{
+    void foo()() if (Sym!(super)) {}
+}
+void test9361b()
+{
+    auto d = new D();
+    d.foo();
+}
+
