9a1dda87e84380708890e9faef4f26f152285498
Andrei Alexandrescu <andrei@erdani.com>
2013-02-10 18:27:32 -0500

    Added test cases as suggested by AndrejMitrovic

---
+++ test/compilable/ddoc9497a.d
@@ -0,0 +1,12 @@
+// EXTRA_SOURCES: extra-files/ddoc1653a.ddoc
+// PERMUTE_ARGS:
+// REQUIRED_ARGS: -D -Ddtest_results/compilable -o-
+// POST_SCRIPT: compilable/extra-files/ddocAny-postscript.sh 1653a
+
+/**
+    foo function.
+    Args: $(XYZ arg1, arg2)
+*/
+void foo()
+{
+}
