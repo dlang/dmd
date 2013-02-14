cc806e5eba1bc062d86db9e292732da1cae8585b
ibuclaw <ibuclaw@ubuntu.com>
2013-02-14 09:56:56 +0000

    Mark libc ctype functions as pure where applicable.

--- src/core/stdc/ctype.d
+++ src/core/stdc/ctype.d
@@ -18,17 +18,17 @@ extern (C):
 @trusted: // All of these operate on integers only.
 nothrow:
 
-int isalnum(int c);
-int isalpha(int c);
-int isblank(int c);
-int iscntrl(int c);
-int isdigit(int c);
-int isgraph(int c);
-int islower(int c);
-int isprint(int c);
-int ispunct(int c);
-int isspace(int c);
-int isupper(int c);
-int isxdigit(int c);
-int tolower(int c);
-int toupper(int c);
+pure int isalnum(int c);
+pure int isalpha(int c);
+pure int isblank(int c);
+pure int iscntrl(int c);
+pure int isdigit(int c);
+pure int isgraph(int c);
+pure int islower(int c);
+pure int isprint(int c);
+pure int ispunct(int c);
+pure int isspace(int c);
+pure int isupper(int c);
+pure int isxdigit(int c);
+pure int tolower(int c);
+pure int toupper(int c);
