30b4252dff7d8bff734413dd8c521982b7cd0528
k-hara <k.hara.pg@gmail.com>
2013-04-06 23:02:49 +0900

    fix Issue 8698 - Forward reference error with interfaces

---
+++ test/compilable/fwdref8698b.d
@@ -0,0 +1,14 @@
+// REQUIRED_ARGS: -o-
+// PERMUTE_ARGS:
+
+//interface IRoot {}
+
+interface IClass : IRoot { }
+
+struct Struct { }
+
+class Class : IClass { alias Struct Value; }
+
+void test(Class.Value) { }
+
+interface IRoot {}
