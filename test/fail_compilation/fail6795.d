c8af3c7abd4f41e1809fb8dc9da4d4808b01cf03
k-hara <k.hara.pg@gmail.com>
2013-04-11 12:18:57 +0900

    imrove issue 6795 test case

--- test/fail_compilation/fail6795.d
+++ test/fail_compilation/fail6795.d
@@ -1,4 +1,11 @@
 // 6795
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fail6795.d(11): Error: constant 0 is not an lvalue
+fail_compilation/fail6795.d(11): Error: constant 0 is not an lvalue
+---
+*/
 
 void main() {
     enum int[] array = [0];
