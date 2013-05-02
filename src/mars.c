303fc8117b3f052e7176ba6ea499f6e5c94bce8d
Daniel Murphy <yebblies@gmail.com>
2013-05-03 09:00:57 +1000

    Get rid of nasty EXP_CANT_INTERPRET cast hack

--- src/mars.c
+++ src/mars.c
@@ -1078,6 +1078,7 @@ int tryMain(size_t argc, char *argv[])
     Id::initialize();
     Module::init();
     Target::init();
+    Expression::init();
     initPrecedence();
 
     if (global.params.verbose)
