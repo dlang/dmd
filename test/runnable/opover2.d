ed8f7c697d21c4d8e887f1d2b3809195a29e1e1f
k-hara <k.hara.pg@gmail.com>
2013-05-07 13:23:20 +0900

    fix Issue 10037 - Compiler should not generate opEquals method implicitly

--- test/runnable/opover2.d
+++ test/runnable/opover2.d
@@ -787,6 +787,42 @@ bool test3789()
 static assert(test3789());
 
 /**************************************/
+// 10037
+
+struct S10037
+{
+    bool opEquals(ref const S10037) { assert(0); }
+}
+
+struct T10037
+{
+    S10037 s;
+    // Compiler should not generate 'opEquals' here implicitly:
+}
+
+struct Sub10037(TL...)
+{
+    TL data;
+    int value;
+    alias value this;
+}
+
+void test10037()
+{
+    S10037 s;
+    T10037 t;
+    static assert( __traits(hasMember, S10037, "opEquals"));
+    static assert(!__traits(hasMember, T10037, "opEquals"));
+    assert(thrown!Error(s == s));
+    assert(thrown!Error(t == t));
+
+    Sub10037!(S10037) lhs;
+    Sub10037!(S10037) rhs;
+    static assert(!__traits(hasMember, Sub10037!(S10037), "opEquals"));
+    assert(lhs == rhs);     // lowered to: lhs.value == rhs.value
+}
+
+/**************************************/
 // 7641
 
 mixin template Proxy7641(alias a)
@@ -1167,6 +1203,7 @@ int main()
     test16();
     test17();
     test3789();
+    test10037();
     test7641();
     test8434();
     test18();
