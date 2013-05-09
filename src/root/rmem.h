7b67a875b7b909da85d85af898535baf04379257
Daniel Murphy <yebblies@gmail.com>
2013-05-10 03:43:07 +1000

    Remove some unused parts of root/rmem

--- src/root/rmem.h
+++ src/root/rmem.h
@@ -23,33 +23,17 @@ struct Mem
 
     void init();
 
-    // Derive from Mem to get these storage allocators instead of global new/delete
-    void * operator new(size_t m_size);
-    void * operator new(size_t m_size, Mem *mem);
-    void * operator new(size_t m_size, GC *gc);
-    void operator delete(void *p);
-
-    void * operator new[](size_t m_size);
-    void operator delete[](void *p);
-
     char *strdup(const char *s);
     void *malloc(size_t size);
-    void *malloc_uncollectable(size_t size);
     void *calloc(size_t size, size_t n);
     void *realloc(void *p, size_t size);
     void free(void *p);
-    void free_uncollectable(void *p);
     void *mallocdup(void *o, size_t size);
     void error();
-    void check(void *p);        // validate pointer
     void fullcollect();         // do full garbage collection
-    void fullcollectNoStack();  // do full garbage collection, no scan stack
     void mark(void *pointer);
     void addroots(char* pStart, char* pEnd);
-    void removeroots(char* pStart);
-    void setFinalizer(void* pObj, FINALIZERPROC pFn, void* pClientData);
     void setStackBottom(void *bottom);
-    GC *getThreadGC();          // get apartment allocator for this thread
 };
 
 extern Mem mem;
