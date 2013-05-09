2df2571896d647a6c9d32f754fd21bae1a70cd1e
k-hara <k.hara.pg@gmail.com>
2013-05-09 16:14:17 +0900

    Move issue 9068 test to runnable/foreach5.d

--- test/runnable/foreach5.d
+++ test/runnable/foreach5.d
@@ -575,6 +575,86 @@ string test8595()
 }
 
 /***************************************/
+// 9068
+
+struct Foo9068
+{
+    static int[] destroyed;
+    int x;
+    ~this() { destroyed ~= x; }
+}
+
+struct SimpleCounter9068
+{
+    static int destroyedCount;
+    const(int) limit = 5;
+    int counter;
+    ~this() { destroyedCount++; }
+
+    // Range primitives.
+    @property bool empty() const { return counter >= limit; }
+    @property int front() { return counter; }
+    void popFront() { counter++; }
+}
+
+// ICE when trying to break outer loop from inside switch statement
+void test9068()
+{
+    //----------------------------------------
+    // There was never a bug in this case (no range).
+    int sum;
+loop_simple:
+    foreach (i; [10, 20]) {
+        sum += i;
+        break loop_simple;
+    }
+    assert(sum == 10);
+
+    //----------------------------------------
+    // There was a bug with loops over ranges.
+    int last = -1;
+X:  foreach (i; SimpleCounter9068()) {
+       switch(i) {
+           case 3: break X;
+           default: last = i;
+       }
+    }
+    assert(last == 2);
+    assert(SimpleCounter9068.destroyedCount == 1);
+
+    //----------------------------------------
+    // Simpler case: the compiler error had nothing to do with the switch.
+    last = -1;
+loop_with_range:
+    foreach (i; SimpleCounter9068()) {
+        last = i;
+        break loop_with_range;
+    }
+    assert(last == 0);
+    assert(SimpleCounter9068.destroyedCount == 2);
+
+    //----------------------------------------
+    // Test with destructors: the loop is implicitly wrapped into two
+    // try/finally clauses.
+loop_with_dtors:
+    for (auto x = Foo9068(4), y = Foo9068(5); x.x != 10; ++x.x) {
+        if (x.x == 8)
+            break loop_with_dtors;
+    }
+    assert(Foo9068.destroyed == [5, 8]);
+    Foo9068.destroyed.clear();
+
+    //----------------------------------------
+    // Same with an unlabelled break.
+    for (auto x = Foo9068(4), y = Foo9068(5); x.x != 10; ++x.x) {
+        if (x.x == 7)
+            break;
+    }
+    assert(Foo9068.destroyed == [5, 7]);
+    Foo9068.destroyed.clear();
+}
+
+/***************************************/
 
 int main()
 {
@@ -594,6 +674,7 @@ int main()
     test6659c();
     test7814();
     test6652();
+    test9068();
 
     printf("Success\n");
     return 0;
