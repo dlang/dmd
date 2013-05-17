40c00264079cbacc58f63703da1a02abb6a57b4d
Walter Bright <walter@walterbright.com>
2013-05-17 12:59:02 -0700

    Merge pull request #2040 from 9rnsr/fix10089
    [REG2.063a] Issue 10089 - Strange function call error message with specified module

---
+++ test/fail_compilation/diag10089.d
@@ -0,0 +1,18 @@
+/*
+TEST_OUTPUT:
+---
+fail_compilation/diag10089.d(15): Error: undefined identifier 'chunks'
+fail_compilation/diag10089.d(17): Error: no property 'chunks' for type 'void'
+---
+*/
+
+import imports.diag10089a, imports.diag10089b;
+
+template Foo() {}
+
+void main()
+{
+    imports.chunks("abcdef", 2);
+
+    Foo.chunks("abcdef", 2);
+}
