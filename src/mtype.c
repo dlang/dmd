0d5bddae95174b8b8cc5636d7d433f0cef143818
k-hara <k.hara.pg@gmail.com>
2013-05-27 12:55:50 +0900

    Fix up for bug 10178
    Treat side effect correctly

--- src/mtype.c
+++ src/mtype.c
@@ -7826,7 +7826,7 @@ Expression *TypeStruct::dotExp(Scope *sc, Expression *e, Identifier *ident, int 
 
         Expression *e0 = NULL;
         Expression *ev = e;
-        if (sc->func && sym->fields.dim > 1 && e->hasSideEffect())
+        if (sc->func && e->hasSideEffect())
         {
             Identifier *id = Lexer::uniqueId("__tup");
             ExpInitializer *ei = new ExpInitializer(e->loc, e);
@@ -8369,7 +8369,7 @@ Expression *TypeClass::dotExp(Scope *sc, Expression *e, Identifier *ident, int f
 
         Expression *e0 = NULL;
         Expression *ev = e;
-        if (sc->func && sym->fields.dim > 1 && e->hasSideEffect())
+        if (sc->func && e->hasSideEffect())
         {
             Identifier *id = Lexer::uniqueId("__tup");
             ExpInitializer *ei = new ExpInitializer(e->loc, e);
