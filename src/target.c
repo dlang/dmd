f08673b5b695ff38767d63f48a0de715bcf0a0f6
ibuclaw <ibuclaw@ubuntu.com>
2013-06-02 23:28:30 +0100

    Add front-end support for 64bit X32 target.

--- src/target.c
+++ src/target.c
@@ -123,7 +123,10 @@ unsigned Target::critsecsize()
     else if (global.params.isLinux)
     {
         // sizeof(pthread_mutex_t) for Linux.
-        return global.params.isLP64 ? 40 : 24;
+        if (global.params.is64bit)
+            return global.params.isLP64 ? 40 : 32;
+        else
+            return global.params.isLP64 ? 40 : 24;
     }
     else if (global.params.isFreeBSD)
     {
