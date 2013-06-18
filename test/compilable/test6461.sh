f14149de17193b12244f6eb60ec532e164c422b4
Rainer Schuetze <r.sagitario@gmx.de>
2013-06-18 22:23:58 +0200

    add -m$(MODEL) to test and simplify

--- test/compilable/test6461.sh
+++ test/compilable/test6461.sh
@@ -4,24 +4,18 @@ name=`basename $0 .sh`
 dir=${RESULTS_DIR}/compilable
 src=compilable/extra-files/test6461
 
-$DMD -lib -od${dir} -I${src} ${src}/a.d || exit 1
-$DMD -lib -od${dir} -I${src} ${src}/b.d || exit 1
-
 if [ "${OS}" == "win32" -o "${OS}" == "Windows_NT" ]; then
-
-    $DMD -od${dir} -I${src} ${src}/main.d ${dir}/a.lib ${dir}/b.lib || exit 1
-    rm -f ${dir}/a.lib
-    rm -f ${dir}/b.lib
-    rm -f ${dir}/main.obj
-    rm -f ${dir}/main.exe
+    LIBEXT=.lib
 else
-    $DMD -od${dir} -I${src} ${src}/main.d ${dir}/a.a ${dir}/b.a || exit 1
-
-    rm -f ${dir}/a.a
-    rm -f ${dir}/b.a
-    rm -f ${dir}/main.o
-    rm -f ${dir}/main
+    LIBEXT=.a
 fi
+
+$DMD -lib -m${MODEL} -of${dir}/a${LIBEXT} -I${src} ${src}/a.d || exit 1
+$DMD -lib -m${MODEL} -of${dir}/b${LIBEXT} -I${src} ${src}/b.d || exit 1
+
+$DMD -m${MODEL} -od${dir} -I${src} ${src}/main.d ${dir}/a${LIBEXT} ${dir}/b${LIBEXT} || exit 1
+
+rm -f ${dir}/{a${LIBEXT} b${LIBEXT} main${EXE} main${OBJ}}
 
 echo Success >${dir}/test6461.sh.out
 
