797832d6d2537f1e75f58550b55bdcc6fdef32a9
Henning Pohl <henning@still-hidden.de>
2013-07-11 22:17:06 +0200

    fix issue 10612 - ICE on using enum as hash key with mutual module imports

--- src/mtype.c
+++ src/mtype.c
@@ -4509,7 +4509,7 @@ Type *TypeAArray::semantic(Loc loc, Scope *sc)
             return tsa->semantic(loc, sc);
         }
         else if (t)
-            index = t;
+            index = t->semantic(loc, sc);
         else
         {   index->error(loc, "index is not a type or an expression");
             return Type::terror;
@@ -7272,7 +7272,11 @@ Type *TypeEnum::syntaxCopy()
 Type *TypeEnum::semantic(Loc loc, Scope *sc)
 {
     //printf("TypeEnum::semantic() %s\n", toChars());
-    //sym->semantic(sc);
+    if (!sym->isdone)
+    {
+        assert(sym->scope);
+        sym->semantic(sym->scope);
+    }
     return merge();
 }
 
