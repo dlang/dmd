a688ce1870880c2e9e646351de5e5d2329105b7e
Rainer Schuetze <r.sagitario@gmx.de>
2013-07-13 11:27:08 +0200

    fix sh-tests for win64

--- test/runnable/test10386.sh
+++ test/runnable/test10386.sh
@@ -2,6 +2,7 @@
 
 src=runnable${SEP}extra-files
 dir=${RESULTS_DIR}${SEP}runnable
+output_file=${dir}/test10386.sh.out
 
 if [ $OS == "win32" -o  $OS == "win64" ]; then
 	LIBEXT=.lib
@@ -10,8 +11,9 @@ else
 fi
 libname=${dir}${SEP}lib10386${LIBEXT}
 
-$DMD -m${MODEL} -I${src} -of${libname} -c ${src}${SEP}lib10386${SEP}foo${SEP}bar.d ${src}${SEP}lib10386${SEP}foo${SEP}package.d -lib
-$DMD -m${MODEL} -I${src} -of${dir}${SEP}test10386 ${src}${SEP}test10386.d ${libname}
+$DMD -m${MODEL} -I${src} -of${libname} -c ${src}${SEP}lib10386${SEP}foo${SEP}bar.d ${src}${SEP}lib10386${SEP}foo${SEP}package.d -lib || exit 1
+$DMD -m${MODEL} -I${src} -of${dir}${SEP}test10386${EXE} ${src}${SEP}test10386.d ${libname} || exit 1
 
 rm ${dir}/{lib10386${LIBEXT},test10386${OBJ},test10386${EXE}}
 
+echo Success >${output_file}
