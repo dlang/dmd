b108d1db39440d69b35b0414f345b2227e0b5c46
Rainer Schuetze <r.sagitario@gmx.de>
2013-07-16 20:00:32 +0200

    updated comment regarding chunk size

--- src/root/rmem.c
+++ src/root/rmem.c
@@ -143,7 +143,11 @@ void Mem::addroots(char* pStart, char* pEnd)
 /* Allocate, but never release
  */
 
-#define CHUNK_SIZE (4096 * 16 - 64) // let it fit into 64kB, even if the runtime adds some overhead
+// Allocate a little less than 64kB because the C runtime adds some overhead that
+// causes the actual memory block to be larger than 64kB otherwise. E.g. the dmc
+// runtime rounds the size up to 128kB, but the remaining space in the chunk is less 
+// than 64kB, so it cannot be used by another chunk.
+#define CHUNK_SIZE (4096 * 16 - 64)
 
 static size_t heapleft = 0;
 static void *heapp;
