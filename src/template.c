da93bfdf9021b832ff40512e6910600e753755a3
k-hara <k.hara.pg@gmail.com>
2013-09-05 18:57:13 +0900

    fix Issue 10969 - Variadic template parameter re-use in function signature

--- src/template.c
+++ src/template.c
@@ -1193,7 +1193,7 @@ MATCH TemplateDeclaration::deduceFunctionTemplateMatch(FuncDeclaration *f, Loc l
             if (!paramscope->insert(sparam))
                 goto Lnomatch;
         }
-        if (n < parameters->dim)
+        if (n < parameters->dim && !tp_is_declared)
         {
             inferparams = new TemplateParameters();
             inferparams->setDim(parameters->dim - n);
