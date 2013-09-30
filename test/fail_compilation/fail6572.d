f5ab4e1f1b834c23977c3205ff86c690704c5b26
k-hara <k.hara.pg@gmail.com>
2013-09-30 14:38:55 +0900

    Improve fail_compilation tests by using `TEST_OUTPUT`

--- test/fail_compilation/fail6572.d
+++ test/fail_compilation/fail6572.d
@@ -1,3 +1,10 @@
 // REQUIRED_ARGS: -de
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fail6572.d(10): Deprecation: use of typedef is deprecated; use alias instead
+fail_compilation/fail6572.d(10): Deprecation: use of typedef is deprecated; use alias instead
+---
+*/
 
 typedef int y;
