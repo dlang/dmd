28485485ceb6a31ec6feaf42f8ba532129fbad6e
k-hara <k.hara.pg@gmail.com>
2013-09-30 16:25:28 +0900

    Improve fail_compilation tests by using `TEST_OUTPUT`

--- test/fail_compilation/fail91.d
+++ test/fail_compilation/fail91.d
@@ -1,7 +1,14 @@
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fail91.d(13): Error: struct fail91.S unknown size
+fail_compilation/fail91.d(13): Error: struct fail91.S no size yet for forward reference
+---
+*/
+
 struct S;
 
 void main()
 {
-	S* s = new S();
+    S* s = new S();
 }
-
