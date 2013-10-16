699f0f8aaea6da475a1cb2956bb1d9087b48bf39
Walter Bright <walter@walterbright.com>
2013-10-16 00:48:09 -0700

    Merge pull request #2671 from 9rnsr/fix11225
    [REG2.061] Issue 11225 - Module dependency cycle causes import statements inside typeof() expressions inside templates to fail

--- test/fail_compilation/diag9210a.d
+++ test/fail_compilation/diag9210a.d
@@ -4,6 +4,7 @@
 /*
 TEST_OUTPUT:
 ---
+fail_compilation/imports/diag9210stdcomplex.d(13): Error: template instance Complex!real does not match template declaration Complex(T) if (isFloatingPoint!T)
 fail_compilation/imports/diag9210b.d(6): Error: undefined identifier A, did you mean interface B?
 ---
 */
