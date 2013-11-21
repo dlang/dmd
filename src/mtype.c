25c245a3d6144066281270faa96d67d3d2636f7e
Daniel Murphy <yebblies@gmail.com>
2013-11-21 22:37:52 +1100

    Fix Issue 10439 - Deprecate float.min, double.min, real.min

--- src/mtype.c
+++ src/mtype.c
@@ -2957,7 +2957,7 @@ Expression *TypeBasic::getProperty(Loc loc, Identifier *ident, int flag)
             case Tcomplex80:
             case Timaginary80:
             case Tfloat80:
-                warning(loc, "min property is deprecated, use min_normal instead");
+                deprecation(loc, "min property is deprecated, use min_normal instead");
                 goto Lmin_normal;
         }
     }
