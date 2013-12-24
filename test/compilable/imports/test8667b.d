ab492b2e8db15d500ee87e195752fed8f3f14b2c
k-hara <k.hara.pg@gmail.com>
2013-12-24 09:50:26 +0900

    fix Issue 8667 - selective import breaks normal overload resolution

---
+++ test/compilable/imports/test8667b.d
@@ -0,0 +1,6 @@
+module imports.test8667b;
+
+void split(T)(T k)
+if (is(T : string))
+{
+}
---
+++ test/compilable/test8667.d
@@ -0,0 +1,8 @@
+import imports.test8667a;
+import imports.test8667b : split; // removing : split makes it work
+
+void main()
+{
+    split("abc");
+    split(123);
+}
