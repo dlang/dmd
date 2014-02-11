9301288d178322f2c202e1cefc0c31f8574ea7c6
Daniel Murphy <yebblies@gmail.com>
2014-02-11 19:16:40 +1100

    Allow calling .get on a const AA

--- src/object_.d
+++ src/object_.d
@@ -2045,15 +2045,15 @@ Value[] values(T : Value[Key], Value, Key)(T *aa)
     return (*aa).values();
 }
 
-Value get(T : Value[Key], Value, Key, K, V)(T aa, K key, lazy V defaultValue)
-    if (is(typeof(aa[key] = defaultValue)))
+auto get(T : Value[Key], Value, Key, K, V)(T aa, K key, lazy V defaultValue)
+    if (is(typeof(true ? aa[key] : defaultValue)))
 {
     auto p = key in aa;
-    return p ? *p : cast(Value)defaultValue;
+    return p ? *p : defaultValue;
 }
 
-Value get(T : Value[Key], Value, Key, K, V)(T* aa, K key, lazy V defaultValue)
-    if (is(typeof((*aa).get(key, defaultValue))))
+auto get(T : Value[Key], Value, Key, K, V)(T* aa, K key, lazy V defaultValue)
+    if (is(typeof(true ? (*aa)[key] : defaultValue)))
 {
     return (*aa).get(key, defaultValue);
 }
