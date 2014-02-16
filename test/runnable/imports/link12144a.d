2df7c729466c057f8e07db103d350bed2a8dae94
k-hara <k.hara.pg@gmail.com>
2014-02-16 14:10:54 +0900

    Fix up issue 12144 test case

--- test/runnable/imports/link12144a.d
+++ test/runnable/imports/link12144a.d
@@ -9,7 +9,8 @@ struct S8 { bool opEquals(T : typeof(this))(T) { return false; } }
 struct S9 { bool opEquals(T : typeof(this))(T) { return false; } }
 
 struct S10 { bool opEquals(T : typeof(this))(T) { return false; } }
-struct S11 { int opCmp(T : typeof(this))(T) { return 0; } }
+struct S11 { bool opEquals(T : typeof(this))(T) { return false; }
+             int opCmp(T : typeof(this))(T) const { return 0; } }
 struct S12 { bool opEquals(T : typeof(this))(T) { return false; } }
 struct S13 { bool opEquals(T : typeof(this))(T) { return false; } }
 struct S14 { bool opEquals(T : typeof(this))(T) { return false; } }
