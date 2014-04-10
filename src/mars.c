9d2d7cca1fd2dedd2924974fa84319f763a2ec44
k-hara <k.hara.pg@gmail.com>
2014-04-10 21:58:24 +0900

    fix Issue 12555 - Incorrect error ungagging for speculatively instantiated class
    `ungagSpeculative()` should do nothing for function local symbols.

--- src/mars.c
+++ src/mars.c
@@ -81,7 +81,7 @@ Ungag Dsymbol::ungagSpeculative()
 {
     unsigned oldgag = global.gag;
 
-    if (global.isSpeculativeGagging() && !isSpeculative())
+    if (global.isSpeculativeGagging() && !isSpeculative() && !toParent2()->isFuncDeclaration())
         global.gag = 0;
 
     return Ungag(oldgag);
