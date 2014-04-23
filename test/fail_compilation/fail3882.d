4a84d2a39474741d6520c5fcb0a889f317dac239
k-hara <k.hara.pg@gmail.com>
2014-04-23 20:54:30 +0900

    fix Issue 12619 - Invalid warning for unused return value of debug memcpy

--- test/fail_compilation/fail3882.d
+++ test/fail_compilation/fail3882.d
@@ -1,8 +1,9 @@
 // REQUIRED_ARGS: -w
+// PERMUTE_ARGS: -debug
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail3882.d(26): Warning: Call to function fail3882.strictlyPure!int.strictlyPure without side effects discards return value of type int, prepend a cast(void) if intentional
+fail_compilation/fail3882.d(28): Warning: Call to function fail3882.strictlyPure!int.strictlyPure without side effects discards return value of type int, prepend a cast(void) if intentional
 ---
 */
 
@@ -19,9 +20,22 @@ fail_compilation/fail3882.d(26): Warning: Call to function fail3882.strictlyPure
     return x*x;
 }
 
-void main(string args[]) {
+void main()
+{
     int x = 3;
     strictVoidReturn(x);
     nonstrictVoidReturn(x);
     strictlyPure(x);
+}
+
+/******************************************/
+// 12619
+
+extern (C) @system nothrow pure void* memcpy(void* s1, in void* s2, size_t n);
+// -> weakly pure
+
+void test12619() pure
+{
+    ubyte[10] a, b;
+    debug memcpy(a.ptr, b.ptr, 5);  // memcpy call should have side effect
 }
