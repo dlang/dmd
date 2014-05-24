c9120823c6740dc8141ac153f4da56600b147d73
k-hara <k.hara.pg@gmail.com>
2014-05-24 16:32:55 +0900

    [Refactoring] Remove comment-out code for the fixed issue 7469

--- src/template.c
+++ src/template.c
@@ -7482,18 +7482,10 @@ Identifier *TemplateInstance::genIdent(Objects *args)
             ea = ea->ctfeInterpret();
             if (ea->op == TOKerror || olderr != global.errors)
                 continue;
-#if 1
+
             /* Use deco that matches what it would be for a function parameter
              */
             buf.writestring(ea->type->deco);
-#else
-            // Use type of parameter, not type of argument
-            TemplateParameter *tp = (*tempdecl->parameters)[i];
-            assert(tp);
-            TemplateValueParameter *tvp = tp->isTemplateValueParameter();
-            assert(tvp);
-            buf.writestring(tvp->valType->deco);
-#endif
             ea->toMangleBuffer(&buf);
         }
         else if (sa)
@@ -7507,18 +7499,6 @@ Identifier *TemplateInstance::genIdent(Objects *args)
                 error("forward reference of %s %s", d->kind(), d->toChars());
                 continue;
             }
-#if 0
-            VarDeclaration *v = sa->isVarDeclaration();
-            if (v && v->storage_class & STCmanifest)
-            {
-                ExpInitializer *ei = v->init->isExpInitializer();
-                if (ei)
-                {
-                    ea = ei->exp;
-                    goto Lea;
-                }
-            }
-#endif
             const char *p = mangle(sa);
 
             /* Bugzilla 3043: if the first character of p is a digit this
