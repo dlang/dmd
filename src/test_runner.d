1e40a0522c9f5ca9d1d0f1a2f7e371158b666599
Johannes Pfau <johannespfau@gmail.com>
2014-06-08 18:23:26 +0200

    Revert "fix test runner"
    This reverts commit 8c42dd1f6735eddea1d7441f31bb5839441e794d.

--- src/test_runner.d
+++ src/test_runner.d
@@ -15,44 +15,37 @@ bool tester()
     immutable pkg = ".package";
     immutable pkgLen = pkg.length;
 
-    if (name.length > pkgLen && name[$ - pkgLen .. $] == pkg)
+    if(name.length > pkgLen && name[$ - pkgLen .. $] == pkg)
         name = name[0 .. $ - pkgLen];
 
     bool result = true;
-
-    void testFailed(Throwable t)
+    if (auto tests = getModuleInfo(name).unitTests)
     {
-        auto msg = t.toString();
-        printf("****** FAIL %.*s\n%.*s\n", cast(int)name.length, name.ptr,
-               cast(int)msg.length, msg.ptr);
-        result = false;
-    }
+        immutable t0 = TickDuration.currSystemTick;
 
-    auto mi = getModuleInfo(name);
-    immutable t0 = TickDuration.currSystemTick;
-    if (auto tests = mi.unitTests)
-    {
-        foreach (test; tests)
+        foreach(test; tests)
         {
-            if (test.disabled)
+            if(test.disabled)
                 continue;
             try
+            {
                 test.func();
-            catch (Throwable t)
-                testFailed(t);
+            }
+            catch (Throwable e)
+            {
+                auto msg = e.toString();
+                printf("****** FAIL %.*s\n%.*s\n", cast(int)name.length, name.ptr,
+                    cast(int)msg.length, msg.ptr);
+                result = false;
+            }
+        }
+        immutable t1 = TickDuration.currSystemTick;
+        if (result)
+        {
+            printf("%.3fs PASS %.*s\n", (t1 - t0).msecs / 1000.,
+                cast(int)name.length, name.ptr);
         }
     }
-    else if (auto test = mi.unitTest) // old single test per module
-    {
-        try
-            test();
-        catch (Throwable t)
-            testFailed(t);
-    }
-    immutable t1 = TickDuration.currSystemTick;
-    if (result)
-        printf("%.3fs PASS %.*s\n", (t1 - t0).msecs / 1000.,
-               cast(int)name.length, name.ptr);
     return result;
 }
 
