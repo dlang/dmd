3fadb55bd52b5ae9eb9f4358272c653dea3642ff
Jason King <jason.brian.king@gmail.com>
2014-07-07 13:27:09 -0500

    Added Solaris definitions for various stdlib-defined functions

--- src/core/sys/posix/stdlib.d
+++ src/core/sys/posix/stdlib.d
@@ -91,6 +91,10 @@ else version( FreeBSD )
 {
     int posix_memalign(void**, size_t, size_t);
 }
+else version( Solaris )
+{
+    int posix_memalign(void**, size_t, size_t);
+}
 
 //
 // C Extension (CX)
@@ -128,6 +132,13 @@ else version( Android )
 
     void* valloc(size_t);
 }
+else version( Solaris )
+{
+    int setenv(in char*, in char*, int);
+    int unsetenv(in char*);
+
+    void* valloc(size_t); // LEGACY non-standard
+}
 
 //
 // Thread-Safe Functions (TSF)
@@ -145,6 +156,10 @@ else version( OSX )
     int rand_r(uint*);
 }
 else version( FreeBSD )
+{
+    int rand_r(uint*);
+}
+else version( Solaris )
 {
     int rand_r(uint*);
 }
@@ -342,4 +357,61 @@ else version( Android )
     void    srand48(c_long);
     void    srandom(uint s) { srand48(s); }
     int     unlockpt(int);
+}
+else version( Solaris )
+{
+    //WNOHANG     (defined in core.sys.posix.sys.wait)
+    //WUNTRACED   (defined in core.sys.posix.sys.wait)
+    //WEXITSTATUS (defined in core.sys.posix.sys.wait)
+    //WIFEXITED   (defined in core.sys.posix.sys.wait)
+    //WIFSIGNALED (defined in core.sys.posix.sys.wait)
+    //WIFSTOPPED  (defined in core.sys.posix.sys.wait)
+    //WSTOPSIG    (defined in core.sys.posix.sys.wait)
+    //WTERMSIG    (defined in core.sys.posix.sys.wait)
+
+    c_long a64l(in char*);
+    double drand48();
+    char*  ecvt(double, int, int *, int *); // LEGACY
+    double erand48(ref ushort[3]);
+    char*  fcvt(double, int, int *, int *); // LEGACY
+    char*  gcvt(double, int, char*); // LEGACY
+    int    getsubopt(char**, in char**, char**);
+    int    grantpt(int);
+    char*  initstate(uint, char*, size_t);
+    c_long jrand48(ref ushort[3]);
+    char*  l64a(c_long);
+    void   lcong48(ref ushort[7]);
+    c_long lrand48();
+    char*  mktemp(char*); // LEGACY
+    //int    mkstemp(char*);
+    c_long mrand48();
+    c_long nrand48(ref ushort[3]);
+    int    posix_openpt(int);
+    char*  ptsname(int);
+    int    putenv(char*);
+    c_long random();
+    char*  realpath(in char*, char*);
+    ushort seed48(ref ushort[3]);
+    void   setkey(in char*);
+    char*  setstate(in char*);
+    void   srand48(c_long);
+    void   srandom(uint);
+    int    unlockpt(int);
+
+    version (D_LP64)
+    {
+        mkstemp(char*);
+
+        static if ( __USE_LARGEFILE64 )
+            alias mkstemp mkstemp64;
+    }
+    else
+    {
+        mkstemp64(char*);
+
+        static if ( __USE_LARGEFILE64 )
+            alias mkstemp64 mkstemp;
+        else
+            mkstemp(char*);
+    }
 }
