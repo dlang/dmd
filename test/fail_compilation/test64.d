b94f7c2b7b1d969f4f22849d69fba04704981bdf
k-hara <k.hara.pg@gmail.com>
2014-07-30 11:07:35 +0900

    fix Issue 11453 - Compiling packages has a dependency on order of modules passed to the compiler

--- test/fail_compilation/test64.d
+++ test/fail_compilation/test64.d
@@ -1,19 +1,19 @@
 /*
 TEST_OUTPUT:
 ---
-Error: module imports from file fail_compilation/imports/test64a.d conflicts with package name imports
+fail_compilation/imports/test64a.d(1): Error: module imports from file fail_compilation/imports/test64a.d conflicts with package name imports
 ---
 */
 
 // PERMUTE_ARGS:
 
-import std.stdio;
+//import std.stdio;
 
 import imports.test64a;
 
-int main(char[][] args)
+int main(string[] args)
 {
-    writefln(file1);
+    //writefln(file1);
     return 0;
 }
 
