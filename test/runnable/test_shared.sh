358b0fdcc103a24541bd94f2ee99fe86104db1f6
Martin Nowak <code@dawg.eu>
2014-07-31 09:44:51 +0200

    test for -defaultlib=libphobos2.so
    - followup to issue 13210
    
    - depends on phobos directory layout for setting LD_LIBRARY_PATH

---
+++ test/runnable/test_shared.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+dir=${RESULTS_DIR}/runnable
+dmddir=${RESULTS_DIR}${SEP}runnable
+output_file=${dir}/test_shared.sh.out
+
+rm -f ${output_file}
+
+if [ ${OS} != "linux" ]; then
+    echo "Skipping shared library test on ${OS}."
+    touch ${output_file}
+    exit 0
+fi
+
+die()
+{
+    cat ${output_file}
+    rm -f ${output_file}
+    exit 1
+}
+
+$DMD -m${MODEL} -of${dmddir}${SEP}test_shared${EXE} -defaultlib=libphobos2.so runnable/extra-files/test_shared.d >> ${output_file}
+if [ $? -ne 0 ]; then die; fi
+
+LD_LIBRARY_PATH=../../phobos/generated/${OS}/release/${MODEL} ./${dmddir}${SEP}test_shared${EXE}
+if [ $? -ne 0 ]; then die; fi
