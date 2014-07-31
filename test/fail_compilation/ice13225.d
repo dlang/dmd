7bc3289093ede47b4828f024c76d5bbd47900505
k-hara <k.hara.pg@gmail.com>
2014-07-31 11:31:49 +0900

    Remove redundant error

--- test/fail_compilation/ice13225.d
+++ test/fail_compilation/ice13225.d
@@ -2,9 +2,9 @@
 TEST_OUTPUT:
 ---
 fail_compilation/ice13225.d(13): Error: undefined identifier undefined
-fail_compilation/ice13225.d(13): Error: mixin ice13225.S.M!(__error) does not match template declaration M(T)
 ---
 */
+
 mixin template M(T) {}
 
 struct S
