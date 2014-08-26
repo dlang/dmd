4723d594af9dd8649f1f9068d53549c3df05beb8
k-hara <k.hara.pg@gmail.com>
2014-08-26 16:11:29 +0900

    fix Issue 11312 - Avoid auto-dereference for UFCS functions

--- src/expression.c
+++ src/expression.c
@@ -7303,12 +7303,21 @@ Expression *DotIdExp::semanticY(Scope *sc, int flag)
              ident != Id::init && ident != Id::__sizeof &&
              ident != Id::__xalignof && ident != Id::offsetof &&
              ident != Id::mangleof && ident != Id::stringof)
-    {   /* Rewrite:
+    {
+        Type *t1bn = t1b->nextOf();
+        if (flag)
+        {
+            AggregateDeclaration *ad = isAggregate(t1bn);
+            if (ad && !ad->members)   // Bugzilla 11312
+                return NULL;
+        }
+
+        /* Rewrite:
          *   p.ident
          * as:
          *   (*p).ident
          */
-        if (flag && t1b->nextOf()->ty == Tvoid)
+        if (flag && t1bn->ty == Tvoid)
             return NULL;
         e = new PtrExp(loc, e1);
         e = e->semantic(sc);
