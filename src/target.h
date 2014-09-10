db7dbb1bf4fb2b1e66c7fef7c3a61d7c92319778
Walter Bright <walter@walterbright.com>
2014-09-10 15:52:06 -0700

    add c_longdoublesize

--- src/target.h
+++ src/target.h
@@ -26,7 +26,8 @@ struct Target
     static int realpad;         // 'padding' added to the CPU real size to bring it up to realsize
     static int realalignsize;   // alignment for reals
     static bool reverseCppOverloads; // with dmc, overloaded functions are grouped and in reverse order
-    static int longsize;        // size of a C 'long' or 'unsigned long' type
+    static int c_longsize;           // size of a C 'long' or 'unsigned long' type
+    static int c_long_doublesize;    // size of a C 'long double'
 
     static void init();
     static unsigned alignsize(Type* type);
