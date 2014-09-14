14fd83a14e82d0c86eef3e2dc3d316915fa0bedd
k-hara <k.hara.pg@gmail.com>
2014-09-14 21:05:11 +0900

    [Refactoring] Add helper function for array-op classification

--- src/cast.c
+++ src/cast.c
@@ -2942,14 +2942,14 @@ Lcc:
         goto Lt2;
     }
     else if (e->op != TOKquestion && t1->ty == Tarray &&
-             isArrayOperand(e1) && e2->implicitConvTo(t1->nextOf()))
+             isArrayOpOperand(e1) && e2->implicitConvTo(t1->nextOf()))
     {
         // T[] op T
         e2 = e2->castTo(sc, t1->nextOf());
         t = t1->nextOf()->arrayOf();
     }
     else if (e->op != TOKquestion && t2->ty == Tarray &&
-             isArrayOperand(e2) && e1->implicitConvTo(t2->nextOf()))
+             isArrayOpOperand(e2) && e1->implicitConvTo(t2->nextOf()))
     {
         // T op T[]
         e1 = e1->castTo(sc, t2->nextOf());
