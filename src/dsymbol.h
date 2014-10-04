9a5cb5f8f724359e224f37e08200095799b4187a
Jacob Carlborg <doob@me.com>
2014-10-04 11:56:55 +0200

    Remove DMD_OBJC from addObjcSymbols.

--- src/dsymbol.h
+++ src/dsymbol.h
@@ -230,9 +230,7 @@ public:
     virtual bool hasPointers();
     virtual bool hasStaticCtorOrDtor();
     virtual void addLocalClass(ClassDeclarations *) { }
-#if DMD_OBJC
     virtual void addObjcSymbols(ClassDeclarations *classes, ClassDeclarations *categories) { }
-#endif
     
     virtual void checkCtorConstInit() { }
 
