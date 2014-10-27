e1442bcf0b7c756da49553fc961ca6ef6929d7a8
Jacob Carlborg <doob@me.com>
2014-10-27 20:48:07 +0100

    Move implementation of toCBuffer to its own file.

--- src/objc.c
+++ src/objc.c
@@ -1420,35 +1420,6 @@ void objc_Module_genobjfile_initSymbols()
     ObjcSymbols::init();
 }
 
-// MARK: toCBuffer
-
-void objc_toCBuffer_visit_ObjcSelectorExp(OutBuffer *buf, ObjcSelectorExp *e)
-{
-    buf->writeByte('&');
-    if (e->func)
-        buf->writestring(e->func->toChars());
-    else
-        buf->writestring(e->selname);
-}
-
-void objc_toCBuffer_visit_ObjcDotClassExp(OutBuffer *buf, HdrGenState *hgs, ObjcDotClassExp *e)
-{
-    toCBuffer(e->e1, buf, hgs);
-    buf->writestring(".class");
-}
-
-void objc_toCBuffer_visit_ObjcClassRefExp(OutBuffer *buf, ObjcClassRefExp *e)
-{
-    buf->writestring(e->cdecl->objc.ident->string);
-    buf->writestring(".class");
-}
-
-void objc_toCBuffer_visit_ObjcProtocolOfExp(OutBuffer *buf, HdrGenState *hgs, ObjcProtocolOfExp *e)
-{
-    toCBuffer(e->e1, buf, hgs);
-    buf->writestring(".protocolof");
-}
-
 // MARK: inline
 
 void objc_inline_visit_ObjcSelectorExp(int &cost)
