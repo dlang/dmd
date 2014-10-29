3d7a42f039bdefd4f4e03cefa163f45751ccb25c
Jacob Carlborg <doob@me.com>
2014-10-29 21:22:56 +0100

    Move implementation of TypeInfo_toDt to its own file.

--- src/objc.c
+++ src/objc.c
@@ -1455,19 +1455,3 @@ void objc_Type_toCtype_visit_TypeObjcSelector(TypeObjcSelector *t)
         t->ctype = type_fake(totym(t));
     t->ctype->Tcount++;
 }
-
-// MARK: TypeInfo_toDt
-
-void objc_TypeInfo_toDt_visit_TypeInfoObjcSelectorDeclaration(dt_t **pdt, TypeInfoObjcSelectorDeclaration *d)
-{
-    //printf("TypeInfoObjcSelectorDeclaration::toDt()\n");
-    dtxoff(pdt, Type::typeinfodelegate->toVtblSymbol(), 0); // vtbl for TypeInfo_ObjcSelector
-    dtsize_t(pdt, 0);                        // monitor
-
-    assert(d->tinfo->ty == Tobjcselector);
-
-    TypeObjcSelector *tc = (TypeObjcSelector *)d->tinfo;
-
-    tc->next->nextOf()->getTypeInfo(NULL);
-    dtxoff(pdt, toSymbol(tc->next->nextOf()->vtinfo), 0); // TypeInfo for selector return value
-}
--- src/posix.mak
+++ src/posix.mak
@@ -159,7 +159,8 @@ GLUE_OBJS = \
 	irstate.o typinf.o iasm.o
 
 ifeq ($(D_OBJC),1)
-	GLUE_OBJS += objc_e2ir.o objc_glue.o objc_todt.o objc_toobj.o
+	GLUE_OBJS += objc_e2ir.o objc_glue.o objc_todt.o objc_toobj.o \
+		objc_typinf.o
 endif
 
 ifeq (osx,$(OS))
@@ -239,7 +240,8 @@ GLUE_SRC = glue.c msc.c s2ir.c todt.c e2ir.c tocsym.c \
 	tk.c eh.c gluestub.c
 
 ifeq ($(D_OBJC),1)
-	GLUE_SRC += objc_e2ir.c objc_glue.h objc_glue.c objc_todt.c objc_toobj.c
+	GLUE_SRC += objc_e2ir.c objc_glue.h objc_glue.c objc_todt.c objc_toobj.c \
+		objc_typinf.c
 endif
 
 BACK_SRC = \
@@ -526,6 +528,7 @@ ifeq ($(D_OBJC),1)
 	gcov objc_sideeffect.c
 	gcov objc_todt.c
 	gcov objc_toobj.c
+	gcov objc_typinf.c
 endif
 #	gcov hdrgen.c
 #	gcov tocvdebug.c
