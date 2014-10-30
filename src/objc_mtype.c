bddd13e59303ffc17624518cc5099fbaad6cac8f
Jacob Carlborg <doob@me.com>
2014-10-30 18:32:33 +0100

    Move implementation of TypeObjcSelector to its own file.

--- src/objc_mtype.c
+++ src/objc_mtype.c
@@ -16,6 +16,120 @@
 #include "mtype.h"
 #include "objc.h"
 #include "scope.h"
+#include "target.h"
+
+// MARK: TypeObjcSelector
+
+TypeObjcSelector::TypeObjcSelector(Type *t)
+: TypeNext(Tobjcselector, t)
+{
+    assert(((TypeFunction *)t)->linkage == LINKobjc);
+}
+
+Type *TypeObjcSelector::syntaxCopy()
+{
+    Type *t = next->syntaxCopy();
+    if (t == next)
+        t = this;
+    else
+    {   t = new TypeObjcSelector(t);
+        t->mod = mod;
+    }
+    return t;
+}
+
+Type *TypeObjcSelector::semantic(Loc loc, Scope *sc)
+{
+    if (deco)                   // if semantic() already run
+    {
+        //printf("already done\n");
+        return this;
+    }
+    Scope* newScope = new Scope(*sc);
+    newScope->linkage = LINKobjc;
+    next = next->semantic(loc,newScope);
+
+    return merge();
+}
+
+d_uns64 TypeObjcSelector::size(Loc loc)
+{
+    return Target::ptrsize;
+}
+
+unsigned TypeObjcSelector::alignsize()
+{
+    return Target::ptrsize;
+}
+
+MATCH TypeObjcSelector::implicitConvTo(Type *to)
+{
+    //printf("TypeDelegate::implicitConvTo(this=%p, to=%p)\n", this, to);
+    //printf("from: %s\n", toChars());
+    //printf("to  : %s\n", to->toChars());
+    if (this == to)
+        return MATCHexact;
+#if 0 // not allowing covariant conversions because it interferes with overriding
+    if (to->ty == Tdelegate && this->nextOf()->covariant(to->nextOf()) == 1)
+        return MATCHconvert;
+#endif
+    return MATCHnomatch;
+}
+
+Expression *TypeObjcSelector::defaultInit(Loc loc)
+{
+#if LOGDEFAULTINIT
+    printf("TypeObjcSelector::defaultInit() '%s'\n", toChars());
+#endif
+    return new NullExp(loc, this);
+}
+
+bool TypeObjcSelector::isZeroInit(Loc loc)
+{
+    return true;
+}
+
+bool TypeObjcSelector::checkBoolean()
+{
+    return true;
+}
+
+Expression *TypeObjcSelector::dotExp(Scope *sc, Expression *e, Identifier *ident, int flag)
+{
+#if LOGDOTEXP
+    printf("TypeDelegate::dotExp(e = '%s', ident = '%s')\n", e->toChars(), ident->toChars());
+#endif
+    /*    if (ident == Id::ptr)
+     {
+     e->type = tvoidptr;
+     return e;
+     }
+     else if (ident == Id::funcptr)
+     {
+     e = e->addressOf(sc);
+     e->type = tvoidptr;
+     e = new AddExp(e->loc, e, new IntegerExp(PTRSIZE));
+     e->type = tvoidptr;
+     e = new PtrExp(e->loc, e);
+     e->type = next->pointerTo();
+     return e;
+     }
+     else*/
+    {
+        e = Type::dotExp(sc, e, ident, flag);
+    }
+    return e;
+}
+
+int TypeObjcSelector::hasPointers()
+{
+    return false; // not in GC memory
+}
+
+TypeInfoDeclaration *TypeObjcSelector::getTypeInfoDeclaration()
+{
+    return TypeInfoObjcSelectorDeclaration::create(this);
+}
 
 // MARK: Type::init
 
