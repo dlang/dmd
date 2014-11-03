e30213d6672bdf7440be662575be2c1cf29b1eb3
H. S. Teoh <hsteoh@quickfur.ath.cx>
2014-11-03 15:31:23 -0800

    Show config file if object.d can't be found.
    To help troubleshoot problems caused by stray copies of dmd.conf
    (see bugzilla issue 4567).

--- src/module.c
+++ src/module.c
@@ -266,6 +266,7 @@ bool Module::read(Loc loc)
         {
             ::error(loc, "cannot find source code for runtime library file 'object.d'");
             errorSupplemental(loc, "dmd might not be correctly installed. Run 'dmd -man' for installation instructions.");
+            errorSupplemental(loc, "config file: %s", FileName::canonicalName(global.inifilename));
         }
         else
         {
