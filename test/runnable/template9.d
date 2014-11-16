b041ff5440722d3d09fac05deb3c718bfd183c51
k-hara <k.hara.pg@gmail.com>
2014-11-16 18:52:58 +0900

    fix Issue 13714 - Assertion failure: '0' on line 2022 in file 'mtype.c'

--- test/runnable/template9.d
+++ test/runnable/template9.d
@@ -4260,6 +4260,35 @@ void test13675()
 }
 
 /******************************************/
+// 13714
+
+struct JSONValue13714
+{
+    this(T)(T arg)
+    {
+    }
+    this(T : JSONValue13714)(inout T arg) inout
+    {
+        //store = arg.store;
+    }
+
+    void opAssign(T)(T arg)
+    {
+    }
+}
+
+void test13714()
+{
+    enum DummyStringEnum
+    {
+        foo = "bar"
+    }
+
+    JSONValue13714[string] aa;
+    aa["A"] = DummyStringEnum.foo;
+}
+
+/******************************************/
 
 int main()
 {
