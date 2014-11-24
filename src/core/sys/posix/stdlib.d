fee70132210d15e5279a7302fcab282765cba2d1
Daniel Murphy <yebblies@gmail.com>
2014-11-25 08:04:37 +1100

    Revert "Merge pull request #1038 from redstar/solaris"
    This reverts commit e0169cf59401505efbfa336e4ca234e40755560f, reversing
    changes made to f9b55f723cb2a99a83a9e3937334e7c959f3b52b.

--- src/core/sys/posix/stdlib.d
+++ src/core/sys/posix/stdlib.d
@@ -95,10 +95,6 @@ else version( Solaris )
 {
     int posix_memalign(void**, size_t, size_t);
 }
-else version( Solaris )
-{
-    int posix_memalign(void**, size_t, size_t);
-}
 
 //
 // C Extension (CX)
@@ -123,13 +119,6 @@ else version( OSX )
     void* valloc(size_t); // LEGACY non-standard
 }
 else version( FreeBSD )
-{
-    int setenv(in char*, in char*, int);
-    int unsetenv(in char*);
-
-    void* valloc(size_t); // LEGACY non-standard
-}
-else version( Solaris )
 {
     int setenv(in char*, in char*, int);
     int unsetenv(in char*);
@@ -167,10 +156,6 @@ else version( OSX )
     int rand_r(uint*);
 }
 else version( FreeBSD )
-{
-    int rand_r(uint*);
-}
-else version( Solaris )
 {
     int rand_r(uint*);
 }
@@ -347,46 +332,6 @@ else version( FreeBSD )
     c_long random();
     char*  realpath(in char*, char*);
     ushort seed48(ref ushort[3]);
-    void   setkey(in char*);
-    char*  setstate(in char*);
-    void   srand48(c_long);
-    void   srandom(uint);
-    int    unlockpt(int);
-}
-else version( Solaris )
-{
-    //WNOHANG     (defined in core.sys.posix.sys.wait)
-    //WUNTRACED   (defined in core.sys.posix.sys.wait)
-    //WEXITSTATUS (defined in core.sys.posix.sys.wait)
-    //WIFEXITED   (defined in core.sys.posix.sys.wait)
-    //WIFSIGNALED (defined in core.sys.posix.sys.wait)
-    //WIFSTOPPED  (defined in core.sys.posix.sys.wait)
-    //WSTOPSIG    (defined in core.sys.posix.sys.wait)
-    //WTERMSIG    (defined in core.sys.posix.sys.wait)
-
-    c_long a64l(in char*);
-    double drand48();
-    char*  ecvt(double, int, int *, int *);
-    double erand48(ref ushort[3]);
-    char*  fcvt(double, int, int *, int *);
-    char*  gcvt(double, int, char*);
-    int    getsubopt(char**, in char**, char**);
-    int    grantpt(int);
-    char*  initstate(uint, char*, size_t);
-    c_long jrand48(ref ushort[3]);
-    char*  l64a(c_long);
-    void   lcong48(ref ushort[7]);
-    c_long lrand48();
-    char*  mktemp(char*);
-    int    mkstemp(char*);
-    c_long mrand48();
-    c_long nrand48(ref ushort[3]);
-    int    posix_openpt(int);
-    char*  ptsname(int);
-    int    putenv(char*);
-    c_long random();
-    char*  realpath(in char*, char*);
-    ushort *seed48(ref ushort[3]);
     void   setkey(in char*);
     char*  setstate(in char*);
     void   srand48(c_long);
