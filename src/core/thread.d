e75225d4948f98d34b224384b2d4f75336d18c33
Martin Nowak <code@dawg.eu>
2014-12-12 17:55:47 +0100

    add regression test

--- src/core/thread.d
+++ src/core/thread.d
@@ -5194,3 +5194,24 @@ version( D_InlineAsm_X86_64 )
         fib.call();
     }
 }
+
+// regression test for Issue 13416
+version (FreeBSD) unittest
+{
+    static void loop()
+    {
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+        auto thr = pthread_self();
+        foreach (i; 0 .. 50)
+            pthread_attr_get_np(thr, &attr);
+        pthread_attr_destroy(&attr);
+    }
+
+    auto thr = new Thread(&loop).start();
+    foreach (i; 0 .. 50)
+    {
+        thread_suspendAll();
+        thread_resumeAll();
+    }
+}
