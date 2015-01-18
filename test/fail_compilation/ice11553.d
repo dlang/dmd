6aa2755cd5776a354de2e181e5955f33c0e724c0
k-hara <k.hara.pg@gmail.com>
2015-01-18 22:31:27 +0900

    fix Issue 13942 - Bad template error message
    Partially revert changes in #3332.

--- test/fail_compilation/ice11553.d
+++ test/fail_compilation/ice11553.d
@@ -1,7 +1,8 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/ice11553.d(21): Error: recursive template expansion while looking for A!().A()
+fail_compilation/ice11553.d(22): Error: recursive template expansion while looking for A!().A()
+fail_compilation/ice11553.d(22): Error: expression template A() of type void does not have a boolean value
 ---
 */
 
