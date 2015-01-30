6d59dd758b09a233d8262eebbd58fd80324f0435
k-hara <k.hara.pg@gmail.com>
2015-01-31 00:18:56 +0900

    fix Issue 14083 - Remained unresolved forward reference issue with template classes

--- test/fail_compilation/ice8100.d
+++ test/fail_compilation/ice8100.d
@@ -1,12 +1,12 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/ice8100.d(11): Error: class ice8100.Bar!bool.Bar is forward referenced when looking for 'Q'
+fail_compilation/ice8100.d(10): Error: no property 'Q' for type 'ice8100.Bar!bool.Bar'
+fail_compilation/ice8100.d(10): Error: T1.Q is used as a type
 fail_compilation/ice8100.d(11): Error: template instance ice8100.Foo!(Bar!bool) error instantiating
 fail_compilation/ice8100.d(12):        instantiated from here: Bar!bool
 ---
 */
-
 class Foo(T1) { T1.Q r; }
 class Bar(T2) : Foo!(Bar!T2) {}
 Bar!bool b;
