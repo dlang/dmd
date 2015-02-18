a0ea10074922ad589b25ae2641f19967e3a36687
Rainer Schuetze <r.sagitario@gmx.de>
2015-02-18 22:28:16 +0100

    replace MonoTimeNative with Monotime.currTime.ticks

--- src/gc/gc.d
+++ src/gc/gc.d
@@ -59,61 +59,7 @@ else                   import core.stdc.stdio : sprintf, printf; // needed to ou
 
 import core.time;
 alias currTime = MonoTime.currTime;
-
-// high performance time measurements without additional translations
-alias MonoTimeNative = long;
-alias DurationNative = long;
-
-version(OSX) extern(C) ulong mach_absolute_time() nothrow @nogc;
-
-@property MonoTimeNative currTimeNative() nothrow
-{
-    version(Windows)
-    {
-        import core.sys.windows.windows;
-        long ticks;
-        if(QueryPerformanceCounter(&ticks) == 0)
-        {
-            // This probably cannot happen on Windows 95 or later
-            assert(0, "Call to QueryPerformanceCounter failed.");
-        }
-        return ticks;
-    }
-    else version(OSX)
-    {
-        return mach_absolute_time();
-    }
-    else version(Posix)
-    {
-        import core.sys.posix.time;
-        import core.sys.posix.sys.time;
-
-        timespec ts;
-        if(clock_gettime(CLOCK_MONOTONIC, &ts) != 0)
-            assert(0, "Call to clock_gettime failed.");
-
-        return ts.tv_sec * 1_000_000_000L + ts.tv_nsec;
-    }
-    else
-        static assert(false, "no native timer function");
-}
-
-MonoTime toMonoTime(MonoTimeNative tm)
-{
-    version(Windows)
-        return MonoTime(tm);
-    else version(OSX)
-        return MonoTime(tm);
-    else version(Posix)
-        return MonoTime(convClockFreq(tm, 1_000_000_000L, MonoTime.ticksPerSecond));
-    else
-        static assert(false, "no native timer function");
-}
-
-Duration toDuration(DurationNative dur)
-{
-    return toMonoTime(dur) - MonoTime(0);
-}
+long currTicks() { return MonoTime.ticksPerSecond ? MonoTime.currTime.ticks : 0; }
 
 debug(PRINTF_TO_FILE)
 {
@@ -171,12 +117,12 @@ __gshared long numFrees;
 __gshared long numReallocs;
 __gshared long numExtends;
 __gshared long numOthers;
-__gshared DurationNative mallocTime;
-__gshared DurationNative freeTime;
-__gshared DurationNative reallocTime;
-__gshared DurationNative extendTime;
-__gshared DurationNative otherTime;
-__gshared DurationNative lockTime;
+__gshared long mallocTime; // using ticks instead of MonoTime for better performance
+__gshared long freeTime;
+__gshared long reallocTime;
+__gshared long extendTime;
+__gshared long otherTime;
+__gshared long lockTime;
 
 private
 {
@@ -403,14 +349,14 @@ struct GC
     {
         debug(PROFILE_API)
         {
-            MonoTimeNative tm = (GC.config.profile > 1 ? currTimeNative : 0);
+            long tm = (GC.config.profile > 1 ? currTicks : 0);
         }
 
         bool locked = (gcLock.lock(), true);
 
         debug(PROFILE_API)
         {
-            MonoTimeNative tm2 = (GC.config.profile > 1 ? currTimeNative : 0);
+            long tm2 = (GC.config.profile > 1 ? currTicks : 0);
         }
     }
 
@@ -422,7 +368,7 @@ struct GC
                 if (GC.config.profile > 1)
                 {
                     count++;
-                    MonoTimeNative now = currTimeNative;
+                    long now = currTicks;
                     lockTime += tm2 - tm;
                     time += now - tm2;
                 }
@@ -1529,17 +1475,22 @@ struct Gcx
             apitxt[0] = 0;
             debug(PROFILE_API) if (GC.config.profile > 1)
             {
-                printf("\n");
-                printf("\tmalloc:  %llu calls, %lld ms\n", cast(ulong)numMallocs, mallocTime.toDuration.total!"msecs");
-                printf("\trealloc: %llu calls, %lld ms\n", cast(ulong)numReallocs, reallocTime.toDuration.total!"msecs");
-                printf("\tfree:    %llu calls, %lld ms\n", cast(ulong)numFrees, freeTime.toDuration.total!"msecs");
-                printf("\textend:  %llu calls, %lld ms\n", cast(ulong)numExtends, extendTime.toDuration.total!"msecs");
-                printf("\tother:   %llu calls, %lld ms\n", cast(ulong)numOthers, otherTime.toDuration.total!"msecs");
-                printf("\tlock time: %lld ms\n", lockTime.toDuration.total!"msecs");
+                static Duration toDuration(long dur)
+                {
+                    return MonoTime(dur) - MonoTime(0);
+                }
 
-                DurationNative apiTime = mallocTime + reallocTime + freeTime + extendTime + otherTime + lockTime;
-                printf("\tGC API: %lld ms\n", apiTime.toDuration.total!"msecs");
-                sprintf(apitxt.ptr, " API%5ld ms", apiTime.toDuration.total!"msecs");
+                printf("\n");
+                printf("\tmalloc:  %llu calls, %lld ms\n", cast(ulong)numMallocs, toDuration(mallocTime).total!"msecs");
+                printf("\trealloc: %llu calls, %lld ms\n", cast(ulong)numReallocs, toDuration(reallocTime).total!"msecs");
+                printf("\tfree:    %llu calls, %lld ms\n", cast(ulong)numFrees, toDuration(freeTime).total!"msecs");
+                printf("\textend:  %llu calls, %lld ms\n", cast(ulong)numExtends, toDuration(extendTime).total!"msecs");
+                printf("\tother:   %llu calls, %lld ms\n", cast(ulong)numOthers, toDuration(otherTime).total!"msecs");
+                printf("\tlock time: %lld ms\n", toDuration(lockTime).total!"msecs");
+
+                long apiTime = mallocTime + reallocTime + freeTime + extendTime + otherTime + lockTime;
+                printf("\tGC API: %lld ms\n", toDuration(apiTime).total!"msecs");
+                sprintf(apitxt.ptr, " API%5ld ms", toDuration(apiTime).total!"msecs");
             }
 
             printf("GC summary:%5lld MB,%5lld GC%5lld ms, Pauses%5lld ms <%5lld ms%s\n",
