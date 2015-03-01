2203b4caed800b6c3a36e22b189d99bea934437b
Lionello Lunesu <lio+git@lunesu.com>
2015-03-01 14:44:53 +0800

    Fix: .sh tests should manually create an .out file

--- test/compilable/ddoc9764.sh
+++ test/compilable/ddoc9764.sh
@@ -8,4 +8,4 @@ rm -f ${output_file}
 
 $DMD -m${MODEL} -D -o- compilable/extra-files/ddoc9764.dd -Df${output_file}
 
-compilable/extra-files/ddocAny-postscript.sh 9764
+compilable/extra-files/ddocAny-postscript.sh 9764 && touch ${dir}/`basename $0`.out
