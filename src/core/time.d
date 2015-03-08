smolt/druntime

Fixed ticksPerSec for OSX (in particular iOS)
Needed to flip denom and numer.  It worked on MacOS because they are
both 1, but on iOS, not so.

src/core/time.d
@@ -1849,7 +1849,7 @@ struct TickDuration
                 if(mach_timebase_info(&info))
                     ticksPerSec = 0;
                 else
-                    ticksPerSec = (1_000_000_000 * info.numer) / info.denom;
+                    ticksPerSec = (1_000_000_000 * info.denom) / info.numer;
             }
             else
                 ticksPerSec = 1_000_000;

dlang/dmd

src/core/time.d
@@ -2241,7 +2241,7 @@ private:
         {
             mach_timebase_info_data_t info;
             if(mach_timebase_info(&info) == 0)
-                _ticksPerSecond = 1_000_000_000L * info.numer / info.denom;
+                _ticksPerSecond = (1_000_000_000L * info.denom) / info.numer;
         }
         else version(Posix)
         {
@@ -2492,7 +2492,7 @@ struct TickDuration
                 if(mach_timebase_info(&info))
                     ticksPerSec = 0;
                 else
-                    ticksPerSec = (1_000_000_000 * info.numer) / info.denom;
+                    ticksPerSec = (1_000_000_000L * info.denom) / info.numer;
             }
             else
                 ticksPerSec = 1_000_000;