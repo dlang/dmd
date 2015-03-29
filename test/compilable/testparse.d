bd0385c5410eec129f40b16d109f4719f97c4b21
k-hara <k.hara.pg@gmail.com>
2015-03-29 11:56:50 +0900

    Move issue 9232 test case to compilable/testparse.d

--- test/compilable/testparse.d
+++ test/compilable/testparse.d
@@ -5,3 +5,21 @@
 // 6719
 
 pragma(msg, __traits(compiles, mixin("(const(A))[0..0]")));
+
+/***************************************************/
+// 9232
+
+struct Foo9232
+{
+    void bar(T)() {}
+    void baz() {}
+}
+
+void test9232()
+{
+    Foo9232 foo;
+    (foo).bar!int();   // OK <- Error: found '!' when expecting ';' following statement
+    ((foo)).bar!int(); // OK
+    foo.bar!int();     // OK
+    (foo).baz();       // OK
+}
