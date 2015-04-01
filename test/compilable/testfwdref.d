302d42186a30452ddb0c7bd02f4f8c682a8f4c93
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2015-04-01 11:31:46 +0300

    [Fix] Fix incorrect test for issue 12984.
    Causing commit: 8efa502a209c64a75f682956cab0b3647737c614.

--- test/compilable/testfwdref.d
+++ test/compilable/testfwdref.d
@@ -325,7 +325,7 @@ class E12984b(T) : D12984b!int
 }
 
 static assert(__traits(classInstanceSize, B12984b) == (void*).sizeof * 2 + int.sizeof);
-static assert(__traits(classInstanceSize, C12984b) == (void*).sizeof * 2 + int.sizeof);
+static assert(__traits(classInstanceSize, C12984b) == (void*).sizeof * 2 + int.sizeof * 2);
 
 /***************************************************/
 // 13860
