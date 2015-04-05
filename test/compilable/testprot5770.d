4cd6985729ae349d869624146c5e57ec1fe8ff2b
k-hara <k.hara.pg@gmail.com>
2015-04-06 00:27:23 +0900

    fix Issue 5770 - Template constructor bypass access check
    It had caused by the incomplete implementation of access check. Most part of `access.c` was old code from ancient D ages.

--- test/compilable/testprot5770.d
+++ test/compilable/testprot5770.d
@@ -13,9 +13,9 @@ version(all)    // access module members from module scope
     static assert(!is(typeof(C.init.  privateF())));
 
     static assert( is(typeof(C.init.   publicTF())));
-  //static assert(!is(typeof(C.init.protectedTF())));   // Bugzilla 5770
+    static assert(!is(typeof(C.init.protectedTF())));
     static assert( is(typeof(C.init.  packageTF())));
-  //static assert(!is(typeof(C.init.  privateTF())));   // Bugzilla 5770
+    static assert(!is(typeof(C.init.  privateTF())));
 }
 version(all)    // access class members from module scope
 {
@@ -25,9 +25,9 @@ version(all)    // access class members from module scope
     static assert(!is(typeof(C.init.  privateF())));
 
     static assert( is(typeof(C.init.   publicTF())));
-  //static assert(!is(typeof(C.init.protectedTF())));   // Bugzilla 5770
+    static assert(!is(typeof(C.init.protectedTF())));
     static assert( is(typeof(C.init.  packageTF())));
-  //static assert(!is(typeof(C.init.  privateTF())));   // Bugzilla 5770
+    static assert(!is(typeof(C.init.  privateTF())));
 
     static assert( is(typeof(D.init.   publicF())));
     static assert(!is(typeof(D.init.protectedF())));
@@ -35,9 +35,9 @@ version(all)    // access class members from module scope
     static assert(!is(typeof(D.init.  privateF())));
 
     static assert( is(typeof(D.init.   publicTF())));
-  //static assert(!is(typeof(D.init.protectedTF())));   // Bugzilla 5770
+    static assert(!is(typeof(D.init.protectedTF())));
     static assert( is(typeof(D.init.  packageTF())));
-  //static assert(!is(typeof(D.init.  privateTF())));   // Bugzilla 5770
+    static assert(!is(typeof(D.init.  privateTF())));
 }
 version(all)    // access struct members from module scope
 {
@@ -47,9 +47,9 @@ version(all)    // access struct members from module scope
     static assert(!is(typeof(S.init.  privateF())));
 
     static assert( is(typeof(S.init.   publicTF())));
-  //static assert(!is(typeof(S.init.protectedTF())));   // Bugzilla 5770
+    static assert(!is(typeof(S.init.protectedTF())));
     static assert( is(typeof(S.init.  packageTF())));
-  //static assert(!is(typeof(S.init.  privateTF())));   // Bugzilla 5770
+    static assert(!is(typeof(S.init.  privateTF())));
 }
 
 void testC1()
@@ -62,21 +62,21 @@ void testC1()
     static assert(!is(typeof(c.  privateF())));
 
     static assert( is(typeof(c.   publicTF())));
-  //static assert(!is(typeof(c.protectedTF())));    // Bugzilla 5770
+    static assert(!is(typeof(c.protectedTF())));
     static assert( is(typeof(c.  packageTF())));
-  //static assert(!is(typeof(c.  privateTF())));    // Bugzilla 5770
+    static assert(!is(typeof(c.  privateTF())));
 
     D d = new D();
 
     static assert( is(typeof(d.   publicF())));
-  //static assert(!is(typeof(d.protectedF())));     // Bugzilla 5770
+    static assert(!is(typeof(d.protectedF())));
     static assert( is(typeof(d.  packageF())));
     static assert(!is(typeof(d.  privateF())));
 
     static assert( is(typeof(d.   publicTF())));
-  //static assert(!is(typeof(d.protectedTF())));    // Bugzilla 5770
+    static assert(!is(typeof(d.protectedTF())));
     static assert( is(typeof(d.  packageTF())));
-  //static assert(!is(typeof(d.  privateTF())));    // Bugzilla 5770
+    static assert(!is(typeof(d.  privateTF())));
 }
 void testC2()()
 {
@@ -88,21 +88,21 @@ void testC2()()
     static assert(!is(typeof(c.  privateF())));
 
     static assert( is(typeof(c.   publicTF())));
-  //static assert(!is(typeof(c.protectedTF())));    // Bugzilla 5770
+    static assert(!is(typeof(c.protectedTF())));
     static assert( is(typeof(c.  packageTF())));
-  //static assert(!is(typeof(c.  privateTF())));    // Bugzilla 5770
+    static assert(!is(typeof(c.  privateTF())));
 
     D d = new D();
 
     static assert( is(typeof(d.   publicF())));
-  //static assert(!is(typeof(d.protectedF())));     // Bugzilla 5770
+    static assert(!is(typeof(d.protectedF())));
     static assert( is(typeof(d.  packageF())));
     static assert(!is(typeof(d.  privateF())));
 
     static assert( is(typeof(d.   publicTF())));
-  //static assert(!is(typeof(d.protectedTF())));    // Bugzilla 5770
+    static assert(!is(typeof(d.protectedTF())));
     static assert( is(typeof(d.  packageTF())));
-  //static assert(!is(typeof(d.  privateTF())));    // Bugzilla 5770
+    static assert(!is(typeof(d.  privateTF())));
 }
 alias testC2x = testC2!();
 
@@ -116,9 +116,9 @@ void testS1()
     static assert(!is(typeof(s.  privateF())));
 
     static assert( is(typeof(s.   publicTF())));
-  //static assert(!is(typeof(s.protectedTF())));    // Bugzilla 5770
+    static assert(!is(typeof(s.protectedTF())));
     static assert( is(typeof(s.  packageTF())));
-  //static assert(!is(typeof(s.  privateTF())));    // Bugzilla 5770
+    static assert(!is(typeof(s.  privateTF())));
 }
 void testS2()()
 {
@@ -130,9 +130,9 @@ void testS2()()
     static assert(!is(typeof(s.  privateF())));
 
     static assert( is(typeof(s.   publicTF())));
-  //static assert(!is(typeof(s.protectedTF())));    // Bugzilla 5770
+    static assert(!is(typeof(s.protectedTF())));
     static assert( is(typeof(s.  packageTF())));
-  //static assert(!is(typeof(s.  privateTF())));    // Bugzilla 5770
+    static assert(!is(typeof(s.  privateTF())));
 }
 alias testS2x = testS2!();
 
@@ -148,7 +148,7 @@ class D : C
         static assert( is(typeof(this.   publicTF())));
         static assert( is(typeof(this.protectedTF())));
         static assert( is(typeof(this.  packageTF())));
-      //static assert(!is(typeof(this.  privateTF())));     // Bugzilla 5770
+        static assert(!is(typeof(this.  privateTF())));
 
         static assert( is(typeof(super.   publicF())));
         static assert( is(typeof(super.protectedF())));
@@ -158,7 +158,7 @@ class D : C
         static assert( is(typeof(super.   publicTF())));
         static assert( is(typeof(super.protectedTF())));
         static assert( is(typeof(super.  packageTF())));
-      //static assert(!is(typeof(super.  privateTF())));    // Bugzilla 5770
+        static assert(!is(typeof(super.  privateTF())));
 
         auto dg = {
             static assert( is(typeof(this.   publicF())));
@@ -169,17 +169,17 @@ class D : C
             static assert( is(typeof(this.   publicTF())));
             static assert( is(typeof(this.protectedTF())));
             static assert( is(typeof(this.  packageTF())));
-          //static assert(!is(typeof(this.  privateTF())));     // Bugzilla 5770
+            static assert(!is(typeof(this.  privateTF())));
 
             static assert( is(typeof(super.   publicF())));
-          //static assert( is(typeof(super.protectedF())));     // Bugzilla 5770
+            static assert( is(typeof(super.protectedF())));
             static assert( is(typeof(super.  packageF())));
             static assert(!is(typeof(super.  privateF())));
 
             static assert( is(typeof(super.   publicTF())));
             static assert( is(typeof(super.protectedTF())));
             static assert( is(typeof(super.  packageTF())));
-          //static assert(!is(typeof(super.  privateTF())));    // Bugzilla 5770
+            static assert(!is(typeof(super.  privateTF())));
         };
     }
     void test2()()
@@ -192,17 +192,17 @@ class D : C
         static assert( is(typeof(this.   publicTF())));
         static assert( is(typeof(this.protectedTF())));
         static assert( is(typeof(this.  packageTF())));
-      //static assert(!is(typeof(this.  privateTF())));     // Bugzilla 5770
+        static assert(!is(typeof(this.  privateTF())));
 
         static assert( is(typeof(super.   publicF())));
-      //static assert( is(typeof(super.protectedF())));     // Bugzilla 5770
+        static assert( is(typeof(super.protectedF())));
         static assert( is(typeof(super.  packageF())));
         static assert(!is(typeof(super.  privateF())));
 
         static assert( is(typeof(super.   publicTF())));
         static assert( is(typeof(super.protectedTF())));
         static assert( is(typeof(super.  packageTF())));
-      //static assert(!is(typeof(super.  privateTF())));    // Bugzilla 5770
+        static assert(!is(typeof(super.  privateTF())));
 
         auto dg = {
             static assert( is(typeof(this.   publicF())));
@@ -213,18 +213,71 @@ class D : C
             static assert( is(typeof(this.   publicTF())));
             static assert( is(typeof(this.protectedTF())));
             static assert( is(typeof(this.  packageTF())));
-          //static assert(!is(typeof(this.  privateTF())));     // Bugzilla 5770
+            static assert(!is(typeof(this.  privateTF())));
 
             static assert( is(typeof(super.   publicF())));
-          //static assert( is(typeof(super.protectedF())));     // Bugzilla 5770
+            static assert( is(typeof(super.protectedF())));
             static assert( is(typeof(super.  packageF())));
             static assert(!is(typeof(super.  privateF())));
 
             static assert( is(typeof(super.   publicTF())));
             static assert( is(typeof(super.protectedTF())));
             static assert( is(typeof(super.  packageTF())));
-          //static assert(!is(typeof(super.  privateTF())));    // Bugzilla 5770
+            static assert(!is(typeof(super.  privateTF())));
         };
     }
     alias test2x = test2!();    // instantiate
+
+    class N
+    {
+        void test1()
+        {
+            static assert( is(typeof(   publicF())));
+            static assert( is(typeof(protectedF())));
+            static assert( is(typeof(  packageF())));
+            static assert(!is(typeof(  privateF())));
+
+            static assert( is(typeof(   publicTF())));
+            static assert( is(typeof(protectedTF())));
+            static assert( is(typeof(  packageTF())));
+            static assert(!is(typeof(  privateTF())));
+
+            auto dg = {
+                static assert( is(typeof(   publicF())));
+                static assert( is(typeof(protectedF())));
+                static assert( is(typeof(  packageF())));
+                static assert(!is(typeof(  privateF())));
+
+                static assert( is(typeof(   publicTF())));
+                static assert( is(typeof(protectedTF())));
+                static assert( is(typeof(  packageTF())));
+                static assert(!is(typeof(  privateTF())));
+            };
+        }
+        void test2()()
+        {
+            static assert( is(typeof(   publicF())));
+            static assert( is(typeof(protectedF())));
+            static assert( is(typeof(  packageF())));
+            static assert(!is(typeof(  privateF())));
+
+            static assert( is(typeof(   publicTF())));
+            static assert( is(typeof(protectedTF())));
+            static assert( is(typeof(  packageTF())));
+            static assert(!is(typeof(  privateTF())));
+
+            auto dg = {
+                static assert( is(typeof(   publicF())));
+                static assert( is(typeof(protectedF())));
+                static assert( is(typeof(  packageF())));
+                static assert(!is(typeof(  privateF())));
+
+                static assert( is(typeof(   publicTF())));
+                static assert( is(typeof(protectedTF())));
+                static assert( is(typeof(  packageTF())));
+                static assert(!is(typeof(  privateTF())));
+            };
+        }
+        alias test2x = test2!();    // instantiate
+    }
 }
