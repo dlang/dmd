c893b80612f8b661f1424fe7ffaab3784343d52c
Kai Nacke <kai@redstar.de>
2015-04-06 20:58:21 +0200

    Add TypeInfo classes for cent/ucent.
    The types are defined in the spec but not yet implemented. Add the
    required TypeInfo classes for supporting compilers. The classes are
    guarded to prevent compile errors on non-supporting compilers.
    
    See https://github.com/D-Programming-Language/dmd/pull/4565 for
    the name mangling.

---
+++ src/rt/typeinfo/ti_cent.d
@@ -0,0 +1,68 @@
+/**
+ * TypeInfo support code.
+ *
+ * Copyright: Copyright Digital Mars 2004 - 2015.
+ * License:   $(WEB www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+ * Authors:   Walter Bright
+ */
+
+/*          Copyright Digital Mars 2004 - 2015.
+ * Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ */
+module rt.typeinfo.ti_cent;
+
+private import rt.util.hash;
+
+static if(__traits(compiles, { cent c = 42; })):
+
+// cent
+
+class TypeInfo_zi : TypeInfo
+{
+    @trusted:
+    const:
+    pure:
+    nothrow:
+
+    override string toString() const pure nothrow @safe { return "cent"; }
+
+    override size_t getHash(in void* p)
+    {
+        return rt.util.hash.hashOf(p, cent.sizeof);
+    }
+
+    override bool equals(in void* p1, in void* p2)
+    {
+        return *cast(cent *)p1 == *cast(cent *)p2;
+    }
+
+    override int compare(in void* p1, in void* p2)
+    {
+        if (*cast(cent *)p1 < *cast(cent *)p2)
+            return -1;
+        else if (*cast(cent *)p1 > *cast(cent *)p2)
+            return 1;
+        return 0;
+    }
+
+    override @property size_t tsize() nothrow pure
+    {
+        return cent.sizeof;
+    }
+
+    override void swap(void *p1, void *p2)
+    {
+        cent t;
+
+        t = *cast(cent *)p1;
+        *cast(cent *)p1 = *cast(cent *)p2;
+        *cast(cent *)p2 = t;
+    }
+
+    override @property size_t talign() nothrow pure
+    {
+        return cent.alignof;
+    }
+}
