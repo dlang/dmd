611b9359d14b1fe3f67b6bdadc3923faabb6e396
anonymous <aG0aep6G@users.noreply.github.com>
2015-07-31 00:01:06 +0200

    make -property do nothing
    fixes issue 14564 - [REG2.067] dmd -property -unittest combination causes
    compiler error
    
    Regarding the changed tests:
    
    Issue 8629 occurred with other errors than property errors, too. Replacing
    the property error with an undefined identifier error in the test.
    
    The test for issue 9241 was about the exact error message for when a
    non-property is called without parentheses. That's allowed now, even with
    -property. Removing the test.

--- test/fail_compilation/diag8629.d
+++ test/fail_compilation/diag8629.d
@@ -1,20 +1,18 @@
-// REQUIRED_ARGS: -property
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/diag8629.d(9): Error: not a property s.gunc
+fail_compilation/diag8629.d(8): Error: undefined identifier 'undef'
 ---
 */
 
 #line 1
 struct S {}
 
-auto gunc(S s, int n) { return 13; }
 @property grop(S s, int n) { return 14; }
 
 void main()
 {
     S s;
-    assert((s.gunc = 1) == 13);     // line 9
-    assert((s.grop = 1) == 14);     // line 10
+    undef(); // line 8
+    assert((s.grop = 1) == 14);
 }
