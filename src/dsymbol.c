84c3642d4890cb9c30174b982712b6a8885d9f5a
k-hara <k.hara.pg@gmail.com>
2015-08-06 19:39:10 +0900

    Remove the RTInfo specific code for issue 13738

--- src/dsymbol.c
+++ src/dsymbol.c
@@ -644,11 +644,6 @@ bool Dsymbol::isDeprecated()
     return false;
 }
 
-bool Dsymbol::muteDeprecationMessage()
-{
-    return false;
-}
-
 bool Dsymbol::isOverloadable()
 {
     return false;
@@ -745,11 +740,12 @@ void Dsymbol::deprecation(const char *format, ...)
 
 void Dsymbol::checkDeprecated(Loc loc, Scope *sc)
 {
-    if (global.params.useDeprecated != 1 && isDeprecated() && !muteDeprecationMessage())
+    if (global.params.useDeprecated != 1 && isDeprecated())
     {
         // Don't complain if we're inside a deprecated symbol's scope
         for (Dsymbol *sp = sc->parent; sp; sp = sp->parent)
-        {   if (sp->isDeprecated())
+        {
+            if (sp->isDeprecated())
                 goto L1;
         }
 
@@ -763,7 +759,7 @@ void Dsymbol::checkDeprecated(Loc loc, Scope *sc)
                 goto L1;
         }
 
-        char *message = NULL;
+        const char *message = NULL;
         for (Dsymbol *p = this; p; p = p->parent)
         {
             message = p->depmsg;
