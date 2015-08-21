b21eeb05900d1dc0e816297a011f9cde64e6ebdd
k-hara <k.hara.pg@gmail.com>
2015-08-21 19:57:59 +0900

    fix Issue 10378 - Local imports hide local symbols

---
+++ test/fail_compilation/imports/m10378a.d
@@ -0,0 +1,7 @@
+module imports.m10378a;
+
+string text(A...)(A args) { return ""; }
+
+int foo() { return 1; }
+
+string makeImportCode() { return "import imports.m10378b;"; }
---
+++ test/fail_compilation/imports/m10378b.d
@@ -0,0 +1,3 @@
+module imports.m10378b;
+
+string foo(int) { return "a"; }
