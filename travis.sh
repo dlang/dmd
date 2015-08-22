38880df21aba878abb633d2a4ed4b813190d52d2
Martin Nowak <code@dawg.eu>
2015-08-23 01:17:11 +0200

    use parallel builds

--- travis.sh
+++ travis.sh
@@ -2,13 +2,15 @@
 
 set -exo pipefail
 
-make -C src -f posix.mak ddmd HOST_DMD=$DMD
-make -C src -f posix.mak dmd.conf
+N=2
+
+make -j$N -C src -f posix.mak ddmd HOST_DMD=$DMD
+make -j$N -C src -f posix.mak dmd.conf
 git clone --depth=1 https://github.com/D-Programming-Language/druntime.git ../druntime
 git clone --depth=1 https://github.com/D-Programming-Language/phobos.git ../phobos
-make -C ../druntime -f posix.mak DMD=../dmd/src/ddmd
-make -C ../phobos -f posix.mak DMD=../dmd/src/ddmd
+make -j$N -C ../druntime -f posix.mak DMD=../dmd/src/ddmd
+make -j$N -C ../phobos -f posix.mak DMD=../dmd/src/ddmd
 
-make -C ../druntime -f posix.mak DMD=../dmd/src/ddmd unittest
-make -C ../phobos -f posix.mak DMD=../dmd/src/ddmd unittest
-make -C test DMD=../src/ddmd MODEL=64
+make -j$N -C ../druntime -f posix.mak DMD=../dmd/src/ddmd unittest
+make -j$N -C ../phobos -f posix.mak DMD=../dmd/src/ddmd unittest
+make -j$N -C test DMD=../src/ddmd MODEL=64
