5315d2db913a3353c049006bfad49fc97d99b407
Daniel Murphy <yebblies@gmail.com>
2015-09-17 23:01:38 +1000

    Remove C-style arrays and empty statements

--- src/root/longdouble.d
+++ src/root/longdouble.d
@@ -22,13 +22,13 @@ size_t ld_sprint(char* str, int fmt, real x)
         // ((1.5 -> 1 -> 1.0) == 1.5) is false
         // ((1.0 -> 1 -> 1.0) == 1.0) is true
         // see http://en.cppreference.com/w/cpp/io/c/fprintf
-        char sfmt[5] = "%#Lg\0";
+        char[5] sfmt = "%#Lg\0";
         sfmt[3] = cast(char)fmt;
         return sprintf(str, sfmt.ptr, x);
     }
     else
     {
-        char sfmt[4] = "%Lg\0";
+        char[4] sfmt = "%Lg\0";
         sfmt[2] = cast(char)fmt;
         return sprintf(str, sfmt.ptr, x);
     }
