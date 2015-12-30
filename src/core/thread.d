c4f6acf74a092ff9823598970a0ca696ce17d74e
tcak <tcak@users.noreply.github.com>
2015-12-30 20:34:56 +0200

    Example code to start thread with an anonymous function

--- src/core/thread.d
+++ src/core/thread.d
@@ -1808,6 +1808,8 @@ unittest
     // create and start instances of each type
     auto derived = new DerivedThread().start();
     auto composed = new Thread(&threadFunc).start();
+
+    new Thread( (){ writeln("Hello world!"); } ).start();
 }
 
 unittest
