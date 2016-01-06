31cd3b5dbeba43cf5809a477ad7079ac21854a86
Walter Bright <walter@walterbright.com>
2016-01-06 00:07:20 -0800

    fix Issue 15519 - Circular imports leads to fwd ref error with aliased imports

--- test/fail_compilation/ice9865.d
+++ test/fail_compilation/ice9865.d
@@ -1,9 +1,9 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/ice9865.d(9): Error: struct ice9865.Foo no size yet for forward reference
 fail_compilation/ice9865.d(8): Error: alias ice9865.Baz recursive alias declaration
 ---
 */
+
 import imports.ice9865b : Baz;
 struct Foo { Baz f; }
