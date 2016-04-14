ebf5dfc90d00c7c693ba50c25e38bce15ddd7827
Walter Bright <walter@walterbright.com>
2016-04-13 18:06:49 -0700

    add test cases for issue 12624

---
+++ test/compilable/test12624.d
@@ -0,0 +1,30 @@
+// REQUIRED_ARGS: -lib -Icompilable/extra-files
+// https://issues.dlang.org/show_bug.cgi?id=12624
+
+struct SysTime
+{
+    import imp12624;
+
+    Rebindable!(immutable TimeZone) _timezone = UTC();
+}
+
+
+class TimeZone
+{
+    this(string , string , string ) immutable {}
+}
+
+
+class UTC : TimeZone
+{
+    static immutable(UTC) opCall()
+    {
+        return _utc;
+    }
+
+    this() immutable {
+        super("UTC", "UTC", "UTC");
+    }
+
+    static _utc = new immutable(UTC);
+}
