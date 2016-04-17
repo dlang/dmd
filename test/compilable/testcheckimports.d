082e1199c258e99f7867ca33509db9ae9a31f3ae
k-hara <k.hara.pg@gmail.com>
2016-04-18 00:35:53 +0900

    Issue 15857 - incorrect checkimports mismatch for overload sets
    Cache the generated `OverloadSet` object into scope, and prevent repetition of identical objects.

--- test/compilable/testcheckimports.d
+++ test/compilable/testcheckimports.d
@@ -13,3 +13,17 @@ template anySatisfy15825(T...)
 }
 
 alias T15825 = anySatisfy15825!(int);
+
+// https://issues.dlang.org/show_bug.cgi?id=15857
+
+template Mix15857(T)
+{
+    void foo15857(T) {}
+}
+mixin Mix15857!int;
+mixin Mix15857!string;
+
+void test15857()
+{
+    foo15857(1);
+}
