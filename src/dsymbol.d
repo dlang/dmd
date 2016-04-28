5a3f0ce90dde476729076ba86a8bf63ee6c4f6db
Mathias Lang <mathias.lang@sociomantic.com>
2016-04-28 17:48:36 +0200

    [REG 2.071] Fix Issue 15925 - Import declaration from mixin templates are ignored
    There can be 3 types of AST node in `importedScopes`: `Module`, `TemplateMixin` and `Nspace`.
    `MixinTemplate` can also introduce imports, so we need to look into them when doing the second
    search pass.

--- src/dsymbol.d
+++ src/dsymbol.d
@@ -1349,7 +1349,7 @@ public:
                         continue;
                     }
                 }
-                else
+                else if (!ss.isTemplateMixin)
                 {
                     if (flags & SearchImportsOnly)
                         continue;
---
+++ test/compilable/imports/test15925.d
@@ -0,0 +1,9 @@
+module imports.test15925;
+
+public mixin template AddImports ()
+{
+    import core.thread;
+
+    ScanAllThreadsFn fun;
+    Thread th;
+}
