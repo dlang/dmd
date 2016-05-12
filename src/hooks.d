1142d56005f7e5135665381744fa740119010538
Kai Nacke <kai@redstar.de>
2016-05-12 19:33:13 +0200

    2nd attempt

--- src/hooks.d
+++ src/hooks.d
@@ -6,28 +6,12 @@
 // Distributed under the Boost Software License, Version 1.0.
 // http://www.boost.org/LICENSE_1_0.txt
 
-module ddmd.appl;
+module ddmd.hooks;
 
 import ddmd.dscope;
 import ddmd.expression;
 
-struct Hooks
-{
-    Expression function(TraitsExp e, Scope* sc) semanticTraits;
-}
-
-void setHooks(Hooks hooks)
-{
-    .hooks = hooks;
-}
-
 Expression semanticTraitsHook(TraitsExp e, Scope* sc)
 {
-    if (hooks.semanticTraits)
-        return hooks.semanticTraits(e, sc);
     return null;
 }
-
-package:
-
-__gshared Hooks hooks;
