ad419f24f498526e112f07bf9fbe26e31de319bc
Walter Bright <walter@walterbright.com>
2016-06-30 00:06:53 -0700

    fix Issue 15192 - DIP25: Nested ref returns are type checked unsoundly

---
+++ test/fail_compilation/test15192.d
@@ -0,0 +1,23 @@
+/*
+REQUIRED_ARGS: -dip25
+TEST_OUTPUT:
+---
+fail_compilation/test15192.d(14): Error: escaping reference to outer local ref variable x
+---
+*/
+
+
+// https://issues.dlang.org/show_bug.cgi?id=15192
+
+ref int fun(ref int x) @safe
+{
+    ref int bar(){ return x; }
+    return bar();
+}
+
+ref int fun2(return ref int x) @safe
+{
+    ref int bar(){ return x; }
+    return bar();
+}
+
