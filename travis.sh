73f5a90d93647240467da49d904922f70b883ce0
Martin Nowak <code@dawg.eu>
2016-08-06 20:17:30 +0200

    Revert "Fix CI checkout branch for non Travis CIs"
    This reverts commit cffa4c3691a4323f277eca236207375302e726a0.

--- travis.sh
+++ travis.sh
@@ -8,14 +8,8 @@ if [ $DC = gdc ] && [ ! -f $(dirname $(which gdc))/cc ]; then
 fi
 N=2
 
-CI_BRANCH="master"
-
-if [ -n "$TRAVIS_BRANCH" ] ; then
-    CI_BRANCH="$TRAVIS_BRANCH"
-fi
-
-git clone --depth=1 --branch $CI_BRANCH https://github.com/dlang/druntime.git ../druntime
-git clone --depth=1 --branch $CI_BRANCH https://github.com/dlang/phobos.git ../phobos
+git clone --depth=1 --branch $TRAVIS_BRANCH https://github.com/D-Programming-Language/druntime.git ../druntime
+git clone --depth=1 --branch $TRAVIS_BRANCH https://github.com/D-Programming-Language/phobos.git ../phobos
 
 make -j$N -C src -f posix.mak MODEL=$MODEL HOST_DMD=$DMD all
 make -j$N -C src -f posix.mak MODEL=$MODEL HOST_DMD=$DMD dmd.conf
@@ -33,23 +27,18 @@ while [ $SELF_COMPILE -gt 0 ]; do
     make -j$N -C src -f posix.mak MODEL=$MODEL HOST_DMD=./host_dmd clean
     make -j$N -C src -f posix.mak MODEL=$MODEL HOST_DMD=./host_dmd dmd.conf
     make -j$N -C src -f posix.mak MODEL=$MODEL HOST_DMD=./host_dmd
-
-    if [ -z ${DMD_TEST_COVERAGE} ] ; then
-        make -j$N -C ../druntime -f posix.mak MODEL=$MODEL clean
-        make -j$N -C ../druntime -f posix.mak MODEL=$MODEL
-        make -j$N -C ../phobos -f posix.mak MODEL=$MODEL clean
-        make -j$N -C ../phobos -f posix.mak MODEL=$MODEL
-    else
-    	echo "Skipping druntime and Phobos rebuilds with coverage builds"
-    fi
+    make -j$N -C ../druntime -f posix.mak MODEL=$MODEL clean
+    make -j$N -C ../druntime -f posix.mak MODEL=$MODEL
+    make -j$N -C ../phobos -f posix.mak MODEL=$MODEL clean
+    make -j$N -C ../phobos -f posix.mak MODEL=$MODEL
     rm src/host_dmd
     SELF_COMPILE=$(($SELF_COMPILE - 1))
 done
 
 # Only run runtime + phobos tests on Travis
 if [ "${CIRCLECI}" != "true" ] ; then
-    make -j$N -C ../druntime -f posix.mak MODEL=$MODEL unittest
-    make -j$N -C ../phobos -f posix.mak MODEL=$MODEL unittest
+	make -j$N -C ../druntime -f posix.mak MODEL=$MODEL unittest
+	make -j$N -C ../phobos -f posix.mak MODEL=$MODEL unittest
 fi
 
 QUICK_BUILD=0
