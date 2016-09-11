515dc20d735a24ea56f2aee34422b58d3698af3c
Stefan Koch <Uplink.Coder@googlemail.com>
2016-09-11 21:27:07 +0200

    ctfe-code is often annotated with pure @safe we want to print anyway

--- src/object.d
+++ src/object.d
@@ -3186,8 +3186,8 @@ bool _xopCmp(in void*, in void*)
 {
     throw new Error("TypeInfo.compare is not implemented");
 }
-void __ctfeWrite(const string s) {}
-void __ctfeWriteln(const string s)
+void __ctfeWrite(const string s) @nogc @safe pure nothrow {}
+void __ctfeWriteln(const string s) @nogc @safe pure nothrow
 {
   __ctfeWrite(s);
   __ctfeWrite("\n");
