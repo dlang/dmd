31f070f607398bcef72110f40c125a7f2ea7761b
Mathias Lang <mathias.lang@sociomantic.com>
2016-09-26 14:25:41 +0200

    Fix issue 3827 - Deprecate implicit string concatenation
    Implicit string concatenation is an old feature that can now be replaced by explicit concatenation.
    It was also a source of bug, even for experienced D programmer, and the deprecation has been agreed
    on in 2010: http://forum.dlang.org/post/ibi742$gi2$1@digitalmars.com

--- test/fail_compilation/diag10805.d
+++ test/fail_compilation/diag10805.d
@@ -1,9 +1,10 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/diag10805.d(10): Error: delimited string must end in FOO"
-fail_compilation/diag10805.d(12): Error: unterminated string constant starting at fail_compilation/diag10805.d(12)
-fail_compilation/diag10805.d(13): Error: semicolon expected following auto declaration, not 'EOF'
+fail_compilation/diag10805.d(11): Error: delimited string must end in FOO"
+fail_compilation/diag10805.d(13): Error: unterminated string constant starting at fail_compilation/diag10805.d(13)
+fail_compilation/diag10805.d(13): Deprecation: Implicit string concatenation is deprecated, use "" ~ "" instead
+fail_compilation/diag10805.d(14): Error: semicolon expected following auto declaration, not 'EOF'
 ---
 */
 
