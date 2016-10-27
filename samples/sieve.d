2ee0b79ebc1b515df429c140081728a351dabbcd
majiang <neuron.ohori@gmail.com>
2016-10-27 21:37:38 +0900

    samples/prime: early continue

--- samples/sieve.d
+++ samples/sieve.d
@@ -15,19 +15,18 @@ void main(string[] args)
 
     foreach (i; 0..flags.length)
     {
-        if (flags[i])
+        if (!flags[i])
+            continue;
+        auto prime = i + i + 3;
+        auto k     = i + prime;
+
+        while (k < flags.length)
         {
-            auto prime = i + i + 3;
-            auto k     = i + prime;
-
-            while (k < flags.length)
-            {
-                flags[k] = false;
-                k       += prime;
-            }
-
-            count += 1;
+            flags[k] = false;
+            k       += prime;
         }
+
+        count += 1;
     }
     writefln("%d primes", count);
 }
