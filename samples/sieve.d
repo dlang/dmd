20b1316ba5f8c19440a3a16e9e1188710c8a72fb
majiang <neuron.ohori@gmail.com>
2016-10-27 21:35:30 +0900

    samples/prime: count 2

--- samples/sieve.d
+++ samples/sieve.d
@@ -1,13 +1,16 @@
-
 /* Eratosthenes Sieve prime number calculation. */
 
+import std.conv;
 import std.stdio;
-
 
 void main(string[] args)
 {
-    size_t count;
-    auto flags = new bool[8191];
+    immutable max = (1 < args.length)
+        ? args[1].to!size_t
+        : 0x4000;
+    size_t count = 1; // we have 2.
+    // flags[i] = isPrime(2 * i + 3)
+    auto flags = new bool[(max-1)/2];
     flags[] = true;
 
     foreach (i; 0..flags.length)
