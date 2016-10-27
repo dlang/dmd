6864a8c0dcaea7e4978e6fb729a40cbe23958c62
majiang <neuron.ohori@gmail.com>
2016-10-27 21:27:16 +0900

    samples/prime: main signature; remove iteration

--- samples/sieve.d
+++ samples/sieve.d
@@ -4,34 +4,27 @@
 import std.stdio;
 
 
-int main()
+void main(string[] args)
 {
-    writefln("10 iterations");
+    size_t count;
+    auto flags = new bool[8191];
+    flags[] = true;
 
-    foreach (iter; 0..10)
+    foreach (i; 0..flags.length)
     {
-        size_t count;
-        auto flags = new bool[8191];
-        flags[] = true;
-
-        foreach (i; 0..flags.length)
+        if (flags[i])
         {
-            if (flags[i])
+            auto prime = i + i + 3;
+            auto k     = i + prime;
+
+            while (k < flags.length)
             {
-                auto prime = i + i + 3;
-                auto k     = i + prime;
-
-                while (k < flags.length)
-                {
-                    flags[k] = false;
-                    k       += prime;
-                }
-
-                count += 1;
+                flags[k] = false;
+                k       += prime;
             }
-        }
-        writefln("%d primes", count);
-    }
 
-    return 0;
+            count += 1;
+        }
+    }
+    writefln("%d primes", count);
 }
