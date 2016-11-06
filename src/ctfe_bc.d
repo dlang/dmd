d8682c33ef70f3a7b457b0c81d005c6ced09d143
Stefan Koch <uplink.coder@googlemail.com>
2016-11-06 14:31:56 +0000

    add another bailout

--- src/ctfe_bc.d
+++ src/ctfe_bc.d
@@ -974,7 +974,8 @@ public:
                 || fd.ident == Identifier.idPool("divideRoundUp")
                 || fd.ident == Identifier.idPool("isSameLength")
                 || fd.ident == Identifier.idPool("wrapperParameters")
-                || fd.ident == Identifier.idPool("wrap") || fd.ident == Identifier.idPool("args"))
+                || fd.ident == Identifier.idPool("wrap")
+                || fd.ident == Identifier.idPool("args") || fd.ident == Identifier.idPool("check"))
         {
             IGaveUp = true;
             return;
