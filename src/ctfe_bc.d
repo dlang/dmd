7a9b78af3ed5cd1aa51950008ca92791712bf5e9
Stefan Koch <uplink.coder@googlemail.com>
2016-12-12 23:30:45 +0000

    remove fixme

--- src/ctfe_bc.d
+++ src/ctfe_bc.d
@@ -1939,10 +1939,6 @@ public:
 
     override void visit(ForStatement fs)
     {
-        //FIXME
-        // There might be a problem with continueing for loops
-        // I think we should execute the increment step when we continue
-        // However that will not be done with the current system
         debug (ctfe)
         {
             import std.stdio;
