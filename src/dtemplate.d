bdf0df926f90b9243932e2ff76f79de644224b26
LemonBoy <thatlemon@gmail.com>
2016-12-22 18:33:35 +0100

    Fix issue #16278 - Do not emit TypeInfo for speculative instantiations.

--- src/dtemplate.d
+++ src/dtemplate.d
@@ -8342,8 +8342,7 @@ void unSpeculative(Scope* sc, RootObject o)
     if (!s)
         return;
 
-    Declaration d = s.isDeclaration();
-    if (d)
+    if (Declaration d = s.isDeclaration())
     {
         if (VarDeclaration vd = d.isVarDeclaration())
             o = vd.type;
