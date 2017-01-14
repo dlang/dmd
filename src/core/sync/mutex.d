5e4b740b1fd9c4b02caa4c8b1edcd3711d2e492b
ZombineDev <petar.p.kirov@gmail.com>
2017-01-14 02:33:08 +0200

    [core.sync.mutex] Cleanup existing unittest

--- src/core/sync/mutex.d
+++ src/core/sync/mutex.d
@@ -295,40 +295,32 @@ package:
 }
 
 
-////////////////////////////////////////////////////////////////////////////////
-// Unit Tests
-////////////////////////////////////////////////////////////////////////////////
 
-
-version (unittest)
+unittest
 {
-    private import core.thread;
+    import core.thread;
 
+    auto mutex      = new Mutex;
+    int  numThreads = 10;
+    int  numTries   = 1000;
+    int  lockCount  = 0;
 
-    unittest
+    void testFn()
     {
-        auto mutex      = new Mutex;
-        int  numThreads = 10;
-        int  numTries   = 1000;
-        int  lockCount  = 0;
-
-        void testFn()
+        for (int i = 0; i < numTries; ++i)
         {
-            for (int i = 0; i < numTries; ++i)
+            synchronized (mutex)
             {
-                synchronized (mutex)
-                {
-                    ++lockCount;
-                }
+                ++lockCount;
             }
         }
-
-        auto group = new ThreadGroup;
-
-        for (int i = 0; i < numThreads; ++i)
-            group.create(&testFn);
-
-        group.joinAll();
-        assert(lockCount == numThreads * numTries);
     }
+
+    auto group = new ThreadGroup;
+
+    for (int i = 0; i < numThreads; ++i)
+        group.create(&testFn);
+
+    group.joinAll();
+    assert(lockCount == numThreads * numTries);
 }
