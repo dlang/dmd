2a6e5b01feb98c4b6f76aa76d69f1e98ebda58bc
Rainer Schuetze <r.sagitario@gmx.de>
2017-02-12 11:50:06 +0100

    scan DATA and TLS according to compiler generated pointer infos

--- src/rt/sections_win32.d
+++ src/rt/sections_win32.d
@@ -18,6 +18,8 @@ version(CRuntime_DigitalMars):
 debug(PRINTF) import core.stdc.stdio;
 import rt.minfo;
 
+//version = conservative;
+
 struct SectionGroup
 {
     static int opApply(scope int delegate(ref SectionGroup) dg)
@@ -47,25 +49,48 @@ struct SectionGroup
 
 private:
     ModuleGroup _moduleGroup;
-    void[][2] _gcRanges;
+    version(conservative)
+        void[][2] _gcRanges;
+    else
+        void[][] _gcRanges;
 }
 
 void initSections() nothrow @nogc
 {
     _sections._moduleGroup = ModuleGroup(getModuleInfos());
 
-    auto databeg = cast(void*)&_xi_a;
-    auto dataend = cast(void*)_moduleinfo_array.ptr;
-    _sections._gcRanges[0] = databeg[0 .. dataend - databeg];
+    version(conservative)
+    {
+        auto databeg = cast(void*)&_xi_a;
+        auto dataend = cast(void*)_moduleinfo_array.ptr;
+        _sections._gcRanges[0] = databeg[0 .. dataend - databeg];
 
-    // skip module info and CONST segment
-    auto bssbeg = cast(void*)&_edata;
-    auto bssend = cast(void*)&_end;
-    _sections._gcRanges[1] = bssbeg[0 .. bssend - bssbeg];
+        // skip module info and CONST segment
+        auto bssbeg = cast(void*)&_edata;
+        auto bssend = cast(void*)&_end;
+        _sections._gcRanges[1] = bssbeg[0 .. bssend - bssbeg];
+    }
+    else
+    {
+        import core.stdc.stdlib : malloc;
+
+        size_t count = &_DPend - &_DPbegin;
+        auto ranges = cast(void[]*) malloc(count * (void[]).sizeof);
+        for (size_t i = 0; i < count; i++)
+        {
+            void* addr = (&_DPbegin)[i];
+            ranges[i] = (cast(void**)addr)[0..1]; // TODO: optimize consecutive pointers into single range
+        }
+        _sections._gcRanges = ranges[0..count];
+    }
 }
 
 void finiSections() nothrow @nogc
 {
+    import core.stdc.stdlib : free;
+
+    version(conservative) {} else
+        free(_sections._gcRanges.ptr);
 }
 
 void[] initTLSRanges() nothrow @nogc
@@ -81,7 +106,16 @@ void finiTLSRanges(void[] rng) nothrow @nogc
 
 void scanTLSRanges(void[] rng, scope void delegate(void* pbeg, void* pend) nothrow dg) nothrow
 {
-    dg(rng.ptr, rng.ptr + rng.length);
+    version(conservative)
+    {
+        dg(rng.ptr, rng.ptr + rng.length);
+    }
+    else
+    {
+        size_t count = &_TPend - &_TPbegin;
+        for (auto p = &_TPbegin; p < &_TPend; p++)
+            dg(rng.ptr + *p, rng.ptr + *p + (void*).sizeof);
+    }
 }
 
 private:
@@ -112,6 +146,11 @@ extern(C)
         int _xi_a;      // &_xi_a just happens to be start of data segment
         int _edata;     // &_edata is start of BSS segment
         int _end;       // &_end is past end of BSS
+
+        void* _DPbegin; // first entry in the array of pointers addresses
+        void* _DPend;   // &_DPend points after last entry of array
+        uint _TPbegin;  // first entry in the array of TLS offsets of pointers
+        uint _TPend;    // &_DPend points after last entry of array
     }
 
     extern
