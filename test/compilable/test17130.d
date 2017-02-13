cfeb41ae8db207d708b48155760a52dba0c57791
Martin Nowak <code@dawg.eu>
2017-02-14 00:48:21 +0100

    fix Issue 17130 - ambiguous implicit super call
    - need to pass class type to resolveFuncCall
    - also make implicit ctor shared for shared base ctor

--- test/compilable/test17130.d
+++ test/compilable/test17130.d
@@ -7,10 +7,32 @@ class Base
     {}
 }
 
-class Derived : Base
+class Derived1 : Base
 {
     this()
     {
         // implicit super();
     }
+}
+
+class Derived2 : Base
+{
+    // implicit this()
+}
+
+class Base2
+{
+    this() shared
+    {}
+}
+
+class Derived3 : Base2
+{
+    // implicit this() shared
+}
+
+void test()
+{
+    auto d2 = new Derived2;
+    auto d3 = new shared(Derived3);
 }
