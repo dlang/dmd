f8dd223c8eafb8dc30015f3ab3c2e476694d830d
Martin Nowak <code@dawg.eu>
2017-03-13 14:47:41 +0100

    change plot to relative numbers

--- benchmark/arrayops/plot.R
+++ benchmark/arrayops/plot.R
@@ -1,32 +1,30 @@
-# Use `R --vanilla < plot.R` to run this script.
-# It will read all *.csv files from the current folder and create a comparison plot for them.
+# Use `Rscript --vanilla plot.R old.csv new.csv` to run this script.
+# It will read old.csv and new.csv files and create a comparison plot for them.
 library(ggplot2)
 library(dplyr)
 library(tidyr)
 
 dat <- NULL
-files <- list.files(pattern='*.csv')
-for (file in files)
-{
-  datFile <- read.csv(file) %>% tbl_df() %>%
-    mutate(file=file)
-  if (is.null(dat))
-     dat = datFile
-  else
-     dat = bind_rows(dat, datFile)
-}
+args <- commandArgs(trailingOnly=T)
+old <- read.csv(args[1]) %>% tbl_df()
+new <- read.csv(args[2]) %>% tbl_df()
 
-latencies <- gather(dat %>% select(-starts_with('throughput')), num_elems, latency, starts_with('latency')) %>%
+col.indices <- which(!colnames(new) %in% c("type", "op"))
+
+# relative values
+new[,col.indices] <- 100 * new[,col.indices] / old[,col.indices]
+
+latencies <- gather(new %>% select(-starts_with('throughput')), num_elems, latency, starts_with('latency')) %>%
     mutate(num_elems = factor(as.integer(sub("latency(\\d+)", "\\1", num_elems))))
-throughputs <- gather(dat %>% select(-starts_with('latency')), array_size, throughput, starts_with('throughput')) %>%
+throughputs <- gather(new %>% select(-starts_with('latency')), array_size, throughput, starts_with('throughput')) %>%
     mutate(array_size = factor(as.integer(sub("throughput(\\d+)KB", "\\1", array_size))))
 
 img <- qplot(num_elems, latency, group=type, data=latencies, geom="line", color=type) +
-  facet_grid(op ~ file, scales="free_y") +
-  labs(x="num elements", y="latency / ns")
-ggsave('array_ops_latency.png', plot = img, width = 2 + 3 * length(files), height = 40)
+  facet_grid(op ~ ., scales="free_y") +
+  labs(x="num elements", y="relative latency / %")
+ggsave('array_ops_latency.png', plot = img, width = 8, height = 40)
 
 img <- qplot(array_size, throughput, group=type, data=throughputs, geom="line", color=type) +
-  facet_grid(op ~ file, scales="free_y") +
-  labs(x="array size / KB", y="throughput / (ops / ns)")
-ggsave('array_ops_throughput.png', plot = img, width = 2 + 3 * length(files), height = 40)
+  facet_grid(op ~ ., scales="free_y") +
+  labs(x="array size / KB", y="relative throughput / %")
+ggsave('array_ops_throughput.png', plot = img, width = 8, height = 40)
