6be1794e8700041e348efcf0b5a1731623c85fbb
Nemanja Boric <nemanja.boric@sociomantic.com>
2017-05-06 12:16:47 +0200

    Simplify fiber guard page test

--- test/thread/src/fiber_guard_page.d
+++ test/thread/src/fiber_guard_page.d
@@ -2,32 +2,45 @@ import core.thread;
 import core.sys.posix.sys.mman;
 
 // this should be true for most architectures
+// (taken from core.thread)
 version = StackGrowsDown;
 
-int recurse(int i)
+enum stackSize = 4096;
+
+// Simple method that causes a stack overflow
+void stackMethod()
 {
-    return i == 0 ? 0 : recurse(i - 1) + i;
+    // Over the stack size, so it overflows the stack
+    int[stackSize/int.sizeof+100] x;
 }
 
 void main()
 {
-    import core.stdc.stdio;
-    enum stackSize = 4096;
-    enum n = size_t.sizeof == 8 ? 128 : 512;
-    auto fib1 = new Fiber(function{ recurse(n); }, stackSize);
+    auto test_fiber = new Fiber(&stackMethod, stackSize);
+
     // allocate a page below (above) the fiber's stack to make stack overflows possible (w/o segfaulting)
     version (StackGrowsDown)
     {
-        auto stackBottom1 = fib1.tupleof[8]; // m_pmem
-        auto p = mmap(stackBottom1 - 8 * stackSize, 8 * stackSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+        static assert(__traits(identifier, test_fiber.tupleof[8]) == "m_pmem");
+        auto stackBottom = test_fiber.tupleof[8];
+        auto p = mmap(stackBottom - 8 * stackSize, 8 * stackSize,
+                      PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
         assert(p !is null, "failed to allocate page");
     }
     else
     {
-        auto stackTop1 = fib1.tupleof[8] + fib1.tupleof[7]; // m_pmem + m_sz
-        auto p = mmap(stackTop1, 8 * stackSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+        auto m_sz = test_fiber.tupleof[7];
+        auto m_pmem = test_fiber.tupleof[8];
+        static assert(__traits(identifier, test_fiber.tupleof[7]) == "m_size");
+        static assert(__traits(identifier, test_fiber.tupleof[8]) == "m_pmem");
+
+        auto stackTop = m_pmem + m_sz;
+        auto p = mmap(stackTop, 8 * stackSize,
+                      PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
         assert(p !is null, "failed to allocate page");
     }
-    // the guard page should prevent a mem corruption by stack overflow and cause a segfault instead
-    fib1.call();
+
+    // the guard page should prevent a mem corruption by stack
+    // overflow and cause a segfault instead (or generate SIGBUS on *BSD flavors)
+    test_fiber.call();
 }
