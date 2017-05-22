0d40de43e9fbe41817a9ace030260a94fd211cc1
Stefan Koch <uplink.coder@googlemail.com>
2017-05-22 23:02:02 +0200

    fix @LemonBoy's PR

--- test/compilable/b6227.d
+++ test/compilable/b6227.d
@@ -1,7 +1,7 @@
 /* TEST_OUTPUT:
 ---
-compilable/b6227.d(17): Deprecation: Comparison between different enumeration types (got X and Y)
-compilable/b6227.d(18): Deprecation: Comparison between different enumeration types (got X and Y)
+compilable/b6227.d(17): Deprecation: Comparison between different enumeration types `X` and `Y`; If this behavior is intended consider using `std.conv.asOriginalType`
+compilable/b6227.d(18): Deprecation: Comparison between different enumeration types `X` and `Y`; If this behavior is intended consider using `std.conv.asOriginalType`
 ---
 */
 enum X {
