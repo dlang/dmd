962354f3796df5129a6c0854e97a79944cf6ff3c
Martin Nowak <code@dawg.eu>
2017-05-25 07:33:02 +0200

    better comment and issue link for C++ case

--- src/ddmd/declaration.d
+++ src/ddmd/declaration.d
@@ -2236,9 +2236,10 @@ extern (C++) class VarDeclaration : Declaration
                 //if (cd.isInterfaceDeclaration())
                 //    error("interface %s cannot be scope", cd.toChars());
 
+                // Destroying C++ scope classes crashes currently. Since C++ class dtors are not currently supported, simply do not run dtors for them.
+                // See https://issues.dlang.org/show_bug.cgi?id=13182
                 if (cd.cpp)
                 {
-                    // Destructors are not supported on extern(C++) classes
                     break;
                 }
                 if (mynew || onstack) // if any destructors
