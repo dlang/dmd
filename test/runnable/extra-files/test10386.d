53582a8966649f30362dd3e6e7573926da782530
Rainer Schuetze <r.sagitario@gmx.de>
2017-05-27 12:57:16 +0200

    adjust tests to support to test both the current mangling as well as mangling with back references

--- test/runnable/extra-files/test10386.d
+++ test/runnable/extra-files/test10386.d
@@ -3,9 +3,11 @@ module test10386;
 // import lib.foo.bar;  // ok
 import lib10386.foo;  // linker failure
 
+import imports.testmangle;
+
 void main()
 {
-    static assert(foo.mangleof == "_D8lib103863foo3bar3fooFiZv");
+    static assert(equalDemangle(foo.mangleof, "_D8lib103863foo3bar3fooFiZv"));
     foo(1);
 }
 
