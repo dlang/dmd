92c89af6184b2a545426bd87bcbf7566e520e152
Timon Gehr <timon.gehr@gmx.ch>
2017-06-06 17:52:18 +0200

    Update tests.

--- src/test_staticforeach.d
+++ src/test_staticforeach.d
@@ -1,6 +1,59 @@
 // -*- compile-command: "cd src && make -fposix.mak DEBUG=1 && cd .. && ./src/dmd test_staticforeach.d" -*-
 
-import std.range, std.algorithm, std.conv;
+struct Iota(T){
+	T s,e;
+	@property bool empty(){ return s>=e; }
+	@property T front(){ return s; }
+	@property T back(){ return cast(T)(e-1); }
+	void popFront(){ s++; }
+	void popBack(){ e--; }
+}
+auto iota(T)(T s, T e){ return Iota!T(s,e); }
+
+template map(alias a){
+	struct Map(R){
+		R r;
+		this(R r){ this.r=r; }
+		@property front(){ return a(r.front); }
+		@property back(){ return a(r.back); }
+		@property bool empty(){ return r.empty; }
+		void popFront(){ r.popFront(); }
+		void popBack(){ r.popBack(); }
+	}
+	auto map(R)(R r){ return Map!R(r); }
+}
+
+template to(T:string){
+	string to(S)(S x)if(is(S:int)||is(S:size_t)||is(S:char)){
+		static if(is(S==char)) return cast(string)[x];
+		if(x<0) return "-"~to(-x);
+		if(x==0) return "0";
+		return (x>=10?to(x/10):"")~cast(char)(x%10+'0');
+	}
+}
+auto text(T)(T arg){ return to!string(arg); };
+
+template all(alias a){
+	bool all(R)(R r){
+		foreach(x;r) if(!a(x)) return false;
+		return true;
+	}
+}
+template any(alias a){
+	bool any(R)(R r){
+		foreach(x;r) if(a(x)) return true;
+		return false;
+	}
+}
+auto join(R)(R r,string sep=""){
+	string a;
+	int first=0;
+	foreach(x;r){
+		if(first++) a~=sep;
+		a~=x;
+	}
+	return a;
+}
 
 static foreach_reverse(x;iota(0,10).map!(to!string)){
     pragma(msg, x);
@@ -9,7 +62,7 @@ static foreach_reverse(x;iota(0,10).map!(to!string)){
 // create struct members iteratively
 struct S{
     static foreach(i;a){
-        mixin("int x"~to!string(i)~";");
+	    mixin("int x"~to!string(i)~";");
     }
     immutable int[] a = [0,1,2];
 }
@@ -22,8 +75,9 @@ static foreach(member;__traits(allMembers,S)){
 }
 
 // print prime numbers using overload sets as state variables. :)
-static assert(is(typeof(bad57))); // forward reference works
+static assert(is(typeof(bad57)));
 static assert(!is(typeof(bad53)));
+
 static foreach(x;iota(2,100)){
     static foreach(y;iota(2,x)){
         static if(!(x%y)){
@@ -38,35 +92,52 @@ static foreach(x;iota(2,100)){
     }
 }
 
-// simple boilerplate-free visitor pattern
-static foreach(char T;'A'..'F'){
-    mixin("class "~T~q{{
-        void accept(Visitor v){
-            return v.visit(this);
-        }
-    }});
-}
 
 alias Seq(T...)=T;
 
+// simple boilerplate-free visitor pattern
+static foreach(char T;'A'..'F'){
+	mixin("class "~T~q{{
+		void accept(Visitor v){
+			return v.visit(this);
+		}
+	}});
+}
 alias Types = Seq!(mixin("Seq!("~iota('A','F').map!(to!string).join(", ")~")"));
-
-class Visitor{
-    static foreach(T;Types){
-        void visit(T){
-            import std.stdio;
-            writeln("visited: ",T.stringof);
-        }
-    }
+abstract class Visitor{
+	static foreach(T;Types){
+		abstract void visit(T);
+	}
 }
 
-void main(){
-    auto v=new Visitor;
-    static foreach(T;Types){
-        v.visit(new T);
-    }
-    testest();
+string testVisitor(){
+	string r;
+	void writeln(T...)(T args){
+		static foreach(x;args) r~=x;
+		r~='\n';
+	}
+	class Visitor: .Visitor{
+		static foreach(T;Types){
+			override void visit(T){
+				writeln("visited: ",T.stringof);
+			}
+		}
+	}
+	void main(){
+		auto v=new Visitor;
+		static foreach(T;Types){
+			v.visit(new T);
+		}
+	}
+	main();
+	return r;
 }
+static assert(testVisitor()=="visited: A
+visited: B
+visited: C
+visited: D
+visited: E
+");
 
 // iterative computation over AliasSeq:
 // (TODO: nice version with X template)
@@ -78,12 +149,39 @@ template staticMap(alias F,T...){
     alias staticMap = Seq!(mixin("state"~to!string(T.length)));
 }
 
-import std.traits;
 alias arrayOf(T)=T[];
 static assert(is(staticMap!(arrayOf,int,char,bool,Object)==Seq!(int[], char[], bool[], Object[])));
 pragma(msg, staticMap!(arrayOf,int,char,bool,Object));
 
 
+
+
+struct StaticForeachLoopVariable{
+    int x;
+    static foreach(i;0..1){
+        mixin("enum x"~text(i)~" = i;");
+    }
+    int y;
+    static assert(__traits(allMembers, StaticForeachLoopVariable).length==3);
+    static assert(!is(typeof(StaticForeachLoopVariable.i)));
+    static assert(!is(typeof(__traits(getMember, StaticForeachLoopVariable, "i"))));
+}
+
+struct StaticForeachScopeExit{
+static:
+    int[] test(){
+        int[] r;
+        scope(exit) r ~= 1234;
+        {
+            static foreach(i;0..5){
+                scope(exit) r ~= i;
+            }
+            r ~= 5;
+        }
+        return r;
+    }
+    static assert(test()==[5,4,3,2,1,0]);
+}
 
 struct StaticForeachReverseHiding{
     static foreach(i;[0]){
@@ -155,12 +253,12 @@ struct StaticForeachByAliasDefault{
 static:
     alias Seq(T...)=T;
 
-    /+int[] test(){
+    int[] test(){
         int a,b,c;
-        static foreach(i,x;Seq!(a,b,c)) x=i; // error
+        static foreach(i,x;Seq!(a,b,c)) x=i;
         return [a,b,c];
     }
-    static assert(test()==[0,1,2]);+/
+    static assert(test()==[0,1,2]);
 
     int[] test2(){
         int x=0;
@@ -170,12 +268,14 @@ static:
     }
     static assert(test2()==[1,2,3]);
 
-    /+int[] test3(){
+    void test3(){
         int x=0;
-        int foo(){ return ++x; } // error
-        static foreach(enum y;Seq!foo)
-            return [y,y,y];
-    }+/
+        int foo(){ return ++x; }
+        static assert(!is(typeof({
+            static foreach(enum y;Seq!foo)
+                return [y,y,y];
+        })));
+    }
 }
 
 struct NestedStaticForeach{
@@ -250,9 +350,12 @@ struct RangeStaticForeach{
     static foreach(i;0..5){
         static assert(mixin(`x`~cast(char)('0'+2*i))==2*i);
     }
-    /+static foreach(i,k;Range()){ // error
-
-    }+/
+    static assert(!is(typeof({
+        struct S{
+            static foreach(i,k;Range()){}
+        }
+    })));
+    static foreach(k;Range()){} // ok
 }
 
 struct OpApplySingleStaticForeach{
@@ -289,18 +392,20 @@ struct AliasForeach{
 static:
     alias Seq(T...)=T;
     int[] test(){
-        /+int a,b,c;
-        static foreach(x;Seq!(a,b,c,2)){ // error
+        int a,b,c;
+        static foreach(x;Seq!(a,b,c,2)){
             static if(is(typeof({x=2;}))) x=2;
-        }+/
+        }
         int x,y,z;
         static foreach(alias k;Seq!(x,y,z,2)){
             static if(is(typeof({k=2;}))) k=2;
         }
-        /+int j,k,l;
-        static foreach(ref x;Seq!(j,k,l,2)){ // error
-            static if(is(typeof({x=2;}))) x=2;
-        }+/
+        int j,k,l;
+        static assert(!is(typeof({
+            static foreach(ref x;Seq!(j,k,l,2)){
+                static if(is(typeof({x=2;}))) x=2;
+            }
+        })));
         return [x,y,z];
     }
     static assert(test()==[2,2,2]);
@@ -328,21 +433,38 @@ static:
     alias Seq(T...)=T;
     auto test(){
         int k;
-        /+static foreach(x;[k]){ // error
-
-        }+/
-        foreach(enum x;[1,2,3]){} // TODO: BUG. this should print an error
+        static assert(!is(typeof({
+            static foreach(x;[k]){}
+        })));
+        static assert(!is(typeof({
+            foreach(enum x;[1,2,3]){}
+        })));
+        static assert(!is(typeof({
+            foreach(alias x;[1,2,3]){}
+        })));
         foreach(enum x;Seq!(1,2,3)){} // ok
-        static foreach(enum x;[1,2,3]){} //ok
+        foreach(alias x;Seq!(1,2,3)){} // ok
+        static foreach(enum x;[1,2,3]){} // ok
+        static foreach(alias x;[1,2,3]){} // ok
+        static assert(!is(typeof({
+            static foreach(enum alias x;[1,2,3]){}
+            // static foreach(ref x;[1,2,3]){}
+        })));
+        int x;
+        static foreach(i;Seq!x){ } // ok
+        static foreach(i,j;Seq!(1,2,x)){ } // ok
+        static assert(!is(typeof({
+            static foreach(ref x;[1,2,3]){}
+        })));
     }
 }
 
 struct SeqForeachConstant{
 static:
     alias Seq(T...)=T;
-    /+void test(){
-        foreach(x;Seq!1) x=2; // error
-    }+/
+	static assert(!is(typeof({
+        foreach(x;Seq!1) x=2;
+	})));
     int test2(){
         int r=0;
         foreach(x;Seq!(1,2,3)){
@@ -368,6 +490,7 @@ static:
     }
     static assert(test()==[0,1,3]);
 }
+
 struct TestStaticForeach{
 static:
     int test(int x){
@@ -399,8 +522,8 @@ static:
     int[] noBreakNoContinue(){
         int[] r;
         static foreach(i;0..1){
-            // if(i==3) continue; // error
-            // if(i==7) break; // error
+            // if(i==3) continue; // TODO: error?
+            // if(i==7) break; // TODO: error?
             r~=i;
         }
         return r;
@@ -432,7 +555,7 @@ int fun(int x){
     switch(x){
         static foreach(i;Seq!(0,1,2,3,4,5,6)){
             static if (i < 5)
-                case i: r=i;break; // TODO: error?
+                case i: r=i; break; // TODO: error?
         }
         default: r=-1; break;
     }
@@ -447,7 +570,7 @@ void testEmpty(){
 }
 
 
-void testest(){
+/+void testest(){
     import std.stdio: writeln;
     import std.conv: to;
     import std.range: iota;
@@ -498,7 +621,7 @@ void testest(){
     foreach(i,j;[Tuple("0",0),Tuple("1",1)].map!(x=>x)){
         writeln(i," ",j);
     }+/
-}
+}+/
 
 /+static foreach(i;[1,2]~[3]){
     int k=i;
