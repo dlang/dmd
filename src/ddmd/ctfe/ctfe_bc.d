994ec68be08b57d8d1e6a81e99b734ad8813f1f5
Stefan Koch <uplink.coder@googlemail.com>
2017-06-09 04:11:18 +0200

    find string8 set

--- src/ddmd/ctfe/ctfe_bc.d
+++ src/ddmd/ctfe/ctfe_bc.d
@@ -1517,7 +1517,7 @@ struct BCScope
 debug = nullPtrCheck;
 debug = nullAllocCheck;
 //debug = andand;
-//debug = SetLocation;
+debug = SetLocation;
 extern (C++) final class BCV(BCGenT) : Visitor
 {
     uint unresolvedGotoCount;
@@ -1743,7 +1743,7 @@ extern (C++) final class BCV(BCGenT) : Visitor
         import std.stdio;
         void Set(BCValue lhs, BCValue rhs, size_t line = __LINE__)
         {
-            if (lhs.vType != BCValueType.StackValue || lhs.vType != BCValueType.Parameter)
+            if (lhs.type.type == BCTypeEnum.string8 || lhs.type.type == BCTypeEnum.string8)
  writeln("Set(", lhs.toString, ", ", rhs.toString, ") called at: ", line);
             gen.Set(lhs, rhs);
         }
