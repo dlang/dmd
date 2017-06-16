3412e831d34b7bdb90b935827bc351bb408221ee
Martin Nowak <code@dawg.eu>
2017-06-16 17:02:21 +0200

    fix typo in assertion

--- src/ddmd/optimize.d
+++ src/ddmd/optimize.d
@@ -610,7 +610,7 @@ extern (C++) Expression Expression_optimize(Expression e, int result, bool keepL
                 // https://issues.dlang.org/show_bug.cgi?id=16980
                 cdfrom.size(e.loc);
                 assert(cdfrom.sizeok == SIZEOKdone);
-                assert(cdto.sizeok == SIZEOKdone || !cdto.isBaseOf(cdto, null));
+                assert(cdto.sizeok == SIZEOKdone || !cdto.isBaseOf(cdfrom, null));
                 int offset;
                 if (cdto.isBaseOf(cdfrom, &offset) && offset == 0)
                 {
