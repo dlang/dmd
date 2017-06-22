7c5ed7c8f4a1173117a1e2a1f0f0540038976095
Basile Burg <basile.b@gmx.com>
2017-06-22 15:27:47 +0200

    deprecate hex string literals

--- test/fail_compilation/fail136.d
+++ test/fail_compilation/fail136.d
@@ -1,7 +1,8 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail136.d(10): Error: `string` has no effect in expression `"\xef\xbb\xbf"`
+fail_compilation/fail136.d(11): Deprecation: built-in hex string literals are deprecated, use `std.conv.hexString` instead
+fail_compilation/fail136.d(11): Error: `string` has no effect in expression `"\xef\xbb\xbf"`
 ---
 */
 
