4d1fe86f6ebe8352b9f8c0fcbce1c2ee632d7a8c
Johan Engelen <jbc.engelen@gmail.com>
2017-07-06 01:28:58 +0200

    Add extra testing of closure allocation when returning local non-static struct.

--- test/compilable/test17590.d
+++ test/compilable/test17590.d
@@ -1,5 +1,6 @@
 // REQUIRED_ARGS: -o-
 
+// Test that returning a local _static_ struct does not lead to allocation of a closure.
 auto foo(int a, bool b) @nogc {
     static struct SInside {}
 
@@ -10,5 +11,18 @@ auto foo(int a, bool b) @nogc {
     return res;
 }
 
-
 void lazyfun(scope lazy int a) @nogc;
+
+
+// Test that returning a local _non-static_ struct does lead to allocation of a closure.
+static assert(!__traits(compiles, () @nogc => goo(1)));
+static assert(__traits(compiles, () => goo(1)));
+auto goo(T)(T a) {
+    struct SInside {}
+
+    SInside res;
+
+    lazyfun(a);
+
+    return res;
+}
