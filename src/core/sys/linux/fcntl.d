24d8d849d99e0589bbdba0e37a2856095bb95af7
Nemanja Boric <nemanja.boric@sociomantic.com>
2017-07-18 11:02:56 +0200

    Add ddoc for FALLOC_* consts

--- src/core/sys/linux/fcntl.d
+++ src/core/sys/linux/fcntl.d
@@ -7,13 +7,25 @@ extern(C):
 nothrow:
 
 // From linux/falloc.h
+/// fallocate(2) params
 enum {
+    /// Allocates and initializes to zero the disk space
+    /// within the specified range, but the file size
+    /// will not be modified.
     FALLOC_FL_KEEP_SIZE = 0x01,
+    /// Deallocates space (i.e. creates a hole)
     FALLOC_FL_PUNCH_HOLE = 0x02,
+    /// Newly allocated blocks will be marked as initialized.
     FALLOC_FL_NO_HIDE_STALE = 0x04,
+    /// Removes a byte range from a file, without leaving a hole
     FALLOC_FL_COLLAPSE_RANGE = 0x08,
+    /// Zeroes space in the specified byte range
     FALLOC_FL_ZERO_RANGE = 0x10,
+    /// Increases the file space by inserting a hole
+    /// without overwriting any existing data
     FALLOC_FL_INSERT_RANGE = 0x20,
+    /// Used to unshare shared blocks within
+    /// the file size without overwriting any existing data
     FALLOC_FL_UNSHARE_RANGE = 0x40
 }
 
