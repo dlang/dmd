cd11c48f4297750cff5ce1cf270a5e635d7bd692
Stefan Koch <uplink.coder@googlemail.com>
2017-07-21 14:36:25 +0200

    WIP complex structs and multi-dimensional arrays

--- test/compilable/ctfeTest.d
+++ test/compilable/ctfeTest.d
@@ -542,36 +542,23 @@ float fmaddf(float a, float b, float c)
 static assert(fmaddf(0x1.acccccp+2f, 0x1.166666p+3f, 0x1.4cccccp+0f) == 0x1.168f5cp+4f);
 static assert(fmaddf(0x1.acccccp+2f, 0x1.166666p+3f, -0x1.4cccccp+0f) == -0x1.47a8p-7f);
 
-struct S {
+struct SMinMax {
     long l;
     ulong u;
     long v;
 }
 
-ulong u()
+SMinMax makeSMin()
 {
-    return ulong(ushort.max | ulong(ushort.max) << 32);
-}
-
-S v()
-{
-  S s;
-  s.v = ulong(ushort.max | ulong(ushort.max) << 32);
-  return s;
-}
-
-
-S makeSMin()
-{
-  S s;
+  SMinMax s;
   s.l = long.min;
   s.u = ulong.min;
   return s;
 }
 
-S makeSMax()
+SMinMax makeSMax()
 {
-  S s;
+  SMinMax s;
   s.l = long.max;
   s.u = ulong.max;
   return s;
