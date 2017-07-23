4398853f6d627e079a3b6d37347e988961e9a391
Iain Buclaw <ibuclaw@gdcproject.org>
2017-07-23 16:21:55 +0200

    Bump library version to 2.071.2, build druntime in CI test

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 set -exo pipefail
 
-VERSION=v2.068.2
+VERSION=v2.071.2
 N=2
 
 ## build dmd.
@@ -10,9 +10,9 @@ make -j$N -C src -f posix.mak dmd HOST_CC="$CXX"
 make -j$N -C src -f posix.mak dmd.conf
 
 ## build druntime and phobos.
-#git clone --depth=1 --branch=$VERSION https://github.com/dlang/druntime.git ../druntime
+git clone --depth=1 --branch=$VERSION https://github.com/dlang/druntime.git ../druntime
 #git clone --depth=1 --branch=$VERSION https://github.com/dlang/phobos.git ../phobos
-#make -j$N -C ../druntime -f posix.mak DMD=../dmd/src/dmd
+make -j$N -C ../druntime -f posix.mak DMD=../dmd/src/dmd
 #make -j$N -C ../phobos -f posix.mak DMD=../dmd/src/dmd
 
 ## run unittest and testsuite.
