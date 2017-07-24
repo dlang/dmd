973f3a29a66e15e27e196604b2594bf3c0d4bf44
Martin Nowak <code@dawg.eu>
2017-07-25 00:15:49 +0200

    switch to easier to read bar plot

--- benchmark/arrayops/plot.R
+++ benchmark/arrayops/plot.R
@@ -14,17 +14,22 @@ col.indices <- which(!colnames(new) %in% c("type", "op"))
 # relative values
 new[,col.indices] <- 100 * new[,col.indices] / old[,col.indices]
 
+# arrange type factor levels
+new$type <- factor(new$type, levels = c('byte', 'ubyte', 'short', 'ushort', 'int', 'uint', 'long', 'ulong', 'float', 'double'))
+
 latencies <- gather(new %>% select(-starts_with('throughput')), num_elems, latency, starts_with('latency')) %>%
     mutate(num_elems = factor(as.integer(sub("latency(\\d+)", "\\1", num_elems))))
 throughputs <- gather(new %>% select(-starts_with('latency')), array_size, throughput, starts_with('throughput')) %>%
     mutate(array_size = factor(as.integer(sub("throughput(\\d+)KB", "\\1", array_size))))
 
-img <- qplot(num_elems, latency, group=type, data=latencies, geom="line", color=type) +
+img <- ggplot(latencies, aes(x=num_elems, y=latency, fill=type)) +
+  geom_bar(position="dodge", stat="identity") +
   facet_grid(op ~ ., scales="free_y") +
   labs(x="num elements", y="relative latency / %")
 ggsave('array_ops_latency.png', plot = img, width = 8, height = 40)
 
-img <- qplot(array_size, throughput, group=type, data=throughputs, geom="line", color=type) +
+img <- ggplot(throughputs, aes(x=array_size, y=throughput, fill=type)) +
+  geom_bar(position="dodge", stat="identity") +
   facet_grid(op ~ ., scales="free_y") +
   labs(x="array size / KB", y="relative throughput / %")
 ggsave('array_ops_throughput.png', plot = img, width = 8, height = 40)
