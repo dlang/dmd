22464ace249c80f2da02e0b8b8ba00c4f9683b26
Nemanja Boric <nemanja.boric@sociomantic.com>
2017-08-03 17:13:35 +0200

    Introduce Throwable.message
    This patch introduces `Throwable.message` method which returns
    `Throwable.msg`'s contents. In order to prevent name clashes with the
    code already defining `message` method inside their own subclasses of
    Throwable, `Throwable.message` is marked as `@__future`, providing
    "forward deprecation" message to users.

--- src/object.d
+++ src/object.d
@@ -1771,6 +1771,19 @@ class Throwable : Object
             }
         }
     }
+
+    /**
+     * Get the message describing the error.
+     * Base behavior is to return the `Throwable.msg` field.
+     * Override to return some other error message.
+     *
+     * Returns:
+     *  Error message
+     */
+    @__future const(char)[] message() const
+    {
+        return this.msg;
+    }
 }
 
 
