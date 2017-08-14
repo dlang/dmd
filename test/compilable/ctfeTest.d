0e8bbf4854ce3c83a8eb9193140b5ce6a2f636f3
Stefan Koch <uplink.coder@googlemail.com>
2017-08-14 17:17:00 +0200

    add oror test

--- test/compilable/ctfeTest.d
+++ test/compilable/ctfeTest.d
@@ -662,6 +662,27 @@ static assert(aaa2(1, 0, 1, 0) == [1, 0]);
 static assert(aaa2(1, 1, 1, 0) == [3, 1]);
 static assert(aaa2(1, 1, 1, 1) == [2, 1]);
 
+int[2] ooo2(bool b1, bool b2, bool b3, bool b4)
+{
+  int x1 = 0;
+  int x2 = 0;
+  int x3 = 0;
+  if (b1 || x1++ || b2 || x2++ || b3 || (++x3 && b4))
+  {
+    return [x1 + x2 + x3, 1];
+  }
+  else
+  {
+    return [x1 + x2 + x3, 0];
+  }
+}
+
+static assert(ooo2(1, 0, 1, 0) == [0, 1]);
+static assert(ooo2(0, 1, 1, 0) == [1, 1]);
+static assert(ooo2(0, 0, 0, 0) == [3, 0]);
+static assert(ooo2(0, 0, 1, 0) == [2, 1]);
+static assert(ooo2(0, 0, 0, 1) == [3, 1]);
+
 bool alwaysFalse(int v = -4)
 {
   bool b1 = v > int(1073741823);
