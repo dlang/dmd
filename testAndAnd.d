2cad2622d3cb1c09e72301f44362ff139f248e32
Stefan Koch <uplink.coder@googlemail.com>
2017-08-14 12:35:25 +0200

    trivial test change

--- testAndAnd.d
+++ testAndAnd.d
@@ -73,6 +73,5 @@ static assert(ooo2(1, 0, 1, 0) == [0, 1]);
 static assert(ooo2(0, 1, 1, 0) == [1, 1]);
 static assert(ooo2(0, 0, 0, 0) == [3, 0]);
 static assert(ooo2(0, 0, 1, 0) == [2, 1]);
-pragma(msg, ooo2(0, 0, 1, 0));
-static assert(ooo2(0, 0, 0, 1) == [3, 1]); // oh god ...
+static assert(ooo2(0, 0, 0, 1) == [3, 1]);
 
