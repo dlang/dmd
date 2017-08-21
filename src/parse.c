dlang/dmd

commit 4f703abb5677e491752d8cf137c4163b5accf977

Allow final switch to be parsed in D1

src/parse.c
@@ -2974,7 +2974,6 @@ Statement *Parser::parseStatement(int flags)
         case TOKconst:
         case TOKauto:
         case TOKextern:
-        case TOKfinal:
         case TOKinvariant:
 #if DMDV2
         case TOKimmutable:
@@ -3361,6 +3360,12 @@ Statement *Parser::parseStatement(int flags)
             s = new PragmaStatement(loc, ident, args, body);
             break;
         }
+
+        case TOKfinal:
+            nextToken();
+            if (token.value != TOKswitch)
+                error("found '%s' when expecting 'switch' following final", token.toChars());
+            // Implicit fallthrough on purpose
 
         case TOKswitch:
         {
@@ -5752,4 +5757,3 @@ void initPrecedence()
     precedence[TOKcomma] = PREC_expr;
     precedence[TOKdeclaration] = PREC_expr;
 }
-

commit d214a274fec96bd5ebb94fdada5e03e16050ec5e

[DMD1 REG] Parsing nested final class declaration
Minor regression introduced by 4f703abb5677e491752d8cf137c4163b5accf977

src/parse.c
@@ -3364,8 +3364,8 @@ Statement *Parser::parseStatement(int flags)
         case TOKfinal:
             nextToken();
             if (token.value != TOKswitch)
-                error("found '%s' when expecting 'switch' following final", token.toChars());
-            // Implicit fallthrough on purpose
+                goto Ldeclaration;
+            // else fallthrough
 
         case TOKswitch:
         {