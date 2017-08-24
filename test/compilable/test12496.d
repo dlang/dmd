0c27a934599a7dd242ad7d108f87a3cb28090abd
JinShil <slavo5150@yahoo.com>
2017-08-24 20:43:25 +0900

    Fix Issue 12496: __traits(parent, x) returns incorrect symbol

---
+++ test/compilable/test12496.d
@@ -0,0 +1,19 @@
+// https://issues.dlang.org/show_bug.cgi?id=12496
+
+final abstract class T1 
+{
+    final abstract class C(uint value)
+    {
+        static uint getValue() 
+        {
+            return value;
+        }
+    }
+
+    alias Child = C!2;
+}
+
+void main()
+{
+    static assert(__traits(isSame, __traits(parent, T1.Child), T1));
+}
