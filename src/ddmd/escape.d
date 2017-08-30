66d203585f3f12dc4e3b56dc1178bebbedc1f1ba
Walter Bright <walter@walterbright.com>
2017-08-29 22:49:38 -0700

    fix Issue 17790 - [scope] Escaping pointer possible through array of aggregates

--- src/ddmd/escape.d
+++ src/ddmd/escape.d
@@ -37,6 +37,7 @@ import ddmd.arraytypes;
  * Print error messages when these are detected.
  * Params:
  *      sc = used to determine current function and module
+ *      fdc = function being called, `null` if called indirectly
  *      par = identifier of function parameter
  *      arg = initializer for param
  *      gag = do not print error messages
@@ -212,6 +213,8 @@ bool checkAssignEscape(Scope* sc, Expression e, bool gag)
             va = (cast(VarExp)ie.e1).var.isVarDeclaration();
     }
 
+    if (log && va) printf("va: %s\n", va.toChars());
+
     // Try to infer 'scope' for va if in a function not marked @system
     bool inferScope = false;
     if (va && sc.func && sc.func.type && sc.func.type.ty == Tfunction)
@@ -245,10 +248,10 @@ bool checkAssignEscape(Scope* sc, Expression e, bool gag)
 
             // If va's lifetime encloses v's, then error
             if (va &&
-                (va.enclosesLifetimeOf(v) && !(v.storage_class & STCparameter) ||
+                (va.enclosesLifetimeOf(v) && !(v.storage_class & (STCparameter | STCtemp)) ||
                  // va is class reference
                  ae.e1.op == TOKdotvar && va.type.toBasetype().ty == Tclass && (va.enclosesLifetimeOf(v) || !va.isScope) ||
-                 va.storage_class & STCref) &&
+                 va.storage_class & STCref && !(v.storage_class & STCtemp)) &&
                 sc.func.setUnsafe())
             {
                 if (!gag)
@@ -503,7 +506,8 @@ bool checkReturnEscapeRef(Scope* sc, Expression e, bool gag)
 
 private bool checkReturnEscapeImpl(Scope* sc, Expression e, bool refs, bool gag)
 {
-    //printf("[%s] checkReturnEscapeImpl, e: `%s`\n", e.loc.toChars(), e.toChars());
+    enum log = false;
+    if (log) printf("[%s] checkReturnEscapeImpl, refs: %d e: `%s`\n", e.loc.toChars(), refs, e.toChars());
     EscapeByResults er;
 
     if (refs)
@@ -517,7 +521,7 @@ private bool checkReturnEscapeImpl(Scope* sc, Expression e, bool refs, bool gag)
     bool result = false;
     foreach (VarDeclaration v; er.byvalue)
     {
-        //printf("byvalue `%s`\n", v.toChars());
+        if (log) printf("byvalue `%s`\n", v.toChars());
         if (v.isDataseg())
             continue;
 
@@ -579,7 +583,7 @@ private bool checkReturnEscapeImpl(Scope* sc, Expression e, bool refs, bool gag)
 
     foreach (VarDeclaration v; er.byref)
     {
-        //printf("byref `%s`\n", v.toChars());
+        if (log) printf("byref `%s`\n", v.toChars());
 
         void escapingRef(VarDeclaration v)
         {
@@ -665,7 +669,7 @@ private bool checkReturnEscapeImpl(Scope* sc, Expression e, bool refs, bool gag)
 
     foreach (Expression ee; er.byexp)
     {
-        //printf("byexp %s\n", ee.toChars());
+        if (log) printf("byexp %s\n", ee.toChars());
         if (!gag)
             error(ee.loc, "escaping reference to stack allocated value returned by `%s`", ee.toChars());
         result = true;
@@ -886,6 +890,14 @@ private void escapeByValue(Expression e, EscapeByResults* er)
             }
             else
                 e.e1.accept(this);
+        }
+
+        override void visit(IndexExp e)
+        {
+            if (e.type.hasPointers())
+            {
+                e.e1.accept(this);
+            }
         }
 
         override void visit(BinExp e)
