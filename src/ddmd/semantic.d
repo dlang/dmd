465571c4a9f79ea8cbf1d5b35860b127725f08ac
Walter Bright <walter@walterbright.com>
2017-09-11 17:46:49 -0700

    add Ddoc header comments

--- src/ddmd/semantic.d
+++ src/ddmd/semantic.d
@@ -1,4 +1,16 @@
+/**
+ * Compiler implementation of the
+ * $(LINK2 http://www.dlang.org, D programming language).
+ *
+ * Copyright:   Copyright (c) 1999-2017 by Digital Mars, All Rights Reserved
+ * Authors:     $(LINK2 http://www.digitalmars.com, Walter Bright)
+ * License:     $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+ * Source:      $(LINK2 https://github.com/dlang/dmd/blob/master/src/ddmd/semantic.d, _semantic.d)
+ */
+
 module ddmd.semantic;
+
+// Online documentation: https://dlang.org/phobos/ddmd_semantic.html
 
 import ddmd.arraytypes;
 import ddmd.dsymbol;
