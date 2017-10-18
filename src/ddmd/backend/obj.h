5e47656353884dc9f79a9153e780c1c478f61b1d
Walter Bright <walter@walterbright.com>
2017-10-18 16:42:48 -0700

    backend/obj.d: sync with DMC++

--- src/ddmd/backend/obj.h
+++ src/ddmd/backend/obj.h
@@ -5,7 +5,7 @@
  * Copyright:   Copyright (c) 2012-2017 by Digital Mars, All Rights Reserved
  * Authors:     $(LINK2 http://www.digitalmars.com, Walter Bright)
  * License:     $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
- * Source:      $(LINK2 https://github.com/dlang/dmd/blob/master/src/ddmd/backend/obj.h, backend/obj.h)
+ * Source:      $(LINK2 https://github.com/dlang/dmd/blob/master/src/ddmd/backend/obj.d, backend/obj.d)
  */
 
 /* Interface to object file format
@@ -17,11 +17,108 @@
 
 struct seg_data;
 
-#if MARS && TARGET_WINDOS
+#if MARS && TARGET_WINDOS && !HTOD
+#define OMFandMSCOFF 1
 #define VIRTUAL virtual
 #else
+
+#if TARGET_WINDOS
+#define OMF 1
+#elif (TARGET_LINUX || TARGET_FREEBSD || TARGET_OPENBSD || TARGET_SOLARIS)
+#define ELF 1
+#elif TARGET_OSX
+#define MACH 1
+#endif
+
 #define VIRTUAL static
 #endif
+
+
+#if OMF
+    class Obj
+    {
+      public:
+        static Obj *init(Outbuffer *, const char *filename, const char *csegname);
+        static void initfile(const char *filename, const char *csegname, const char *modname);
+        static void termfile();
+        static void term(const char *objfilename);
+        static size_t mangle(Symbol *s,char *dest);
+        static void _import(elem *e);
+        static void linnum(Srcpos srcpos, int seg, targ_size_t offset);
+        static int codeseg(char *name,int suffix);
+        static void dosseg(void);
+        static void startaddress(Symbol *);
+        static bool includelib(const char *);
+        static bool allowZeroSize();
+        static void exestr(const char *p);
+        static void user(const char *p);
+        static void compiler();
+        static void wkext(Symbol *,Symbol *);
+        static void lzext(Symbol *,Symbol *);
+        static void _alias(const char *n1,const char *n2);
+        static void theadr(const char *modname);
+        static void segment_group(targ_size_t codesize, targ_size_t datasize, targ_size_t cdatasize, targ_size_t udatasize);
+        static void staticctor(Symbol *s,int dtor,int seg);
+        static void staticdtor(Symbol *s);
+        static void setModuleCtorDtor(Symbol *s, bool isCtor);
+        static void ehtables(Symbol *sfunc,targ_size_t size,Symbol *ehsym);
+        static void ehsections();
+        static void moduleinfo(Symbol *scc);
+        int  comdat(Symbol *);
+        int  comdatsize(Symbol *, targ_size_t symsize);
+        int readonly_comdat(Symbol *s);
+        static void setcodeseg(int seg);
+        static seg_data *tlsseg();
+        static seg_data *tlsseg_bss();
+        static seg_data *tlsseg_data();
+        static int  fardata(char *name, targ_size_t size, targ_size_t *poffset);
+        static void export_symbol(Symbol *s, unsigned argsize);
+        static void pubdef(int seg, Symbol *s, targ_size_t offset);
+        static void pubdefsize(int seg, Symbol *s, targ_size_t offset, targ_size_t symsize);
+        static int external_def(const char *);
+        static int data_start(Symbol *sdata, targ_size_t datasize, int seg);
+        static int external(Symbol *);
+        static int common_block(Symbol *s, targ_size_t size, targ_size_t count);
+        static int common_block(Symbol *s, int flag, targ_size_t size, targ_size_t count);
+        static void lidata(int seg, targ_size_t offset, targ_size_t count);
+        static void write_zeros(seg_data *pseg, targ_size_t count);
+        static void write_byte(seg_data *pseg, unsigned byte);
+        static void write_bytes(seg_data *pseg, unsigned nbytes, void *p);
+        static void _byte(int seg, targ_size_t offset, unsigned byte);
+        static unsigned bytes(int seg, targ_size_t offset, unsigned nbytes, void *p);
+        static void ledata(int seg, targ_size_t offset, targ_size_t data, unsigned lcfd, unsigned idx1, unsigned idx2);
+        static void write_long(int seg, targ_size_t offset, unsigned data, unsigned lcfd, unsigned idx1, unsigned idx2);
+        static void reftodatseg(int seg, targ_size_t offset, targ_size_t val, unsigned targetdatum, int flags);
+        static void reftofarseg(int seg, targ_size_t offset, targ_size_t val, int farseg, int flags);
+        static void reftocodeseg(int seg, targ_size_t offset, targ_size_t val);
+        static int reftoident(int seg, targ_size_t offset, Symbol *s, targ_size_t val, int flags);
+        static void far16thunk(Symbol *s);
+        static void fltused();
+        static int data_readonly(char *p, int len, int *pseg);
+        static int data_readonly(char *p, int len);
+        static int string_literal_segment(unsigned sz);
+        static symbol *sym_cdata(tym_t, char *, int);
+        static void func_start(Symbol *sfunc);
+        static void func_term(Symbol *sfunc);
+        static void write_pointerRef(Symbol* s, unsigned off);
+        static int jmpTableSegment(Symbol* s);
+
+        static symbol *tlv_bootstrap();
+
+        static void gotref(symbol *s);
+
+#if TARGET_LINUX || TARGET_OSX || TARGET_FREEBSD || TARGET_OPENBSD || TARGET_SOLARIS
+        static unsigned addstr(Outbuffer *strtab, const char *);
+        static symbol *getGOTsym();
+        static void refGOTsym();
+#endif
+
+#if TARGET_WINDOS
+        static int seg_debugT();           // where the symbolic debug type data goes
+#endif
+    };
+
+#else
 
 class Obj
 {
@@ -225,8 +322,15 @@ class MsCoffObj : public Obj
     VIRTUAL symbol *tlv_bootstrap();
 };
 
-#undef VIRTUAL
+#endif
 
 extern Obj *objmod;
+
+#undef OMF
+#undef OMFandMSCOFF
+#undef ELF
+#undef MACH
+
+#undef VIRTUAL
 
 #endif
