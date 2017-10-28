37d5be48bdc403fc52f7746a0d02ef25df6680ce
Basile Burg <basile.b@gmx.com>
2017-10-28 11:38:07 +0200

    warn when returning from functions

--- test/fail_compilation/warn17933.d
+++ test/fail_compilation/warn17933.d
@@ -5,6 +5,7 @@ TEST_OUTPUT:
 ---
 fail_compilation/warn17933.d(14): Warning: float = double is performing truncating conversion
 fail_compilation/warn17933.d(15): Warning: float += double is performing truncating conversion
+fail_compilation/warn17933.d(22): Warning: return double from float function is performing truncating conversion
 ---
 */
 
@@ -14,5 +15,10 @@ void test17933()
     double d;
     f = d;
     f += 0.1;
+}
+
+float testReturn(float f)
+{
+    return f * 0.5;
 }
 
