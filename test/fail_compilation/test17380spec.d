77691c47442b173762ee5f02c82c566afdee57ba
Walter Bright <WalterBright@users.noreply.github.com>
2017-11-19 20:37:53 -0800

    another typo

--- test/fail_compilation/test17380spec.d
+++ test/fail_compilation/test17380spec.d
@@ -1,7 +1,7 @@
 /* REQUIRED_ARGS: -verrors=spec
 TEST_OUTPUT:
 ---
-(spec:1) fail_compilation/test17380spec.d(14): Error: cannot resolve identifier `ThisTypeDoesNotExistAndCrasheTheCompiler`
+(spec:1) fail_compilation/test17380spec.d(14): Error: cannot resolve identifier `ThisTypeDoesNotExistAndCrashesTheCompiler`
 (spec:1) fail_compilation/test17380spec.d(14): Error: no property 'ThisTypeDoesNotExistAndCrashesTheCompiler' for type 'Uint128'
 fail_compilation/test17380spec.d(14): Error: undefined identifier `ThisTypeDoesNotExistAndCrashesTheCompiler`
 ---
