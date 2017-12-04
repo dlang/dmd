95f1ac96f232ad2263a520196635e77341d99285
Andrei Alexandrescu <andrei@erdani.com>
2017-12-03 20:09:22 -0500

    Use LINK macro

--- src/ddmd/glue.d
+++ src/ddmd/glue.d
@@ -6,8 +6,8 @@
  * Authors:     $(LINK2 http://www.digitalmars.com, Walter Bright)
  * License:     $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
  * Source:      $(LINK2 https://github.com/dlang/dmd/blob/master/src/glue.d, _glue.d)
- * Documentation: https://dlang.org/phobos/ddmd_glue.html
- * Coverage:    https://codecov.io/gh/dlang/dmd/src/master/src/ddmd/glue.d
+ * Documentation: $(LINK https://dlang.org/phobos/ddmd_glue.html)
+ * Coverage:    $(LINK https://codecov.io/gh/dlang/dmd/src/master/src/ddmd/glue.d)
  */
 
 module ddmd.glue;
