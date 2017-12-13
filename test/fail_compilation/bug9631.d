27b272c000f4b5a1de637adb70512a5c001c8555
Nick Treleaven <n@trelsoft.com>
2017-12-13 09:44:12 +0000

    Issue 9631 - Error message not using fully qualified name (part 3)
    Disambiguate cast error messages.

--- test/fail_compilation/bug9631.d
+++ test/fail_compilation/bug9631.d
@@ -40,3 +40,21 @@ void equal()
     auto y = tem!().S();
     bool b = x == y;
 }
+
+/*
+TEST_OUTPUT:
+---
+fail_compilation/bug9631.d(55): Error: cannot cast expression `x` of type `bug9631.S` to `bug9631.tem!().S` because of different sizes
+fail_compilation/bug9631.d(58): Error: cannot cast expression `ta` of type `bug9631.tem!().S[1]` to `bug9631.S[1]` because of different sizes
+fail_compilation/bug9631.d(59): Error: cannot cast expression `sa` of type `S[1]` to `S[]` since sizes don't line up
+---
+*/
+void test3()
+{
+    S x;
+    auto y = cast(tem!().S)x;
+
+    tem!().S[1] ta;
+    S[1] sa = cast(S[1])ta;
+    auto t2 = cast(tem!().S[])sa;
+}
