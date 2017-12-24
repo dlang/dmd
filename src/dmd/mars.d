43d1a82db43ee466e465f92ccef314a4d2c4139e
Sebastian Wilzbach <seb@wilzba.ch>
2017-12-24 14:43:19 +0100

    Start to generate the CLI options dynamically

--- src/dmd/mars.d
+++ src/dmd/mars.d
@@ -74,29 +74,13 @@ private void logo()
 /**
  * Print DMD's usage message on stdout
  */
-private  void usage()
+private void usage()
 {
-    static if (TARGET_LINUX)
-    {
-        const(char)* fpic = "\n  -fPIC            generate position independent code";
-    }
-    else
-    {
-        const(char)* fpic = "";
-    }
-    static if (TARGET_WINDOS)
-    {
-        const(char)* m32mscoff = "\n  -m32mscoff       generate 32 bit code and write MS-COFF object files";
-        const(char)* mscrtlib  = "\n  -mscrtlib=<name> MS C runtime library to reference from main/WinMain/DllMain";
-    }
-    else
-    {
-        const(char)* m32mscoff = "";
-        const(char)* mscrtlib  = "";
-    }
+    import dmd.cli : CLIUsage;
     logo();
+    auto help = CLIUsage.usage;
     printf("
-Documentation: http://dlang.org/
+Documentation: https://dlang.org/
 Config file: %s
 Usage:
   dmd [<option>...] <file>...
@@ -108,84 +92,7 @@ Where:
 
 <option>:
   @<cmdfile>       read arguments from cmdfile
-  -allinst         generate code for all template instantiations
-  -betterC         omit generating some runtime information and helper functions
-  -boundscheck=[on|safeonly|off]   bounds checks on, in @safe only, or off
-  -c               do not link
-  -color           turn colored console output on
-  -color=[on|off]  force colored console output on or off
-  -conf=<filename> use config file at filename
-  -cov             do code coverage analysis
-  -cov=<nnn>       require at least nnn%% code coverage
-  -D               generate documentation
-  -Dd<directory>   write documentation file to directory
-  -Df<filename>    write documentation file to filename
-  -d               silently allow deprecated features
-  -dw              show use of deprecated features as warnings (default)
-  -de              show use of deprecated features as errors (halt compilation)
-  -debug           compile in debug code
-  -debug=<level>   compile in debug code <= level
-  -debug=<ident>   compile in debug code identified by ident
-  -debuglib=<name> set symbolic debug library to name
-  -defaultlib=<name>
-                   set default library to name
-  -deps            print module dependencies (imports/file/version/debug/lib)
-  -deps=<filename> write module dependencies to filename (only imports)" ~
-  "%s" /* placeholder for fpic */ ~ "
-  -dip25           implement http://wiki.dlang.org/DIP25
-  -dip1000         implement https://github.com/dlang/DIPs/blob/master/DIPs/DIP1000.md
-  -dip1008         implement https://github.com/dlang/DIPs/blob/master/DIPs/DIP1008.md
-  -g               add symbolic debug info
-  -gf              emit debug info for all referenced types
-  -gs              always emit stack frame
-  -gx              add stack stomp code
-  -H               generate 'header' file
-  -Hd=<directory>  write 'header' file to directory
-  -Hf=<filename>   write 'header' file to filename
-  --help           print help and exit
-  -I=<directory>   look for imports also in directory
-  -ignore          ignore unsupported pragmas
-  -inline          do function inlining
-  -J=<directory>   look for string imports also in directory
-  -L=<linkerflag>  pass linkerflag to link
-  -lib             generate library rather than object files
-  -m32             generate 32 bit code" ~
-  "%s" /* placeholder for m32mscoff */ ~ "
-  -m64             generate 64 bit code
-  -main            add default main() (e.g. for unittesting)
-  -man             open web browser on manual page
-  -map             generate linker .map file
-  -mcpu=<id>       generate instructions for architecture identified by 'id'
-  -mcpu=?          list all architecture options " ~
-  "%s" /* placeholder for mscrtlib */ ~ "
-  -mv=<package.module>=<filespec>  use <filespec> as source file for <package.module>
-  -noboundscheck   no array bounds checking (deprecated, use -boundscheck=off)
-  -O               optimize
-  -o-              do not write object file
-  -od=<directory>  write object & library files to directory
-  -of=<filename>   name output file to filename
-  -op              preserve source path for output files
-  -profile         profile runtime performance of generated code
-  -profile=gc      profile runtime allocations
-  -release         compile release version
-  -shared          generate shared library (DLL)
-  -transition=<id> help with language change identified by 'id'
-  -transition=?    list all language changes
-  -unittest        compile in unit tests
-  -v               verbose
-  -vcolumns        print character (column) numbers in diagnostics
-  -verrors=<num>   limit the number of error messages (0 means unlimited)
-  -verrors=spec    show errors from speculative compiles such as __traits(compiles,...)
-  -vgc             list all gc allocations including hidden ones
-  -vtls            list all variables going into thread local storage
-  --version        print compiler version and exit
-  -version=<level> compile in version code >= level
-  -version=<ident> compile in version code identified by ident
-  -w               warnings as errors (compilation will halt)
-  -wi              warnings as messages (compilation will continue)
-  -X               generate JSON file
-  -Xf=<filename>   write JSON file to filename
-", FileName.canonicalName(global.inifilename), fpic, m32mscoff, mscrtlib);
+%.*s", FileName.canonicalName(global.inifilename), help.length, &help[0]);
 }
 
 /// DMD-generated module `__entrypoint` where the C main resides
@@ -376,29 +283,17 @@ private int tryMain(size_t argc, const(char)** argv)
 
     if (global.params.mcpuUsage)
     {
-        printf("
-CPU architectures supported by -mcpu=id:
-  =?             list information on all architecture choices
-  =baseline      use default architecture as determined by target
-  =avx           use AVX 1 instructions
-  =avx2          use AVX 2 instructions
-  =native        use CPU architecture that this compiler is running on
-");
+        import dmd.cli : CLIUsage;
+        auto help = CLIUsage.mcpu;
+        printf("%.*s", help.length, &help[0]);
         return EXIT_SUCCESS;
     }
 
     if (global.params.transitionUsage)
     {
-         printf("
-Language changes listed by -transition=id:
-  =all           list information on all language changes
-  =checkimports  give deprecation messages about 10378 anomalies
-  =complex,14488 give deprecation messages about all usages of complex or imaginary types
-  =field,3449    list all non-mutable fields which occupy an object instance
-  =import,10378  revert to single phase name lookup
-  =intpromote,16997 fix integral promotions for unary + - ~ operators
-  =tls           list all variables going into thread local storage
-");
+        import dmd.cli : CLIUsage;
+        auto help = CLIUsage.transitionUsage;
+        printf("%.*s", help.length, &help[0]);
         return EXIT_SUCCESS;
     }
 
