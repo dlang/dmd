c1ea18221b1317c916608e3b96e2feeaf837a780
JinShil <slavo5150@yahoo.com>
2017-12-30 15:16:09 +0900

    Refactor: Move @disable check out of checkDeprecated

--- test/fail_compilation/test17908a.d
+++ test/fail_compilation/test17908a.d
@@ -1,7 +1,7 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/test17908a.d(10): Error: function test17908a.foo is not callable because it is annotated with @disable
+fail_compilation/test17908a.d(10): Error: function test17908a.foo is not callable because it is annotated with `@disable`
 ---
 */
 
