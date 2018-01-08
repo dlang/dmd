fd577de4650baba7b8aab015dd7747f7a133d18d
Sebastian Wilzbach <seb@wilzba.ch>
2018-01-08 04:23:48 +0100

    add STC enum

--- src/dmd/declaration.d
+++ src/dmd/declaration.d
@@ -186,62 +186,65 @@ extern (C++) void ObjectNotFound(Identifier id)
     fatal();
 }
 
-enum STCundefined           = 0L;
-enum STCstatic              = (1L << 0);
-enum STCextern              = (1L << 1);
-enum STCconst               = (1L << 2);
-enum STCfinal               = (1L << 3);
-enum STCabstract            = (1L << 4);
-enum STCparameter           = (1L << 5);
-enum STCfield               = (1L << 6);
-enum STCoverride            = (1L << 7);
-enum STCauto                = (1L << 8);
-enum STCsynchronized        = (1L << 9);
-enum STCdeprecated          = (1L << 10);
-enum STCin                  = (1L << 11);   // in parameter
-enum STCout                 = (1L << 12);   // out parameter
-enum STClazy                = (1L << 13);   // lazy parameter
-enum STCforeach             = (1L << 14);   // variable for foreach loop
-//                            (1L << 15)
-enum STCvariadic            = (1L << 16);   // the 'variadic' parameter in: T foo(T a, U b, V variadic...)
-enum STCctorinit            = (1L << 17);   // can only be set inside constructor
-enum STCtemplateparameter   = (1L << 18);   // template parameter
-enum STCscope               = (1L << 19);
-enum STCimmutable           = (1L << 20);
-enum STCref                 = (1L << 21);
-enum STCinit                = (1L << 22);   // has explicit initializer
-enum STCmanifest            = (1L << 23);   // manifest constant
-enum STCnodtor              = (1L << 24);   // don't run destructor
-enum STCnothrow             = (1L << 25);   // never throws exceptions
-enum STCpure                = (1L << 26);   // pure function
-enum STCtls                 = (1L << 27);   // thread local
-enum STCalias               = (1L << 28);   // alias parameter
-enum STCshared              = (1L << 29);   // accessible from multiple threads
-enum STCgshared             = (1L << 30);   // accessible from multiple threads, but not typed as "shared"
-enum STCwild                = (1L << 31);   // for "wild" type constructor
-enum STCproperty            = (1L << 32);
-enum STCsafe                = (1L << 33);
-enum STCtrusted             = (1L << 34);
-enum STCsystem              = (1L << 35);
-enum STCctfe                = (1L << 36);   // can be used in CTFE, even if it is static
-enum STCdisable             = (1L << 37);   // for functions that are not callable
-enum STCresult              = (1L << 38);   // for result variables passed to out contracts
-enum STCnodefaultctor       = (1L << 39);   // must be set inside constructor
-enum STCtemp                = (1L << 40);   // temporary variable
-enum STCrvalue              = (1L << 41);   // force rvalue for variables
-enum STCnogc                = (1L << 42);   // @nogc
-enum STCvolatile            = (1L << 43);   // destined for volatile in the back end
-enum STCreturn              = (1L << 44);   // 'return ref' or 'return scope' for function parameters
-enum STCautoref             = (1L << 45);   // Mark for the already deduced 'auto ref' parameter
-enum STCinference           = (1L << 46);   // do attribute inference
-enum STCexptemp             = (1L << 47);   // temporary variable that has lifetime restricted to an expression
-enum STCmaybescope          = (1L << 48);   // parameter might be 'scope'
-enum STCscopeinferred       = (1L << 49);   // 'scope' has been inferred and should not be part of mangling
-enum STCfuture              = (1L << 50);   // introducing new base class function
-enum STClocal               = (1L << 51);   // do not forward (see dmd.dsymbol.ForwardingScopeDsymbol).
+enum STC : long
+{
+    undefined_          = 0L,
+    static_             = (1L << 0),
+    extern_             = (1L << 1),
+    const_              = (1L << 2),
+    final_              = (1L << 3),
+    abstract_           = (1L << 4),
+    parameter           = (1L << 5),
+    field               = (1L << 6),
+    override_           = (1L << 7),
+    auto_               = (1L << 8),
+    synchronized_       = (1L << 9),
+    deprecated_         = (1L << 10),
+    in_                 = (1L << 11),   // in parameter
+    out_                = (1L << 12),   // out parameter
+    lazy_               = (1L << 13),   // lazy parameter
+    foreach_            = (1L << 14),   // variable for foreach loop
+                          //(1L << 15)
+    variadic            = (1L << 16),   // the 'variadic' parameter in: T foo(T a, U b, V variadic...)
+    ctorinit            = (1L << 17),   // can only be set inside constructor
+    templateparameter   = (1L << 18),   // template parameter
+    scope_              = (1L << 19),
+    immutable_          = (1L << 20),
+    ref_                = (1L << 21),
+    init                = (1L << 22),   // has explicit initializer
+    manifest            = (1L << 23),   // manifest constant
+    nodtor              = (1L << 24),   // don't run destructor
+    nothrow_            = (1L << 25),   // never throws exceptions
+    pure_               = (1L << 26),   // pure function
+    tls                 = (1L << 27),   // thread local
+    alias_              = (1L << 28),   // alias parameter
+    shared_             = (1L << 29),   // accessible from multiple threads
+    gshared             = (1L << 30),   // accessible from multiple threads, but not typed as "shared"
+    wild                = (1L << 31),   // for "wild" type constructor
+    property            = (1L << 32),
+    safe                = (1L << 33),
+    trusted             = (1L << 34),
+    system              = (1L << 35),
+    ctfe                = (1L << 36),   // can be used in CTFE, even if it is static
+    disable             = (1L << 37),   // for functions that are not callable
+    result              = (1L << 38),   // for result variables passed to out contracts
+    nodefaultctor       = (1L << 39),   // must be set inside constructor
+    temp                = (1L << 40),   // temporary variable
+    rvalue              = (1L << 41),   // force rvalue for variables
+    nogc                = (1L << 42),   // @nogc
+    volatile_           = (1L << 43),   // destined for volatile in the back end
+    return_             = (1L << 44),   // 'return ref' or 'return scope' for function parameters
+    autoref             = (1L << 45),   // Mark for the already deduced 'auto ref' parameter
+    inference           = (1L << 46),   // do attribute inference
+    exptemp             = (1L << 47),   // temporary variable that has lifetime restricted to an expression
+    maybescope          = (1L << 48),   // parameter might be 'scope'
+    scopeinferred       = (1L << 49),   // 'scope' has been inferred and should not be part of mangling
+    future              = (1L << 50),   // introducing new base class function
+    local               = (1L << 51),   // do not forward (see dmd.dsymbol.ForwardingScopeDsymbol).
 
-enum STC_TYPECTOR = (STCconst | STCimmutable | STCshared | STCwild);
-enum STC_FUNCATTR = (STCref | STCnothrow | STCnogc | STCpure | STCproperty | STCsafe | STCtrusted | STCsystem);
+    TYPECTOR = (STC.const_ | STC.immutable_ | STC.shared_ | STC.wild),
+    FUNCATTR = (STC.ref_ | STC.nothrow_ | STC.nogc | STC.pure_ | STC.property | STC.safe | STC.trusted | STC.system),
+}
 
 extern (C++) __gshared const(StorageClass) STCStorageClass =
     (STCauto | STCscope | STCstatic | STCextern | STCconst | STCfinal | STCabstract | STCsynchronized |
