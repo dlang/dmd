b14d8f83d8194867f24aa823aa3a1ff9891f015a
Nick Treleaven <n@trelsoft.com>
2018-01-08 12:14:20 +0000

    Fix Issue 16165 - Show count of arguments vs parameters on mismatch

--- src/dmd/mtype.d
+++ src/dmd/mtype.d
@@ -4514,6 +4514,13 @@ extern (C++) final class TypeFunction : TypeNext
         return buf.extractString();
     }
 
+    private const(char)* getMatchError(A...)(const(char)* format, A args)
+    {
+        OutBuffer buf;
+        buf.printf(format, args);
+        return buf.extractString();
+    }
+
     /********************************
      * 'args' are being matched to function 'this'
      * Determine match level.
@@ -4566,7 +4573,10 @@ extern (C++) final class TypeFunction : TypeNext
         else if (nargs > nparams)
         {
             if (varargs == 0)
+            {
+                if (pMessage) *pMessage = getMatchError("expected %d argument(s), not %d", nparams, nargs);
                 goto Nomatch;
+            }
             // too many args; no match
             match = MATCH.convert; // match ... with a "conversion" match level
         }
@@ -4733,7 +4743,11 @@ extern (C++) final class TypeFunction : TypeNext
                         tsa = cast(TypeSArray)tb;
                         sz = tsa.dim.toInteger();
                         if (sz != nargs - u)
+                        {
+                            if (pMessage) *pMessage = getMatchError("expected %d variadic argument(s), not %d",
+                                sz, nargs - u);
                             goto Nomatch;
+                        }
                         goto case Tarray;
                     case Tarray:
                         {
@@ -4784,6 +4798,8 @@ extern (C++) final class TypeFunction : TypeNext
                 }
                 if (pMessage && u < nargs)
                     *pMessage = getParamError((*args)[u], p);
+                else if (pMessage)
+                    *pMessage = getMatchError("expected %d argument(s), not %d", nparams, nargs);
                 goto Nomatch;
             }
             if (m < match)
--- test/fail_compilation/fail332.d
+++ test/fail_compilation/fail332.d
@@ -1,15 +1,56 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail332.d(14): Error: function `fail332.foo(int _param_0, ...)` is not callable using argument types `()`
+fail_compilation\fail332.d(32): Error: function `fail332.foo(int _param_0, ...)` is not callable using argument types `()`
+fail_compilation\fail332.d(32):        expected 1 argument(s), not 0
+fail_compilation\fail332.d(33): Error: function `fail332.foo(int _param_0, ...)` is not callable using argument types `(typeof(null))`
+fail_compilation\fail332.d(33):        cannot pass argument `null` of type `typeof(null)` to parameter `int _param_0`
+fail_compilation\fail332.d(35): Error: function `fail332.baz(int[] _param_0...)` is not callable using argument types `(string)`
+fail_compilation\fail332.d(35):        cannot pass argument `""` of type `string` to parameter `int[] _param_0...`
+fail_compilation\fail332.d(36): Error: function `fail332.baz(int[] _param_0...)` is not callable using argument types `(int, typeof(null))`
+fail_compilation\fail332.d(36):        cannot pass argument `null` of type `typeof(null)` to parameter `int[] _param_0...`
 ---
 */
 
 import core.vararg;
 
 void foo(int, ...) {}
+void baz(int[]...) {}
 
-void bar()
+void test()
 {
     foo();
+    foo(null);
+    
+    baz("");
+    baz(3, null);
+}
+
+/*
+TEST_OUTPUT:
+---
+fail_compilation\fail332.d(50): Error: function `fail332.bar(Object, int[2]...)` is not callable using argument types `()`
+fail_compilation\fail332.d(50):        expected 2 argument(s), not 0
+fail_compilation\fail332.d(51): Error: function `fail332.bar(Object, int[2]...)` is not callable using argument types `(int)`
+fail_compilation\fail332.d(51):        cannot pass argument `4` of type `int` to parameter `Object`
+fail_compilation\fail332.d(52): Error: function `fail332.bar(Object, int[2]...)` is not callable using argument types `(typeof(null))`
+fail_compilation\fail332.d(52):        expected 2 variadic argument(s), not 0
+fail_compilation\fail332.d(53): Error: function `fail332.bar(Object, int[2]...)` is not callable using argument types `(typeof(null), int)`
+fail_compilation\fail332.d(53):        expected 2 variadic argument(s), not 1
+fail_compilation\fail332.d(54): Error: function `fail332.bar(Object, int[2]...)` is not callable using argument types `(typeof(null), int, string)`
+fail_compilation\fail332.d(54):        cannot pass argument `""` of type `string` to parameter `int[2]...`
+fail_compilation\fail332.d(55): Error: function `fail332.bar(Object, int[2]...)` is not callable using argument types `(typeof(null), int, int, int)`
+fail_compilation\fail332.d(55):        expected 2 variadic argument(s), not 3
+---
+*/
+void bar(Object, int[2]...);
+
+void test2()
+{
+    bar();
+    bar(4);
+    bar(null);
+    bar(null, 2);
+    bar(null, 2, "");
+    bar(null, 2,3,4);
 }
