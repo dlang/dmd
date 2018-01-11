5c2a6320776ce93e60c8fd3c9ae7d00b170644fa
JinShil <slavo5150@yahoo.com>
2018-01-11 19:07:56 +0900

    highlight canthrow.d error messages

--- test/fail_compilation/fail4082.d
+++ test/fail_compilation/fail4082.d
@@ -1,7 +1,7 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail4082.d(14): Error: destructor `fail4082.Foo.~this` is not nothrow
+fail_compilation/fail4082.d(14): Error: destructor `fail4082.Foo.~this` is not `nothrow`
 fail_compilation/fail4082.d(12): Error: nothrow function `fail4082.test1` may throw
 ---
 */
@@ -21,7 +21,7 @@ NEXT:
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail4082.d(32): Error: destructor `fail4082.Bar.~this` is not nothrow
+fail_compilation/fail4082.d(32): Error: destructor `fail4082.Bar.~this` is not `nothrow`
 fail_compilation/fail4082.d(32): Error: nothrow function `fail4082.test2` may throw
 ---
 */
--- test/fail_compilation/fail7848.d
+++ test/fail_compilation/fail7848.d
@@ -6,22 +6,22 @@ TEST_OUTPUT:
 fail_compilation/fail7848.d(35): Error: pure function 'fail7848.C.__unittest_fail_compilation_fail7848_d_33_0' cannot call impure function 'fail7848.func'
 fail_compilation/fail7848.d(35): Error: @safe function 'fail7848.C.__unittest_fail_compilation_fail7848_d_33_0' cannot call @system function 'fail7848.func'
 fail_compilation/fail7848.d(35): Error: @nogc function 'fail7848.C.__unittest_fail_compilation_fail7848_d_33_0' cannot call non-@nogc function 'fail7848.func'
-fail_compilation/fail7848.d(35): Error: function `fail7848.func` is not nothrow
+fail_compilation/fail7848.d(35): Error: function `fail7848.func` is not `nothrow`
 fail_compilation/fail7848.d(33): Error: nothrow function `fail7848.C.__unittest_fail_compilation_fail7848_d_33_0` may throw
 fail_compilation/fail7848.d(40): Error: pure function 'fail7848.C.__invariant1' cannot call impure function 'fail7848.func'
 fail_compilation/fail7848.d(40): Error: @safe function 'fail7848.C.__invariant1' cannot call @system function 'fail7848.func'
 fail_compilation/fail7848.d(40): Error: @nogc function 'fail7848.C.__invariant1' cannot call non-@nogc function 'fail7848.func'
-fail_compilation/fail7848.d(40): Error: function `fail7848.func` is not nothrow
+fail_compilation/fail7848.d(40): Error: function `fail7848.func` is not `nothrow`
 fail_compilation/fail7848.d(38): Error: nothrow function `fail7848.C.__invariant1` may throw
 fail_compilation/fail7848.d(45): Error: pure allocator 'fail7848.C.new' cannot call impure function 'fail7848.func'
 fail_compilation/fail7848.d(45): Error: @safe allocator 'fail7848.C.new' cannot call @system function 'fail7848.func'
 fail_compilation/fail7848.d(45): Error: @nogc allocator 'fail7848.C.new' cannot call non-@nogc function 'fail7848.func'
-fail_compilation/fail7848.d(45): Error: function `fail7848.func` is not nothrow
+fail_compilation/fail7848.d(45): Error: function `fail7848.func` is not `nothrow`
 fail_compilation/fail7848.d(43): Error: nothrow allocator `fail7848.C.new` may throw
 fail_compilation/fail7848.d(51): Error: pure deallocator 'fail7848.C.delete' cannot call impure function 'fail7848.func'
 fail_compilation/fail7848.d(51): Error: @safe deallocator 'fail7848.C.delete' cannot call @system function 'fail7848.func'
 fail_compilation/fail7848.d(51): Error: @nogc deallocator 'fail7848.C.delete' cannot call non-@nogc function 'fail7848.func'
-fail_compilation/fail7848.d(51): Error: function `fail7848.func` is not nothrow
+fail_compilation/fail7848.d(51): Error: function `fail7848.func` is not `nothrow`
 fail_compilation/fail7848.d(49): Error: nothrow deallocator `fail7848.C.delete` may throw
 ---
 */
