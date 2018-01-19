1a6e1eda22a17eb44c59ab58d4fbe84d5b7d3bca
Nicholas Wilson <thewilsonator@users.noreply.github.com>
2018-01-19 08:30:13 +0800

    Update test17899.d

--- test/runnable/test17899.d
+++ test/runnable/test17899.d
@@ -1,5 +1,8 @@
 module test17899;
 
+// Test that the ICE in 13259 does not ICE but produces correct code
+auto dg = delegate {}; 
+
 int setme = 0;
 void delegate() bar1 = (){ setme = 1;};
 
@@ -7,6 +10,7 @@ __gshared void delegate() bar2 = (){ setme = 2;};
 
 void main()
 {
+    dg();
     assert(setme == 0);
     bar1();
     assert(setme == 1);
