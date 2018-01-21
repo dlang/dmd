b75c8d64ae1a133b85288be7f41b24d621069d8e
Nicholas Wilson <thewilsonator@users.noreply.github.com>
2018-01-21 11:39:24 +0800

    Update to new Token enum

--- src/dmd/todt.d
+++ src/dmd/todt.d
@@ -544,7 +544,7 @@ extern (C++) void Expression_toDt(Expression e, DtBuilder dtb)
             }
             Symbol *s = toSymbol(e.fd);
             toObjFile(e.fd, false);
-            if (e.fd.tok == TOKdelegate)
+            if (e.fd.tok == TOK.delegate_)
                 dtb.size(0);
             dtb.xoff(s, 0);
         }
