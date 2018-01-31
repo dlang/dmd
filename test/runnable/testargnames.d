06255f95c30a1c898742b8aa3152d3a70be82313
Timothee Cour <timothee.cour2@gmail.com>
2018-01-31 00:11:36 -0800

    cleanup

--- test/runnable/testargnames.d
+++ test/runnable/testargnames.d
@@ -40,28 +40,29 @@ void fun_template(T)(T a, string expected, string name=__ARG_STRING__!a)
 
 void main()
 {
-  int a=42;
+  int a = 42;
 
-  check(log(1+a), `1 + a:43`);
+  // NOTE: __ARG_STRING__ will pretty-print the expression so passing log(1+a) or log(1 + a) won't make a difference.
+  check(log(1 + a), `1 + a:43`);
 
-  fun1(41+a, `41 + a`);
+  fun1(41 + a, `41 + a`);
 
-  string bar="bob";
-  fun2(41+a, "foo"~bar, 0.0, `"foo" ~ bar`);
+  string bar = "bob";
+  fun2(41 + a, "foo"~bar, 0.0, `"foo" ~ bar`);
 
-  (1+1).fun_UFCS("1 + 1");
+  (1 + 1).fun_UFCS("1 + 1");
 
-  fun_template(1+3, `1 + 3`);
+  fun_template(1 + 3, `1 + 3`);
 
-  fun1(a+a+a, `a + a + a`);
+  fun1(a + a + a, `a + a + a`);
 
   // Checks that no constant folding happens, cf D20180130T161632.
-  fun1(1+1+2, `1 + 1 + 2`);
+  fun1(1 + 1 + 2, `1 + 1 + 2`);
 
-  static const int x=44;
-  fun1(x+x+x, `x + x + x`);
+  static const int x = 44;
+  fun1(x + x + x, `x + x + x`);
 
   // TODO: we'd like to have `t + t` instead; the conversion happens in arrayExpressionSemantic before we can tell whether candidate matching function has argument stringification (cf callMatch)
-  enum t=44;
-  fun1(t+t, `44 + 44`);
+  enum t = 44;
+  fun1(t + t, `44 + 44`);
 }
