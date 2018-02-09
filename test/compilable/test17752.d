1af17a72b8629b087418c3dc909fd17f40e0a51c
Sebastian Wilzbach <seb@wilzba.ch>
2018-02-09 12:06:48 +0100

    Fix Issue 17752 - Switch skips over declaration issued for explicitly uninitialized variables (#7842)

---
+++ test/compilable/test17752.d
@@ -0,0 +1,17 @@
+// https://issues.dlang.org/show_bug.cgi?id=17752
+// REQUIRED_ARGS: -c -de
+/*
+TEST_OUTPUT:
+---
+---
+*/
+void main (string[] args)
+{
+    switch (args.length)
+    {
+        // initialization not done on purpose is allowed
+        int x = void;
+    default:
+        break;
+    }
+}
