3c8074bdf9d6c2a5ba5c7dedc9308690acfc6546
JinShil <slavo5150@yahoo.com>
2018-02-13 09:36:47 +0900

    Fix no_Throwable test so it doesn't import druntime's object.d

--- test/fail_compilation/no_Throwable.d
+++ test/fail_compilation/no_Throwable.d
@@ -1,8 +1,10 @@
-/* REQUIRED_ARGS: -c -I=fail_compilation/extra-files/no_Throwable/
+/* 
+DFLAGS:
+REQUIRED_ARGS: -c -I=fail_compilation/extra-files/no_Throwable/
 TEST_OUTPUT:
 ---
-fail_compilation/no_Throwable.d(11): Error: Cannot use `throw` statements because `object.Throwable` was not declared
-fail_compilation/no_Throwable.d(16): Error: Cannot use try-catch statements because `object.Throwable` was not declared
+fail_compilation/no_Throwable.d(13): Error: Cannot use `throw` statements because `object.Throwable` was not declared
+fail_compilation/no_Throwable.d(18): Error: Cannot use try-catch statements because `object.Throwable` was not declared
 ---
 */
 
