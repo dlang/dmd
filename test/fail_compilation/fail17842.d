940cfd79fa681809e0e983faa1576d2efebf0953
Walter Bright <walter@walterbright.com>
2018-03-19 00:52:04 -0700

    fix Issue 17784 - [scope][DIP1000] Confusing error message for escaping local via new-expression

--- test/fail_compilation/fail17842.d
+++ test/fail_compilation/fail17842.d
@@ -2,7 +2,7 @@
  * TEST_OUTPUT:
 ---
 fail_compilation/fail17842.d(14): Error: scope variable `p` assigned to non-scope `*q`
-fail_compilation/fail17842.d(23): Error: scope variable `obj` may not be returned
+fail_compilation/fail17842.d(23): Error: scope variable `obj` may not be copied into allocated memory
 ---
  */
 
