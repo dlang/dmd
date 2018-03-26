07777acca8882710e3a8ca0593ff68940200cd5a
Sebastian Wilzbach <seb@wilzba.ch>
2018-03-26 11:47:22 +0200

    Small bash cleanup

--- test/runnable/test17619.sh
+++ test/runnable/test17619.sh
@@ -5,8 +5,8 @@ if [ ${OS} != "linux" ]; then
     exit 0
 fi
 
-$DMD -m${MODEL} -I${EXTRA_FILES} -of${OUTPUT_BASE}${OBJ} -c ${EXTRA_FILES}${SEP}test17619.d || exit 1
+$DMD -m${MODEL} -I${EXTRA_FILES} -of${OUTPUT_BASE}${OBJ} -c ${EXTRA_FILES}${SEP}test17619.d
 # error out if there is an advance by 0 for a non.zero address
-objdump -Wl ${RESULTS_DIR}/runnable/test17619${OBJ} | grep "advance Address by 0 to 0x[1-9]" && exit 1
+! objdump -Wl ${OUTPUT_BASE}${OBJ} | grep "advance Address by 0 to 0x[1-9]"
 
 rm ${OUTPUT_BASE}${OBJ}
--- test/runnable/test18076.sh
+++ test/runnable/test18076.sh
@@ -3,5 +3,7 @@
 output_file=${OUTPUT_BASE}.log
 
 echo 'import std.stdio; void main() { writeln("Success"); }' | \
-	$DMD -m${MODEL} -run - > ${output_file} || exit 1
-grep -q '^Success$' ${output_file} || exit 1
+	$DMD -m${MODEL} -run - > ${output_file}
+grep -q '^Success$' ${output_file}
+
+rm "${output_file}"
