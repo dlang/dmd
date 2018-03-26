1c6cf3aa123e9373477b88af5c5654957287d78a
Rainer Schuetze <r.sagitario@gmx.de>
2018-03-26 10:37:17 +0200

    test18367: use proper library extension for Windows

--- test/compilable/test18367.sh
+++ test/compilable/test18367.sh
@@ -2,7 +2,7 @@
 
 
 # dmd should not segfault on -X with libraries, but no source files
-out=$(! "$DMD" -conf= -X foo.a 2>&1)
+out=$(! "$DMD" -conf= -X foo${LIBEXT} 2>&1)
 echo "$out" | grep 'Error: cannot determine JSON filename, use `-Xf=<file>` or provide a source file'
 
 out=$(! "$DMD" -conf= -Xi=compilerInfo 2>&1)
