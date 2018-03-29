28a1b7401e6822fc3c4ca75dd7a5b5f09b98875d
Walter Bright <WalterBright@users.noreply.github.com>
2018-03-28 19:24:38 -0700

    add link to associated bug report

--- test/fail_compilation/b17918.d
+++ test/fail_compilation/b17918.d
@@ -1,9 +1,9 @@
-/*
-TEST_OUTPUT:
+/* TEST_OUTPUT:
 ---
 fail_compilation/imports/b17918a.d(7): Error: undefined identifier `_listMap`
 ---
 */
+// https://issues.dlang.org/show_bug.cgi?id=17918
 import imports.b17918a;
 
 class Derived : Base
