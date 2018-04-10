47036d35838dc83076bbc994e54f2c0f9240ba18
Jonathan Marler <jonathan.j.marler@hp.com>
2018-04-10 08:34:59 -0600

    Isolate tests to one file using HAR

--- test/compilable/imports/art4769b.d
+++
@@ -1,9 +0,0 @@
-private import imports.art4769a;
-private import art4769;
-
-int main(char [][] args)
-{
-   Vector!(wchar) str;
-   return 0;
-}
-
--- test/compilable/imports/b3682.d
+++
@@ -1,5 +0,0 @@
-module imports.b3682;
-
-import a3682;
-alias Tuple!(int) tint;
-
