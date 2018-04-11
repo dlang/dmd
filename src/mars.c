4d6c279c756520b86e459780e73c90141ee88eb7
Stefan Koch <stefan.koch@sociomantic.com>
2018-04-11 15:15:58 +0200

    run sema3 for all modules when generating deps
    This makes sure we don't miss depdenecies introduces imports functions or unittests
    backport of https://github.com/dlang/dmd/pull/6748

--- src/mars.c
+++ src/mars.c
@@ -1531,6 +1531,20 @@ int main(int iargc, const char *argv[])
     {
         assert(global.params.moduleDepsFile != NULL);
 
+        // when generating depdencies we have to run sema3
+        // because otherwise we will miss some depdencies
+        // introdcued in function bodies
+        for (size_t i = 0; i < Module::amodules.dim; i++)
+        {
+            m = Module::amodules[i];
+            if (global.params.verbose)
+                printf("[deps] semantic(2/3) %s\n", m->toChars());
+            // not sure if it can happen that we havn't run sema2 ?
+            // better safe then sorry, it does not cost more then a few cycles
+            m->semantic2();
+            m->semantic3();
+        }
+
         File deps(global.params.moduleDepsFile);
         OutBuffer* ob = global.params.moduleDeps;
         deps.setbuffer((void*)ob->data, ob->offset);
