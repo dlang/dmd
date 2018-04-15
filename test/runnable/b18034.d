2a2bc9db1e39f91af0a9e7c394969956ea7dfa2c
Mike Franklin <JinShil@users.noreply.github.com>
2018-04-16 07:45:10 +0900

    Change formatting of b18034.d

--- test/runnable/b18034.d
+++ test/runnable/b18034.d
@@ -1,24 +1,28 @@
 // REQUIRED_ARGS: -O
 version (D_SIMD)
 {
-import core.simd;
+    import core.simd;
 
-void check(void16 a) {
-    foreach (x; (cast(ushort8)a).array) {
-	assert(x == 1);
+    void check(void16 a) 
+    {
+        foreach (x; (cast(ushort8)a).array) 
+        {
+	        assert(x == 1);
+        }
     }
-}
 
-void make(ushort x) {
-    ushort8 v = ushort8(x);
-    check(v);
-}
+    void make(ushort x) 
+    {
+        ushort8 v = ushort8(x);
+        check(v);
+    }
 
-void main() {	
-    make(1);
-}
+    void main() 
+    {	
+        make(1);
+    }
 }
 else
 {
-void main() { }
+    void main() { }
 }
