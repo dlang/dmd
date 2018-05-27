451d0d89c2e9fe60f6af45971d40224dd49aee18
Johan Engelen <jbc.engelen@gmail.com>
2018-05-27 12:26:21 +0200

    Also strip directory separator symbol at the end.

--- test/compilable/filefullpath_18911.d
+++ test/compilable/filefullpath_18911.d
@@ -5,4 +5,4 @@ import a18911;
 enum THIS_FILE = __FILE_FULL_PATH__;
 enum suffix_this = "filefullpath_18911.d";
 
-static assert(THIS_FILE[0..$-suffix_this.length] == A_FILE[0..$-suffix_a.length]);
+static assert(THIS_FILE[0..$-suffix_this.length-1] == A_FILE[0..$-suffix_a.length-1]);
