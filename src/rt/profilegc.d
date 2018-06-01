d676ffa836204dded541101b74dff8ae8131ad5d
JinShil <slavo5150@yahoo.com>
2018-06-02 07:53:33 +0900

    Fix Issue 18493 - [betterC] Can't use aggregated type with postblit

--- src/rt/profilegc.d
+++ src/rt/profilegc.d
@@ -113,7 +113,7 @@ shared static ~this()
         Entry entry;
 
         // qsort() comparator to sort by count field
-        extern (C) static int qsort_cmp(scope const void *r1, scope const void *r2)
+        extern (C) static int qsort_cmp(scope const void *r1, scope const void *r2) @nogc nothrow
         {
             auto result1 = cast(Result*)r1;
             auto result2 = cast(Result*)r2;
