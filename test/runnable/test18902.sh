a491c80064c6a2c7845fed613a4c77b2b6f1887b
Martin Nowak <code@dawg.eu>
2018-06-02 11:22:54 +0200

    fail test18902 when exit code is zero

--- test/runnable/test18902.sh
+++ test/runnable/test18902.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-$DMD -m${MODEL} -lib 18902.a || test $? = 1
+# dmd -lib should fail without input sources/object files
+if $DMD -m${MODEL} -lib 18902.a; then
+    exit 1
+else
+    [ $? -eq 1 ]
+fi
