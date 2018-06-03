356560560256fd26cad27f19d7ac16d8ddd51817
Walter Bright <walter@walterbright.com>
2018-06-02 22:28:07 -0700

    test for cgxmm line 684 assert failure

---
+++ test/compilable/cgxmm684.d
@@ -0,0 +1,29 @@
+// REQUIRED_ARGS: -fPIC -O -release -inline -m64 -betterC
+
+// produces assert failure cgxmm.c line 684
+
+import core.stdc.math;
+
+struct S
+{
+    double re, im;
+
+
+    static S sqrtcx(S* z)
+    {
+        S c;
+        real x,y,w,r;
+
+        {
+            x = fabs(z.re);
+            y = fabs(z.im);
+            if (z.re >= 0)
+            {
+                c.im = (z.im >= 0) ? w : -w;
+                c.re = z.im / (c.im + c.im);
+            }
+        }
+        return c;
+    }
+}
+
