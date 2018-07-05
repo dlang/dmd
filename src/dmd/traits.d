d3f65c73d521f0279c57fec4aec09d03f49b4028
RazvanN7 <razvan.nitu1305@gmail.com>
2018-07-05 17:42:36 +0300

    Fix Issue 19064 - [REG2.081] Vibe.d's InterfaceProxy no longer works

--- src/dmd/traits.d
+++ src/dmd/traits.d
@@ -1009,6 +1009,7 @@ extern (C++) Expression semanticTraits(TraitsExp e, Scope* sc)
                 ifd = sym.isInterfaceDeclaration();
             // If the symbol passed as a parameter is an
             // interface that inherits other interfaces
+            overloadApply(f, &dg);
             if (ifd && ifd.interfaces)
             {
                 // check the overloads of each inherited interface individually
@@ -1018,8 +1019,6 @@ extern (C++) Expression semanticTraits(TraitsExp e, Scope* sc)
                         overloadApply(fd, &dg);
                 }
             }
-            else
-                overloadApply(f, &dg);
 
             auto tup = new TupleExp(e.loc, exps);
             return tup.expressionSemantic(scx);
