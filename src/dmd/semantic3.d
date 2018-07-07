16caf2489e2d13ad2c8d29099ca426cf44511457
Iain Buclaw <ibuclaw@gdcproject.org>
2018-07-07 10:56:04 +0200

    Move check for NRVO from ReturnStatement semantic into FuncDeclaration

--- src/dmd/semantic3.d
+++ src/dmd/semantic3.d
@@ -628,7 +628,9 @@ private extern(C++) final class Semantic3Visitor : Visitor
                     if (funcdecl.storage_class & STC.auto_)
                         funcdecl.storage_class &= ~STC.auto_;
                 }
-                if (!Target.isReturnOnStack(f, funcdecl.needThis()))
+
+                // handle NRVO
+                if (!Target.isReturnOnStack(f, funcdecl.needThis()) || funcdecl.checkNrvo())
                     funcdecl.nrvo_can = 0;
 
                 if (funcdecl.fbody.isErrorStatement())
