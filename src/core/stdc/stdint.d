c20e71c85c10ee805bea236da34f1781735abda5
Martin Kinkelin <noone@nowhere.com>
2018-09-20 21:28:02 +0200

    core.stdc.stdint: Fix 32-bit aliases for 32-bit MSVC

--- src/core/stdc/stdint.d
+++ src/core/stdc/stdint.d
@@ -37,32 +37,40 @@ static if (is(ucent))
 
 version (Win32)
 {
-    alias int8_t   = byte;      ///
-    alias int16_t  = short;     ///
-    alias uint8_t  = ubyte;     ///
-    alias uint16_t = ushort;    ///
-    alias int32_t  = cpp_long;  ///
-    alias uint32_t = cpp_ulong; ///
-    alias int64_t  = long;      ///
-    alias uint64_t = ulong;     ///
+    alias int8_t   = byte;   ///
+    alias int16_t  = short;  ///
+    alias uint8_t  = ubyte;  ///
+    alias uint16_t = ushort; ///
+    version (CRuntime_DigitalMars)
+    {
+        alias int32_t  = cpp_long;  ///
+        alias uint32_t = cpp_ulong; ///
+    }
+    else
+    {
+        alias int32_t  = int;  ///
+        alias uint32_t = uint; ///
+    }
+    alias int64_t  = long;   ///
+    alias uint64_t = ulong;  ///
 
-    alias int_least8_t   = byte;      ///
-    alias uint_least8_t  = ubyte;     ///
-    alias int_least16_t  = short;     ///
-    alias uint_least16_t = ushort;    ///
-    alias int_least32_t  = cpp_long;  ///
-    alias uint_least32_t = cpp_ulong; ///
-    alias int_least64_t  = long;      ///
-    alias uint_least64_t = ulong;     ///
+    alias int_least8_t   = byte;     ///
+    alias uint_least8_t  = ubyte;    ///
+    alias int_least16_t  = short;    ///
+    alias uint_least16_t = ushort;   ///
+    alias int_least32_t  = int32_t;  ///
+    alias uint_least32_t = uint32_t; ///
+    alias int_least64_t  = long;     ///
+    alias uint_least64_t = ulong;    ///
 
-    alias int_fast8_t   = byte;      ///
-    alias uint_fast8_t  = ubyte;     ///
-    alias int_fast16_t  = int;       ///
-    alias uint_fast16_t = uint;      ///
-    alias int_fast32_t  = cpp_long;  ///
-    alias uint_fast32_t = cpp_ulong; ///
-    alias int_fast64_t  = long;      ///
-    alias uint_fast64_t = ulong;     ///
+    alias int_fast8_t   = byte;     ///
+    alias uint_fast8_t  = ubyte;    ///
+    alias int_fast16_t  = int;      ///
+    alias uint_fast16_t = uint;     ///
+    alias int_fast32_t  = int32_t;  ///
+    alias uint_fast32_t = uint32_t; ///
+    alias int_fast64_t  = long;     ///
+    alias uint_fast64_t = ulong;    ///
 
     alias intptr_t  = int;   ///
     alias uintptr_t = uint;  ///
