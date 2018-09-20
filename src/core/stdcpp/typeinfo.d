a0325966e7bb1348786eee23962e7b1df52a198e
Geod24 <pro.mathias.lang@gmail.com>
2018-09-20 15:56:48 +0900

    core.stdcpp: Use new extern(C++, string) and reduce nesting

--- src/core/stdcpp/typeinfo.d
+++ src/core/stdcpp/typeinfo.d
@@ -15,49 +15,50 @@ version (CRuntime_DigitalMars)
 {
     import core.stdcpp.exception;
 
-    extern (C++, std)
+    extern (C++, "std"):
+
+    class type_info
     {
-        class type_info
-        {
-            void* pdata;
+        void* pdata;
 
-          public:
-            //virtual ~this();
-            void dtor() { }     // reserve slot in vtbl[]
+    public:
+        //virtual ~this();
+        void dtor() { }     // reserve slot in vtbl[]
 
-            //bool operator==(const type_info rhs) const;
-            //bool operator!=(const type_info rhs) const;
-            final bool before(const type_info rhs) const;
-            final const(char)* name() const;
-          protected:
-            //type_info();
-          private:
-            //this(const type_info rhs);
-            //type_info operator=(const type_info rhs);
-        }
+        //bool operator==(const type_info rhs) const;
+        //bool operator!=(const type_info rhs) const;
+        final bool before(const type_info rhs) const;
+        final const(char)* name() const;
+    protected:
+        //type_info();
+    private:
+        //this(const type_info rhs);
+        //type_info operator=(const type_info rhs);
+    }
 
-        class bad_cast : core.stdcpp.exception.std.exception
-        {
-            this() nothrow { }
-            this(const bad_cast) nothrow { }
-            //bad_cast operator=(const bad_cast) nothrow { return this; }
-            //virtual ~this() nothrow;
-            override const(char)* what() const nothrow;
-        }
+    class bad_cast : exception
+    {
+        this() nothrow { }
+        this(const bad_cast) nothrow { }
+        //bad_cast operator=(const bad_cast) nothrow { return this; }
+        //virtual ~this() nothrow;
+        override const(char)* what() const nothrow;
+    }
 
-        class bad_typeid : core.stdcpp.exception.std.exception
-        {
-            this() nothrow { }
-            this(const bad_typeid) nothrow { }
-            //bad_typeid operator=(const bad_typeid) nothrow { return this; }
-            //virtual ~this() nothrow;
-            override const (char)* what() const nothrow;
-        }
+    class bad_typeid : exception
+    {
+        this() nothrow { }
+        this(const bad_typeid) nothrow { }
+        //bad_typeid operator=(const bad_typeid) nothrow { return this; }
+        //virtual ~this() nothrow;
+        override const (char)* what() const nothrow;
     }
 }
 else version (CRuntime_Microsoft)
 {
     import core.stdcpp.exception;
+
+    extern (C++, "std"):
 
     struct __type_info_node
     {
@@ -67,82 +68,78 @@ else version (CRuntime_Microsoft)
 
     extern __gshared __type_info_node __type_info_root_node;
 
-    extern (C++, std)
+    class type_info
     {
-        class type_info
-        {
-            //virtual ~this();
-            void dtor() { }     // reserve slot in vtbl[]
-            //bool operator==(const type_info rhs) const;
-            //bool operator!=(const type_info rhs) const;
-            final bool before(const type_info rhs) const;
-            final const(char)* name(__type_info_node* p = &__type_info_root_node) const;
+        //virtual ~this();
+        void dtor() { }     // reserve slot in vtbl[]
+        //bool operator==(const type_info rhs) const;
+        //bool operator!=(const type_info rhs) const;
+        final bool before(const type_info rhs) const;
+        final const(char)* name(__type_info_node* p = &__type_info_root_node) const;
 
-          private:
-            void* pdata;
-            char[1] _name;
-            //type_info operator=(const type_info rhs);
-        }
+    private:
+        void* pdata;
+        char[1] _name;
+        //type_info operator=(const type_info rhs);
+    }
 
-        class bad_cast : core.stdcpp.exception.std.exception
-        {
-            this(const(char)* msg = "bad cast");
-            //virtual ~this();
-        }
+    class bad_cast : exception
+    {
+        this(const(char)* msg = "bad cast");
+        //virtual ~this();
+    }
 
-        class bad_typeid : core.stdcpp.exception.std.exception
-        {
-            this(const(char)* msg = "bad typeid");
-            //virtual ~this();
-        }
+    class bad_typeid : exception
+    {
+        this(const(char)* msg = "bad typeid");
+        //virtual ~this();
     }
 }
 else version (CRuntime_Glibc)
 {
     import core.stdcpp.exception;
 
-    extern (C++, __cxxabiv1)
+    extern (C++, "__cxxabiv1")
     {
         class __class_type_info;
     }
 
-    extern (C++, std)
+    extern (C++, "std"):
+
+    class type_info
     {
-        class type_info
-        {
-            void dtor1();                           // consume destructor slot in vtbl[]
-            void dtor2();                           // consume destructor slot in vtbl[]
-            final const(char)* name()() const nothrow {
-                return _name[0] == '*' ? _name + 1 : _name;
-            }
-            final bool before()(const type_info _arg) const {
-                import core.stdc.string : strcmp;
-                return (_name[0] == '*' && _arg._name[0] == '*')
-                    ? _name < _arg._name
-                    : strcmp(_name, _arg._name) < 0;
-            }
-            //bool operator==(const type_info) const;
-            bool __is_pointer_p() const;
-            bool __is_function_p() const;
-            bool __do_catch(const type_info, void**, uint) const;
-            bool __do_upcast(const __cxxabiv1.__class_type_info, void**) const;
-
-            const(char)* _name;
-            this(const(char)*);
+        void dtor1();                           // consume destructor slot in vtbl[]
+        void dtor2();                           // consume destructor slot in vtbl[]
+        final const(char)* name()() const nothrow {
+            return _name[0] == '*' ? _name + 1 : _name;
         }
-
-        class bad_cast : core.stdcpp.exception.std.exception
-        {
-            this();
-            //~this();
-            override const(char)* what() const;
+        final bool before()(const type_info _arg) const {
+            import core.stdc.string : strcmp;
+            return (_name[0] == '*' && _arg._name[0] == '*')
+                ? _name < _arg._name
+                : strcmp(_name, _arg._name) < 0;
         }
+        //bool operator==(const type_info) const;
+        bool __is_pointer_p() const;
+        bool __is_function_p() const;
+        bool __do_catch(const type_info, void**, uint) const;
+        bool __do_upcast(const __class_type_info, void**) const;
 
-        class bad_typeid : core.stdcpp.exception.std.exception
-        {
-            this();
-            //~this();
-            override const(char)* what() const;
-        }
+        const(char)* _name;
+        this(const(char)*);
+    }
+
+    class bad_cast : exception
+    {
+        this();
+        //~this();
+        override const(char)* what() const;
+    }
+
+    class bad_typeid : exception
+    {
+        this();
+        //~this();
+        override const(char)* what() const;
     }
 }
