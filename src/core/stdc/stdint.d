72ba2c327d130f11810f620deb7c441cfa89e0d6
Martin Kinkelin <noone@nowhere.com>
2018-09-22 16:00:50 +0200

    core.stdc.stdint: Merge Win32/64 blocks (NFC)

--- src/core/stdc/stdint.d
+++ src/core/stdc/stdint.d
@@ -35,7 +35,7 @@ static if (is(ucent))
     alias uint128_t = ucent; ///
 }
 
-version (Win32)
+version (Windows)
 {
     alias int8_t   = byte;   ///
     alias int16_t  = short;  ///
@@ -63,7 +63,10 @@ version (Win32)
     alias int_least64_t  = long;     ///
     alias uint_least64_t = ulong;    ///
 
-    alias int_fast8_t   = byte;     ///
+    version (Win64) // TODO: this is wrong but tested in compilable/teststdint.d
+        alias int_fast8_t = char;   ///
+    else
+        alias int_fast8_t = byte;   ///
     alias uint_fast8_t  = ubyte;    ///
     alias int_fast16_t  = int;      ///
     alias uint_fast16_t = uint;     ///
@@ -72,44 +75,10 @@ version (Win32)
     alias int_fast64_t  = long;     ///
     alias uint_fast64_t = ulong;    ///
 
-    alias intptr_t  = int;   ///
-    alias uintptr_t = uint;  ///
-    alias intmax_t  = long;  ///
-    alias uintmax_t = ulong; ///
-}
-else version (Win64)
-{
-    alias int8_t   = byte;   ///
-    alias int16_t  = short;  ///
-    alias uint8_t  = ubyte;  ///
-    alias uint16_t = ushort; ///
-    alias int32_t  = int;    ///
-    alias uint32_t = uint;   ///
-    alias int64_t  = long;   ///
-    alias uint64_t = ulong;  ///
-
-    alias int_least8_t   = byte;   ///
-    alias uint_least8_t  = ubyte;  ///
-    alias int_least16_t  = short;  ///
-    alias uint_least16_t = ushort; ///
-    alias int_least32_t  = int;    ///
-    alias uint_least32_t = uint;   ///
-    alias int_least64_t  = long;   ///
-    alias uint_least64_t = ulong;  ///
-
-    alias int_fast8_t   = char;  ///
-    alias uint_fast8_t  = ubyte; ///
-    alias int_fast16_t  = int;   ///
-    alias uint_fast16_t = uint;  ///
-    alias int_fast32_t  = int;   ///
-    alias uint_fast32_t = uint;  ///
-    alias int_fast64_t  = long;  ///
-    alias uint_fast64_t = ulong; ///
-
-    alias intptr_t  = long;  ///
-    alias uintptr_t = ulong; ///
-    alias intmax_t  = long;  ///
-    alias uintmax_t = ulong; ///
+    alias intptr_t  = ptrdiff_t; ///
+    alias uintptr_t = size_t;    ///
+    alias intmax_t  = long;      ///
+    alias uintmax_t = ulong;     ///
 }
 else version (OSX)
 {
