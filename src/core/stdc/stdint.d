f2ce39c80a14830f6c8880d45a1b672074f177ff
Martin Kinkelin <noone@nowhere.com>
2018-09-22 15:52:18 +0200

    core.stdc.stdint: Simplify Posix block (NFC)

--- src/core/stdc/stdint.d
+++ src/core/stdc/stdint.d
@@ -147,74 +147,37 @@ else version (OSX)
 }
 else version (Posix)
 {
-    version (D_LP64)
-    {
-        alias int8_t   = byte;      ///
-        alias int16_t  = short;     ///
-        alias uint8_t  = ubyte;     ///
-        alias uint16_t = ushort;    ///
-        alias int32_t  = int;       ///
-        alias uint32_t = uint;      ///
-        alias int64_t  = cpp_long;  ///
-        alias uint64_t = cpp_ulong; ///
+    alias int8_t   = byte;   ///
+    alias int16_t  = short;  ///
+    alias uint8_t  = ubyte;  ///
+    alias uint16_t = ushort; ///
+    alias int32_t  = int;    ///
+    alias uint32_t = uint;   ///
+    alias int64_t  = long;   ///
+    alias uint64_t = ulong;  ///
 
-        alias int_least8_t   = byte;      ///
-        alias uint_least8_t  = ubyte;     ///
-        alias int_least16_t  = short;     ///
-        alias uint_least16_t = ushort;    ///
-        alias int_least32_t  = int;       ///
-        alias uint_least32_t = uint;      ///
-        alias int_least64_t  = cpp_long;  ///
-        alias uint_least64_t = cpp_ulong; ///
+    alias int_least8_t   = byte;   ///
+    alias uint_least8_t  = ubyte;  ///
+    alias int_least16_t  = short;  ///
+    alias uint_least16_t = ushort; ///
+    alias int_least32_t  = int;    ///
+    alias uint_least32_t = uint;   ///
+    alias int_least64_t  = long;   ///
+    alias uint_least64_t = ulong;  ///
 
-        alias int_fast8_t   = byte;      ///
-        alias uint_fast8_t  = ubyte;     ///
-        alias int_fast16_t  = cpp_long;  ///
-        alias uint_fast16_t = cpp_ulong; ///
-        alias int_fast32_t  = cpp_long;  ///
-        alias uint_fast32_t = cpp_ulong; ///
-        alias int_fast64_t  = cpp_long;  ///
-        alias uint_fast64_t = cpp_ulong; ///
+    alias int_fast8_t   = byte;      ///
+    alias uint_fast8_t  = ubyte;     ///
+    alias int_fast16_t  = ptrdiff_t; ///
+    alias uint_fast16_t = size_t;    ///
+    alias int_fast32_t  = ptrdiff_t; ///
+    alias uint_fast32_t = size_t;    ///
+    alias int_fast64_t  = long;      ///
+    alias uint_fast64_t = ulong;     ///
 
-        alias intptr_t  = cpp_long;  ///
-        alias uintptr_t = cpp_ulong; ///
-        alias intmax_t  = cpp_long;  ///
-        alias uintmax_t = cpp_ulong; ///
-    }
-    else
-    {
-        alias int8_t   = byte;   ///
-        alias int16_t  = short;  ///
-        alias uint8_t  = ubyte;  ///
-        alias uint16_t = ushort; ///
-        alias int32_t  = int;    ///
-        alias uint32_t = uint;   ///
-        alias int64_t  = long;   ///
-        alias uint64_t = ulong;  ///
-
-        alias int_least8_t   = byte;   ///
-        alias uint_least8_t  = ubyte;  ///
-        alias int_least16_t  = short;  ///
-        alias uint_least16_t = ushort; ///
-        alias int_least32_t  = int;    ///
-        alias uint_least32_t = uint;   ///
-        alias int_least64_t  = long;   ///
-        alias uint_least64_t = ulong;  ///
-
-        alias int_fast8_t   = byte;  ///
-        alias uint_fast8_t  = ubyte; ///
-        alias int_fast16_t  = int;   ///
-        alias uint_fast16_t = uint;  ///
-        alias int_fast32_t  = int;   ///
-        alias uint_fast32_t = uint;  ///
-        alias int_fast64_t  = long;  ///
-        alias uint_fast64_t = ulong; ///
-
-        alias intptr_t  = int ;  ///
-        alias uintptr_t = uint;  ///
-        alias intmax_t  = long;  ///
-        alias uintmax_t = ulong; ///
-    }
+    alias intptr_t  = ptrdiff_t; ///
+    alias uintptr_t = size_t;    ///
+    alias intmax_t  = long;      ///
+    alias uintmax_t = ulong;     ///
 }
 else
 {
