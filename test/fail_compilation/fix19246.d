1caed612a7c37c1e9cf30b73bdfde37ad9aa4c0f
Nicholas Wilson <thewilsonator@users.noreply.github.com>
2018-11-06 18:42:22 +0800

    Add test for `0B`

--- test/fail_compilation/fix19246.d
+++ test/fail_compilation/fix19246.d
@@ -2,9 +2,10 @@
  * PERMUTE_ARGS:
  * TEST_OUTPUT:
 ---
-fail_compilation/fix19246.d(15): Deprecation: `0b_` isn't a valid integer literal, use `0b0` instead
-fail_compilation/fix19246.d(16): Deprecation: `0B_` isn't a valid integer literal, use `0B0` instead
-fail_compilation/fix19246.d(17): Deprecation: `0b` isn't a valid integer literal, use `0b0` instead
+fail_compilation/fix19246.d(16): Deprecation: `0b_` isn't a valid integer literal, use `0b0` instead
+fail_compilation/fix19246.d(17): Deprecation: `0B_` isn't a valid integer literal, use `0B0` instead
+fail_compilation/fix19246.d(18): Deprecation: `0b` isn't a valid integer literal, use `0b0` instead
+fail_compilation/fix19246.d(19): Deprecation: `0B` isn't a valid integer literal, use `0B0` instead
 ---
  */
 
@@ -15,4 +16,5 @@ void foo()
     auto a = 0b_;
     auto b = 0B_;
     auto c = 0b;
+    auto d = 0B;
 }
