bfda6e0447cbd7301b78c69ad3634d415f2026d7
Vladimir Panteleev <git@thecybershadow.net>
2018-11-14 23:11:34 +0000

    Try fixing test

--- test/compilable/testcolor.sh
+++ test/compilable/testcolor.sh
@@ -1,47 +1,45 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# We never use ANSI codes on Windows, so, we can't test them.
+if [[ $OS = *"win"* ]]; then exit 0; fi
+
 compare()
 {
     local actual="$1"
     local expected="$2"
 
     if [ "$actual" != "$expected" ] ; then
-        echo "Expected: $expected"
-        echo "Actual:   $actual"
-        echo "$expected" | hexdump -C
-        echo "$actual" | hexdump -C
+        printf 'Expected: %q\n' "$expected"
+        printf 'Actual:   %q\n' "$actual"
+        hexdump -C <<<"$expected"
+        hexdump -C <<<"$actual"
         exit 1
     fi
 }
 
-filterAnsi()
-{
-    # try to emulate the `strings` command
-    # sed on some OSes doesn't support direct hex byte in its input string
-    sed "s/$'\x1B''\(\[[0-9;]*m\)/[\1/g" | sed "s/\[\[/[/g" | sed "s/m\[m/m/g" | tr -d "\n\r"
-}
+normalize() { tr -d "\n\r" ; }
 
 check()
 {
     local actual expected
-    actual=$(echo "$2" | ("$DMD" -c -o- "$1" - 2>&1 || true) | filterAnsi)
+    actual=$(echo "$2" | ("$DMD" -c -o- "$1" - 2>&1 || true) | normalize)
     compare "$actual" "$3"
 }
 
-expectedWithoutColor='__stdin.d(2): Error: no identifier for declarator `test`'
-expectedWithColor="[1m__stdin.d(2): [1;31mError: [mno identifier for declarator [0;36m[1mtest[0;36m"
+expectedWithoutColor=__stdin.d\(2\):\ Error:\ no\ identifier\ for\ declarator\ \`test\`
+expectedWithColor=$'\033'\[1m__stdin.d\(2\):\ $'\033'\[1\;31mError:\ $'\033'\[mno\ identifier\ for\ declarator\ $'\033'\[0\;36m$'\033'\[m$'\033'\[1mtest$'\033'\[0\;36m$'\033'\[m
 
 check -c "test" "$expectedWithoutColor"
 check -color=auto "test" "$expectedWithoutColor"
 check -color=on "test" "$expectedWithColor"
 check -color=off "test" "$expectedWithoutColor"
 
-# Faking a TTY only works on Linux
-#if [ "$OS" == "linux" ] ; then
-    export SHELL="bash"
-    script -q --return -c "echo test | $DMD -c -o- -"
-    script -q --return -c "$DMD -c -o- foo.d"
-    actual="$(! script -q --return -c "echo test | $DMD -c -o- -" /dev/null | filterAnsi)"
-    compare "$actual" "$expectedWithColor"
-#fi
+if [[ "$(script --version)" == script\ from\ util-linux\ * ]]
+then
+    actual="$(SHELL="$(command -v bash)" TERM="faketerm" script -q -c "echo test | ( $DMD -c -o- -)" /dev/null | normalize)" || true
+    if [[ "$actual" != '^@^@'* ]] # 'script' weirdness on CircleCI
+    then
+        compare "$actual" "$expectedWithColor"
+    fi
+fi
