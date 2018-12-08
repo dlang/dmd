3f1392dfb22c7c7fffbf37e5c90bb9feaf96aaf2
Martin Kinkelin <noone@nowhere.com>
2018-12-08 03:31:18 +0100

    core.elf: Prepare for generic ELF I/O
    I.e., allowing to read non-native ELF files too. The byte order must
    match the target's though.

--- src/core/elf.d
+++ src/core/elf.d
@@ -144,113 +144,173 @@ struct SharedObject
     }
 }
 
-// -------------------------------
-//  File-based memory-mapped I/O:
-// -------------------------------
-
-struct ElfFile
+/**
+ * File-based memory-mapped I/O (read-only).
+ * Only supports ELF files with a byte-order matching the target platform's.
+ * Params:
+ *     Elf_Ehdr = Expected type of the ELF header (Elf{32,64}_Ehdr)
+ *     Elf_Shdr = Expected type of the ELF section header (Elf{32,64}_Shdr)
+ *     ELFCLASS = Expected ELF class (ELFCLASS{32,64})
+ */
+template ElfIO(Elf_Ehdr, Elf_Shdr, ubyte ELFCLASS)
 {
-@nogc nothrow:
-    static bool open(const(char)* path, out ElfFile file)
+    struct ElfFile
     {
-        file = ElfFile(.open(path, O_RDONLY));
-        return file.isValid();
-    }
-
-    this(int fd)
-    {
-        this.fd = fd;
-        if (fd != -1)
+    @nogc nothrow:
+        static bool open(const(char)* path, out ElfFile file)
         {
-            // memory map header
-            this.ehdr = MMapRegion!Elf_Ehdr(fd, 0);
+            file = ElfFile(.open(path, O_RDONLY));
+            return file.isValid();
+        }
+
+        this(int fd)
+        {
+            this.fd = fd;
+            if (fd != -1)
+            {
+                // memory map header
+                this.ehdr = MMapRegion!Elf_Ehdr(fd, 0);
+            }
+        }
+
+        @disable this(this);
+
+        ~this()
+        {
+            if (fd != -1)
+                close(fd);
+        }
+
+        private int fd = -1;
+        MMapRegion!Elf_Ehdr ehdr;
+
+        bool isValid() const
+        {
+            enum EI_MAG0 = 0;
+            enum EI_MAG1 = 1;
+            enum EI_MAG2 = 2;
+            enum EI_MAG3 = 3;
+            enum EI_CLASS = 4;
+            enum EI_DATA = 5;
+
+            enum ELFMAG0 = 0x7f;
+            enum ELFMAG1 = 'E';
+            enum ELFMAG2 = 'L';
+            enum ELFMAG3 = 'F';
+
+            enum ELFCLASS32 = 1;
+            enum ELFCLASS64 = 2;
+
+            enum ELFDATA2LSB = 1;
+            enum ELFDATA2MSB = 2;
+
+            version (LittleEndian)   alias ELFDATA = ELFDATA2LSB;
+            else version (BigEndian) alias ELFDATA = ELFDATA2MSB;
+            else static assert(0, "unsupported byte order");
+
+            if (fd == -1)
+                return false;
+
+            const ident = ehdr.e_ident;
+
+            if (!(ident[EI_MAG0] == ELFMAG0 &&
+                  ident[EI_MAG1] == ELFMAG1 &&
+                  ident[EI_MAG2] == ELFMAG2 &&
+                  ident[EI_MAG3] == ELFMAG3))
+                return false;
+
+            if (ident[EI_CLASS] != ELFCLASS)
+                return false;
+
+            // the file's byte order must match the target's
+            if (ident[EI_DATA] != ELFDATA)
+                return false;
+
+            return true;
+        }
+
+        bool findSectionHeaderByName(const(char)[] sectionName, out ElfSectionHeader header) const
+        {
+            const index = findSectionIndexByName(sectionName);
+            if (index == -1)
+                return false;
+            header = ElfSectionHeader(this, index);
+            return true;
+        }
+
+        size_t findSectionIndexByName(const(char)[] sectionName) const
+        {
+            const stringSectionHeader = ElfSectionHeader(this, ehdr.e_shstrndx);
+            const stringSection = ElfSection(this, stringSectionHeader);
+
+            foreach (i; 0 .. ehdr.e_shnum)
+            {
+                auto sectionHeader = ElfSectionHeader(this, i);
+                auto pCurrentName = cast(const(char)*) (stringSection.data.ptr + sectionHeader.sh_name);
+                auto currentName = pCurrentName[0 .. strlen(pCurrentName)];
+                if (sectionName == currentName)
+                    return i;
+            }
+
+            // not found
+            return -1;
         }
     }
 
-    @disable this(this);
-
-    ~this()
+    struct ElfSectionHeader
     {
-        if (fd != -1)
-            close(fd);
-    }
-
-    private int fd = -1;
-    MMapRegion!Elf_Ehdr ehdr;
-
-    bool isValid() const
-    {
-        return fd != -1 && isValidElfHeader(*ehdr);
-    }
-
-    bool findSectionHeaderByName(const(char)[] sectionName, out ElfSectionHeader header) const
-    {
-        const index = findSectionIndexByName(sectionName);
-        if (index == -1)
-            return false;
-        header = ElfSectionHeader(this, index);
-        return true;
-    }
-
-    size_t findSectionIndexByName(const(char)[] sectionName) const
-    {
-        const stringSectionHeader = ElfSectionHeader(this, ehdr.e_shstrndx);
-        const stringSection = ElfSection(this, stringSectionHeader);
-
-        foreach (i; 0 .. ehdr.e_shnum)
+    @nogc nothrow:
+        this(ref const ElfFile file, size_t index)
         {
-            auto sectionHeader = ElfSectionHeader(this, i);
-            auto pCurrentName = cast(const(char)*) (stringSection.data.ptr + sectionHeader.sh_name);
-            auto currentName = pCurrentName[0 .. strlen(pCurrentName)];
-            if (sectionName == currentName)
-                return i;
+            assert(Elf_Shdr.sizeof == file.ehdr.e_shentsize);
+            shdr = MMapRegion!Elf_Shdr(
+                file.fd,
+                file.ehdr.e_shoff + index * Elf_Shdr.sizeof
+            );
         }
 
-        // not found
-        return -1;
+        @disable this(this);
+
+        alias shdr this;
+        MMapRegion!Elf_Shdr shdr;
+    }
+
+    struct ElfSection
+    {
+    @nogc nothrow:
+        this(ref const ElfFile file, ref const ElfSectionHeader shdr)
+        {
+            mappedRegion = MMapRegion!void(file.fd, shdr.sh_offset, shdr.sh_size);
+            size = shdr.sh_size;
+        }
+
+        @disable this(this);
+
+        const(void)[] data() const
+        {
+            return mappedRegion.data[0 .. size];
+        }
+
+        alias data this;
+
+    private:
+        MMapRegion!void mappedRegion;
+        size_t size;
     }
 }
 
-struct ElfSectionHeader
-{
-@nogc nothrow:
-    this(ref const ElfFile file, size_t index)
-    {
-        assert(Elf_Shdr.sizeof == file.ehdr.e_shentsize);
-        shdr = MMapRegion!Elf_Shdr(
-            file.fd,
-            file.ehdr.e_shoff + index * Elf_Shdr.sizeof
-        );
-    }
+enum ELFCLASS32 = 1;
+enum ELFCLASS64 = 2;
 
-    @disable this(this);
+version (D_LP64) alias ELFCLASS = ELFCLASS64;
+else             alias ELFCLASS = ELFCLASS32;
 
-    alias shdr this;
-    MMapRegion!Elf_Shdr shdr;
-}
+alias NativeElfIO = ElfIO!(Elf_Ehdr, Elf_Shdr, ELFCLASS);
 
-struct ElfSection
-{
-@nogc nothrow:
-    this(ref const ElfFile file, ref const ElfSectionHeader shdr)
-    {
-        mappedRegion = MMapRegion!void(file.fd, shdr.sh_offset, shdr.sh_size);
-        size = shdr.sh_size;
-    }
-
-    @disable this(this);
-
-    const(void)[] data() const
-    {
-        return mappedRegion.data[0 .. size];
-    }
-
-    alias data this;
-
-private:
-    MMapRegion!void mappedRegion;
-    size_t size;
-}
+// convenience aliases for native ELF files
+alias ElfFile = NativeElfIO.ElfFile;
+alias ElfSectionHeader = NativeElfIO.ElfSectionHeader;
+alias ElfSection = NativeElfIO.ElfSection;
 
 private @nogc nothrow:
 
@@ -265,27 +325,6 @@ version (linux)
 else
 {
     extern(C) const(char)* getprogname();
-}
-
-bool isValidElfHeader(ref const Elf_Ehdr ehdr)
-{
-    version (D_LP64) alias ELFCLASS = ELFCLASS64;
-    else             alias ELFCLASS = ELFCLASS32;
-
-    version (LittleEndian)   alias ELFDATA = ELFDATA2LSB;
-    else version (BigEndian) alias ELFDATA = ELFDATA2MSB;
-    else static assert(0, "unsupported byte order");
-
-    if (ehdr.e_ident[EI_MAG0] != ELFMAG0) return false;
-    if (ehdr.e_ident[EI_MAG1] != ELFMAG1) return false;
-    if (ehdr.e_ident[EI_MAG2] != ELFMAG2) return false;
-    if (ehdr.e_ident[EI_MAG3] != ELFMAG3) return false;
-
-    // elf class and data encoding should match target's config
-    if (ehdr.e_ident[EI_CLASS] != ELFCLASS) return false;
-    if (ehdr.e_ident[EI_DATA]  != ELFDATA ) return false;
-
-    return true;
 }
 
 struct MMapRegion(T)
