e3b3a0dac9de003ef5b9146091c9a3eed2236ae7
Martin Kinkelin <noone@nowhere.com>
2018-12-08 20:53:03 +0100

    version(GC): Switch to opt-in GC via explicit -lowmem switch
    Changed semantics for `version (GC)`:
    
    Old: run with enabled GC.
    New: enable switching at runtime between enabled GC and disabled GC +
         bump-pointer allocation scheme (faster, less memory overhead over
         disabled GC alone), i.e., the mode used for non-version(GC).
    
    => No change if the GC version isn't defined.
    
    If defined, it adds a negligible runtime overhead, as the allocation
    functions in dmd.root.rmem need to check whether to forward to the GC or
    to use the bump-pointer scheme.
    
    If DMD is built with `-version=GC`, a new switch, `-lowmem`, enables the
    GC; it is disabled by default. Unfortunately, we need to set the mode
    before initializing druntime (and running module constructors), so I had
    to add a C main function.
    
    In the allocation functions performing an initialization, I changed the
    slice-assignments to memcpy/memset, in order to skip superfluous bounds
    checks when compiling the front-end without `-release`.

--- src/dmd/mars.d
+++ src/dmd/mars.d
@@ -812,53 +812,108 @@ private void generateJson(Modules* modules)
 }
 
 
-/**
- * Entry point which forwards to `tryMain`.
- *
- * Returns:
- *   Return code of the application
- */
-version(NoMain) {} else
-int main()
+version (NoMain) {} else
 {
-    import core.memory;
-    import core.runtime;
+    // in druntime:
+    alias MainFunc = extern(C) int function(char[][] args);
+    extern (C) int _d_run_main(int argc, char** argv, MainFunc dMain);
 
-    version (GC)
+    // When using a C main, host DMD may not link against host druntime by default.
+    version (DigitalMars)
     {
-    }
-    else
-    {
-        GC.disable();
-    }
-    version(D_Coverage)
-    {
-        // for now we need to manually set the source path
-        string dirName(string path, char separator)
+        version (Win64)
+            pragma(lib, "phobos64");
+        else version (Win32)
         {
-            for (size_t i = path.length - 1; i > 0; i--)
-            {
-                if (path[i] == separator)
-                    return path[0..i];
-            }
-            return path;
+            version (CRuntime_Microsoft)
+                pragma(lib, "phobos32mscoff");
+            else
+                pragma(lib, "phobos");
         }
-        version (Windows)
-            enum sourcePath = dirName(dirName(__FILE_FULL_PATH__, '\\'), '\\');
-        else
-            enum sourcePath = dirName(dirName(__FILE_FULL_PATH__, '/'), '/');
-
-        dmd_coverSourcePath(sourcePath);
-        dmd_coverDestPath(sourcePath);
-        dmd_coverSetMerge(true);
     }
 
-    scope(failure) stderr.printInternalFailure;
+    /**
+     * DMD's entry point, C main.
+     *
+     * Without `-lowmem`, we need to switch to the bump-pointer allocation scheme
+     * right from the start, before any module ctors are run, so we need this hook
+     * before druntime is initialized and `_Dmain` is called.
+     *
+     * Returns:
+     *   Return code of the application
+     */
+    extern (C) int main(int argc, char** argv)
+    {
+        static if (isGCAvailable)
+        {
+            bool lowmem = false;
+            foreach (i; 1 .. argc)
+            {
+                if (strcmp(argv[i], "-lowmem") == 0)
+                {
+                    lowmem = true;
+                    break;
+                }
+            }
+            if (!lowmem)
+                mem.disableGC();
+        }
 
-    auto args = Runtime.cArgs();
-    return tryMain(args.argc, cast(const(char)**)args.argv, global.params);
-}
+        // initialize druntime and call _Dmain() below
+        return _d_run_main(argc, argv, &_Dmain);
+    }
 
+    /**
+     * Manual D main (for druntime initialization), which forwards to `tryMain`.
+     *
+     * Returns:
+     *   Return code of the application
+     */
+    extern (C) int _Dmain(char[][])
+    {
+        import core.memory;
+        import core.runtime;
+
+        // Older host druntime versions need druntime to be initialized before
+        // disabling the GC, so we cannot disable it in C main above.
+        static if (isGCAvailable)
+        {
+            if (!mem.isGCEnabled)
+                GC.disable();
+        }
+        else
+        {
+            GC.disable();
+        }
+
+        version(D_Coverage)
+        {
+            // for now we need to manually set the source path
+            string dirName(string path, char separator)
+            {
+                for (size_t i = path.length - 1; i > 0; i--)
+                {
+                    if (path[i] == separator)
+                        return path[0..i];
+                }
+                return path;
+            }
+            version (Windows)
+                enum sourcePath = dirName(dirName(__FILE_FULL_PATH__, '\\'), '\\');
+            else
+                enum sourcePath = dirName(dirName(__FILE_FULL_PATH__, '/'), '/');
+
+            dmd_coverSourcePath(sourcePath);
+            dmd_coverDestPath(sourcePath);
+            dmd_coverSetMerge(true);
+        }
+
+        scope(failure) stderr.printInternalFailure;
+
+        auto args = Runtime.cArgs();
+        return tryMain(args.argc, cast(const(char)**)args.argv, global.params);
+    }
+} // !NoMain
 
 /**
  * Parses an environment variable containing command-line flags
@@ -1673,6 +1728,18 @@ bool parseCommandLine(const ref Strings arguments, const size_t argc, ref Param 
             params.alwaysframe = true;
         else if (arg == "-gx")  // https://dlang.org/dmd.html#switch-gx
             params.stackstomp = true;
+        else if (arg == "-lowmem") // https://dlang.org/dmd.html#switch-lowmem
+        {
+            static if (isGCAvailable)
+            {
+                // ignore, already handled in C main
+            }
+            else
+            {
+                error("switch '%s' requires DMD to be built with '-version=GC'", arg.ptr);
+                continue;
+            }
+        }
         else if (arg == "-m32") // https://dlang.org/dmd.html#switch-m32
         {
             static if (TARGET.DragonFlyBSD) {
