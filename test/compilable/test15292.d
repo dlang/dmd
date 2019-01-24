6716c02dae44d6a186ac96af8b0e1f331bcb3069
RazvanN7 <razvan.nitu1305@gmail.com>
2019-01-24 11:55:56 +0200

    Fix Issue 16657 and 11161 - alias this interacts with generated opCmp and opEquals

--- test/compilable/test15292.d
+++ test/compilable/test15292.d
@@ -1,11 +1,3 @@
-// REQUIRED_ARGS: -o-
-/*
-TEST_OUTPUT:
----
-fail_compilation/fail15292.d(27): Error: cannot compare `S15292` because its auto generated member-wise equality has recursive definition
----
-*/
-
 struct NullableRef15292(T)
 {
     inout(T) get() inout
