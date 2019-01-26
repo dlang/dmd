fc6ef822456847316fc597a11f8977910fdb4755
Basile Burg <basile.b@gmx.com>
2019-01-26 11:24:56 +0100

    add a test with func calls in the nested aggregate

--- test/runnable/b16360.d
+++ test/runnable/b16360.d
@@ -25,8 +25,26 @@ T bitCast(T, S)(auto ref S s)
     return bt.tt;
 }
 
+pragma(inline, true)
+int withFuncCalls()
+{
+    static struct WithFuncCalls
+    {
+        int v;
+        pragma(inline, true)
+        int call(){return v;}
+        pragma(inline, true)
+        void otherCall(){v++;}
+    }
+    auto bt = WithFuncCalls(50);
+    bt.v += -9;
+    bt.otherCall();
+    return bt.call();
+}
+
 void main()
 {
     assert(foo == 42);
     assert(bitCast!int(1.0f) == 0x3f800000);
+    assert(withFuncCalls() == 42);
 }
