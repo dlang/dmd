da3c114cc2bd8d58eb140c785439c1cd46947ab8
Iain Buclaw <ibuclaw@gdcproject.org>
2019-01-29 00:59:06 +0100

    fix Issue 19628 - ICE indexing vector array

--- test/compilable/ctfesimd.d
+++ test/compilable/ctfesimd.d
@@ -4,4 +4,9 @@ version (D_SIMD)
 
     // https://issues.dlang.org/show_bug.cgi?id=19627
     enum int[4] fail19627 = cast(int[4])int4(0);
+
+    // https://issues.dlang.org/show_bug.cgi?id=19628
+    enum ice19628a = int4.init[0];
+    enum ice19628b = int4.init.array[0];
+    enum ice19628c = (cast(int[4])int4.init.array)[0];
 }
