20d48da3b012bbfbc300836f8ebdb858c3a621f8
Iain Buclaw <ibuclaw@gdcproject.org>
2019-01-29 01:10:00 +0100

    fix Issie 19629 - Error: cannot determine length of `cast(__vector(int[4]))0` at compile time

--- test/compilable/ctfesimd.d
+++ test/compilable/ctfesimd.d
@@ -9,4 +9,9 @@ version (D_SIMD)
     enum ice19628a = int4.init[0];
     enum ice19628b = int4.init.array[0];
     enum ice19628c = (cast(int[4])int4.init.array)[0];
+
+    // https://issues.dlang.org/show_bug.cgi?id=19629
+    enum fail19629a = int4(0)[0];
+    enum fail19629b = int4(0).array[0];
+    enum fail19629c = (cast(int[4])int4(0).array)[0];
 }
