adbe7161070b1972371ec8c6869000ba6f3aacd0
Iain Buclaw <ibuclaw@gdcproject.org>
2019-01-29 22:43:07 +0100

    fix Issue 19630 - Error: cannot determine length of vector slice at compile time

--- test/compilable/ctfesimd.d
+++ test/compilable/ctfesimd.d
@@ -14,4 +14,14 @@ version (D_SIMD)
     enum fail19629a = int4(0)[0];
     enum fail19629b = int4(0).array[0];
     enum fail19629c = (cast(int[4])int4(0).array)[0];
+
+    // https://issues.dlang.org/show_bug.cgi?id=19630
+    enum fail19630a = int4.init[1..2];
+    enum fail19630b = int4.init.array[1..2];
+    enum fail19630c = (cast(int[4])int4.init.array)[1..2];
+    enum fail19630d = int4(0)[1..2];
+    enum fail19630e = int4(0).array[1..2];
+    enum fail19630f = (cast(int[4])int4(0).array)[1..2];
+    enum fail19630g = (cast(int[4])int4.init)[1..2];
+    enum fail19630h = (cast(int[4])int4(0))[1..2];
 }
