7d3e991e8400e5d92451df931e6835f446aabf83
Iain Buclaw <ibuclaw@gdcproject.org>
2019-01-29 01:18:17 +0100

    fix Issue 19224 - Error: __r2.length cannot be evaluated at compile time

---
+++ test/compilable/test19224.d
@@ -0,0 +1,15 @@
+// https://issues.dlang.org/show_bug.cgi?id=19224
+version (D_SIMD)
+{
+    float sum(const float[4] val)
+    {
+        float sum = 0;
+        foreach (x; val) sum += x;
+        return sum;
+    }
+
+    import core.simd : float4;
+
+    enum x = sum(float4.init.array);
+    static assert(x is float.nan);
+}
