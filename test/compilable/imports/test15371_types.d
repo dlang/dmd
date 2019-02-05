cc9a930f79431b392ffbb276e7e6a56cae5813f6
Adam Wilson <adamw@ellipticbit.com>
2019-02-05 00:36:21 -0800

    Address PR feedback.

--- test/compilable/imports/test15371_types.d
+++ test/compilable/imports/test15371_types.d
@@ -1,4 +1,4 @@
-module compilable.test15371_types;
+module test.compilable.imports.test15371_types;
 
 //This needs to be in a seperate module to trigger this issue.
 public struct type_test15371 {
--- test/compilable/test15371.d
+++ test/compilable/test15371.d
@@ -1,11 +1,12 @@
 module compilable.test15371;
 
-import test15371_types : type_test15371;
+import imports.test15371_types : type_test15371;
+import std.stdio: writeln;
 
 version(unittest)
 
 void main() {
     static foreach(m; __traits(allMembers, type_test15371)) {
-        __traits(getMember, m);
+        writeln(__traits(getMember, m));
     }
 }
