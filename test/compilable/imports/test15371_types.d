0b910a37b28805dd533cfa16590b3f3ed575bf2e
Adam Wilson <adamw@ellipticbit.com>
2019-02-06 23:10:34 -0800

    Added test for all affected traits. Added changelog entry.

--- test/compilable/imports/test15371_types.d
+++ test/compilable/imports/test15371_types.d
@@ -1,7 +1,9 @@
 module test.compilable.imports.test15371_types;
 
 //This needs to be in a seperate module to trigger this issue.
-public struct type_test15371 {
-    public int publicField;
+public class type_test15371 {
     private int privateField;
+
+    private int overload(int param1) { return param1; }
+    private int overload(int param1, int param2) { return param2; }
 }
--- test/compilable/test15371.d
+++ test/compilable/test15371.d
@@ -6,7 +6,9 @@ import core.stdc.stdio;
 version(unittest)
 
 void main() {
-    static foreach(m; __traits(allMembers, type_test15371)) {
-        printf(__traits(getMember, m));
-    }
+    printf(__traits(hasMember, type_test15371, "privateField"));
+    printf(__traits(getMember, type_test15371, "privateField"));
+    printf(__traits(getOverloads, type_test15371, "overload"));
+    printf(__traits(getVirtualMethods, type_test15371));
+    printf(__traits(getVirtualFunctions, type_test15371));
 }
