7d962f138d300dc405d8bd28e3dc916339af739c
Iain Buclaw <ibuclaw@gdcproject.org>
2019-02-19 22:31:45 +0100

    rt.adi: Remove unused compiler helpers

--- src/rt/adi.d
+++ src/rt/adi.d
@@ -16,67 +16,14 @@ module rt.adi;
 private
 {
     debug(adi) import core.stdc.stdio;
-    import core.stdc.string;
-    import core.stdc.stdlib;
-    import core.memory;
-    import core.internal.utf;
-
-    extern (C) void[] _adSort(void[] a, TypeInfo ti);
 }
 
-private dchar[] mallocUTF32(C)(in C[] s)
-{
-    size_t j = 0;
-    auto p = cast(dchar*)malloc(dchar.sizeof * s.length);
-    auto r = p[0..s.length]; // r[] will never be longer than s[]
-    foreach (dchar c; s)
-        r[j++] = c;
-    return r[0 .. j];
-}
-
-/**********************************************
- * Sort array of chars.
+/***************************************
+ * Support for array equality test.
+ * Returns:
+ *      1       equal
+ *      0       not equal
  */
-
-extern (C) char[] _adSortChar(char[] a)
-{
-    if (a.length > 1)
-    {
-        auto da = mallocUTF32(a);
-        _adSort(*cast(void[]*)&da, typeid(da[0]));
-        size_t i = 0;
-        foreach (dchar d; da)
-        {   char[4] buf = void;
-            auto t = toUTF8(buf, d);
-            a[i .. i + t.length] = t[];
-            i += t.length;
-        }
-        free(da.ptr);
-    }
-    return a;
-}
-
-/**********************************************
- * Sort array of wchars.
- */
-
-extern (C) wchar[] _adSortWchar(wchar[] a)
-{
-    if (a.length > 1)
-    {
-        auto da = mallocUTF32(a);
-        _adSort(*cast(void[]*)&da, typeid(da[0]));
-        size_t i = 0;
-        foreach (dchar d; da)
-        {   wchar[2] buf = void;
-            auto t = toUTF16(buf, d);
-            a[i .. i + t.length] = t[];
-            i += t.length;
-        }
-        free(da.ptr);
-    }
-    return a;
-}
 
 extern (C) int _adEq2(void[] a1, void[] a2, TypeInfo ti)
 {
@@ -87,6 +34,7 @@ extern (C) int _adEq2(void[] a1, void[] a2, TypeInfo ti)
         return 0;
     return 1;
 }
+
 unittest
 {
     debug(adi) printf("array.Eq unittest\n");
@@ -119,21 +67,4 @@ unittest
     assert(a <= "hello");
     assert(a >= "hello");
     assert(a <  "я");
-}
-
-unittest
-{
-    debug(adi) printf("array.CmpChar unittest\n");
-
-    auto a = "hello"c;
-
-    assert(a >  "hel");
-    assert(a >= "hel");
-    assert(a <  "helloo");
-    assert(a <= "helloo");
-    assert(a >  "betty");
-    assert(a >= "betty");
-    assert(a == "hello");
-    assert(a <= "hello");
-    assert(a >= "hello");
 }
