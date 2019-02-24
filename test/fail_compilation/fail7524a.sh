50cde4008da0b51bd08630d387b88837d0890555
Martin Kinkelin <noone@nowhere.com>
2019-02-24 22:15:14 +0100

    Fix fail_compilation/fail7524a.sh on Windows

--- test/fail_compilation/fail7524a.sh
+++ test/fail_compilation/fail7524a.sh
@@ -4,7 +4,7 @@
 output="$(echo '#line 47 __DATE__' | (! "$DMD" -c -o- - 2>&1))"
 
 line1='__stdin.d(1): Error: #line integer ["filespec"]\n expected'
-if [ "$(echo "$output" | head -n1)"  != "$line1" ] ; then
+if [ "$(echo "$output" | head -n1 | tr -d "\r")"  != "$line1" ] ; then
     exit 1
 fi
 if [ "$("$output" | wc -l)"  -eq 2 ] ; then
