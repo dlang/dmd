0b9642d38ea29ca38abe8145abdb2d881f05465a
Rainer Schuetze <r.sagitario@gmx.de>
2019-03-10 11:56:27 +0100

    relax test18076 abort on build/test failure

--- test/runnable/test18076.sh
+++ test/runnable/test18076.sh
@@ -4,6 +4,6 @@ output_file=${OUTPUT_BASE}.log
 
 echo 'import std.stdio; void main() { writeln("Success"); }' | \
 	$DMD -m${MODEL} -run - > ${output_file}
-grep -q '^Success\r*$' ${output_file}
+grep -q 'Success' ${output_file}
 
 rm "${output_file}"
