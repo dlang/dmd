137d0982b241c9e71a54a106fc14015edffa90bb
Basile Burg <basile.b@gmx.com>
2019-03-12 13:05:03 +0100

    cover IfCondition with explicit storage class and type

--- test/fail_compilation/b19730.d
+++ test/fail_compilation/b19730.d
@@ -1,8 +1,8 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/b19730.d(9): Error: found `)` while expecting `=` or identifier
 fail_compilation/b19730.d(10): Error: found `)` while expecting `=` or identifier
+fail_compilation/b19730.d(11): Error: found `)` while expecting `=` or identifier
 ---
 */
 void func() {
