bc7aa71d5613a4d400c8ea4e3ab008644958350a
سليمان السهمي  (Suleyman Sahmi) <sahmi.soulaimane@gmail.com>
2019-03-18 17:14:51 +0000

    test cases

--- test/runnable/template10.d
+++ test/runnable/template10.d
@@ -53,8 +53,128 @@ void test2()
 
 /********************************************/
 
+struct S3
+{
+    int m;
+    auto exec(alias f)()
+    {
+        f(m);
+    }
+}
+
+void test3()
+{
+    int a = 1;
+
+    auto set(alias b)(int c) { b = c; }
+
+    S3(10).exec!(set!a)();
+    assert(a == 10);
+
+    auto dg = &S3(20).exec!(set!a);
+    dg();
+    assert(a == 20);
+}
+
+/********************************************/
+
+struct S4
+{
+    int m;
+
+    auto add(alias a)(int b)
+    {
+        return a + m + b;
+    }
+
+    auto inner(alias a)(int i)
+    {
+        class I
+        {
+            int n;
+            this() { n = i; }
+            auto add(int b)
+            {
+                return a + n + m + b;
+            }
+            auto add2(alias c)(int b)
+            {
+                return a + n + m + b + c;
+            }
+        }
+        return new I;
+    }
+}
+
+void test4i(T)(T i)
+{
+    int b = 2;
+
+    assert(i.add(7) == 5+1+6+7);
+    auto dg = &i.add;
+    assert(dg(8) == 5+1+6+8);
+
+    assert(i.add2!b(7) == 5+1+6+7+2);
+    auto dg2 = &i.add2!b;
+    assert(dg2(8) == 5+1+6+8+2);
+}
+
+auto get4i(int a)
+{
+    return S4(5).inner!a(6);
+}
+
+void test4()
+{
+    int a = 1;
+
+    assert(S4(4).add!a(10) == 4+1+10);
+
+    auto dg = &S4(4).add!a;
+    assert(dg(11) == 4+1+11);
+
+    auto i = get4i(a);
+    test4i(i);
+
+    // type checking
+    auto o0 = S4(10);
+    auto o1 = S4(11);
+    alias T0 = typeof(o0.inner!a(1));
+    alias T1 = typeof(o1.inner!a(1));
+    static assert(is(T0 == T1));
+}
+
+/********************************************/
+
+struct S5
+{
+    int m;
+    auto add(alias f)(int a)
+    {
+        auto add(int b) { return a + b; }
+        return exec2!(f, add)();
+    }
+    auto exec2(alias f, alias g)()
+    {
+        return g(f(m));
+    }
+}
+
+void test5()
+{
+    int a = 10;
+    auto o = S5(1);
+    auto fun(int m) { return m + a; }
+    assert(o.add!fun(20) == 1+10+20);
+}
+
+/********************************************/
+
 void main()
 {
     test1();
     test2();
+    test3();
+    test4();
+    test5();
 }
