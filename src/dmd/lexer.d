5e61922dcf3bd15deed1628a207d12a4d8cebb6f
Jacob Carlborg <doob@me.com>
2019-03-25 11:28:15 +0100

    Rename `Diagnostic` to `DiagnosticReporting`

--- src/dmd/lexer.d
+++ src/dmd/lexer.d
@@ -333,7 +333,7 @@ This is usually the default diagnostic reporter.
 */
 final class StderrDiagnosticReporter : DiagnosticReporter
 {
-    private const Diagnostic useDeprecated;
+    private const DiagnosticReporting useDeprecated;
 
     private int errorCount_;
     private int warningCount_;
@@ -346,7 +346,7 @@ final class StderrDiagnosticReporter : DiagnosticReporter
         useDeprecated = indicates how deprecation diagnostics should be
             handled
     */
-    this(Diagnostic useDeprecated)
+    this(DiagnosticReporting useDeprecated)
     {
         this.useDeprecated = useDeprecated;
     }
@@ -392,7 +392,7 @@ final class StderrDiagnosticReporter : DiagnosticReporter
     {
         vdeprecation(loc, format, args);
 
-        if (useDeprecated == Diagnostic.error)
+        if (useDeprecated == DiagnosticReporting.error)
             errorCount_++;
         else
             deprecationCount_++;
--- src/dmd/semantic3.d
+++ src/dmd/semantic3.d
@@ -1384,7 +1384,7 @@ private extern(C++) final class Semantic3Visitor : Visitor
 
         // don't do it for unused deprecated types
         // or error ypes
-        if (!ad.getRTInfo && Type.rtinfo && (!ad.isDeprecated() || global.params.useDeprecated != Diagnostic.error) && (ad.type && ad.type.ty != Terror))
+        if (!ad.getRTInfo && Type.rtinfo && (!ad.isDeprecated() || global.params.useDeprecated != DiagnosticReporting.error) && (ad.type && ad.type.ty != Terror))
         {
             // Evaluate: RTinfo!type
             auto tiargs = new Objects();
