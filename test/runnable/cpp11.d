1bb36043f0e27a6dfe649be796796267c38a31de
Martin Kinkelin <noone@nowhere.com>
2019-04-12 21:45:03 +0200

    Fix Issue 19658 - MSVC++ enum mangling

--- test/runnable/cpp11.d
+++ test/runnable/cpp11.d
@@ -5,6 +5,8 @@
 
 // Disabled on win32 because the compiler is too old
 
+import core.stdc.stdint;
+
 /****************************************/
 alias nullptr_t = typeof(null);
 
@@ -28,7 +30,43 @@ else
     }
 }
 
+/****************************************/
+// https://issues.dlang.org/show_bug.cgi?id=19658
+
+enum i8_19658 : int8_t { a }
+enum u8_19658 : uint8_t { a }
+enum i16_19658 : int16_t { a }
+enum u16_19658 : uint16_t { a }
+enum i32_19658 : int32_t { a }
+enum u32_19658 : uint32_t { a }
+enum i64_19658 : int64_t { a }
+enum u64_19658 : uint64_t { a }
+
+extern(C++) void test19658_i8(i8_19658);
+extern(C++) void test19658_u8(u8_19658);
+extern(C++) void test19658_i16(i16_19658);
+extern(C++) void test19658_u16(u16_19658);
+extern(C++) void test19658_i32(i32_19658);
+extern(C++) void test19658_u32(u32_19658);
+extern(C++) void test19658_i64(i64_19658);
+extern(C++) void test19658_u64(u64_19658);
+
+void test19658()
+{
+    test19658_i8(i8_19658.a);
+    test19658_u8(u8_19658.a);
+    test19658_i16(i16_19658.a);
+    test19658_u16(u16_19658.a);
+    test19658_i32(i32_19658.a);
+    test19658_u32(u32_19658.a);
+    test19658_i64(i64_19658.a);
+    test19658_u64(u64_19658.a);
+}
+
+/****************************************/
+
 void main()
 {
     test17();
+    test19658();
 }
