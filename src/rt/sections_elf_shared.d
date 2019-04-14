82805b1a04335b3228cdaff800ed8f3d610e796b
Iain Buclaw <ibuclaw@gdcproject.org>
2019-04-15 00:30:34 +0200

    rt.sections_elf_shared.d: Populate _tlsRanges in every thread

--- src/rt/sections_elf_shared.d
+++ src/rt/sections_elf_shared.d
@@ -263,7 +263,13 @@ else
      */
     Array!(void[])* initTLSRanges() nothrow @nogc
     {
-        return &_tlsRanges();
+        auto rngs = &_tlsRanges();
+        if (rngs.empty)
+        {
+            foreach (ref pdso; _loadedDSOs)
+                rngs.insertBack(pdso.tlsRange());
+        }
+        return rngs;
     }
 
     void finiTLSRanges(Array!(void[])* rngs) nothrow @nogc
