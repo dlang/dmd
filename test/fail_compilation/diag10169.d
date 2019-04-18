b7f2e634fc5d24b4a7eee5d69d57a5edff7c7d37
JinShil <slavo5150@yahoo.com>
2019-04-18 13:53:12 +0900

    Remove some redundant 'not visible from module' errors

--- test/fail_compilation/diag10169.d
+++ test/fail_compilation/diag10169.d
@@ -1,8 +1,7 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/diag10169.d(12): Error: `imports.a10169.B.x` is not visible from module `diag10169`
-fail_compilation/diag10169.d(12): Error: no property `x` for type `B`, did you mean `imports.a10169.B.x`?
+fail_compilation/diag10169.d(11): Error: no property `x` for type `B`, did you mean `imports.a10169.B.x`?
 ---
 */
 import imports.a10169;
--- test/fail_compilation/diag5385.d
+++ test/fail_compilation/diag5385.d
@@ -1,22 +1,18 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/diag5385.d(27): Error: `imports.fail5385.C.privX` is not visible from module `diag5385`
-fail_compilation/diag5385.d(27): Error: no property `privX` for type `imports.fail5385.C`, did you mean `imports.fail5385.C.privX`?
-fail_compilation/diag5385.d(28): Error: `imports.fail5385.C.packX` is not visible from module `diag5385`
-fail_compilation/diag5385.d(28): Error: no property `packX` for type `imports.fail5385.C`, did you mean `imports.fail5385.C.packX`?
-fail_compilation/diag5385.d(29): Error: `imports.fail5385.C.privX2` is not visible from module `diag5385`
-fail_compilation/diag5385.d(29): Error: no property `privX2` for type `imports.fail5385.C`, did you mean `imports.fail5385.C.privX2`?
-fail_compilation/diag5385.d(30): Error: `imports.fail5385.C.packX2` is not visible from module `diag5385`
-fail_compilation/diag5385.d(30): Error: no property `packX2` for type `imports.fail5385.C`, did you mean `imports.fail5385.C.packX2`?
-fail_compilation/diag5385.d(31): Error: `imports.fail5385.S.privX` is not visible from module `diag5385`
-fail_compilation/diag5385.d(31): Error: no property `privX` for type `S`, did you mean `imports.fail5385.S.privX`?
-fail_compilation/diag5385.d(32): Error: `imports.fail5385.S.packX` is not visible from module `diag5385`
-fail_compilation/diag5385.d(32): Error: no property `packX` for type `S`, did you mean `imports.fail5385.S.packX`?
-fail_compilation/diag5385.d(33): Error: `imports.fail5385.S.privX2` is not visible from module `diag5385`
-fail_compilation/diag5385.d(33): Error: no property `privX2` for type `S`, did you mean `imports.fail5385.S.privX2`?
-fail_compilation/diag5385.d(34): Error: `imports.fail5385.S.packX2` is not visible from module `diag5385`
-fail_compilation/diag5385.d(34): Error: no property `packX2` for type `S`, did you mean `imports.fail5385.S.packX2`?
+fail_compilation/diag5385.d(23): Error: `imports.fail5385.C.privX` is not visible from module `diag5385`
+fail_compilation/diag5385.d(23): Error: no property `privX` for type `imports.fail5385.C`, did you mean `imports.fail5385.C.privX`?
+fail_compilation/diag5385.d(24): Error: `imports.fail5385.C.packX` is not visible from module `diag5385`
+fail_compilation/diag5385.d(24): Error: no property `packX` for type `imports.fail5385.C`, did you mean `imports.fail5385.C.packX`?
+fail_compilation/diag5385.d(25): Error: `imports.fail5385.C.privX2` is not visible from module `diag5385`
+fail_compilation/diag5385.d(25): Error: no property `privX2` for type `imports.fail5385.C`, did you mean `imports.fail5385.C.privX2`?
+fail_compilation/diag5385.d(26): Error: `imports.fail5385.C.packX2` is not visible from module `diag5385`
+fail_compilation/diag5385.d(26): Error: no property `packX2` for type `imports.fail5385.C`, did you mean `imports.fail5385.C.packX2`?
+fail_compilation/diag5385.d(27): Error: no property `privX` for type `S`, did you mean `imports.fail5385.S.privX`?
+fail_compilation/diag5385.d(28): Error: no property `packX` for type `S`, did you mean `imports.fail5385.S.packX`?
+fail_compilation/diag5385.d(29): Error: no property `privX2` for type `S`, did you mean `imports.fail5385.S.privX2`?
+fail_compilation/diag5385.d(30): Error: no property `packX2` for type `S`, did you mean `imports.fail5385.S.packX2`?
 ---
 */
 
