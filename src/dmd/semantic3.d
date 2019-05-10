be4013b2aa1f5c7076869cbd9d1a48f3908277b6
سليمان السهمي  (Suleyman Sahmi) <sahmi.soulaimane@gmail.com>
2019-05-10 19:44:05 +0000

    Fix issue 15984 - Pass parameters explicitly by ref to contract functions

--- src/dmd/semantic3.d
+++ src/dmd/semantic3.d
@@ -878,8 +878,8 @@ private extern(C++) final class Semantic3Visitor : Visitor
                 sc2 = sc2.pop();
             }
 
-            funcdecl.frequire = funcdecl.mergeFrequire(funcdecl.frequire);
-            funcdecl.fensure = funcdecl.mergeFensure(funcdecl.fensure, Id.result);
+            funcdecl.frequire = funcdecl.mergeFrequire(funcdecl.frequire, funcdecl.fdrequireParams);
+            funcdecl.fensure = funcdecl.mergeFensure(funcdecl.fensure, Id.result, funcdecl.fdensureParams);
 
             Statement freq = funcdecl.frequire;
             Statement fens = funcdecl.fensure;
