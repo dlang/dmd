13521cbad884058d1fb8c5a6f67ede60b46e4e48
Hiroki Noda <kubo39@gmail.com>
2019-05-13 11:53:27 +0900

    Refactor type_jparam2 function
    Returns bool, not int.

--- src/dmd/backend/cod1.d
+++ src/dmd/backend/cod1.d
@@ -2921,18 +2921,18 @@ FuncParamRegs FuncParamRegs_create(tym_t tyf)
  * Params:
  *      t = type, valid only if ty is TYstruct or TYarray
  * Returns:
- *      0       not allocated to any register
- *      1       *preg1, *preg2 set to allocated register pair
+ *      false       not allocated to any register
+ *      true        *preg1, *preg2 set to allocated register pair
  */
 
-//int type_jparam2(type* t, tym_t ty);
+//bool type_jparam2(type* t, tym_t ty);
 
-private int type_jparam2(type* t, tym_t ty)
+private bool type_jparam2(type* t, tym_t ty)
 {
     ty = tybasic(ty);
 
     if (tyfloating(ty))
-    { }
+        return false;
     else if (ty == TYstruct || ty == TYarray)
     {
         type_debug(t);
@@ -2941,8 +2941,8 @@ private int type_jparam2(type* t, tym_t ty)
                (config.exe == EX_WIN64 || sz == 1 || sz == 2 || sz == 4 || sz == 8);
     }
     else if (tysize(ty) <= _tysize[TYnptr])
-        return 1;
-    return 0;
+        return true;
+    return false;
 }
 
 int FuncParamRegs_alloc(ref FuncParamRegs fpr, type* t, tym_t ty, reg_t* preg1, reg_t* preg2)
