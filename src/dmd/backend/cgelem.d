f15e0419949c4cf7ddac56b34b7c30869942fb26
سليمان السهمي  (Suleyman Sahmi) <sahmi.soulaimane@gmail.com>
2019-06-18 21:28:04 +0000

    Mark SROA with mixed registers with a flag make two new type flags for SROA `mTYxmmgpr` and `mTYgprxmm`

--- src/dmd/backend/cgelem.d
+++ src/dmd/backend/cgelem.d
@@ -3357,7 +3357,8 @@ elem * elstruct(elem *e, goal_t goal)
 
         L1:
             if (ty == TYstruct)
-            {   // This needs to match what TypeFunction::retStyle() does
+            {
+                // This needs to match what TypeFunction::retStyle() does
                 if (config.exe == EX_WIN64)
                 {
                     //if (t.Ttag.Sstruct.Sflags & STRnotpod)
@@ -3382,6 +3383,10 @@ elem * elstruct(elem *e, goal_t goal)
                         tym = TYcdouble;
                     else
                         tym = TYucent;
+                    if ((0 == tyfloating(targ1.Tty)) ^ (0 == tyfloating(targ2.Tty)))
+                    {
+                        tym |= tyfloating(targ1.Tty) ? mTYxmmgpr : mTYgprxmm;
+                    }
                 }
                 assert(tym != TYstruct);
             }
@@ -5752,6 +5757,8 @@ private elem *elToPair(elem *e)
              */
             tym_t ty0;
             tym_t ty = e.Ety;
+            if (ty & (mTYxmmgpr | mTYgprxmm))
+                break; // register allocation doesn't support it yet.
             switch (tybasic(ty))
             {
                 case TYcfloat:      ty0 = TYfloat  | (ty & ~mTYbasic); goto L1;
@@ -5778,6 +5785,8 @@ private elem *elToPair(elem *e)
              */
             tym_t ty0;
             tym_t ty = e.Ety;
+            if (ty & (mTYxmmgpr | mTYgprxmm))
+                break; // register allocation doesn't support it yet.
             switch (tybasic(ty))
             {
                 case TYcfloat:      ty0 = TYfloat  | (ty & ~mTYbasic); goto L2;
