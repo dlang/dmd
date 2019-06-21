5d67cfb0a82bb0777bf41f61938deb6f8263e7a0
سليمان السهمي  (Suleyman Sahmi) <sahmi.soulaimane@gmail.com>
2019-06-21 18:59:32 +0000

    C++ headers Integrity check - follows #10065

--- src/tests/cxxfrontend.c
+++ src/tests/cxxfrontend.c
@@ -227,6 +227,14 @@ void test_semantic()
     semantic2(m, NULL);
     semantic3(m, NULL);
 
+    Dsymbol *s = m->search(Loc(), Identifier::idPool("Error"));
+    assert(s);
+    AggregateDeclaration *ad = s->isAggregateDeclaration();
+    assert(ad && ad->ctor);
+    CtorDeclaration *ctor = ad->ctor->isCtorDeclaration();
+    assert(ctor->isMember() && !ctor->isNested());
+    assert(0 == strcmp(ctor->type->toChars(), "Error(string)"));
+
     assert(!global.endGagging(errors));
 }
 
