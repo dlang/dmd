a40a6f862ec6d57e223462f17aa8049244f34ac3
RazvanN7 <razvan.nitu1305@gmail.com>
2019-07-16 12:19:18 +0300

    Fix Issue 20051 - ICE in func literal used in __traits(compiles)

--- src/dmd/dsymbolsem.d
+++ src/dmd/dsymbolsem.d
@@ -6299,7 +6299,7 @@ void aliasSemantic(AliasDeclaration ds, Scope* sc)
     }
     if (global.gag && errors != global.errors)
     {
-        ds.type = oldtype;
+        ds.type = Type.terror;
         ds.aliassym = null;
     }
     ds.inuse = 0;
