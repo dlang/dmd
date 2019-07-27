91af080df2bc19ab270455cabad189a90ce35074
Rainer Schuetze <r.sagitario@gmx.de>
2019-07-27 20:47:21 +0200

    fix building tests with debug compiler

--- test/runnable/test18772.d
+++ test/runnable/test18772.d
@@ -8,5 +8,6 @@ void main()
     cfloat[1] A;
     float[1] B;
     int i = 0;
+    version(D_LP64) {} else // disabled because of wrong codegen: https://issues.dlang.org/show_bug.cgi?id=20089
     double C = fun(A[i] * B[i]);
 }
