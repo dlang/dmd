95d6012a20bc93b80dd6a1a7bc444d5fbbf36765
Dan Printzell <xwildn00bx@gmail.com>
2019-08-03 16:46:21 +0200

    Don't allow _d_arrayappend{T,cTX} to be called in CTFE
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- src/dmd/dinterpret.d
+++ src/dmd/dinterpret.d
@@ -4837,6 +4837,10 @@ public:
                 result = interpret(ce, istate);
                 return;
             }
+            else if (fd.ident == Id._d_arrayappendT)
+                assert(0, "CTFE cannot interpret _d_arrayappendT!");
+            else if (fd.ident == Id._d_arrayappendcTX)
+                assert(0, "CTFE cannot interpret _d_arrayappendcTX!");
         }
         else if (auto soe = ecall.isSymOffExp())
         {
