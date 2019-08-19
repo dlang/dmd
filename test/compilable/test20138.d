98d27f2574b09869236182dc503f44af4a1389bc
aG0aep6G <aG0aep6G@users.noreply.github.com>
2019-08-19 15:36:31 +0200

    remove stray newline

--- test/compilable/test20138.d
+++ test/compilable/test20138.d
@@ -2,7 +2,6 @@ alias C = const int;
 static assert(is(shared(C) U == shared U) && is(U == C));
 static assert(is(shared(C) == shared U, U) && is(U == C));
 
-
 alias I = inout int;
 static assert(is(shared(I) U == shared U) && is(U == I));
 static assert(is(shared(I) == shared U, U) && is(U == I));
