462e6cad7e323114e18dea96444a7674a6e8b0a8
Iain Buclaw <ibuclaw@gdcproject.org>
2019-08-26 17:05:23 +0200

    Remove headers now redundant since conversion to D

--- src/dmd/root/dsystem.h
+++ src/dmd/root/dsystem.h
@@ -12,8 +12,6 @@
 
 // Get common system includes from the host.
 
-#define POSIX (__linux__ || __GLIBC__ || __gnu_hurd__ || __APPLE__ || __FreeBSD__ || __OpenBSD__ || __DragonFly__  || __sun)
-
 #define __C99FEATURES__ 1       // Needed on Solaris for NaN and more
 #define __USE_ISOC99 1          // so signbit() gets defined
 
@@ -22,52 +20,11 @@
 #endif
 
 #include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <limits.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 
 #include <new>
-
-#if POSIX
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#endif
-
-// For alloca()
-#if _MSC_VER
-#include <alloca.h>
-#endif
-#if defined (__sun)
-#include <alloca.h>
-#endif
-
-// For getcwd()
-#if _WIN32
-#include <direct.h>
-#endif
-#if POSIX
-#include <unistd.h>
-#endif
-
-// For malloc()
-#if _MSC_VER
-#include <malloc.h>
-#elif __MINGW32__
-#include <malloc.h>
-#endif
-
-// If not present, dmc will error 'number is not representable'.
-#ifdef __DMC__
-#undef UINT64_MAX
-#define UINT64_MAX      18446744073709551615ULL
-#undef UINT32_MAX
-#define UINT32_MAX      4294967295U
-#endif
