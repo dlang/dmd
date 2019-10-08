2146390959bc25df42f1154101942e932f3a8ca4
Walter Bright <WalterBright@users.noreply.github.com>
2019-10-07 21:43:38 -0700

    Revert "Remove backend globals from header modules"

--- src/dmd/backend/cod2.d
+++ src/dmd/backend/cod2.d
@@ -26,7 +26,6 @@ import core.stdc.string;
 
 import dmd.backend.cc;
 import dmd.backend.cdef;
-import dmd.backend.cg87;
 import dmd.backend.code;
 import dmd.backend.code_x86;
 import dmd.backend.codebuilder;
