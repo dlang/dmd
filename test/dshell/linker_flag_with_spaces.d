3182209820853e794ffd9e054be1d5b8cf99ef17
Martin Kinkelin <noone@nowhere.com>
2019-10-10 23:21:55 +0200

    Work around dshell framework limitation

--- test/dshell/linker_flag_with_spaces.d
+++ test/dshell/linker_flag_with_spaces.d
@@ -1,9 +1,23 @@
 import dshell;
+
+void expandAndRun(string[] args...)
+{
+    import std.algorithm.iteration, std.array;
+    const expandedArgs = args
+        .map!(a => a.replace("/", "${SEP}"))
+        .map!shellExpand
+        .array;
+    run(expandedArgs);
+}
+
 void main()
 {
     const dir = "$OUTPUT_BASE/dir with spaces";
     const lib = dir ~ "/b$LIBEXT";
 
-    run(`$DMD -m$MODEL "-of`~lib~`" -lib $EXTRA_FILES/linker_flag_with_spaces_b.d`);
-    run(`$DMD -m$MODEL -I$EXTRA_FILES -of$OUTPUT_BASE/a$EXE $EXTRA_FILES/linker_flag_with_spaces_a.d "-L`~lib~`"`);
+    mkdirFor(lib);
+    expandAndRun("$DMD", "-m$MODEL", "-of" ~ lib,
+        "-lib", "$EXTRA_FILES/linker_flag_with_spaces_b.d");
+    expandAndRun("$DMD", "-m$MODEL", "-I$EXTRA_FILES", "-of$OUTPUT_BASE/a$EXE",
+        "$EXTRA_FILES/linker_flag_with_spaces_a.d", "-L" ~ lib);
 }
