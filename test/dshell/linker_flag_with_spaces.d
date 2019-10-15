ae587dbf7b0bbe8766733ef29dab96ca2d4528f2
Martin Kinkelin <noone@nowhere.com>
2019-10-15 23:38:59 +0200

    Simplify test and disable test for OMF target
    As DMD doesn't support specifying libs as direct linker flags for
    Optlink.

--- test/dshell/linker_flag_with_spaces.d
+++ test/dshell/linker_flag_with_spaces.d
@@ -1,23 +1,18 @@
 import dshell;
 
-void expandAndRun(string[] args...)
-{
-    import std.algorithm.iteration, std.array;
-    const expandedArgs = args
-        .map!(a => a.replace("/", "${SEP}"))
-        .map!shellExpand
-        .array;
-    run(expandedArgs);
-}
-
 void main()
 {
-    const dir = "$OUTPUT_BASE/dir with spaces";
-    const lib = dir ~ "/b$LIBEXT";
+    version (DigitalMars)
+    {
+        if (OS == "windows" && MODEL == "32")
+        {
+            writeln("Skipping test when using Optlink.");
+            return;
+        }
+    }
 
-    mkdirFor(lib);
-    expandAndRun("$DMD", "-m$MODEL", "-of" ~ lib,
-        "-lib", "$EXTRA_FILES/linker_flag_with_spaces_b.d");
-    expandAndRun("$DMD", "-m$MODEL", "-I$EXTRA_FILES", "-of$OUTPUT_BASE/a$EXE",
-        "$EXTRA_FILES/linker_flag_with_spaces_a.d", "-L" ~ lib);
+    Vars.set("lib", "$OUTPUT_BASE/dir with spaces/b$LIBEXT");
+
+    run("$DMD -m$MODEL -of$lib -lib $EXTRA_FILES/linker_flag_with_spaces_b.d");
+    run("$DMD -m$MODEL -I$EXTRA_FILES -of$OUTPUT_BASE/a$EXE $EXTRA_FILES/linker_flag_with_spaces_a.d -L$lib");
 }
