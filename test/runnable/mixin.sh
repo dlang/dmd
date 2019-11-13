74cde9b1d005fa5f9b7e420eaf7a32c04353f669
Alexandru Militaru <alexandru.cmilitaru@gmail.com>
2019-11-13 15:07:24 +0200

    updated with -o- flag

--- test/runnable/mixin.sh
+++ test/runnable/mixin.sh
@@ -4,9 +4,8 @@ src_file=${EXTRA_FILES}/${TEST_NAME}.d
 expect_file=${EXTRA_FILES}/${TEST_NAME}.mixin
 tmp_file=${RESULTS_TEST_DIR}/${TEST_NAME}.mixin
 
-$DMD -c -mixin=${tmp_file} -g ${src_file}
+$DMD -o- -mixin=${tmp_file} -g ${src_file}
 tr '\\' '/' < ${tmp_file} > ${tmp_file}2
 diff -pu --strip-trailing-cr ${expect_file} ${tmp_file}2
 
 rm ${tmp_file} ${tmp_file}2
-rm ${TEST_NAME}.o
--- test/runnable/test_switches.sh
+++ test/runnable/test_switches.sh
@@ -18,7 +18,6 @@ src_file=${OUTPUT_BASE}/src.d
 clean()
 {
     rm -rf ${OUTPUT_BASE}
-    rm -rf mymod.o
 }
 
 prepare()
@@ -56,11 +55,11 @@ checkFiles()
 # as there's no common linker switch which all linkers support
 
 prepare;
-$DMD -c -of=mymod.o -od=${OUTPUT_BASE} -D -Df=${OUTPUT_BASE}/src.html -Hf=${OUTPUT_BASE}/src.di -I=${OUTPUT_BASE} -L=-v -Xf=${OUTPUT_BASE}/json.json ${src_file}
+$DMD -o- -od=${OUTPUT_BASE} -D -Df=${OUTPUT_BASE}/src.html -Hf=${OUTPUT_BASE}/src.di -I=${OUTPUT_BASE} -L=-v -Xf=${OUTPUT_BASE}/json.json ${src_file}
 checkFiles;
 
 prepare;
-$DMD -c -of=mymod.o -od=${OUTPUT_BASE} -D -Dd=${OUTPUT_BASE} -Hd=${OUTPUT_BASE} -I=${OUTPUT_BASE} -L=-v -Xf=${OUTPUT_BASE}/json.json ${src_file}
+$DMD -o- -od=${OUTPUT_BASE} -D -Dd=${OUTPUT_BASE} -Hd=${OUTPUT_BASE} -I=${OUTPUT_BASE} -L=-v -Xf=${OUTPUT_BASE}/json.json ${src_file}
 checkFiles;
 
 clean;
