9bcdf83796e622606c23d3ec618fd89af110b2af
Per Nordlöw <per.nordlow@gmail.com>
2019-11-29 13:08:27 +0100

    Remove assert on return value of search_function

--- src/dmd/mtype.d
+++ src/dmd/mtype.d
@@ -6752,7 +6752,7 @@ bool isIndexableNonAggregate(Type t)
  * Returns:
  *      true if we can copy it
  */
-bool isCopyable(const Type t)
+bool isCopyable(Type t)
 {
     //printf("isCopyable() %s\n", t.toChars());
     if (auto ts = t.isTypeStruct())
@@ -6763,15 +6763,17 @@ bool isCopyable(const Type t)
         if (ts.sym.hasCopyCtor)
         {
             // check if there is a matching overload of the copy constructor and whether it is disabled or not
-            Dsymbol ctor = search_function(cast() ts.sym, Id.This);
-            assert(ctor);
-            scope el = new IdentifierExp(Loc.initial, Id.p); // dummy lvalue
-            el.type = cast() ts;
-            Expressions args;
-            args.push(el);
-            FuncDeclaration f = resolveFuncCall(Loc.initial, null, ctor, null, cast()ts, &args, FuncResolveFlag.quiet);
-            if (!f || f.storage_class & STC.disable)
-                return false;
+            // `assert(ctor)` fails on Win32 and Win_32_64. See: https://auto-tester.puremagic.com/pull-history.ghtml?projectid=1&repoid=1&pullid=10575
+            if (Dsymbol ctor = search_function(ts.sym, Id.This))
+            {
+                scope el = new IdentifierExp(Loc.initial, Id.p); // dummy lvalue
+                el.type = cast() ts;
+                Expressions args;
+                args.push(el);
+                FuncDeclaration f = resolveFuncCall(Loc.initial, null, ctor, null, cast()ts, &args, FuncResolveFlag.quiet);
+                if (!f || f.storage_class & STC.disable)
+                    return false;
+            }
         }
     }
     return true;
