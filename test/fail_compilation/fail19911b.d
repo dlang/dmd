205be4e32552724e973a48ce26b5d60629918e29
Iain Buclaw <ibuclaw@gdcproject.org>
2019-12-08 10:20:18 +0100

    test: Use EXTRA_SOURCES instead of REQUIRED_ARGS for sources

--- test/fail_compilation/fail19911b.d
+++ test/fail_compilation/fail19911b.d
@@ -1,6 +1,6 @@
 /* 
 DFLAGS:
-REQUIRED_ARGS: -I=fail_compilation/extra-files/minimal/
+EXTRA_SOURCES: extra-files/minimal/object.d
 TEST_OUTPUT:
 ---
 fail_compilation/fail19911b.d(10): Error: function `fail19911b.fun` `object.TypeInfo_Tuple` could not be found, but is implicitly used in D-style variadic functions
