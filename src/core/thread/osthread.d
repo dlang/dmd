19ef6336d1940e770d696dc71f7605eb3f618a85
Jacob Carlborg <doob@me.com>
2020-01-02 22:21:47 +0100

    Add support for suspending threads on Apple ARM

--- src/core/thread/osthread.d
+++ src/core/thread/osthread.d
@@ -2768,6 +2768,45 @@ private bool suspend( Thread t ) nothrow
             t.m_reg[14] = state.r14;
             t.m_reg[15] = state.r15;
         }
+        else version (AArch64)
+        {
+            arm_thread_state64_t state = void;
+            mach_msg_type_number_t count = ARM_THREAD_STATE64_COUNT;
+
+            if (thread_get_state(t.m_tmach, ARM_THREAD_STATE64, &state, &count) != KERN_SUCCESS)
+                onThreadError("Unable to load thread state");
+            // TODO: ThreadException here recurses forever!  Does it
+            //still using onThreadError?
+            //printf("state count %d (expect %d)\n", count ,ARM_THREAD_STATE64_COUNT);
+            if (!t.m_lock)
+                t.m_curr.tstack = cast(void*) state.sp;
+
+            t.m_reg[0..29] = state.x;  // x0-x28
+            t.m_reg[29] = state.fp;    // x29
+            t.m_reg[30] = state.lr;    // x30
+            t.m_reg[31] = state.sp;    // x31
+            t.m_reg[32] = state.pc;
+        }
+        else version (ARM)
+        {
+            arm_thread_state32_t state = void;
+            mach_msg_type_number_t count = ARM_THREAD_STATE32_COUNT;
+
+            // Thought this would be ARM_THREAD_STATE32, but that fails.
+            // Mystery
+            if (thread_get_state(t.m_tmach, ARM_THREAD_STATE, &state, &count) != KERN_SUCCESS)
+                onThreadError("Unable to load thread state");
+            // TODO: in past, ThreadException here recurses forever!  Does it
+            //still using onThreadError?
+            //printf("state count %d (expect %d)\n", count ,ARM_THREAD_STATE32_COUNT);
+            if (!t.m_lock)
+                t.m_curr.tstack = cast(void*) state.sp;
+
+            t.m_reg[0..13] = state.r;  // r0 - r13
+            t.m_reg[13] = state.sp;
+            t.m_reg[14] = state.lr;
+            t.m_reg[15] = state.pc;
+        }
         else
         {
             static assert(false, "Architecture not supported." );
