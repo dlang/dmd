847c8ce847f9df23283a18c0c31546de23aeca90
MoonlightSentinel <moonlightsentinel@disroot.org>
2020-02-01 20:30:37 +0100

    Add missing TEST_OUTPUT sections to most tests in fail_compilation
    These test were simply missing an appropriate section. The remaining ones need
    some extensions to d_do_test.d to handle variable output.

--- test/fail_compilation/ice7782.d
+++ test/fail_compilation/ice7782.d
@@ -1,3 +1,13 @@
+/*
+TEST_OUTPUT:
+----
+fail_compilation/ice7782.d(12): Error: module `ice7782math` is in file 'imports/ice7782range/imports/ice7782math.d' which cannot be read
+import path[0] = fail_compilation
+import path[1] = $p:druntime/import$
+import path[2] = $p:phobos$
+----
+*/
+
 import imports.ice7782algorithm;
 import imports.ice7782range. imports.ice7782math;
 
