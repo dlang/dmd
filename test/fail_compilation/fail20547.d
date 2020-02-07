0d85d8b5c0e612539cf7350f6fb9d34ee4bf05e0
Ben Jones <ben.james.jones@gmail.com>
2020-02-06 20:56:25 -0700

    merge malformed new exp tests

--- test/fail_compilation/fail20547.d
+++ test/fail_compilation/fail20547.d
@@ -1,13 +1,15 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail20547.d(11): Error: cannot create a `int[string]` with `new`
-fail_compilation/fail20547.d(12): Error: cannot create a `int[string]` with `new`
+fail_compilation/fail20547.d(12): Error: cannot create a `string[string]` with `new`
+fail_compilation/fail20547.d(14): Error: cannot create a `string[string]` with `new`
 ---
 */
 
 void main()
 {
-    int[string] b = new int[string];
-    int[string] c = new typeof(b);
+    //https://issues.dlang.org/show_bug.cgi?id=11790
+    string[string] crash = new string[string];
+    //https://issues.dlang.org/show_bug.cgi?id=20547
+    int[string] c = new typeof(crash);
 }
--- test/fail_compilation/ice11790.d
+++
@@ -1,8 +0,0 @@
-/*
-TEST_OUTPUT:
----
-fail_compilation/ice11790.d(8): Error: cannot create a `string[string]` with `new`
----
-*/
-
-string[string] crash = new string[string];
