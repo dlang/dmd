0d45cd756561da4c0dbaf57ec26503422eb71289
MoonlightSentinel <moonlightsentinel@disroot.org>
2020-03-14 18:03:07 +0100

    Make printenv.d print all variables except BUILD_SOURCEVERSIONAUTHOR
    Instead of only printing the value of BUILD_SOURCEVERSIONAUTHOR.

--- test/dshell/extra-files/printenv.d
+++ test/dshell/extra-files/printenv.d
@@ -4,7 +4,7 @@ auto filterEnv(T)(T env)
 {
     // Windows shell output is not in UTF-8
     // https://github.com/dlang/phobos/pull/7342#issuecomment-571154708
-    return env.filter!(e => e.key.among("BUILD_SOURCEVERSIONAUTHOR"));
+    return env.filter!(e => !e.key.among("BUILD_SOURCEVERSIONAUTHOR"));
 }
 
 void main()
