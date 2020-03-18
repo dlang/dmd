f13b3f9cca827dc58c80b9fdbb161aa35cb1ecc3
Ben Jones <ben.james.jones@gmail.com>
2020-03-18 08:56:51 -0600

    add test to make sure instantions are properly emitted

--- test/compilable/test20653.d
+++ test/compilable/test20653.d
@@ -1,4 +1,4 @@
-
+// LINK:
 enum x = 10;
 enum Thing { A, B }
 enum IsThing(T) = is(T == enum) || (__traits(getLinkage, T) == "C++");
@@ -9,3 +9,12 @@ static assert(Is);
 enum Another(T) = is(T == int) && (__traits(getLinkage, T) == "C++");
 
 static assert(!Another!Thing);
+
+T myFunc(T)() { return T.init; }
+enum Foo(T) = is(T == int) || myFunc!T();
+
+void main ()
+{
+    assert(myFunc!int() == 0);
+}
+
