4800987680baaf7f34efdf41d7ab01a9a8c4f696
Martin Kinkelin <noone@nowhere.com>
2020-04-18 17:57:10 +0200

    Restructure core.stdc.stdarg for simpler extensibility
    There's a whole lot of additional architectures and ABIs out there.
    For LDC, the va_{start,copy,end} functions are compiler intrinsics, but
    va_arg is to be implemented in druntime, so adding support for other
    ABIs mostly consists of extending va_arg. This restructuring makes that
    painless.

--- src/core/stdc/stdarg.d
+++ src/core/stdc/stdarg.d
@@ -3,7 +3,7 @@
  *
  * $(C_HEADER_DESCRIPTION pubs.opengroup.org/onlinepubs/009695399/basedefs/_stdarg.h.html, _stdarg.h)
  *
- * Copyright: Copyright Digital Mars 2000 - 2009.
+ * Copyright: Copyright Digital Mars 2000 - 2020.
  * License:   $(HTTP www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
  * Authors:   Walter Bright, Hauke Duden
  * Standards: ISO/IEC 9899:1999 (E)
@@ -15,6 +15,28 @@ module core.stdc.stdarg;
 @system:
 //@nogc:    // Not yet, need to make TypeInfo's member functions @nogc first
 nothrow:
+
+version (X86_64)
+{
+    version (Windows) { /* different ABI */ }
+    else version = SysV_x64;
+}
+
+version (SysV_x64)
+{
+    static import core.internal.vararg.sysv_x64;
+
+    version (DigitalMars)
+    {
+        align(16) struct __va_argsave_t
+        {
+            size_t[6] regs;   // RDI,RSI,RDX,RCX,R8,R9
+            real[8] fpregs;   // XMM0..XMM7
+            __va_list va;
+        }
+    }
+}
+
 
 T alignUp(size_t alignment = size_t.sizeof, T)(T base) pure
 {
@@ -34,49 +56,102 @@ unittest
     assert((-9).alignUp!8 == -8);
 }
 
+
+/**
+ * The argument pointer type.
+ */
+version (SysV_x64)
+{
+    alias va_list = core.internal.vararg.sysv_x64.va_list;
+    public import core.internal.vararg.sysv_x64 : __va_list, __va_list_tag;
+}
+else
+{
+    alias va_list = char*;
+}
+
+
+/**
+ * Initialize ap.
+ * parmn should be the last named parameter;
+ * for DMD and non-Windows x86_64 targets, it should be __va_argsave.
+ */
 version (X86)
 {
-    /*********************
-     * The argument pointer type.
-     */
-    alias char* va_list;
-
-    /**********
-     * Initialize ap.
-     * For 32 bit code, parmn should be the last named parameter.
-     * For 64 bit code, parmn should be __va_argsave.
-     */
     void va_start(T)(out va_list ap, ref T parmn)
     {
         ap = cast(va_list) ((cast(void*) &parmn) + T.sizeof.alignUp);
     }
+}
+else
+{
+    void va_start(T)(out va_list ap, ref T parmn); // Compiler intrinsic
+}
 
-    /************
-     * Retrieve and return the next value that is type T.
-     * Should use the other va_arg instead, as this won't work for 64 bit code.
-     */
-    T va_arg(T)(ref va_list ap)
+
+/**
+ * Retrieve and return the next value that is of type T.
+ */
+T va_arg(T)(ref va_list ap)
+{
+    version (X86)
     {
         T arg = *cast(T*) ap;
         ap += T.sizeof.alignUp;
         return arg;
     }
+    else version (Win64)
+    {
+        static if (T.sizeof > size_t.sizeof)
+            T arg = **cast(T**) ap;
+        else
+            T arg = *cast(T*) ap;
+        ap += size_t.sizeof;
+        return arg;
+    }
+    else
+    {
+        T a;
+        va_arg(ap, a);
+        return a;
+    }
+}
 
-    /************
-     * Retrieve and return the next value that is type T.
-     * This is the preferred version.
-     */
-    void va_arg(T)(ref va_list ap, ref T parmn)
+
+/**
+ * Retrieve and store in parmn the next value that is of type T.
+ */
+void va_arg(T)(ref va_list ap, ref T parmn)
+{
+    version (X86)
     {
         parmn = *cast(T*) ap;
         ap += T.sizeof.alignUp;
     }
+    else version (Win64)
+    {
+        static if (T.sizeof > size_t.sizeof)
+            parmn = **cast(T**) ap;
+        else
+            parmn = *cast(T*) ap;
+        ap += size_t.sizeof;
+    }
+    else version (SysV_x64)
+    {
+        core.internal.vararg.sysv_x64.va_arg!T(ap, parmn);
+    }
+    else
+        static assert(0, "Unsupported platform");
+}
 
-    /*************
-     * Retrieve and store through parmn the next value that is of TypeInfo ti.
-     * Used when the static type is not known.
-     */
-    void va_arg()(ref va_list ap, TypeInfo ti, void* parmn)
+
+/**
+ * Retrieve and store through parmn the next value that is of TypeInfo ti.
+ * Used when the static type is not known.
+ */
+void va_arg()(ref va_list ap, TypeInfo ti, void* parmn)
+{
+    version (X86)
     {
         // Wait until everyone updates to get TypeInfo.talign
         //auto talign = ti.talign;
@@ -86,68 +161,7 @@ version (X86)
         ap = cast(va_list) (p + tsize.alignUp);
         parmn[0..tsize] = p[0..tsize];
     }
-
-    /***********************
-     * End use of ap.
-     */
-    void va_end(va_list ap)
-    {
-    }
-
-    ///
-    void va_copy(out va_list dest, va_list src)
-    {
-        dest = src;
-    }
-}
-else version (Windows) // Win64
-{   /* Win64 is characterized by all arguments fitting into a register size.
-     * Smaller ones are padded out to register size, and larger ones are passed by
-     * reference.
-     */
-
-    /*********************
-     * The argument pointer type.
-     */
-    alias char* va_list;
-
-    /**********
-     * Initialize ap.
-     * parmn should be the last named parameter.
-     */
-    void va_start(T)(out va_list ap, ref T parmn); // Compiler intrinsic
-
-    /************
-     * Retrieve and return the next value that is type T.
-     */
-    T va_arg(T)(ref va_list ap)
-    {
-        static if (T.sizeof > size_t.sizeof)
-            T arg = **cast(T**) ap;
-        else
-            T arg = *cast(T*) ap;
-        ap += size_t.sizeof;
-        return arg;
-    }
-
-    /************
-     * Retrieve and return the next value that is type T.
-     * This is the preferred version.
-     */
-    void va_arg(T)(ref va_list ap, ref T parmn)
-    {
-        static if (T.sizeof > size_t.sizeof)
-            parmn = **cast(T**) ap;
-        else
-            parmn = *cast(T*) ap;
-        ap += size_t.sizeof;
-    }
-
-    /*************
-     * Retrieve and store through parmn the next value that is of TypeInfo ti.
-     * Used when the static type is not known.
-     */
-    void va_arg()(ref va_list ap, TypeInfo ti, void* parmn)
+    else version (Win64)
     {
         // Wait until everyone updates to get TypeInfo.talign
         //auto talign = ti.talign;
@@ -158,46 +172,26 @@ else version (Windows) // Win64
         void* q = (tsize > size_t.sizeof) ? *cast(void**) p : p;
         parmn[0..tsize] = q[0..tsize];
     }
-
-    /***********************
-     * End use of ap.
-     */
-    void va_end(va_list ap)
+    else version (SysV_x64)
     {
+        core.internal.vararg.sysv_x64.va_arg(ap, ti, parmn);
     }
-
-    ///
-    void va_copy(out va_list dest, va_list src)
-    {
-        dest = src;
-    }
+    else
+        static assert(0, "Unsupported platform");
 }
-else version (X86_64)
+
+
+/**
+ * End use of ap.
+ */
+void va_end(va_list ap)
 {
-    public import core.internal.vararg.sysv_x64 : va_list, va_arg, __va_list, __va_list_tag;
+}
 
-    align(16) struct __va_argsave_t
-    {
-        size_t[6] regs;   // RDI,RSI,RDX,RCX,R8,R9
-        real[8] fpregs;   // XMM0..XMM7
-        __va_list va;
-    }
 
-    ///
-    void va_start(T)(out va_list ap, ref T parmn); // Compiler intrinsic
-
-    ///
-    T va_arg(T)(va_list ap)
-    {   T a;
-        va_arg(ap, a);
-        return a;
-    }
-
-    ///
-    void va_end(va_list ap)
-    {
-    }
-
+// va_copy
+version (SysV_x64)
+{
     import core.stdc.stdlib : alloca;
 
     ///
@@ -215,5 +209,9 @@ else version (X86_64)
 }
 else
 {
-    static assert(false, "Unsupported platform");
+    ///
+    void va_copy(out va_list dest, va_list src)
+    {
+        dest = src;
+    }
 }
