b4ebaa899ca55d339f0ddfaf0e95c262c6b2204b
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2020-04-21 20:27:18 +0800

    [rt.trace] AArch64 implementation and LDC reposition

--- src/rt/trace.d
+++ src/rt/trace.d
@@ -875,6 +875,29 @@ version (Windows)
         export int QueryPerformanceFrequency(timer_t *);
     }
 }
+else version (AArch64)
+{
+    // We cannot use ldc.intrinsics.llvm_readcyclecounter because that is not an accurate
+    // time counter (it is a counter of CPU cycles, where here we want a time clock).
+    // Also, priviledged execution rights are needed to enable correct counting with
+    // ldc.intrinsics.llvm_readcyclecounter on AArch64.
+    extern (D) void QueryPerformanceCounter(timer_t* ctr)
+    {
+        asm { "mrs %0, cntvct_el0" : "=r" (*ctr); }
+    }
+    extern (D) void QueryPerformanceFrequency(timer_t* freq)
+    {
+        asm { "mrs %0, cntfrq_el0" : "=r" (*freq); }
+    }
+}
+else version (LDC)
+{
+    extern (D) void QueryPerformanceCounter(timer_t* ctr)
+    {
+        import ldc.intrinsics: llvm_readcyclecounter;
+        *ctr = llvm_readcyclecounter();
+    }
+}
 else
 {
     extern (D) void QueryPerformanceCounter(timer_t* ctr)
@@ -905,11 +928,6 @@ else
                 mov   4[RDI],EDX        ;
                 ret                     ;
             }
-        }
-        else version (LDC)
-        {
-            import ldc.intrinsics: llvm_readcyclecounter;
-            *ctr = llvm_readcyclecounter();
         }
         else
         {
