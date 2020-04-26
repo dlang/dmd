8943559d221a67c7ca4783fa4365c8bc4520cb0e
Martin Kinkelin <noone@nowhere.com>
2020-04-26 17:19:29 +0200

    core.cpuid: Simplify GDC-style inline asm for 32-bit x86 hasCPUID()
    Only use the AT&T syntax, making it compatible with LDC too.

--- src/core/cpuid.d
+++ src/core/cpuid.d
@@ -977,21 +977,18 @@ bool hasCPUID()
     else
     {
         uint flags;
-        version (GNU)
+        version (GNU_OR_LDC)
         {
             // http://wiki.osdev.org/CPUID#Checking_CPUID_availability
-            // ASM template supports both AT&T and Intel syntax.
             asm nothrow @nogc { "
-                pushf{l|d}                 # Save EFLAGS
-                pushf{l|d}                 # Store EFLAGS
-                xor{l $0x00200000, (%%esp)| dword ptr [esp], 0x00200000}
-                                           # Invert the ID bit in stored EFLAGS
-                popf{l|d}                  # Load stored EFLAGS (with ID bit inverted)
-                pushf{l|d}                 # Store EFLAGS again (ID bit may or may not be inverted)
-                pop {%%}eax                # eax = modified EFLAGS (ID bit may or may not be inverted)
-                xor {(%%esp), %%eax|eax, [esp]}
-                                           # eax = whichever bits were changed
-                popf{l|d}                  # Restore original EFLAGS
+                pushfl                    # Save EFLAGS
+                pushfl                    # Store EFLAGS
+                xorl $0x00200000, (%%esp) # Invert the ID bit in stored EFLAGS
+                popfl                     # Load stored EFLAGS (with ID bit inverted)
+                pushfl                    # Store EFLAGS again (ID bit may or may not be inverted)
+                popl %%eax                # eax = modified EFLAGS (ID bit may or may not be inverted)
+                xorl (%%esp), %%eax       # eax = whichever bits were changed
+                popfl                     # Restore original EFLAGS
                 " : "=a" (flags);
             }
         }
