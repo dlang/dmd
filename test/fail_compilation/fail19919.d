85b28ee6672dbe6c528170e48a5768d5ad143285
Hiroki Noda <kubo39@gmail.com>
2020-04-30 23:38:31 +0900

    Turn deprecation into error incorrect initialization of union

--- test/fail_compilation/fail19919.d
+++ test/fail_compilation/fail19919.d
@@ -1,26 +1,23 @@
-// REQUIRED_ARGS: -de
+// REQUIRED_ARGS:
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail19919.d(17): Deprecation: union field `f` with default initialization `3.14F` must be before field `n`
-fail_compilation/fail19919.d(24): Deprecation: union field `f` with default initialization `3.14F` must be before field `n`
+fail_compilation/fail19919.d(15): Error: union field `f` with default initialization `3.14F` must be before field `n`
+fail_compilation/fail19919.d(22): Error: union field `f` with default initialization `3.14F` must be before field `n`
 ---
 */
 
-void main()
+struct X
 {
-    struct X
-    {
-        union
-        {
-            int n;
-            float f = 3.14f;
-        }
-    }
-
-    union U
+    union
     {
         int n;
         float f = 3.14f;
     }
+}
+
+union U
+{
+    int n;
+    float f = 3.14f;
 }
