d1f7ced72a342aec413610aa4f0975e07f708351
Iain Buclaw <ibuclaw@gdcproject.org>
2020-06-27 01:05:33 +0200

    core.internal.elf: Add support for NetBSD

--- src/core/internal/elf/io.d
+++ src/core/internal/elf/io.d
@@ -34,6 +34,11 @@ else version (DragonFlyBSD)
     import core.sys.dragonflybsd.sys.link_elf;
     version = LinuxOrBSD;
 }
+else version (NetBSD)
+{
+    import core.sys.netbsd.sys.link_elf;
+    version = LinuxOrBSD;
+}
 else version (OpenBSD)
 {
     import core.sys.openbsd.sys.link_elf;
