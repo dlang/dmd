dlang/dmd

commit 457074e8ea12a56d9db9a8edb9427e21e15ee94f

optimize memory layout of aggregates
On x86_64, StructDeclaration stood on 569 bytes, now 560 which saves allocation of a small block.

src/dmd/aggregate.h
@@ -76,11 +76,9 @@ class AggregateDeclaration : public ScopeDsymbol
 public:
     Type *type;
     StorageClass storage_class;
-    Prot protection;
     unsigned structsize;        // size of struct
     unsigned alignsize;         // size of struct for alignment purposes
     VarDeclarations fields;     // VarDeclaration fields
-    Sizeok sizeok;              // set when structsize contains valid data
     Dsymbol *deferred;          // any deferred semantic2() or semantic3() symbol
 
     ClassKind::Type classKind;  // specifies the linkage type
@@ -108,7 +106,6 @@ public:
     CtorDeclaration *defaultCtor;
 
     AliasThis *aliasthis;       // forward unresolved lookups to aliasthis
-    bool noDefaultCtor;         // no default construction
 
     DtorDeclarations dtors;     // Array of destructors
     DtorDeclaration *dtor;      // aggregate destructor
@@ -117,6 +114,10 @@ public:
     FuncDeclaration *fieldDtor;   // aggregate destructor for just the fields
 
     Expression *getRTInfo;      // pointer to GC info generated by object.RTInfo(this)
+
+    Prot protection;
+    Sizeok sizeok;              // set when structsize contains valid data
+    bool noDefaultCtor;         // no default construction
 
     virtual Scope *newScope(Scope *sc);
     void setScope(Scope *sc);
@@ -163,10 +164,14 @@ public:
     bool hasBlitAssign;         // true if opAssign is a blit
     bool hasIdentityEquals;     // true if has identity opEquals
     bool hasNoFields;           // has no fields
+    bool hasCopyCtor;           // copy constructor
+    // Even if struct is defined as non-root symbol, some built-in operations
+    // (e.g. TypeidExp, NewExp, ArrayLiteralExp, etc) request its TypeInfo.
+    // For those, today TypeInfo_Struct is generated in COMDAT.
+    bool requestTypeInfo;
+
     FuncDeclarations postblits; // Array of postblit functions
     FuncDeclaration *postblit;  // aggregate postblit
-
-    bool hasCopyCtor;           // copy constructor
 
     FuncDeclaration *xeq;       // TypeInfo_Struct.xopEquals
     FuncDeclaration *xcmp;      // TypeInfo_Struct.xopCmp
@@ -179,11 +184,6 @@ public:
 
     // ABI-specific type(s) if the struct can be passed in registers
     TypeTuple *argTypes;
-
-    // Even if struct is defined as non-root symbol, some built-in operations
-    // (e.g. TypeidExp, NewExp, ArrayLiteralExp, etc) request its TypeInfo.
-    // For those, today TypeInfo_Struct is generated in COMDAT.
-    bool requestTypeInfo;
 
     static StructDeclaration *create(Loc loc, Identifier *id, bool inObject);
     Dsymbol *syntaxCopy(Dsymbol *s);

src/dmd/aggregate.d
@@ -77,11 +77,9 @@ extern (C++) abstract class AggregateDeclaration : ScopeDsymbol
 {
     Type type;                  ///
     StorageClass storage_class; ///
-    Prot protection;            /// visibility
     uint structsize;            /// size of struct
     uint alignsize;             /// size of struct for alignment purposes
     VarDeclarations fields;     /// VarDeclaration fields
-    Sizeok sizeok = Sizeok.none;/// set when structsize contains valid data
     Dsymbol deferred;           /// any deferred semantic2() or semantic3() symbol
 
     /// specifies whether this is a D, C++, Objective-C or anonymous struct/class/interface
@@ -118,7 +116,6 @@ extern (C++) abstract class AggregateDeclaration : ScopeDsymbol
     CtorDeclaration defaultCtor;
 
     AliasThis aliasthis;    /// forward unresolved lookups to aliasthis
-    bool noDefaultCtor;     /// no default construction
 
     DtorDeclarations dtors;     /// Array of destructors
     DtorDeclaration dtor;       /// aggregate destructor calling dtors and member constructors
@@ -127,6 +124,10 @@ extern (C++) abstract class AggregateDeclaration : ScopeDsymbol
     FuncDeclaration fieldDtor;  /// aggregate destructor for just the fields
 
     Expression getRTInfo;   /// pointer to GC info generated by object.RTInfo(this)
+
+    Prot protection;                /// visibility
+    bool noDefaultCtor;             /// no default construction
+    Sizeok sizeok = Sizeok.none;    /// set when structsize contains valid data
 
     final extern (D) this(const ref Loc loc, Identifier id)
     {

src/dmd/dstruct.d
@@ -204,10 +204,14 @@ extern (C++) class StructDeclaration : AggregateDeclaration
     bool hasBlitAssign;         // true if opAssign is a blit
     bool hasIdentityEquals;     // true if has identity opEquals
     bool hasNoFields;           // has no fields
+    bool hasCopyCtor;           // copy constructor
+    // Even if struct is defined as non-root symbol, some built-in operations
+    // (e.g. TypeidExp, NewExp, ArrayLiteralExp, etc) request its TypeInfo.
+    // For those, today TypeInfo_Struct is generated in COMDAT.
+    bool requestTypeInfo;
+
     FuncDeclarations postblits; // Array of postblit functions
     FuncDeclaration postblit;   // aggregate postblit
-
-    bool hasCopyCtor;       // copy constructor
 
     FuncDeclaration xeq;        // TypeInfo_Struct.xopEquals
     FuncDeclaration xcmp;       // TypeInfo_Struct.xopCmp
@@ -220,11 +224,6 @@ extern (C++) class StructDeclaration : AggregateDeclaration
 
     // ABI-specific type(s) if the struct can be passed in registers
     TypeTuple argTypes;
-
-    // Even if struct is defined as non-root symbol, some built-in operations
-    // (e.g. TypeidExp, NewExp, ArrayLiteralExp, etc) request its TypeInfo.
-    // For those, today TypeInfo_Struct is generated in COMDAT.
-    bool requestTypeInfo;
 
     extern (D) this(const ref Loc loc, Identifier id, bool inObject)
     {

commit 75356575f2805af5c14cd841d6fbc21925092ec6

[C++] aggregate.h: Fix regression caused by #11370

src/dmd/aggregate.h
@@ -116,8 +116,8 @@ public:
     Expression *getRTInfo;      // pointer to GC info generated by object.RTInfo(this)
 
     Prot protection;
-    Sizeok sizeok;              // set when structsize contains valid data
     bool noDefaultCtor;         // no default construction
+    Sizeok sizeok;              // set when structsize contains valid data
 
     virtual Scope *newScope(Scope *sc);
     void setScope(Scope *sc);

src/tests/cxxfrontend.c
@@ -274,7 +274,7 @@ void test_semantic()
     Dsymbol *s = m->search(Loc(), Identifier::idPool("Error"));
     assert(s);
     AggregateDeclaration *ad = s->isAggregateDeclaration();
-    assert(ad && ad->ctor);
+    assert(ad && ad->ctor && ad->sizeok == SIZEOKdone);
     CtorDeclaration *ctor = ad->ctor->isCtorDeclaration();
     assert(ctor->isMember() && !ctor->isNested());
     assert(0 == strcmp(ctor->type->toChars(), "Error(string)"));