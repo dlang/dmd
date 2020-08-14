f4e6b1ad40afc82815a2ef7d0c770279c288d0a3
nordlow <per.nordlow@gmail.com>
2020-08-15 01:27:01 +0200

    Add more tests

--- test/fail_compilation/diag_self_assign.d
+++ test/fail_compilation/diag_self_assign.d
@@ -3,29 +3,38 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/diag_self_assign.d(28): Warning: assignment of `x` from itself has no side effect
-fail_compilation/diag_self_assign.d(30): Error: construction of member `this._x` from itself
-fail_compilation/diag_self_assign.d(31): Error: construction of member `this._x` from itself
-fail_compilation/diag_self_assign.d(32): Error: construction of member `this._x` from itself
-fail_compilation/diag_self_assign.d(36): Error: construction of member `this._xp` from itself
-fail_compilation/diag_self_assign.d(42): Error: assignment of member `this._x` from itself
-fail_compilation/diag_self_assign.d(43): Error: assignment of member `this._x` from itself
-fail_compilation/diag_self_assign.d(44): Error: assignment of member `this._x` from itself
-fail_compilation/diag_self_assign.d(64): Warning: assignment of `s._x` from itself has no side effect
-fail_compilation/diag_self_assign.d(68): Warning: assignment of `x` from itself has no side effect
-fail_compilation/diag_self_assign.d(74): Warning: assignment of `xp` from itself has no side effect
-fail_compilation/diag_self_assign.d(76): Warning: assignment of `*xp` from itself has no side effect
-fail_compilation/diag_self_assign.d(78): Warning: assignment of `*& x` from itself has no side effect
-fail_compilation/diag_self_assign.d(80): Warning: assignment of `*& x` from itself has no side effect
+fail_compilation/diag_self_assign.d(36): Warning: assignment of `x` from itself has no side effect
+fail_compilation/diag_self_assign.d(37): Warning: assignment of `t` from itself has no side effect
+fail_compilation/diag_self_assign.d(39): Error: construction of member `this._x` from itself
+fail_compilation/diag_self_assign.d(40): Error: construction of member `this._x` from itself
+fail_compilation/diag_self_assign.d(41): Error: construction of member `this._x` from itself
+fail_compilation/diag_self_assign.d(45): Error: construction of member `this._xp` from itself
+fail_compilation/diag_self_assign.d(48): Error: construction of member `this._t` from itself
+fail_compilation/diag_self_assign.d(49): Error: construction of member `this._t._z` from itself
+fail_compilation/diag_self_assign.d(54): Error: assignment of member `this._x` from itself
+fail_compilation/diag_self_assign.d(55): Error: assignment of member `this._x` from itself
+fail_compilation/diag_self_assign.d(56): Error: assignment of member `this._x` from itself
+fail_compilation/diag_self_assign.d(78): Warning: assignment of `s._x` from itself has no side effect
+fail_compilation/diag_self_assign.d(82): Warning: assignment of `x` from itself has no side effect
+fail_compilation/diag_self_assign.d(88): Warning: assignment of `xp` from itself has no side effect
+fail_compilation/diag_self_assign.d(90): Warning: assignment of `*xp` from itself has no side effect
+fail_compilation/diag_self_assign.d(92): Warning: assignment of `*& x` from itself has no side effect
+fail_compilation/diag_self_assign.d(94): Warning: assignment of `*& x` from itself has no side effect
 ---
 */
 struct S
 {
 @safe pure nothrow @nogc:
 
-    this(float x)
+    struct T
     {
-        x = x;                  // warning
+        int _z;
+    }
+
+    this(float x, T t)
+    {
+        x = x;                  // warn
+        t = t;                  // warn
 
         _x = _x;                // error
         this._x = _x;           // error
@@ -33,8 +42,11 @@ struct S
 
         _x = _y;
 
-        _xp = _xp;              // warning
+        _xp = _xp;              // error
         _xp = _yp;
+
+        _t = _t;                // error
+        _t._z = _t._z;          // error (transitive)
     }
 
     void foo()
@@ -53,6 +65,8 @@ struct S
 
     float* _xp;
     float* _yp;
+
+    T _t;
 }
 
 pure nothrow @nogc unittest
@@ -61,23 +75,23 @@ pure nothrow @nogc unittest
     s = s;
 
     S t;
-    s._x = s._x;                // warning
+    s._x = s._x;                // warn
     s._x = t._x;
 
     int x;
-    x = x;                      // warning
+    x = x;                      // warn
 
     int y;
     y = x;
 
     int* xp;
-    xp = xp;                    // warning
+    xp = xp;                    // warn
 
-    *xp = *xp;                  // warning
+    *xp = *xp;                  // warn
 
-    (*&x) = (*&x);              // warning
+    (*&x) = (*&x);              // warn
 
-    (*&*&x) = (*&*&x);          // warning
+    (*&*&x) = (*&*&x);          // warn
 
     static assert(__traits(compiles, { int t; t = t; }));
 }
