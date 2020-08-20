f655770731e28510b053fa857ffd967a2c547f72
nordlow <per.nordlow@gmail.com>
2020-08-20 21:49:01 +0200

    Revert "More debug prints"
    This reverts commit 5e768d853060f81e9a3f5cf822e28fd7a2189805.

--- src/dmd/unused.d
+++ src/dmd/unused.d
@@ -2,44 +2,30 @@ module dmd.unused;
 
 import core.stdc.stdio;
 
-import dmd.dsymbol;
 import dmd.dmodule;
 import dmd.arraytypes;
 import dmd.errors;
 import dmd.declaration;
 
-void printUnusedSymbolStats(const ref Modules modules)
+void printUnusedSymbolStats(ref Modules modules)
 {
-    static void checkMember(const Dsymbol member)
-    {
-        if (const ad = cast(const AliasDeclaration)member)
-        {
-            if (!ad.isReferenced) // TODO: exclude private functions
-            {
-                if (ad._import)
-                    ad.loc.warning("unused imported alias %s", ad.toChars());
-                else
-                    ad.loc.warning("unused alias %s", ad.toChars());
-            }
-            else
-            {
-                if (ad._import)
-                    ad.loc.warning("used imported alias %s", ad.toChars());
-                else
-                    ad.loc.warning("used alias %s", ad.toChars());
-            }
-        }
-    }
-    static void checkModule(const Module m)
+    static void checkModule(Module m)
     {
         if (!m.members)
             return;
-        foreach (const member; *(m.members)) // top-level members
+        foreach (const memb; *(m.members)) // top-level members
         {
-            checkMember(member);
+            if (const ad = cast(const AliasDeclaration)memb)
+                if (!ad.isReferenced) // TODO: exclude private functions
+                {
+                    if (ad._import)
+                        ad.loc.warning("unused imported alias %s", ad.toChars());
+                    else
+                        ad.loc.warning("unused alias %s", ad.toChars());
+                }
         }
     }
 
-    foreach (const m; modules)
+    foreach (m; modules)
         checkModule(m);
 }
