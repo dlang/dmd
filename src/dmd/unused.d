5e768d853060f81e9a3f5cf822e28fd7a2189805
nordlow <per.nordlow@gmail.com>
2020-08-20 21:04:53 +0200

    More debug prints

--- src/dmd/unused.d
+++ src/dmd/unused.d
@@ -2,30 +2,44 @@ module dmd.unused;
 
 import core.stdc.stdio;
 
+import dmd.dsymbol;
 import dmd.dmodule;
 import dmd.arraytypes;
 import dmd.errors;
 import dmd.declaration;
 
-void printUnusedSymbolStats(ref Modules modules)
+void printUnusedSymbolStats(const ref Modules modules)
 {
-    static void checkModule(Module m)
+    static void checkMember(const Dsymbol member)
+    {
+        if (const ad = cast(const AliasDeclaration)member)
+        {
+            if (!ad.isReferenced) // TODO: exclude private functions
+            {
+                if (ad._import)
+                    ad.loc.warning("unused imported alias %s", ad.toChars());
+                else
+                    ad.loc.warning("unused alias %s", ad.toChars());
+            }
+            else
+            {
+                if (ad._import)
+                    ad.loc.warning("used imported alias %s", ad.toChars());
+                else
+                    ad.loc.warning("used alias %s", ad.toChars());
+            }
+        }
+    }
+    static void checkModule(const Module m)
     {
         if (!m.members)
             return;
-        foreach (const memb; *(m.members)) // top-level members
+        foreach (const member; *(m.members)) // top-level members
         {
-            if (const ad = cast(const AliasDeclaration)memb)
-                if (!ad.isReferenced) // TODO: exclude private functions
-                {
-                    if (ad._import)
-                        ad.loc.warning("unused imported alias %s", ad.toChars());
-                    else
-                        ad.loc.warning("unused alias %s", ad.toChars());
-                }
+            checkMember(member);
         }
     }
 
-    foreach (m; modules)
+    foreach (const m; modules)
         checkModule(m);
 }
