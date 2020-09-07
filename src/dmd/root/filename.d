2f0fb5896176d19803ee97f0cc1346524cfa9555
Andrey Zherikov <andrey.zherikov@gmail.com>
2020-09-06 20:41:14 -0400

    Add Windows-specific unit tests for absolute()

--- src/dmd/root/filename.d
+++ src/dmd/root/filename.d
@@ -124,6 +124,13 @@ nothrow:
     {
         assert(absolute("/"[]) == true);
         assert(absolute(""[]) == false);
+
+        version (Windows)
+        {
+            assert(absolute(r"\"[]) == true);
+            assert(absolute(r"\\"[]) == true);
+            assert(absolute(r"c:"[]) == true);
+        }
     }
 
     /**
