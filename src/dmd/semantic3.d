2216410b5b6163a494ce59ce970fa7b6b2493791
Rainer Schuetze <r.sagitario@gmx.de>
2020-09-15 00:20:29 +0200

    RTInfo generation must not be omitted for deprecated aggregates

--- src/dmd/semantic3.d
+++ src/dmd/semantic3.d
@@ -1443,9 +1443,8 @@ private extern(C++) final class Semantic3Visitor : Visitor
 
     final void generateRTInfo(AggregateDeclaration ad, Scope* sc)
     {
-        // don't do it for unused deprecated types
-        // or error ypes
-        if (!ad.getRTInfo && Type.rtinfo && (!ad.isDeprecated() || global.params.useDeprecated != DiagnosticReporting.error) && (ad.type && ad.type.ty != Terror))
+        // don't do it for or error types
+        if (!ad.getRTInfo && Type.rtinfo && (ad.type && ad.type.ty != Terror))
         {
             // Evaluate: RTinfo!type
             auto tiargs = new Objects();
