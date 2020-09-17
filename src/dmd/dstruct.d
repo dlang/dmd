391ca7cec44e81ac6e414e79c374f0fb4e229a35
Rainer Schuetze <r.sagitario@gmx.de>
2020-09-17 23:19:36 +0200

    cannot avoid running semantic on struct declaration if template RTInfo is declared

--- src/dmd/dstruct.d
+++ src/dmd/dstruct.d
@@ -128,7 +128,7 @@ extern (C++) void semanticTypeInfo(Scope* sc, Type t)
          */
         if (!sd.members)
             return; // opaque struct
-        if (!sd.xeq && !sd.xcmp && !sd.postblit && !sd.dtor && !sd.xhash && !search_toString(sd))
+        if (!Type.rtinfo && !sd.xeq && !sd.xcmp && !sd.postblit && !sd.dtor && !sd.xhash && !search_toString(sd))
             return; // none of TypeInfo-specific members
 
         // If the struct is in a non-root module, run semantic3 to get
