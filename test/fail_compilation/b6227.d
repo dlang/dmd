05bf5a11d82676f56751e3e69482df6c22c33a31
Geod24 <pro.mathias.lang@gmail.com>
2020-10-10 07:19:07 +0900

    Fix 8044 - Print name of enum passed a tmpl param

--- test/fail_compilation/b6227.d
+++ test/fail_compilation/b6227.d
@@ -1,9 +1,9 @@
 /* TEST_OUTPUT:
 ---
 fail_compilation/b6227.d(16): Error: Comparison between different enumeration types `X` and `Y`; If this behavior is intended consider using `std.conv.asOriginalType`
-fail_compilation/b6227.d(16):        while evaluating: `static assert(!(cast(X)0 != cast(Y)0))`
+fail_compilation/b6227.d(16):        while evaluating: `static assert(!(X.O != Y.U))`
 fail_compilation/b6227.d(17): Error: Comparison between different enumeration types `X` and `Y`; If this behavior is intended consider using `std.conv.asOriginalType`
-fail_compilation/b6227.d(17):        while evaluating: `static assert(cast(X)0 == cast(Y)0)`
+fail_compilation/b6227.d(17):        while evaluating: `static assert(X.O == Y.U)`
 ---
 */
 enum X {
