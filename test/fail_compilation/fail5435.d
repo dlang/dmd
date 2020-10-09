c32f220dbc64a82226dc6b0bcef41a2ef8ab6f50
Geod24 <pro.mathias.lang@gmail.com>
2020-10-10 07:19:07 +0900

    Fix 8044 - Print name of enum passed a tmpl param

--- test/fail_compilation/fail5435.d
+++ test/fail_compilation/fail5435.d
@@ -2,18 +2,18 @@
 /*
 TEST_OUTPUT:
 ---
-cast(Enum5435)0
-cast(Enum5435)1
-cast(Enum5435)2
+Enum5435.A
+Enum5435.B
+Enum5435.C
 fail_compilation/fail5435.d(38): Error: cannot implicitly convert expression `"foo"` of type `string` to `Enum5435`
 fail_compilation/fail5435.d(38):        while evaluating `pragma(msg, foo)`
 fail_compilation/fail5435.d(38): Error: cannot implicitly convert expression `3.0` of type `double` to `Enum5435`
 fail_compilation/fail5435.d(38):        while evaluating `pragma(msg, foo)`
-fail_compilation/fail5435.d(39): Error: cannot implicitly convert expression `cast(Enum5435)0` of type `Enum5435` to `string`
+fail_compilation/fail5435.d(39): Error: cannot implicitly convert expression `Enum5435.A` of type `Enum5435` to `string`
 fail_compilation/fail5435.d(39):        while evaluating `pragma(msg, foo)`
-fail_compilation/fail5435.d(39): Error: cannot implicitly convert expression `cast(Enum5435)1` of type `Enum5435` to `string`
+fail_compilation/fail5435.d(39): Error: cannot implicitly convert expression `Enum5435.B` of type `Enum5435` to `string`
 fail_compilation/fail5435.d(39):        while evaluating `pragma(msg, foo)`
-fail_compilation/fail5435.d(39): Error: cannot implicitly convert expression `cast(Enum5435)2` of type `Enum5435` to `string`
+fail_compilation/fail5435.d(39): Error: cannot implicitly convert expression `Enum5435.C` of type `Enum5435` to `string`
 fail_compilation/fail5435.d(39):        while evaluating `pragma(msg, foo)`
 foo
 fail_compilation/fail5435.d(39): Error: cannot implicitly convert expression `3.0` of type `double` to `string`
