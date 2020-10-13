03af0c5cfb59351a9e1fa6b296da5d72a3f2b035
Stefan Koch <uplink.coder@googlemail.com>
2020-10-13 16:15:43 +0200

    Allow ? : expressions with types

--- src/dmd/expression.d
+++ src/dmd/expression.d
@@ -4332,11 +4332,15 @@ extern (C++) abstract class BinExp : Expression
 
         // CondExp uses 'a ? b : c' but we're comparing 'b : c'
         TOK thisOp = (op == TOK.question) ? TOK.colon : op;
+        if (!global.params.sk_typefunctions)
+        {
         if (e1.op == TOK.type || e2.op == TOK.type)
         {
             error("incompatible types for `(%s) %s (%s)`: cannot use `%s` with types",
                 e1.toChars(), Token.toChars(thisOp), e2.toChars(), Token.toChars(op));
         }
+        }
+
         else if (e1.type.equals(e2.type))
         {
             error("incompatible types for `(%s) %s (%s)`: both operands are of type `%s`",
