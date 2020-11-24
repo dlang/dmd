2b855b67e524d81fe24bb6eefab6c1bd6ee1f441
Walter Bright <walter@walterbright.com>
2020-11-24 03:01:35 -0800

    compile mscoffobj.d for all

--- src/dmd/backend/code.d
+++ src/dmd/backend/code.d
@@ -266,10 +266,7 @@ struct seg_data
     Barray!(linnum_data) SDlinnum_data;     // array of line number / offset data
 
   nothrow:
-    version (Windows)
-        int isCode() { return seg_data_isCode(this); }
-    version (OSX)
-        int isCode() { return seg_data_isCode(this); }
+    int isCode() { return seg_data_isCode(this); }
 }
 
 extern int seg_data_isCode(const ref seg_data sd) @system;
