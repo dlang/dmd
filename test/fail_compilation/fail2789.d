487e32082ebe51abee747200e4071b4f58b35957
MoonlightSentinel <moonlightsentinel@disroot.org>
2020-12-26 14:41:03 +0100

    Fix 21505 - Rework duplicate function detection without mangling
    The previous implementation compared small parts of the function mangling
    to detect duplicate functions. This approach had several flaws manifesting
    in the referenced issue as well as other false positives for (static)
    members, `alias`ing into different scopes, ... .
    
    Issue 21505 was caused by the assumption that all functions visited by
    `overloadApply` have the same name. This doesn't hold for overloads
    introduced by an `alias`.
    
    A reliable mangling-based implementation would need to compare most of
    the mangled name while also omitting severel aspects e.g. the return type
    to detect `int foo()` and `void foo()`.
    
    Hence the current implementation was replaced by actually comparing the
    `FuncDeclaration`s because it allows for more fine grained checks (and
    also should save some memory allocations).

--- test/fail_compilation/fail2789.d
+++ test/fail_compilation/fail2789.d
@@ -4,7 +4,6 @@ https://issues.dlang.org/show_bug.cgi?id=18385
 TEST_OUTPUT:
 ---
 fail_compilation/fail2789.d(15): Error: function `fail2789.A2789.m()` conflicts with previous declaration at fail_compilation/fail2789.d(10)
-fail_compilation/fail2789.d(25): Error: function `fail2789.A2789.m()` conflicts with previous declaration at fail_compilation/fail2789.d(10)
 ---
 */
 #line 7
@@ -26,7 +25,7 @@ class A2789
         return 3.0;
     }
 
-    static void m() // conflict
+    static void m() // no conflict
     {
     }
 }
@@ -35,10 +34,10 @@ class A2789
 TEST_OUTPUT:
 ---
 fail_compilation/fail2789.d(49): Error: function `fail2789.f4()` conflicts with previous declaration at fail_compilation/fail2789.d(48)
-fail_compilation/fail2789.d(52): Error: function `fail2789.f5()` conflicts with previous declaration at fail_compilation/fail2789.d(51)
 fail_compilation/fail2789.d(55): Error: function `fail2789.f6()` conflicts with previous declaration at fail_compilation/fail2789.d(54)
 ---
 */
+
 
 void f1();
 void f1() {}    // ok
@@ -53,7 +52,7 @@ void f4() {}
 void f4() {}    // conflict
 
 void f5() @safe {}
-void f5() @system {}    // conflict
+void f5() @system {}    // no conflict because of attribute based overloading in in extern(D)
 
 auto f6() { return 10; }    // int()
 auto f6() { return ""; }    // string(), conflict
@@ -64,9 +63,9 @@ TEST_OUTPUT:
 fail_compilation/fail2789.d(67): Error: function `fail2789.f_ExternC1()` conflicts with previous declaration at fail_compilation/fail2789.d(66)
 fail_compilation/fail2789.d(70): Deprecation: function `fail2789.f_ExternC2` cannot overload `extern(C)` function at fail_compilation/fail2789.d(69)
 fail_compilation/fail2789.d(73): Deprecation: function `fail2789.f_ExternC3` cannot overload `extern(C)` function at fail_compilation/fail2789.d(72)
-fail_compilation/fail2789.d(76): Error: function `fail2789.f_MixExtern1()` conflicts with previous declaration at fail_compilation/fail2789.d(75)
 ---
 */
+
 extern(C) void f_ExternC1() {}
 extern(C) void f_ExternC1() {}      // conflict
 
@@ -77,7 +76,7 @@ extern(C) void f_ExternC3(int) {}
 extern(C) void f_ExternC3() {}      // conflict
 
 extern (D) void f_MixExtern1() {}
-extern (C) void f_MixExtern1() {}   // conflict
+extern (C) void f_MixExtern1() {}   // no conflict because of different mangling
 
 extern (D) void f_MixExtern2(int) {}
 extern (C) void f_MixExtern2() {}   // no error
