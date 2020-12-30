01b0156a0ff34d62b590fe5e262637232a6d675a
Geod24 <pro.mathias.lang@gmail.com>
2020-12-30 17:47:13 +0900

    Remove -preview=dip25 from the test-suite
    Since it is now enabled by default, we can do a simple clean-up and not provide it.

--- test/fail_compilation/retref2.d
+++ test/fail_compilation/retref2.d
@@ -1,11 +1,8 @@
-
-// REQUIRED_ARGS: -preview=dip25
-
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/retref2.d(21): Error: function `ref int retref2.D.foo(return ref int)` does not override any function, did you mean to override `ref int retref2.C.foo(ref int)`?
-fail_compilation/retref2.d(22): Error: function `ref scope int retref2.D.bar() return` does not override any function, did you mean to override `ref int retref2.C.bar()`?
+fail_compilation/retref2.d(18): Error: function `ref int retref2.D.foo(return ref int)` does not override any function, did you mean to override `ref int retref2.C.foo(ref int)`?
+fail_compilation/retref2.d(19): Error: function `ref scope int retref2.D.bar() return` does not override any function, did you mean to override `ref int retref2.C.bar()`?
 ---
 */
 
