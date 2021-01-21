c5a4a9d0ae85856b5e0d70d7a4235f9a5f1f04ae
Luís Ferreira <contact@lsferreira.net>
2021-01-21 22:32:28 +0000

    object: improve code style consistency
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/object.d
+++ src/object.d
@@ -13,10 +13,10 @@ module object;
 alias size_t = typeof(int.sizeof);
 alias ptrdiff_t = typeof(cast(void*)0 - cast(void*)0);
 
-alias sizediff_t = ptrdiff_t; //For backwards compatibility only.
+alias sizediff_t = ptrdiff_t; // For backwards compatibility only.
 
-alias hash_t = size_t; //For backwards compatibility only.
-alias equals_t = bool; //For backwards compatibility only.
+alias hash_t = size_t; // For backwards compatibility only.
+alias equals_t = bool; // For backwards compatibility only.
 
 alias string  = immutable(char)[];
 alias wstring = immutable(wchar)[];
@@ -45,7 +45,7 @@ version (AArch64)
     // Apple uses a trivial varargs implementation
     version (OSX) {}
     else version (iOS) {}
-    else version (TVOS){}
+    else version (TVOS) {}
     else version (WatchOS) {}
     else version = WithArgTypes;
 }
