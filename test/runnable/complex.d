e8f605f6650b916eb70ba58ae3cee385dfc549f3
Iain Buclaw <ibuclaw@gdcproject.org>
2021-02-21 23:11:44 +0100

    test: Move test for issue 17965 to own source file

--- test/runnable/complex.d
+++ test/runnable/complex.d
@@ -404,36 +404,6 @@ void test17677()
 }
 
 /***************************************/
-// https://issues.dlang.org/show_bug.cgi?id=17965
-
-import core.stdc.math;
-
-struct Point{double x,y;}
-
-Point foo10()
-{
-    Point result = Point(1.0, 2.0);
-    return result;
-}
-
-Point foo20()
-{
-    Point result;
-    return result;
-}
-
-void test17965()
-{
-    auto p = foo10();
-    assert(p.x == 1.0);
-    assert(p.y == 2.0);
-
-    auto q = foo20();
-    assert(isnan(q.x));
-    assert(isnan(q.y));
-}
-
-/***************************************/
 
 int main(char[][] args)
 {
@@ -462,7 +432,6 @@ int main(char[][] args)
     test15();
     test17087();
     test17677();
-    test17965();
 
     printf("Success!\n");
     return 0;
