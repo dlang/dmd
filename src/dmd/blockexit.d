159e376e3e03d358a7ee81024b62af27dff6fb82
Walter Bright <walter@walterbright.com>
2021-02-22 00:46:27 -0800

    Detect noreturn statements
    This re-introduce commit 0fc1fc0539659d03acb9f39d01415778391bfcda,
    which was merged in #12222 and subsequently reverted in commit
    3c845329c2282a8c374d53a91c520f433afa2628 (PR #12228) as it broke
    some projects on Buildkite. The project has had a new release,
    so it should be safe to re-introduce.

--- src/dmd/blockexit.d
+++ src/dmd/blockexit.d
@@ -107,6 +107,8 @@ int blockExit(Statement s, FuncDeclaration func, bool mustNotThrow)
                         return;
                     }
                 }
+                if (s.exp.type.toBasetype().isTypeNoreturn())
+                    result = BE.halt;
                 if (canThrow(s.exp, func, mustNotThrow))
                     result |= BE.throw_;
             }
