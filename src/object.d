b2b7732ab1c9cdf5e1dda85b239f43a92558c535
Walter Bright <walter@walterbright.com>
2021-02-22 18:32:14 -0800

    DIP1034 add noreturn to object, abort, exit, _Exit

--- src/object.d
+++ src/object.d
@@ -15,6 +15,7 @@ alias size_t = typeof(int.sizeof);
 alias ptrdiff_t = typeof(cast(void*)0 - cast(void*)0);
 
 alias sizediff_t = ptrdiff_t; // For backwards compatibility only.
+alias noreturn = typeof(*null);  /// bottom type
 
 alias hash_t = size_t; // For backwards compatibility only.
 alias equals_t = bool; // For backwards compatibility only.
