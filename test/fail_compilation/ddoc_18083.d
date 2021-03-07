ee0f1f23679821cfd935b931b431d6eac6eac5b0
Jason B. Cox <contact@jasonbcox.com>
2021-03-07 15:40:22 -0800

    Add error message when warnings-as-errors is enabled and a warning occurs

--- test/fail_compilation/ddoc_18083.d
+++ test/fail_compilation/ddoc_18083.d
@@ -1,8 +1,10 @@
 // REQUIRED_ARGS: -D -Dd${RESULTS_DIR}/fail_compilation -o- -w -c
 /* TEST_OUTPUT:
 ---
-fail_compilation/ddoc_18083.d(12): Warning: Ddoc: function declaration has no parameter 'this'
-fail_compilation/ddoc_18083.d(12): Warning: Ddoc: parameter count mismatch, expected 0, got 1
+fail_compilation/ddoc_18083.d(14): Warning: Ddoc: function declaration has no parameter 'this'
+fail_compilation/ddoc_18083.d(14): Warning: Ddoc: parameter count mismatch, expected 0, got 1
+Error: warnings are treated as errors
+       Use -wi if you wish to treat warnings only as informational.
 ---
 */
 /**
