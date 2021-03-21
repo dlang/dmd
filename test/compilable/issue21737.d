ea7a97e26e15dccf1c79a4241ce2d6d69c911d5c
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2021-03-21 12:11:41 +0800

    more tests

--- test/compilable/issue21737.d
+++ test/compilable/issue21737.d
@@ -1,33 +1,63 @@
+/*
+REQUIRED_ARGS: -preview=nosharedaccess
+ */
+import core.atomic;
+struct S
+{
+    int result;
+    void inc(int i) shared @safe {
+        result.atomicOp!"+="(i);
+    }
+}
+
 struct Foo
 {
-    int opApply( int delegate(size_t, int) shared scope) shared
+    int opApply( int delegate(size_t, int) shared scope dg) shared
     {
+        dg(0,42);
         return 0;
     }
 }
 
 struct Foo1
 {
-    int opApply( int delegate(size_t, int) shared ) shared
+    int opApply( int delegate(size_t, int) shared dg) shared
     {
+        dg(0,42);
         return 0;
     }
 }
 
 struct Foo2
 {
-    int opApply( int delegate(size_t, int) shared @safe) shared
+    int opApply( int delegate(size_t, int) shared @safe dg) shared
     {
+        dg(0,42);
         return 0;
     }
 }
-void test()
+int test()
 {
     shared Foo  foo;
     shared Foo1 foo1;
     shared Foo2 foo2;
-    foreach(i, e; foo)  { }
-    foreach(i, e; foo1) { }
-    foreach(i, e; foo2) { }
+    shared S s;
     
+    foreach(i, e; foo)  {
+        s.inc(1);
+    }
+    foreach(i, e; foo1) {
+        s.inc(2);
+    }
+    foreach(i, e; foo2) {
+        s.inc(3);
+    }
+    
+    static assert(!__traits(compiles, {
+        S ss;
+        foreach(i, e; foo)  { ss.inc(1); }
+        foreach(i, e; foo1) { ss.inc(2); }
+        foreach(i, e; foo2) { ss.inc(3); }
+    }));
+    return s.result;
 }
