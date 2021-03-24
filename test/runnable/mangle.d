333a673fe5a1610102eac31330c0e40af2154897
Boris Carvajal <boris2.9@gmail.com>
2021-03-23 23:46:50 -0300

    Fix Issue 21753 - Struct literal with function literal member not allowed as template value argument

--- test/runnable/mangle.d
+++ test/runnable/mangle.d
@@ -618,6 +618,12 @@ static assert(funcd.mangleof == "_D6mangle5funcdFPFZNnZi");
 
 /***************************************************/
 
+struct S21753 { void function() f1; }
+void fun21753(S21753 v)() {}
+alias fl21753 = (){};
+static assert((fun21753!(S21753(fl21753))).mangleof == "_D6mangle__T8fun21753VSQv6S21753S1_DQBi10" ~ fl21753.stringof ~ "MFNaNbNiNfZvZQCaQp");
+
+/***************************************************/
 void main()
 {
     test10077h();
