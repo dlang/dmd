049f1c207c63e6c758528bd7098b2f697a01ce21
Atila Neves <atila.neves@gmail.com>
2021-04-08 14:36:45 +0200

    Delete hack to always do codegen for templates if -debug is used

--- src/dtemplate.c
+++ src/dtemplate.c
@@ -6770,8 +6770,7 @@ Dsymbols *TemplateInstance::appendToModuleMember()
 {
     Module *mi = minst;     // instantiated -> inserted module
 
-    if (global.params.useUnitTests ||
-        global.params.debuglevel)
+    if (global.params.useUnitTests)
     {
         // Turn all non-root instances to speculative
         if (mi && !mi->isRoot())
