0517fc93ceb9e74e979bbbc9de64eb64d610094c
Walter Bright <walter@walterbright.com>
2021-04-08 02:43:51 -0700

    AssertExp should be lowered before inlining happens

--- src/dmd/inline.d
+++ src/dmd/inline.d
@@ -910,10 +910,7 @@ public:
 
         override void visit(ArrayExp e)
         {
-            auto ce = e.copy().isArrayExp();
-            ce.e1 = doInlineAs!Expression(e.e1, ids);
-            ce.arguments = arrayExpressionDoInline(e.arguments);
-            result = ce;
+            assert(0); // this should have been lowered to something else
         }
 
         override void visit(CondExp e)
