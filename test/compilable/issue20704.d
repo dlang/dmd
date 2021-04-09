1ca289414bbe8a24beea76809d7420b79339624c
nordlow <per.nordlow@gmail.com>
2021-04-09 21:32:34 +0200

    More extensive tests

--- test/compilable/issue20704.d
+++ test/compilable/issue20704.d
@@ -3,12 +3,20 @@ https://issues.dlang.org/show_bug.cgi?id=20704
 REQUIRED_ARGS: -preview=rvaluerefparam
  */
 
-void foo (T) (const auto ref T arg = T.init) {}
-void bar (T) (const      ref T arg = T.init) {}
+void f1(T)(const auto ref T arg = T.init) {}
+void f2(T)(const      ref T arg = T.init) {}
+void f3(T)(const auto ref T arg = 0) {}
+void f4(T)(const      ref T arg = 0) {}
 
 void main ()
 {
     int i;
-    foo!int();
-    bar!int(i);
+    f1!int(i);
+    f2!int(i);
+    f3!int(i);
+    f4!int(i);
+    f1!int();
+    f2!int();
+    f3!int();
+    f4!int();
 }
