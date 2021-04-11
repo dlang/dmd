b9457dd2daa0c1fc3e6194466df85d7adca41f8f
nordlow <per.nordlow@gmail.com>
2021-04-11 18:28:17 +0200

    Adjust failing messages once more, this time adjusting line numbers

--- test/fail_compilation/fail7603c.d
+++ test/fail_compilation/fail7603c.d
@@ -1,7 +1,7 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail7603c.d(8): Error: cannot modify constant `3`
+fail_compilation/fail7603c.d(9): Error: cannot modify constant `3`
        use `-preview=in` or `preview=rvaluerefparam`
 ---
 */
