89fb1ddecafbdef65522ca83b0cca93b53c086fe
Nathan Sashihara <21227491+n8sh@users.noreply.github.com>
2021-04-13 07:54:58 -0500

    Fix 21825 - DIP1034: make lambdaHasSideEffect true for noreturn functions
    Prevents spurious warning "calling XXX without side effects discards
    return value of type `noreturn`; prepend a `cast(void)` if intentional".
    
    Also changes the result of hasSideEffect and behavior of isTrivialExp.
    
    There appears to be a general inconsistency where a function that
    performs side-effectful operations may be considered side-effectless
    but if that is indeed a problem addressing it is beyond the scope of
    this PR.

--- test/compilable/noreturn1.d
+++ test/compilable/noreturn1.d
@@ -1,4 +1,5 @@
 /*
+REQUIRED_ARGS: -w
 TEST_OUTPUT:
 ---
 noreturn
@@ -11,6 +12,10 @@ pragma(msg, noreturn);
 noreturn exits(int* p) { *p = 3; }
 
 noreturn exit();
+
+noreturn pureexits() @nogc nothrow pure @safe { assert(0); }
+
+noreturn callpureexits() { pureexits(); }
 
 int test1(int i)
 {
