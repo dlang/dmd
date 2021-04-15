541b27e12b1596d5910104e6f7643df86a47ba4e
Iain Buclaw <ibuclaw@gdcproject.org>
2021-04-16 01:00:38 +0200

    fix Issue 21832 - Wrong deprecation message when importing non-deprecated template in static condition

--- test/fail_compilation/fail21830.d
+++ test/fail_compilation/fail21830.d
@@ -2,8 +2,6 @@
 TEST_OUTPUT
 ---
 fail_compilation/fail21830.d(24): Deprecation: struct `fail21830.OldS21830` is deprecated - Deprecated type
-fail_compilation/fail21830.d(8): Deprecation: struct `fail21830.OldS21830` is deprecated - Deprecated type
-fail_compilation/fail21830.d(24):        instantiated from here: `test21830!(OldS21830)`
 fail_compilation/fail21830.d(24): Deprecation: template `fail21830.test21830(T)(T t) if (is(T == OldS21830))` is deprecated - Deprecated template
 fail_compilation/fail21830.d(24): Deprecation: struct `fail21830.OldS21830` is deprecated - Deprecated type
 ---
