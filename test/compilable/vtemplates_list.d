9fd50d0bed37781ed0fb1b11577d1928d156ea53
Adam D. Ruppe <destructionator@gmail.com>
2021-04-30 12:10:25 -0400

    unique implies there is only one instance of it whereas distinct is more precise in representing its membership in the set

--- test/compilable/vtemplates_list.d
+++ test/compilable/vtemplates_list.d
@@ -1,19 +1,19 @@
 /* REQUIRED_ARGS: -vtemplates=list-instances
 TEST_OUTPUT:
 ---
-compilable/vtemplates_list.d(19): vtemplate: 4 (3 unique) instantiation(s) of template `foo(int I)()` found, they are:
+compilable/vtemplates_list.d(19): vtemplate: 4 (3 distinct) instantiation(s) of template `foo(int I)()` found, they are:
 compilable/vtemplates_list.d(25): vtemplate: explicit instance `foo!1`
 compilable/vtemplates_list.d(26): vtemplate: explicit instance `foo!1`
 compilable/vtemplates_list.d(27): vtemplate: explicit instance `foo!2`
 compilable/vtemplates_list.d(28): vtemplate: explicit instance `foo!3`
-compilable/vtemplates_list.d(20): vtemplate: 3 (1 unique) instantiation(s) of template `goo1(int I)()` found, they are:
+compilable/vtemplates_list.d(20): vtemplate: 3 (1 distinct) instantiation(s) of template `goo1(int I)()` found, they are:
 compilable/vtemplates_list.d(30): vtemplate: explicit instance `goo1!1`
 compilable/vtemplates_list.d(31): vtemplate: explicit instance `goo1!1`
 compilable/vtemplates_list.d(21): vtemplate: implicit instance `goo1!1`
-compilable/vtemplates_list.d(21): vtemplate: 2 (1 unique) instantiation(s) of template `goo2(int I)()` found, they are:
+compilable/vtemplates_list.d(21): vtemplate: 2 (1 distinct) instantiation(s) of template `goo2(int I)()` found, they are:
 compilable/vtemplates_list.d(33): vtemplate: explicit instance `goo2!1`
 compilable/vtemplates_list.d(34): vtemplate: explicit instance `goo2!1`
-compilable/vtemplates_list.d(52): vtemplate: 1 (1 unique) instantiation(s) of template `A()` found, they are:
+compilable/vtemplates_list.d(52): vtemplate: 1 (1 distinct) instantiation(s) of template `A()` found, they are:
 compilable/vtemplates_list.d-mixin-53(53): vtemplate: explicit instance `A!()`
 ---
 */
