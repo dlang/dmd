4a559c100e6cc3a32264b2988fc0710de86c3a14
Iain Buclaw <ibuclaw@gdcproject.org>
2021-05-27 13:20:25 +0200

    test/compilable: Move cstuff tests for bugzilla issues into cstuff2.c

---
+++ test/compilable/imports/cstuff2.c
@@ -0,0 +1,117 @@
+// check bugs in the expression parser
+
+/***************************************************/
+// https://issues.dlang.org/show_bug.cgi?id=21931
+
+typedef long int T21931a;
+typedef T21931a T21931b;
+
+/***************************************************/
+// https://issues.dlang.org/show_bug.cgi?id=21934
+
+typedef int T21934 asm("realtype");
+int init21934 asm("realsym") = 1;
+int var21934 asm("realvsym");
+int fun21934() asm("realfun");
+
+void test21934()
+{
+    typedef int asmreg;
+    register asmreg r1 asm("r1");
+    // asm ignored by C compiler, should be disallowed?
+    asmreg r2 asm("r2");
+
+    register asmreg r3 asm("r3") = 3;
+    // asm ignored by C compiler, should be disallowed?
+    asmreg r4 asm("r4") = 4;
+}
+
+/***************************************************/
+// https://issues.dlang.org/show_bug.cgi?id=21937
+
+__attribute__(()) int test21937a();
+int test21937b() __attribute__(( , nothrow, hot, aligned(2), ));
+int test21937c() __attribute__((nothrow , leaf)) __attribute__((noreturn));
+
+__attribute__((noinline))
+void test21937d()
+{
+  typedef int attr_var_t;
+  attr_var_t attr_local __attribute__((unused));
+}
+
+__attribute__((aligned)) int test21937e;
+int test21937f __attribute__((aligned));
+
+struct __attribute__((packed)) S21937a
+{
+  __attribute__((deprecated("msg"))) char c;
+  int i __attribute__((deprecated));
+};
+
+struct S21937b
+{
+  __attribute__((deprecated("msg"))) char c;
+  int i __attribute__((deprecated));
+} __attribute__((packed));
+
+enum __attribute__((aligned)) E21937a
+{
+  E21937a_A,
+};
+
+enum E21937b
+{
+  E21937b_A,
+} __attribute__((aligned));
+
+typedef int T21937a __attribute__((unused));
+
+/***************************************************/
+// https://issues.dlang.org/show_bug.cgi?id=21945
+
+typedef struct {
+    long var;
+} S21945;
+S21945 test21945a;
+
+typedef enum {
+    E21945_member,
+} E21945;
+E21945 test21945b;
+
+/***************************************************/
+// https://issues.dlang.org/show_bug.cgi?id=21948
+
+void test21948()
+{
+    typedef int myint;
+    typedef struct { int f; } mystruct;
+
+    myint var1;
+    myint var2 = 12;
+    mystruct var3;
+    // Uncomment when bug fixed https://issues.dlang.org/show_bug.cgi?id=21979
+    //mystruct var4 = { 34 };
+}
+
+/***************************************************/
+// https://issues.dlang.org/show_bug.cgi?id=21963
+
+union U21963
+{
+    int iv;
+    float fv;
+};
+
+/***************************************************/
+// https://issues.dlang.org/show_bug.cgi?id=21967
+
+const int test21967a(void);
+const int *test21967b(void);
+
+/***************************************************/
+// https://issues.dlang.org/show_bug.cgi?id=21970
+
+extern int test21970a;
+extern char *test21970b;
