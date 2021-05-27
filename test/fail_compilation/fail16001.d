a2c10e1e669e00f9b229c8681da5dc6be111ec51
Geod24 <pro.mathias.lang@gmail.com>
2021-05-27 11:13:21 +0900

    Fixup for PR12528 - Add changelog, improve message, fix tests
    Most deprecations should be explained to the users.
    Deprecations that trigger on valid user code absolutely must be explained to the user,
    so this adds a changelog entry, instead of relying on the one-liner that comes with
    the bugzilla entry.
    
    Additionally, the message wasn't enlighting: Since it triggers on possibly valid code
    (the user might have intended to write a delegate returning a delegate, or might have not),
    the message should state why the syntax is disallowed, not merely what the deprecated syntax is doing.
    
    Lastly, out of the three tests affected by PR12528, two of them used the syntax correctly:
    the test was actually checking that there was a correct match with a function type.
    Hence, instead of mindlessly adding the deprecation message to the test,
    kicking the can down the road to the person that will actually turn this into an error,
    the deprecation should have fixed those error messages in the first place.

--- test/fail_compilation/fail16001.d
+++ test/fail_compilation/fail16001.d
@@ -2,7 +2,7 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail16001.d(10): Deprecation: `(args) => { ... }` is a lambda that returns a delegate, not a multi-line lambda.
+fail_compilation/fail16001.d(10): Deprecation: Using `(args) => { ... }` to create a delegate that returns a delegate is error-prone.
 fail_compilation/fail16001.d(10):        Use `(args) { ... }` for a multi-statement function literal or use `(args) => () { }` if you intended for the lambda to return a delegate.
 ---
 */
--- test/fail_compilation/ice10212.d
+++ test/fail_compilation/ice10212.d
@@ -1,10 +1,8 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/ice10212.d(15): Deprecation: `(args) => { ... }` is a lambda that returns a delegate, not a multi-line lambda.
-fail_compilation/ice10212.d(15):        Use `(args) { ... }` for a multi-statement function literal or use `(args) => () { }` if you intended for the lambda to return a delegate.
-fail_compilation/ice10212.d(15): Error: Expected return type of `int`, not `int function() pure nothrow @nogc @safe`:
-fail_compilation/ice10212.d(15):        Return type of `int` inferred here.
+fail_compilation/ice10212.d(13): Error: Expected return type of `int`, not `int function() pure nothrow @nogc @safe`:
+fail_compilation/ice10212.d(13):        Return type of `int` inferred here.
 ---
 */
 
@@ -12,7 +10,7 @@ int delegate() foo()
 {
     // returns "int function() pure nothrow @safe function() pure nothrow @safe"
     // and it mismatches to "int delegate()"
-    return () => {
+    return () => () {
         return 1;
     };
 }
