f82aa825af31de42250b5df20df468f41ddb8b9a
Iain Buclaw <ibuclaw@gdcproject.org>
2021-06-16 14:12:47 +0200

    fix Issue 22029 - importC: Parser accepts storage-class specifiers for fields

--- test/fail_compilation/failcstuff1.d
+++ test/fail_compilation/failcstuff1.d
@@ -4,6 +4,12 @@
 fail_compilation/imports/cstuff1.c(101): Error: attributes should be specified before the function definition
 fail_compilation/imports/cstuff1.c(200): Error: no members for `enum E21962`
 fail_compilation/imports/cstuff1.c(201): Error: no members for anonymous enum
+fail_compilation/imports/cstuff1.c(303): Error: storage class not allowed in specifier-qualified-list
+fail_compilation/imports/cstuff1.c(304): Error: storage class not allowed in specifier-qualified-list
+fail_compilation/imports/cstuff1.c(305): Error: storage class not allowed in specifier-qualified-list
+fail_compilation/imports/cstuff1.c(306): Error: storage class not allowed in specifier-qualified-list
+fail_compilation/imports/cstuff1.c(307): Error: storage class not allowed in specifier-qualified-list
+fail_compilation/imports/cstuff1.c(308): Error: storage class not allowed in specifier-qualified-list
 ---
 */
 import imports.cstuff1;
