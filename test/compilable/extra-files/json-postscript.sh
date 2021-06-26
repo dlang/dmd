e6cd5c458ca821892330c8eacbf2428c00f6f3d1
MoonlightSentinel <moonlightsentinel@disroot.org>
2021-06-26 15:48:58 +0200

    d_do_test: Unify dir separator from `TEST_OUTPUT_FILE`s ...
    ... as done for `TEST_OUTPUT`. Currently excludes JSON files because
    those don't support standalone `\` and dmd just emits `/` instead.
    
    Changed the extensions of all expected output files to accomodate
    for this change.

--- test/compilable/extra-files/json-postscript.sh
+++ test/compilable/extra-files/json-postscript.sh
@@ -5,6 +5,6 @@ source tools/common_funcs.sh
 echo SANITIZING JSON...
 ${RESULTS_DIR}/sanitize_json ${OUTPUT_BASE}.out > ${OUTPUT_BASE}.out.sanitized
 
-diff -pu --strip-trailing-cr ${EXTRA_FILES}/${TEST_NAME}.out ${OUTPUT_BASE}.out.sanitized
+diff -pu --strip-trailing-cr ${EXTRA_FILES}/${TEST_NAME}.json ${OUTPUT_BASE}.out.sanitized
 
 rm_retry ${OUTPUT_BASE}.out{,.sanitized}
