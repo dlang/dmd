469c53520a43385a14ba4cd808ba11cc2a4f28bb
SealabJaster <sealabjaster@gmail.com>
2021-07-02 01:58:27 +0100

    I dare myself to miss another space

--- test/fail_compilation/fail_fmt1_implconv.d
+++ test/fail_compilation/fail_fmt1_implconv.d
@@ -3,8 +3,8 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail_fmt1_implconv.d(20): Error: cannot implicitly convert expression `bd` of type
-fail_compilation/fail_fmt1_implconv.d(20):            `void delegate() nothrow @nogc`
+fail_compilation/fail_fmt1_implconv.d(20): Error: cannot implicitly convert expression `bd` of type 
+fail_compilation/fail_fmt1_implconv.d(20):            `void delegate() nothrow @nogc` 
 fail_compilation/fail_fmt1_implconv.d(20):        to 
 fail_compilation/fail_fmt1_implconv.d(20):            `void delegate() pure nothrow @nogc`
 ---
