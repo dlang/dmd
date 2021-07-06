1c81e07faff6aea0b363b088bd419dd3cbc56c36
Iain Buclaw <ibuclaw@gdcproject.org>
2021-07-06 12:20:45 +0200

    fix Issue 22106 - importC: Error: variable 'var' no definition of struct 'type'

--- test/fail_compilation/failcstuff3.c
+++ test/fail_compilation/failcstuff3.c
@@ -2,6 +2,7 @@
 /* TEST_OUTPUT:
 ---
 fail_compilation/failcstuff3.c(54): Error: redeclaration of `S22061`
+fail_compilation/failcstuff3.c(107): Error: variable `failcstuff3.T22106.f1` no definition of struct `S22106_t`
 ---
 */
 
@@ -13,3 +14,16 @@ struct S22061
     int field;
 };
 typedef union S22061 S22061;
+
+/***************************************************/
+// https://issues.dlang.org/show_bug.cgi?id=22106
+#line 100
+typedef struct S22106
+{
+    int field;
+} S22106_t;
+
+struct T22106
+{
+    struct S22106_t f1;
+};
