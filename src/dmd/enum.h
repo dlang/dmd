c2c294b743ab75c97a7a38ec6173f087135a9f8e
Ben Jones <ben.james.jones@gmail.com>
2021-07-08 00:53:07 -0600

    move enum.getMaxMinValue to typesemantic, its only callsite (#12837)

--- src/dmd/enum.h
+++ src/dmd/enum.h
@@ -49,7 +49,6 @@ public:
     Dsymbol *search(const Loc &loc, Identifier *ident, int flags = SearchLocalsOnly);
     bool isDeprecated() const;                // is Dsymbol deprecated?
     Visibility visible();
-    Expression *getMaxMinValue(const Loc &loc, Identifier *id);
     bool isSpecial() const;
     Expression *getDefaultValue(const Loc &loc);
     Type *getMemtype(const Loc &loc);
