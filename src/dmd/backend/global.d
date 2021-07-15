a747785f79c609ee4d0a9725142e7c5094846d2c
Nicholas Wilson <iamthewilsonator@hotmail.com>
2021-07-15 14:08:13 +0800

    remove dead function dmd.backend.util:util_assert
    and make printInternalFailure `private extern(D)` to `dmd.mars`

--- src/dmd/backend/global.d
+++ src/dmd/backend/global.d
@@ -120,16 +120,12 @@ elem *exp2_copytotemp(elem *e);
 /* util.c */
 //#if __clang__
 //void util_exit(int) __attribute__((noreturn));
-//void util_assert(const(char)*, int) __attribute__((noreturn));
 //#elif _MSC_VER
 //__declspec(noreturn) void util_exit(int);
-//__declspec(noreturn) void util_assert(const(char)*, int);
 //#else
 void util_exit(int);
-void util_assert(const(char)*, int);
 //#if __DMC__
 //#pragma ZTC noreturn(util_exit)
-//#pragma ZTC noreturn(util_assert)
 //#endif
 //#endif
 
