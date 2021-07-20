a8a8d4efaf1031898907273953f74f47cd358fc2
Carsten Schlote <schlote@vahanus.net>
2021-07-20 09:40:39 +0200

    Use ${OBJ} instead of .o

--- test/runnable/importc-test1.sh
+++ test/runnable/importc-test1.sh
@@ -16,11 +16,11 @@ ${OUTPUT_BASE}${EXE}
 # Case2: The referenced module compiled from an .i file is NOT passed on commandline
 #        and the compiler has to guess the right name.
 #        Note: object is passed to keep linker happy
-#        Note: if the module object created from the .i file just contains DECLARATIONS, you can omit the .o file.
+#        Note: if the module object created from the .i file just contains DECLARATIONS, you can omit the ${OBJ} file.
 #              There is no code to link in this case. This work e.g. for preprocessed C headers, e.g. /usr/include/zstd.h
-$DMD -c -m${MODEL} -I${OUTPUT_BASE} ${EXTRA_FILES}${SEP}importc_test.i -of=${EXTRA_FILES}${SEP}importc_test.o
-$DMD -m${MODEL} -I${OUTPUT_BASE} -of${OUTPUT_BASE}${EXE} -I${EXTRA_FILES} ${EXTRA_FILES}${SEP}importc_test.o ${EXTRA_FILES}${SEP}importc_main2.d
+$DMD -c -m${MODEL} -I${OUTPUT_BASE} ${EXTRA_FILES}${SEP}importc_test.i -of=${EXTRA_FILES}${SEP}importc_test${OBJ}
+$DMD -m${MODEL} -I${OUTPUT_BASE} -of${OUTPUT_BASE}${EXE} -I${EXTRA_FILES} ${EXTRA_FILES}${SEP}importc_test${OBJ} ${EXTRA_FILES}${SEP}importc_main2.d
 
 ${OUTPUT_BASE}${EXE}
 
-rm_retry ${OUTPUT_BASE}{a${OBJ},${EXE}} ${EXTRA_FILES}${SEP}importc_test.o ${EXTRA_FILES}/importc_test.i
+rm_retry ${OUTPUT_BASE}{a${OBJ},${EXE}} ${EXTRA_FILES}${SEP}importc_test${OBJ} ${EXTRA_FILES}/importc_test.i
