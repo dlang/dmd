e0acbb6c32b9307fbf9d24c66c68597f3ecf8de0
Andrei David <16004190+dandrei279@users.noreply.github.com>
2021-07-25 11:53:37 +0300

    Fix Issue 17400: new line before candidates in error messages

--- test/fail_compilation/test19107.d
+++ test/fail_compilation/test19107.d
@@ -2,8 +2,8 @@
 EXTRA_FILES: imports/imp19661.d imports/test19107a.d imports/test19107b.d
 TEST_OUTPUT:
 ---
-fail_compilation/test19107.d(24): Error: template `test19107.all` cannot deduce function from argument types `!((c) => c)(string[])`, candidates are:
-fail_compilation/test19107.d(18):        `all(alias pred, T)(T t)`
+fail_compilation/test19107.d(24): Error: template `test19107.all` cannot deduce function from argument types `!((c) => c)(string[])`
+fail_compilation/test19107.d(18):        Candidate is: `all(alias pred, T)(T t)`
   with `pred = __lambda2,
        T = string[]`
   must satisfy the following constraint:
