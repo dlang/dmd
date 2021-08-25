fdac5a5d171016351eae968064690e0ea1ba320a
Iain Buclaw <ibuclaw@gdcproject.org>
2021-08-25 17:56:31 +0200

    test: Remove phobos dependency from old runnable tests

--- test/runnable/test28.d
+++ test/runnable/test28.d
@@ -2,9 +2,6 @@ module test;
 
 import core.stdc.stdio;
 import core.vararg;
-import std.stdio;
-import std.string;
-
 
 /*******************************************/
 
@@ -116,20 +113,18 @@ void test8()
 {
    int[] test;
    test.length = 10;
-   // Show address of array start and its length (10)
-   writefln("%s %s", cast(uint)test.ptr, test.length);
+   assert(test.length == 10);
+   assert(test.ptr != null);
 
    test.length = 1;
-   // Show address of array start and its length (1)
-   writefln("%s %s", cast(uint)test.ptr, test.length);
+   assert(test.length == 1);
+   assert(test.ptr != null);
 
    test.length = 8;
-   // Show address of array start and its length (8)
-   writefln("%s %s", cast(uint)test.ptr, test.length);
+   assert(test.length == 8);
+   assert(test.ptr != null);
 
    test.length = 0;
-   // Shows 0 and 0!
-   writefln("%s %s", cast(uint)test.ptr, test.length);
    assert(test.length == 0);
    assert(test.ptr != null);
 }
@@ -386,11 +381,9 @@ void test22()
 void test23()
 {
     auto t=['a','b','c','d'];
-    writeln(typeid(typeof(t)));
     assert(is(typeof(t) == char[]));
 
     const t2=['a','b','c','d','e'];
-    writeln(typeid(typeof(t2)));
     assert(is(typeof(t2) == const(const(char)[])));
 }
 
@@ -662,7 +655,6 @@ template a34(string name, T...)
         string localchar;
         foreach (a34; T)
         {
-            writefln(`hello`);
             localchar ~= a34.mangleof;
         }
         return localchar;
@@ -671,7 +663,7 @@ template a34(string name, T...)
 
 void test34()
 {
-    writeln(a34!("Adf"[], typeof("adf"),uint)("Adf"[],"adf",1234));
+    assert(a34!("Adf"[], typeof("adf"),uint)("Adf"[],"adf",1234) == "Ayak");
 }
 
 /*******************************************/
@@ -696,7 +688,6 @@ template a36(AnotherT,string name,T...){
         AnotherT localchar;
         foreach(a;T)
         {
-            writefln(`hello`);
             localchar~=a.mangleof;
         }
         return cast(AnotherT)localchar;
@@ -708,7 +699,7 @@ void test36()
     string b="adf";
     uint i=123;
     char[3] c="Adf";
-    writeln(a36!(typeof(b),"Adf")());
+    assert(a36!(typeof(b),"Adf")() == "");
 }
 
 /*******************************************/
@@ -754,8 +745,7 @@ void test39()
 {
     void print(string[] strs)
     {
-        writeln(strs);
-        assert(format("%s", strs) == `["Matt", "Andrew"]`);
+        assert(strs == ["Matt", "Andrew"]);
     }
 
     print(["Matt", "Andrew"]);
@@ -777,7 +767,7 @@ void test40()
 
     with (c.propName)
     {
-        writeln(toString());
+        assert(toString() == "test.test40.C");
     }
 
     auto foo = c.propName;
@@ -803,7 +793,7 @@ void test41()
     assert(x3 == 1);
 
     //test [0 .. 2] = 'b'; // this line should assert
-    writef ("%s\n", test.ptr [0 .. 2]);
+    assert(test.ptr[0 .. 2] == "ba");
 }
 
 /*******************************************/
@@ -820,6 +810,12 @@ void test42()
 
 /*******************************************/
 
+void vararg43(string fmt, ...)
+{
+    assert(_arguments[0] is typeid(const string));
+    assert(va_arg!(const string)(_argptr) == "hello");
+}
+
 struct A43
 {
     static const MY_CONST_STRING = "hello";
@@ -828,7 +824,7 @@ struct A43
     {
         // This will either print garbage or throw a UTF exception.
         // But if never_called() is commented out, then it will work.
-        writefln("%s", MY_CONST_STRING);
+        vararg43("%s", MY_CONST_STRING);
     }
 }
 
@@ -836,7 +832,7 @@ void never_called43()
 {
     // This can be anything; there just needs to be a reference to
     // A43.MY_CONST_STRING somewhere.
-    writefln("%s", A43.MY_CONST_STRING);
+    vararg43("%s", A43.MY_CONST_STRING);
 }
 
 void test43()
@@ -847,6 +843,12 @@ void test43()
 
 /*******************************************/
 
+void vararg44(string fmt, ...)
+{
+    assert(_arguments[0] is typeid(const string));
+    assert(va_arg!(const string)(_argptr) == "hello");
+}
+
 class A44
 {
     static const MY_CONST_STRING = "hello";
@@ -855,7 +857,7 @@ class A44
     {
         // This will either print garbage or throw a UTF exception.
         // But if never_called() is commented out, then it will work.
-        writefln("%s", MY_CONST_STRING);
+        vararg44("%s", MY_CONST_STRING);
     }
 }
 
@@ -863,7 +865,7 @@ void never_called44()
 {
     // This can be anything; there just needs to be a reference to
     // A44.MY_CONST_STRING somewhere.
-    writefln("%s", A44.MY_CONST_STRING);
+    vararg44("%s", A44.MY_CONST_STRING);
 }
 
 void test44()
@@ -1050,13 +1052,11 @@ void test58()
     static S a = {i: 1};
     static S b;
 
-    writefln("a.bar: %s, %s", a.bar, a.abc);
     assert(a.i == 1);
     assert(a.bar[0] == 4);
     assert(a.bar[1] == 4);
     assert(a.bar[2] == 4);
     assert(a.bar[3] == 4);
-    writefln("b.bar: %s, %s", b.bar, b.abc);
     assert(b.i == 0);
     assert(b.bar[0] == 4);
     assert(b.bar[1] == 4);
@@ -1067,10 +1067,18 @@ void test58()
 
 /*******************************************/
 
+void vararg59(...)
+{
+    if (_arguments[0] is typeid(size_t))
+        assert(va_arg!size_t(_argptr) == 454);
+    else
+        assert(va_arg!string(_argptr) == "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234");
+}
+
 void bug59(string s)()
 {
-  writeln(s);
-  writeln(s.length);
+  vararg59(s);
+  vararg59(s.length);
 }
 
 void test59()
@@ -1113,7 +1121,6 @@ void test62()
     Vector62 a;
     a.set(1,2,24);
     a = a * 2;
-    writeln(a.x, a.y, a.z);
     assert(a.x == 2);
     assert(a.y == 4);
     assert(a.z == 48);
@@ -1165,8 +1172,6 @@ void test63()
 {
     Data63 d; d.x = 1; d.y = 2;
     d = frob(d);
-    writeln(d.x);
-    writeln(d.y);
     assert(d.x == 3 && d.y == -1);
 }
 
@@ -1174,8 +1179,8 @@ void test63()
 
 class Foo64
 {
-    this() { writefln("Foo64 created"); }
-    ~this() { writefln("Foo64 destroyed"); }
+    this() { }
+    ~this() { }
 }
 
 template Mix64()
