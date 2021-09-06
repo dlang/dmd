7c3f957e98e273c77abaa145901e28b726f0a0d4
Max Haughton <maxhaton@gmail.com>
2021-09-06 11:50:43 +0100

    Move test file to compilable

--- test/compilable/issue22267.c
+++ test/compilable/issue22267.c
@@ -7,14 +7,4 @@ int32_t ret()
     int32_t init = (1 + 3);
     return init;
 }
-
-int main()
-{
-    int32_t retVal = ret();
-    if(retVal != 4)
-    {
-        printf("ret() returned %s, expected 4\n", retVal);
-        exit(1);
-    }
-    return 0;
-}
+_Static_assert(ret() == 4, "Ret != 4");
