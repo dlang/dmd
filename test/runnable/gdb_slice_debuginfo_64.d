045bc7bbfa994e2d0e9ec955cf56b3d75bf7eb7a
Luís Ferreira <contact@lsferreira.net>
2021-10-03 18:03:47 +0100

    test: gdb: fix tests introduced by #13081
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- test/runnable/gdb_slice_debuginfo_64.d
+++ test/runnable/gdb_slice_debuginfo_64.d
@@ -3,7 +3,7 @@ https://issues.dlang.org/show_bug.cgi?id=22311
 REQUIRED_ARGS: -g -c -m64
 GDB_SCRIPT:
 ---
-print sizeof(_Array_int::length)
+print sizeof('int[]'::length)
 ---
 GDB_MATCH: \$1 = 8
 */
