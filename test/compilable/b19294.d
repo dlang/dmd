475eb7e2a8f3ff75267b32cc1ffe5033fbf533ca
Gabriel <soringabrielmateescu@gmail.com>
2021-10-11 20:41:13 +0300

    added suitable testcases

--- test/compilable/b19294.d
+++ test/compilable/b19294.d
@@ -1,26 +1,69 @@
-import std.stdio;
-import std.complex;
+alias MT = MyStruct!int;
 
-void main()
+struct MyStruct(T)
 {
-    alias T = Complex!float;
+    T x;
 
-    T iV = complex(1.0f, 1.0f);
-    T[] arr = [iV, 2 * iV, 3 * iV, 4 * iV, 5 * iV, 6 * iV];
-    Complex!float[] result = new T[arr.length];
+    this(T y)
+    {
+        x = y;
+    }
+
+    MyStruct!T opBinary(string op)(MyStruct!T y) const
+    {
+        alias C = typeof(return);
+        auto w = C(this.x);
+        return w.opOpAssign!(op)(y);
+    }
+
+    MyStruct!T opBinaryRight(string op)(MyStruct!T y) const
+    {
+        return opBinary!(op)(y);
+    }
+
+    ref MyStruct opOpAssign(string op, T)(const MyStruct!T z)
+    {
+        mixin ("x "~op~"= z.x;");
+        return this;
+    }
+
+    MyStruct!T opBinary(string op)(T y) const
+    {
+        alias C = typeof(return);
+        auto w = C(this.x);
+        return w.opOpAssign!(op)(y);
+    }
+
+    MyStruct!T opBinaryRight(string op)(T y) const
+    {
+        return opBinary!(op)(y);
+    }
+
+    ref MyStruct opOpAssign(string op, T)(const T z)
+    {
+        mixin ("x "~op~"= z;");
+        return this;
+    }
+}
+
+void test()
+{
+    MT s = MyStruct!int(1);
+    MT[] arr = [s, 2 * s, 3 * s, 4 * s, 5 * s, 6 * s];
+    MT[] result = new MT[arr.length];
     
-    result[] = arr[] + iV;
-    result[] = iV + arr[];
+    result[] = arr[] + s;
+    result[] = s + arr[];
     
-    result[] = arr[] - iV;
-    result[] = iV - arr[];
+    result[] = arr[] - s;
+    result[] = s - arr[];
     
-    result[] = arr[] * iV;
-    result[] = iV * arr[];
+    result[] = arr[] * s;
+    result[] = s * arr[];
     
-    result[] = arr[] / iV;
-    result[] = iV / arr[];
+    result[] = arr[] / s;
+    result[] = s / arr[];
     
-    result[] = arr[] ^^ iV;
-    result[] = iV ^^ arr[];
+    result[] = arr[] ^^ s;
+    result[] = s ^^ arr[];
 }
