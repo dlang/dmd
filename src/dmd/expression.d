3dfa83b4a0375277c3052788d56c431f631492d9
Boris Carvajal <boris2.9@gmail.com>
2021-10-17 06:09:30 -0300

    Fix Issue 22151 - Compiler crash when attempting to assign to function

--- src/dmd/expression.d
+++ src/dmd/expression.d
@@ -5280,6 +5280,19 @@ extern (C++) final class PtrExp : UnaExp
     override Expression modifiableLvalue(Scope* sc, Expression e)
     {
         //printf("PtrExp::modifiableLvalue() %s, type %s\n", toChars(), type.toChars());
+        Declaration var;
+        if (auto se = e1.isSymOffExp())
+            var = se.var;
+        else if (auto ve = e1.isVarExp())
+            var = ve.var;
+        if (var && var.type.isFunction_Delegate_PtrToFunction())
+        {
+            if (var.type.isTypeFunction())
+                error("function `%s` is not an lvalue and cannot be modified", var.toChars());
+            else
+                error("function pointed to by `%s` is not an lvalue and cannot be modified", var.toChars());
+            return ErrorExp.get();
+        }
         return Expression.modifiableLvalue(sc, e);
     }
 
@@ -6983,4 +6996,3 @@ extern (C++) final class GenericExp : Expression
         v.visit(this);
     }
 }
-
