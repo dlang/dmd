883048c55fe4fef06f09aa517b0baa38574b274e
Luís Ferreira <contact@lsferreira.net>
2021-10-31 00:17:41 +0100

    backend: dwarfdbginf: use distinguishable type names for delegate types
    According to the specification, the memory layout for delegates have the
    context pointer as `ptr` instead of `ctxptr` and this also matches the compiler
    behaviour so debug info should also report correct member names.
    
    Delegates are currently reported as `_Delegate` which is not distinguishable
    among other delegate types with different return types and parameters, so name
    these types with their qualified name is desirable.
    
    Fixes #22459.
    
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/dmd/backend/dwarfdbginf.d
+++ src/dmd/backend/dwarfdbginf.d
@@ -2508,14 +2508,14 @@ static if (1)
 
                 code = dwarf_abbrev_code(abbrevTypeStruct.ptr, (abbrevTypeStruct).sizeof);
                 idx = cast(uint)debug_info.buf.length();
-                debug_info.buf.writeuLEB128(code);        // abbreviation code
-                debug_info.buf.writeString("_Delegate");  // DW_AT_name
+                debug_info.buf.writeuLEB128(code);       // abbreviation code
+                debug_info.buf.writeString(t.Tident);    // DW_AT_name
                 debug_info.buf.writeByte(tysize(t.Tty)); // DW_AT_byte_size
 
-                // ctxptr
+                // ptr
                 code = dwarf_abbrev_code(abbrevTypeMember.ptr, (abbrevTypeMember).sizeof);
                 debug_info.buf.writeuLEB128(code);        // abbreviation code
-                debug_info.buf.writeString("ctxptr");     // DW_AT_name
+                debug_info.buf.writeString("ptr");        // DW_AT_name
                 debug_info.buf.write32(pvoididx);         // DW_AT_type
 
                 debug_info.buf.writeByte(2);              // DW_AT_data_member_location
