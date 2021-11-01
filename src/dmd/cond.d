f18d3263495654039eb3ac6f14c23a5d00b7dc94
Boris Carvajal <boris2.9@gmail.com>
2021-11-01 00:47:59 -0300

    Fix Issue 22421 - static foreach introduces semantic difference between indexing and iteration variable

--- src/dmd/cond.d
+++ src/dmd/cond.d
@@ -452,7 +452,6 @@ extern (C++) final class StaticForeach : RootObject
             sc = sc.startCTFE();
             aggrfe.aggr = aggrfe.aggr.expressionSemantic(sc);
             sc = sc.endCTFE();
-            aggrfe.aggr = aggrfe.aggr.optimize(WANTvalue);
         }
 
         if (aggrfe && aggrfe.aggr.type.toBasetype().ty == Terror)
