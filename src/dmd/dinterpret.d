fcea221e0be0dc21b1703158718466baae90ed3d
Boris Carvajal <boris2.9@gmail.com>
2021-11-21 00:53:26 -0300

    Fix Issue 22530 - Explicit cast between classes incorrectly goes through 'alias this' inside CTFE

--- src/dmd/dinterpret.d
+++ src/dmd/dinterpret.d
@@ -6036,7 +6036,7 @@ public:
             result = pue.exp();
             return;
         }
-        result = ctfeCast(pue, e.loc, e.type, e.to, e1);
+        result = ctfeCast(pue, e.loc, e.type, e.to, e1, true);
     }
 
     override void visit(AssertExp e)
