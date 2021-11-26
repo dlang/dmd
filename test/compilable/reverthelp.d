d5e04072499d531d0cece58cdaf04e446b43d0ab
12345swordy <alexanderheistermann@gmail.com>
2021-11-26 01:17:28 -0600

    Set the preview switch fix16997 to true now. (#13348)
    * Set the preview switch fix16997 to true now.
    
    * Add preview switch.
    
    * Fix test suit
    
    * Fix the reverthelp.d test
    
    * Auto gen the frontend
    
    * Fix changelog/fix16997.dd
    
    * Corrected frontend.h
    
    * Update fix16997.dd
    
    Modify changelog
    
    * Fix trimming of spaces for changelog/fix16997.dd
    
    * Update fix16997.dd
    
    Fix Title description tile for changelog and move some text around
    
    * Remove empty line
    
    * Update fix16997.dd
    
    add markdown
    
    * Update fix16997.dd
    
    * single quotes
    
    Co-authored-by: MoonlightSentinel <moonlightsentinel@disroot.org>

--- test/compilable/reverthelp.d
+++ test/compilable/reverthelp.d
@@ -6,6 +6,7 @@ TEST_OUTPUT:
 Revertable language changes listed by -revert=name:
   =all              Enables all available revertable language changes
   =dip25            revert DIP25 changes https://github.com/dlang/DIPs/blob/master/DIPs/archive/DIP25.md
+  =intpromote       revert integral promotions for unary + - ~ operators
   =markdown         disable Markdown replacements in Ddoc
   =dtorfields       don't destruct fields of partially constructed objects
 ----
