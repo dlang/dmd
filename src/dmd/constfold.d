128811d38a7584a0e27f427dd51131a6e3120dcd
lucica28 <57060141+lucica28@users.noreply.github.com>
2021-11-30 13:00:28 +0200

    Fix Issue 22385 - CTFE fails to iterate over associative array previously indexed with implicit conversion to enum base type (#13326)

--- src/dmd/constfold.d
+++ src/dmd/constfold.d
@@ -1054,6 +1054,12 @@ UnionExp Cast(const ref Loc loc, Type type, Type to, Expression e1)
         emplaceExp!(UnionExp)(&ue, ex);
         return ue;
     }
+    if (e1.type.toBasetype.equals(type) && type.equals(to))
+    {
+        emplaceExp!(UnionExp)(&ue, e1);
+        ue.exp().type = type;
+        return ue;
+    }
     if (e1.type.implicitConvTo(to) >= MATCH.constant || to.implicitConvTo(e1.type) >= MATCH.constant)
     {
         goto L1;
