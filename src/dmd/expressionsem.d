8efc7eb1950e1b4269f9cc22e22128176a3fc4b6
Iain Buclaw <ibuclaw@gdcproject.org>
2021-12-26 23:36:59 +0100

    Use %llu instead of %zu as a format specifier in errors

--- src/dmd/expressionsem.d
+++ src/dmd/expressionsem.d
@@ -6013,7 +6013,7 @@ private extern (C++) final class ExpressionSemanticVisitor : Visitor
                 const attr = FileName.exists(path);
                 const(char)* err = attr == 2 ? "" :
                     (attr == 1 ? " (not a directory)" : " (path not found)");
-                e.errorSupplemental("[%zu]: `%s`%s", idx, path, err);
+                e.errorSupplemental("[%llu]: `%s`%s", cast(ulong)idx, path, err);
             }
             return setError();
         }
@@ -13108,7 +13108,7 @@ private bool fit(StructDeclaration sd, const ref Loc loc, Scope* sc, Expressions
                 // CTFE sometimes creates null as hidden pointer; we'll allow this.
                 continue;
             }
-                .error(loc, "more initializers than fields (%zu) of `%s`", nfields, sd.toChars());
+                .error(loc, "more initializers than fields (%llu) of `%s`", cast(ulong)nfields, sd.toChars());
             return false;
         }
         VarDeclaration v = sd.fields[i];
