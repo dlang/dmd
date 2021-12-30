9acac763f929771c3a2f6b28b75e0eade730da39
MoonlightSentinel <moonlightsentinel@disroot.org>
2021-12-30 18:56:37 +0100

    Add missing colons after test directives
    They were not required by the current test runner but will
    be enforced by future patches

--- test/compilable/objc_class.d
+++ test/compilable/objc_class.d
@@ -1,4 +1,4 @@
-// EXTRA_OBJC_SOURCES
+// EXTRA_OBJC_SOURCES:
 
 import core.attribute : selector;
 
--- test/runnable/objc_call_static.d
+++ test/runnable/objc_call_static.d
@@ -1,4 +1,4 @@
-// EXTRA_OBJC_SOURCES
+// EXTRA_OBJC_SOURCES:
 // REQUIRED_ARGS: -L-framework -LFoundation
 
 import core.attribute : selector;
