9a9aa220ff3ec1a7d796019b74de4650d31ac854
Andrey Penechko <mrsmith33@yandex.ru>
2022-01-20 14:33:34 +0200

    Implement string detabber that accounts for tabs and use it for printing errors with context
    Use OutBuffer for line and cursor printing.
    
    Issue 22678

--- test/fail_compilation/fail22678.d
+++ test/fail_compilation/fail22678.d
@@ -2,12 +2,40 @@
 // REQUIRED_ARGS: -verrors=context -vcolumns
 /* TEST_OUTPUT:
 ---
-fail_compilation/fail22678.d(12,13): Error: cannot implicitly convert expression `1` of type `int` to `string`
-	string a = 1;
-	           ^
+fail_compilation/fail22678.d(24,13): Error: cannot implicitly convert expression `1` of type `int` to `string`
+    string a = 1;
+               ^
+fail_compilation/fail22678.d(28,14): Error: cannot implicitly convert expression `1` of type `int` to `string`
+    string a = 1;
+               ^
+fail_compilation/fail22678.d(32,21): Error: cannot implicitly convert expression `1` of type `int` to `string`
+    string straße = 1;
+                    ^
+fail_compilation/fail22678.d(36,22): Error: cannot implicitly convert expression `1` of type `int` to `string`
+    string straße = 1;
+                    ^
+fail_compilation/fail22678.d(40,23): Error: cannot implicitly convert expression `1` of type `int` to `string`
+        string straße = 1;
+                        ^
 ---
 */
-void fail22678()
+void fail22678_1()
 {
 	string a = 1;
+}
+void fail22678_2()
+{
+ 	string a = 1;
+}
+void fail22678_3()
+{
+  	string straße = 1;
+}
+void fail22678_4()
+{
+   	string straße = 1;
+}
+void fail22678_5()
+{
+    	string straße = 1;
 }
