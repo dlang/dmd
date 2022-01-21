71b0f492564cb2ee19a91ef14c41c98a4b20b9a3
Walter Bright <WalterBright@users.noreply.github.com>
2022-01-21 14:33:46 -0800

    remove dead __builtin code (#13559)

--- src/dmd/frontend.h
+++ src/dmd/frontend.h
@@ -8472,10 +8472,7 @@ struct Id final
     static Identifier* noreturn;
     static Identifier* builtins;
     static Identifier* builtin_va_list;
-    static Identifier* builtin_va_start;
     static Identifier* builtin_va_arg;
-    static Identifier* builtin_va_copy;
-    static Identifier* builtin_va_end;
     static Identifier* va_list_tag;
     static Identifier* va_arg;
     static Identifier* pack;
