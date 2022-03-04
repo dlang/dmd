f5d878fdd1b28e4609840755237be0d41f638845
Iain Buclaw <ibuclaw@gdcproject.org>
2022-03-04 13:10:34 +0100

    fix Issue 22844 - [REG 2.089] SIGBUS, Bus error in _d_newitemU

--- src/rt/lifetime.d
+++ src/rt/lifetime.d
@@ -1118,7 +1118,8 @@ extern (C) void* _d_newitemU(scope const TypeInfo _ti) pure nothrow @weak
 
     if (tiSize)
     {
-        *cast(TypeInfo*)(p + itemSize) = null; // the GC might not have cleared this area
+        // the GC might not have cleared the padding area in the block
+        *cast(TypeInfo*)(p + (itemSize & ~(size_t.sizeof - 1))) = null;
         *cast(TypeInfo*)(p + blkInf.size - tiSize) = cast() ti;
     }
 
