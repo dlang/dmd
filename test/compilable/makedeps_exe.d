315fdaf3040cfc37969549bc95c38ff81a4f4aef
Tobias Pankrath <tobias.pankrath@vattenfall.de>
2022-03-04 12:39:59 +0000

    Fix Issue 21844 - makedeps contain non-existing files

--- test/compilable/makedeps_exe.d
+++ test/compilable/makedeps_exe.d
@@ -9,6 +9,7 @@ $r:.*makedeps_exe_$0$?:windows=.exe$: \
   $p:makedeps_exe.d$ \
   $p:makedeps_a.d$ \
   $p:makedeps-import.txt$ \
+  $p:makedeps-import-codemixin.txt$ \
 ---
 **/
 module makedeps_exe;
@@ -19,6 +20,13 @@ import imports.makedeps_a;
 // Test import expression
 enum text = import("makedeps-import.txt");
 static assert(text == "Imported text");
+
+/*******************************/
+// https://issues.dlang.org/show_bug.cgi?id=21844
+enum bool failingModuleImport = __traits(compiles, ((){ import does.not.exist; })());
+enum bool failingFileImport = __traits(compiles, ((){ return import("does.not.exists.txt");})());
+enum bool workingFileImport = __traits(compiles, ((){ return import("makedeps-import-codemixin.txt");})());
+static assert (workingFileImport);
 
 void main()
 {
