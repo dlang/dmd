53f9e4aa8f817ce9c7a68ed96adbdd0b620fa3d5
RazvanN7 <razvan.nitu1305@gmail.com>
2022-03-08 15:33:23 +0200

    Fix Issue 20015 - [REG 2.086] Deprecated -preview, -revert, and -transition options not documented

--- test/compilable/transitionhelp.d
+++ test/compilable/transitionhelp.d
@@ -6,6 +6,7 @@ TEST_OUTPUT:
 Language transitions listed by -transition=name:
   =all              Enables all available language transitions
   =field            list all non-mutable fields which occupy an object instance
+  =complex          give deprecation messages about all usages of complex or imaginary types [DEPRECATED]
   =tls              list all variables going into thread local storage
   =vmarkdown        list instances of Markdown replacements in Ddoc
   =in               list all usages of 'in' on parameter
