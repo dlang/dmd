69c2da1f9e9fa8e98fd2202ba1211cc825606f86
Luís Ferreira <contact@lsferreira.net>
2022-03-15 03:45:02 +0000

    frontend: make unannotated asm blocks error instead of deprecation
    This is a really old deprecation that already passed the deprecation period for
    more than 5 years.
    
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- test/fail_compilation/test11471.d
+++ test/fail_compilation/test11471.d
@@ -2,7 +2,7 @@
 /*
 TEST_OUTPUT:
 ---
-compilable/test11471.d(10): Deprecation: `asm` statement is assumed to throw - mark it with `nothrow` if it does not
+fail_compilation/test11471.d(10): Error: `asm` statement is assumed to throw - mark it with `nothrow` if it does not
 ---
 */
 
