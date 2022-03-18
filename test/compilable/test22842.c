314e538c672512765e7603c5068031944ad36de2
Walter Bright <walter@walterbright.com>
2022-03-18 01:44:31 -0700

    fix Issue 22876 - importC: expression parsing affected by parentheses that should do nothing

--- test/compilable/test22842.c
+++ test/compilable/test22842.c
@@ -48,3 +48,35 @@ int low2()
 {
     dd = 1;
 }
+
+/***********************************************************************/
+
+// https://issues.dlang.org/show_bug.cgi?id=22876
+
+int mint1()
+{
+    int a = 0;
+    // dmd gives 1, other compilers -1
+    // bug disappears if the parentheses around (a) are removed
+    return - (a) - 1;
+}
+
+_Static_assert(mint1() == -1, "1");
+
+int mint2()
+{
+    int *a, *b;
+    // Error: incompatible types for `(a) - (cast(char*)b)`: `int*` and `char*`
+    // works if the parentheses around (a) are removed
+    long diff = (char*)(a) - (char*)b;
+}
+
+void mint3()
+{
+    int *p;
+    // Error: `p` is not of integral type, it is a `int*`
+    // Error: `cast(int*)1` is not of integral type, it is a `int*`
+    // works if parentheses around (p) are removed
+    unsigned x = (unsigned)(p) & 1;
+}
+
