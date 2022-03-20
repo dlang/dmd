c7b3bf6665c01ae0cc4bf3e463dbbd15d9792bda
Luís Ferreira <contact@lsferreira.net>
2022-03-20 17:08:07 +0000

    Fix issue 22906: DMDLIB hangs on semantic analysis of non regular D files
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/dmd/semantic3.d
+++ src/dmd/semantic3.d
@@ -187,14 +187,17 @@ private extern(C++) final class Semantic3Visitor : Visitor
         // gets imported, it is unaffected by context.
         Scope* sc = Scope.createGlobal(mod); // create root scope
         //printf("Module = %p\n", sc.scopesym);
-        // Pass 3 semantic routines: do initializers and function bodies
-        for (size_t i = 0; i < mod.members.dim; i++)
+        if (mod.members)
         {
-            Dsymbol s = (*mod.members)[i];
-            //printf("Module %s: %s.semantic3()\n", toChars(), s.toChars());
-            s.semantic3(sc);
+            // Pass 3 semantic routines: do initializers and function bodies
+            for (size_t i = 0; i < mod.members.dim; i++)
+            {
+                Dsymbol s = (*mod.members)[i];
+                //printf("Module %s: %s.semantic3()\n", toChars(), s.toChars());
+                s.semantic3(sc);
 
-            mod.runDeferredSemantic2();
+                mod.runDeferredSemantic2();
+            }
         }
         if (mod.userAttribDecl)
         {
