526eae7f822b867c213eff8ef9409e5ab11bf8d7
Atila Neves <atila.neves@gmail.com>
2022-04-15 17:40:52 +0200

    Delete FuncDeclaration.checkUnsafe

--- src/dmd/func.d
+++ src/dmd/func.d
@@ -1445,13 +1445,6 @@ extern (C++) class FuncDeclaration : Declaration
         return false;
     }
 
-    /**
-     * Like setUnsafe, but doesn't change any state
-     */
-    final bool checkUnsafe() {
-        return type.toTypeFunction().trust == TRUST.safe;
-    }
-
     final bool isNogc()
     {
         //printf("isNogc() %s, inprocess: %d\n", toChars(), !!(flags & FUNCFLAG.nogcInprocess));
