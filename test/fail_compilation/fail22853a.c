0061f86cc2c7590011ab54dca4fde8b3032da2c1
Walter Bright <WalterBright@users.noreply.github.com>
2022-04-27 22:52:25 -0700

    fix Issue 23037 - importC: type with only type-qualifier doesn't work (#14018)

--- test/fail_compilation/fail22853a.c
+++ test/fail_compilation/fail22853a.c
@@ -1,6 +1,7 @@
 /+ https://issues.dlang.org/show_bug.cgi?id=22853 +/
 /* TEST_OUTPUT:
 ---
-fail_compilation/fail22853a.c(1): Error: identifier or `(` expected
+fail_compilation/fail22853a.c(1): Error: no type-specifier for declarator
+fail_compilation/fail22853a.c(8): Error: `=`, `;` or `,` expected to end declaration instead of `End of File`
 ---
 */
