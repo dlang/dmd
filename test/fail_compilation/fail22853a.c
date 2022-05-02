6a0ad49d393eb9ff42fe7b8b0e3ef9bd5264cb55
Walter Bright <WalterBright@users.noreply.github.com>
2022-05-02 01:15:18 -0700

    fix Issue 23057 - importC: dmd segfault on invalid syntax (#14049)

--- test/fail_compilation/fail22853a.c
+++ test/fail_compilation/fail22853a.c
@@ -1,7 +1,6 @@
 /+ https://issues.dlang.org/show_bug.cgi?id=22853 +/
 /* TEST_OUTPUT:
 ---
-fail_compilation/fail22853a.c(1): Error: no type-specifier for declarator
-fail_compilation/fail22853a.c(8): Error: `=`, `;` or `,` expected to end declaration instead of `End of File`
+fail_compilation/fail22853a.c(1): Error: no type for declarator before `/`
 ---
 */
