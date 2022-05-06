f9b0ca9e5eee96b9f7c6add24c1caf4f5553e098
Walter Bright <WalterBright@users.noreply.github.com>
2022-05-06 00:39:26 -0700

    Update testxmm.d
    remove trailing whitespace

--- test/runnable/testxmm.d
+++ test/runnable/testxmm.d
@@ -2409,13 +2409,13 @@ float test23085(float x)
 
 // https://issues.dlang.org/show_bug.cgi?id=23084
 
-__vector(int[4]) test23084a(__vector(int[4] a) 
+__vector(int[4]) test23084a(__vector(int[4] a)
 {
     __vector(short[8]) r = cast(short)(a.array[0]);
     return cast(__vector(int[4]))r;
 }
 
-__vector(int[4]) test23084b(__vector(int[4]) a) 
+__vector(int[4]) test23084b(__vector(int[4]) a)
 {
     __vector(byte[16]) r = cast(byte)(a.array[0]);
     return cast(__vector(int[4]))r;
