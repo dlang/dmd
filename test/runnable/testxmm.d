08b1c59bb8b6d41cefb0f931fbccbd172d56b52c
Walter Bright <WalterBright@users.noreply.github.com>
2022-05-06 00:25:13 -0700

    Update test/runnable/testxmm.d
    Co-authored-by: Iain Buclaw <ibuclaw@gdcproject.org>

--- test/runnable/testxmm.d
+++ test/runnable/testxmm.d
@@ -2398,6 +2398,15 @@ float test23077(float x)
     return *cast(float*)&i; // this cast is not allowed in @safe code
 }
 
+// https://issues.dlang.org/show_bug.cgi?id=23085
+
+float test23085(float x)
+{
+    byte i = *cast(byte*)&x;
+    ++i;
+    return *cast(float*)&i; // this cast is not allowed in @safe code
+}
+
 // https://issues.dlang.org/show_bug.cgi?id=23084
 
 __vector(int[4]) test23084a(__vector(int[4] a) 
