b94ed852b7f23a7c246e32a3b5b608bc6a3beac0
Walter Bright <WalterBright@users.noreply.github.com>
2022-05-06 00:25:02 -0700

    Update test/runnable/testxmm.d
    Co-authored-by: Iain Buclaw <ibuclaw@gdcproject.org>

--- test/runnable/testxmm.d
+++ test/runnable/testxmm.d
@@ -2400,17 +2400,17 @@ float test23077(float x)
 
 // https://issues.dlang.org/show_bug.cgi?id=23084
 
-short saturateSignedIntToSignedShort(int value)
+__vector(int[4]) test23084a(__vector(int[4] a) 
 {
-    return cast(short) value;
-}
-
-__vector(int[4]) test23084(__vector(int[4]) a)
-{
-    __vector(short[8]) r;
-    r = saturateSignedIntToSignedShort(a.array[0]);
+    __vector(short[8]) r = cast(short)(a.array[0]);
     return cast(__vector(int[4]))r;
-}
+} 
+
+__vector(int[4]) test23084b(__vector(int[4]) a) 
+{
+    __vector(byte[16]) r = cast(byte)(a.array[0]);
+    return cast(__vector(int[4]))r;
+} 
 
 
 /*****************************************/
