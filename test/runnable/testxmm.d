8c0ce216e936fdf597ddffbd5632c61e805050e0
Walter Bright <WalterBright@users.noreply.github.com>
2022-05-06 00:33:19 -0700

    Update testxmm.d
    remove trailing whitespace

--- test/runnable/testxmm.d
+++ test/runnable/testxmm.d
@@ -2413,13 +2413,13 @@ __vector(int[4]) test23084a(__vector(int[4] a)
 {
     __vector(short[8]) r = cast(short)(a.array[0]);
     return cast(__vector(int[4]))r;
-} 
+}
 
 __vector(int[4]) test23084b(__vector(int[4]) a) 
 {
     __vector(byte[16]) r = cast(byte)(a.array[0]);
     return cast(__vector(int[4]))r;
-} 
+}
 
 
 /*****************************************/
