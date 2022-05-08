3eedace3346b6245695f4c00ce6bae760cf7df57
mhh <maxhaton@gmail.com>
2022-05-08 03:06:26 +0100

    Fix Issue 23088 - This bug is a symptom of a bad error message.
    The expression (or rather it's context is still worthy of being illegal)

--- test/fail_compilation/fail12390.d
+++ test/fail_compilation/fail12390.d
@@ -1,7 +1,8 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail12390.d(14): Error: `fun().i == 4` has no effect
+fail_compilation/fail12390.d(15): Error: `fun().i == 4` may not be discarded, since it is likely a mistake
+fail_compilation/fail12390.d(15):        Note that `fun().i` may have a side effect
 ---
 */
 
