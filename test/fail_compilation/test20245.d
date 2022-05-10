90d7849a9a092e730644ef7bee4deb539aca122f
Dennis Korpel <dkorpel@gmail.com>
2022-05-10 15:25:26 +0200

    Give checkNewEscape errors in safe code only

--- test/fail_compilation/test20245.d
+++ test/fail_compilation/test20245.d
@@ -3,11 +3,11 @@ REQUIRED_ARGS: -preview=dip1000
 TEST_OUTPUT:
 ---
 fail_compilation/test20245.d(20): Error: reference to local variable `x` assigned to non-scope parameter `ptr`
-fail_compilation/test20245.d(21): Error: copying `&x` into allocated memory escapes a reference to parameter variable `x`
+fail_compilation/test20245.d(21): Error: copying `&x` into allocated memory escapes a reference to parameter `x`
 fail_compilation/test20245.d(22): Error: scope variable `a` may not be returned
 fail_compilation/test20245.d(26): Error: cannot take address of `scope` variable `x` since `scope` applies to first indirection only
 fail_compilation/test20245.d(32): Error: reference to local variable `x` assigned to non-scope parameter `ptr`
-fail_compilation/test20245.d(33): Error: copying `&x` into allocated memory escapes a reference to parameter variable `x`
+fail_compilation/test20245.d(33): Error: copying `&x` into allocated memory escapes a reference to parameter `x`
 fail_compilation/test20245.d(49): Error: reference to local variable `price` assigned to non-scope `this.minPrice`
 fail_compilation/test20245.d(68): Error: reference to local variable `this` assigned to non-scope parameter `msg`
 fail_compilation/test20245.d(88): Error: reference to local variable `this` assigned to non-scope parameter `content`
