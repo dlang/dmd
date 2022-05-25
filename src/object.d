e588ec4c922c30130718b6f78db81a661c43bdfb
Walter Bright <walter@walterbright.com>
2022-05-25 14:27:14 -0700

    Object.getHash() shouldn't be casting away const

--- src/object.d
+++ src/object.d
@@ -1481,7 +1481,7 @@ class TypeInfo_Delegate : TypeInfo
 
     override size_t getHash(scope const void* p) @trusted const
     {
-        return hashOf(*cast(void delegate()*)p);
+        return hashOf(*cast(void delegate() const *)p);
     }
 
     override bool equals(in void* p1, in void* p2) const
