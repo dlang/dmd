f283e08d179dec227e74af8fde2e08bb45f341db
Nick Treleaven <n@trelsoft.com>
2022-06-02 16:43:12 +0100

    Fix wrong test output

--- test/fail_compilation/template_decl.d
+++ test/fail_compilation/template_decl.d
@@ -1,8 +1,8 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/template_decl.d(7): Error: `{` expected after template parameter list, not `(`
-fail_compilation/template_decl.d(7): Error: declaration expected, not `(`
+fail_compilation/template_decl.d(8): Error: `{` expected after template parameter list, not `(`
+fail_compilation/template_decl.d(8): Error: declaration expected, not `(`
 ---
 */
 template b(alias d)() {
