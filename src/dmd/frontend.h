5ba0fa2a593582e505d1f9b0ef45fc8d6452b74a
FeepingCreature <feepingcreature@gmail.com>
2022-06-04 13:39:20 +0200

    Mark fixupInvariantIdent as extern(D); it's not needed in the headers.

--- src/dmd/frontend.h
+++ src/dmd/frontend.h
@@ -3022,7 +3022,6 @@ public:
     bool addPostInvariant();
     InvariantDeclaration* isInvariantDeclaration();
     void accept(Visitor* v);
-    void fixupInvariantIdent(size_t offset);
 };
 
 class NewDeclaration final : public FuncDeclaration
