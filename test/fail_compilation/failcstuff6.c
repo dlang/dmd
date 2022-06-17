ee0bd218b3ae73704d013eb7e3ef1d9117a77698
Iain Buclaw <ibuclaw@gdcproject.org>
2022-06-17 10:24:57 +0200

    ImportC: Fix member value fits integer check (#14225)

--- test/fail_compilation/failcstuff6.c
+++ test/fail_compilation/failcstuff6.c
@@ -2,6 +2,11 @@
 /* TEST_OUTPUT:
 ---
 fail_compilation/failcstuff6.c(56): Error: enum member `failcstuff6.test_overflow.boom` initialization with `2147483647+1` causes overflow for type `int`
+fail_compilation/failcstuff6.c(105): Error: enum member `failcstuff6.test_enum_fits.firstMinError` enum member value `-2147483649L` does not fit in an `int`
+fail_compilation/failcstuff6.c(106): Error: enum member `failcstuff6.test_enum_fits.firstMaxError` enum member value `4294967296L` does not fit in an `int`
+fail_compilation/failcstuff6.c(107): Error: enum member `failcstuff6.test_enum_fits.lastMaxError` enum member value `18446744071562067967LU` does not fit in an `int`
+fail_compilation/failcstuff6.c(108): Error: enum member `failcstuff6.test_enum_fits.firstBlindSpot` enum member value `18446744071562067968LU` does not fit in an `int`
+fail_compilation/failcstuff6.c(109): Error: enum member `failcstuff6.test_enum_fits.lastBlindSpot` enum member value `18446744073709551615LU` does not fit in an `int`
 ---
 */
 
@@ -14,4 +19,19 @@ enum test_overflow
     two,
     one,
     boom,
+};
+
+
+/***************************************************/
+#line 100
+
+enum test_enum_fits
+{
+    intMinFits     = -2147483648,
+    intMaxFits     = 4294967295,
+    firstMinError  = -2147483649,
+    firstMaxError  = 4294967296,
+    lastMaxError   = 0xffffffff7fffffff,
+    firstBlindSpot = 0xffffffff80000000,
+    lastBlindSpot  = 0xffffffffffffffff,
 };
