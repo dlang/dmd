8c87b0d96847a91ff654e5790ed18cc8273a01c7
Boris Carvajal <boris2.9@gmail.com>
2022-06-24 01:37:15 -0400

    Replace `DsymbolExp` with `VarExp` when creating `TupleDeclaration` variables

--- src/dmd/dsymbolsem.d
+++ src/dmd/dsymbolsem.d
@@ -664,7 +664,7 @@ private extern(C++) final class DsymbolSemanticVisitor : Visitor
                         sc.scopesym.members.push(v);
                 }
 
-                Expression e = new DsymbolExp(dsym.loc, v);
+                Expression e = new VarExp(dsym.loc, v);
                 (*exps)[i] = e;
             }
             auto v2 = new TupleDeclaration(dsym.loc, dsym.ident, exps);
