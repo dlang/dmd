4df133b57411383be4721685f594ff8ab193f56d
Geod24 <pro.mathias.lang@gmail.com>
2022-07-07 07:04:25 +0200

    Fix 9161 - Linker error if struct has disabled dtor

--- test/runnable/test18973.d
+++ test/runnable/test18973.d
@@ -1,3 +1,6 @@
+// This is a runnable test as we are testing a linker error
+
+// https://issues.dlang.org/show_bug.cgi?id=18973
 struct X {
     @disable size_t toHash() const;
     @disable string toString() const;
@@ -5,7 +8,18 @@ struct X {
     @disable int opCmp(const ref X) const;
 }
 
-void main ()
+// https://issues.dlang.org/show_bug.cgi?id=9161
+public struct dummy
 {
-    // This is a runnable test as we are testing a linker error
+    static auto opCall(C)(in C[] name)
+    {
+        return name;
+    }
+
+    @disable ~this(); //comment this out to avoid error
+}
+
+void main()
+{
+    assert(dummy("ABCDE") == "ABCDE");
 }
