dee0164ae940f39a588fc9e135ece355e8e875ed
Walter Bright <walter@walterbright.com>
2022-07-06 23:48:18 -0700

    fix Issue 23213 - ImportC - variable length array does not compile

--- src/importc.h
+++ src/importc.h
@@ -61,3 +61,8 @@
  * Define it to do what other C compilers do.
  */
 #define __builtin_offsetof(t,i) ((typeof(sizeof(0)))((char *)&((t *)0)->i - (char *)0))
+
+/***************************
+ * C11 6.10.8.3 Conditional feature macros
+ */
+#define __STDC_NO_VLA__ 1
