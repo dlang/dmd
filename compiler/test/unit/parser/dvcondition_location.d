5843effb01ab68a0aabbb0438e6a28351f041cf6
Dennis <dkorpel@users.noreply.github.com>
2022-07-26 08:37:30 +0200

    Deprecate version/debug integers (#14330)

--- compiler/test/unit/parser/dvcondition_location.d
+++ compiler/test/unit/parser/dvcondition_location.d
@@ -47,34 +47,10 @@ unittest
     assert(visitor.l.charnum == 9);
 }
 
-@("version(integer)")
-unittest
-{
-    auto t = parseModule("test.d", "version(1)");
-
-    scope visitor = new Visitor;
-    t.module_.accept(visitor);
-
-    assert(visitor.l.linnum == 1);
-    assert(visitor.l.charnum == 9);
-}
-
 @("debug(identifier)")
 unittest
 {
     auto t = parseModule("test.d", "debug(a)");
-
-    scope visitor = new Visitor;
-    t.module_.accept(visitor);
-
-    assert(visitor.l.linnum == 1);
-    assert(visitor.l.charnum == 7);
-}
-
-@("debug(integer)")
-unittest
-{
-    auto t = parseModule("test.d", "debug(1)");
 
     scope visitor = new Visitor;
     t.module_.accept(visitor);
