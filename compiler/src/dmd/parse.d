a23f97ce49dea59ba6c047c4cba0b554f978c2ca
Walter Bright <walter@walterbright.com>
2022-08-06 08:57:52 -0700

    extend () error msgs for if conditions to other statement conditions

--- compiler/src/dmd/parse.d
+++ compiler/src/dmd/parse.d
@@ -5308,6 +5308,33 @@ class Parser(AST, Lexer = dmd.lexer.Lexer) : Lexer
         error(loc, "instead of C-style syntax, use D-style `%s%s%s`", t.toChars(), sp, s);
     }
 
+    /*****************************
+     * Ad-hoc error message for missing or extra parens that close a condition.
+     * Params:
+     *  start = "if", "while", etc. Must be 0 terminated.
+     *  param = if the condition is a declaration, this will be non-null
+     *  condition = if param is null, then this is the conditional Expression. If condition is null,
+     *      then an error in the condition was already reported.
+     */
+    private void closeCondition(string start, AST.Parameter param, AST.Expression condition)
+    {
+        string format;
+        if (token.value != TOK.rightParenthesis && condition)
+        {
+            format = "missing closing `)` after `%s (%s`";
+        }
+        else
+            check(TOK.rightParenthesis);
+        if (token.value == TOK.rightParenthesis)
+        {
+            if (condition) // if not an error in condition
+                format = "extra `)` after `%s (%s)`";
+            nextToken();
+        }
+        if (format)
+            error(format.ptr, start.ptr, param ? "declaration".ptr : condition.toChars());
+    }
+
     /*****************************************
      * Parses `foreach` statements, `static foreach` statements and
      * `static foreach` declarations.
@@ -5938,12 +5965,12 @@ LagainStc:
             }
         case TOK.while_:
             {
-                AST.Parameter param = null;
                 nextToken();
                 check(TOK.leftParenthesis);
-                param = parseAssignCondition();
-                AST.Expression condition = parseExpression();
-                check(TOK.rightParenthesis);
+                auto param = parseAssignCondition();
+                auto condition = parseExpression();
+                closeCondition("while", param, condition);
+
                 Loc endloc;
                 AST.Statement _body = parseStatement(ParseStatementFlags.scope_, null, &endloc);
                 s = new AST.WhileStatement(loc, condition, _body, endloc, param);
@@ -5964,7 +5991,6 @@ LagainStc:
         case TOK.do_:
             {
                 AST.Statement _body;
-                AST.Expression condition;
 
                 nextToken();
                 const lookingForElseSave = lookingForElse;
@@ -5973,8 +5999,8 @@ LagainStc:
                 lookingForElse = lookingForElseSave;
                 check(TOK.while_);
                 check(TOK.leftParenthesis);
-                condition = parseExpression();
-                check(TOK.rightParenthesis);
+                auto condition = parseExpression();
+                closeCondition("do .. while", null, condition);
                 if (token.value == TOK.semicolon)
                     nextToken();
                 else
@@ -6035,25 +6061,11 @@ LagainStc:
             }
         case TOK.if_:
             {
-                AST.Parameter param = null;
-                AST.Expression condition;
-
                 nextToken();
                 check(TOK.leftParenthesis);
-                param = parseAssignCondition();
-                condition = parseExpression();
-                if (token.value != TOK.rightParenthesis && condition)
-                {
-                    error("missing closing `)` after `if (%s`", param ? "declaration".ptr : condition.toChars());
-                }
-                else
-                    check(TOK.rightParenthesis);
-                if (token.value == TOK.rightParenthesis)
-                {
-                    if (condition) // if not an error in condition
-                        error("extra `)` after `if (%s)`", param ? "declaration".ptr : condition.toChars());
-                    nextToken();
-                }
+                auto param = parseAssignCondition();
+                auto condition = parseExpression();
+                closeCondition("if", param, condition);
 
                 {
                     const lookingForElseSave = lookingForElse;
@@ -6200,7 +6212,7 @@ LagainStc:
                 nextToken();
                 check(TOK.leftParenthesis);
                 AST.Expression condition = parseExpression();
-                check(TOK.rightParenthesis);
+                closeCondition("switch", null, condition);
                 AST.Statement _body = parseStatement(ParseStatementFlags.scope_);
                 s = new AST.SwitchStatement(loc, condition, _body, isfinal);
                 break;
@@ -6379,7 +6391,7 @@ LagainStc:
                 {
                     nextToken();
                     exp = parseExpression();
-                    check(TOK.rightParenthesis);
+                    closeCondition("synchronized", null, exp);
                 }
                 else
                     exp = null;
@@ -6396,7 +6408,7 @@ LagainStc:
                 nextToken();
                 check(TOK.leftParenthesis);
                 exp = parseExpression();
-                check(TOK.rightParenthesis);
+                closeCondition("with", null, exp);
                 _body = parseStatement(ParseStatementFlags.scope_, null, &endloc);
                 s = new AST.WithStatement(loc, exp, _body, endloc);
                 break;
