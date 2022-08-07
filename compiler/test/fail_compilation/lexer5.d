858ca50831942fd59bb93a464eca4db4439ae96b
mhh <maxhaton@gmail.com>
2022-08-07 03:05:34 +0100

    Fix Issue 23284 - better floating point error messages.
    There was also a bug here in that real literals did not get a suffix attached.

--- compiler/test/fail_compilation/lexer5.d
+++ compiler/test/fail_compilation/lexer5.d
@@ -1,7 +1,8 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/lexer5.d(8): Error: number `1e-100f` is not representable
+fail_compilation/lexer5.d(9): Error: number `1e-100f` is not representable as a `float`
+fail_compilation/lexer5.d(9):        https://dlang.org/spec/lex.html#floatliteral
 ---
 */
 
