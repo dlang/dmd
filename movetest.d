9bf21bd46684813115403261efb126ea57ccbb8c
Per Nordlöw <per.nordlow@gmail.com>
2022-08-20 10:26:19 +0200

    Improve movetest.d

--- movetest.d
+++ movetest.d
@@ -1,26 +1,22 @@
-struct S
-{
+struct S {
     int x;
-    ~this() @safe pure nothrow @nogc
-    {
-        // import std.stdio;
-        // debug writeln(__FUNCTION__);
-    }
+    ~this() @safe pure nothrow @nogc {}
 }
 
-void moveOnAssign1(S s) @safe pure nothrow @nogc
-{
+void moveOnAssign1(S s) @safe pure nothrow @nogc {
     S t = s;                    // s is moved
 }
-
-void moveOnAssign2(S s) @safe pure nothrow @nogc
-{
+void moveOnAssign2(S s) @safe pure nothrow @nogc {
     S t = s;                    // s is moved
     S u = t;                    // TODO: t should move here
 }
 
-void moveOff(S s) @safe pure nothrow @nogc
-{
+struct S2 { @disable this(this); }
+void moveOnDisabledPostblit(S2 s) @safe pure nothrow @nogc {
+    S2 t = s;                   // TODO: should not error and s is moved
+}
+
+void moveOff(S s) @safe pure nothrow @nogc {
     S t = s; // is not moved here because
     s.x = 42; // it's reference here
 }
