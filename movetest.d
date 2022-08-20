85b2bb94435ee25a9bebe4694d86c8e345c33368
Per Nordlöw <per.nordlow@gmail.com>
2022-08-20 10:20:39 +0200

    Add test file for now to be moved later

---
+++ movetest.d
@@ -0,0 +1,25 @@
+struct S
+{
+    int x;
+    ~this() @safe pure nothrow @nogc
+    {
+        // import std.stdio;
+        // debug writeln(__FUNCTION__);
+    }
+}
+
+void moveOnAssign1(S s) @safe pure nothrow @nogc
+{
+    S t = s; // s is moved here and t i
+}
+
+void moveOnAssign2(S s) @safe pure nothrow @nogc
+{
+    S t = s; // s is moved here and t i
+}
+
+void moveOff(S s) @safe pure nothrow @nogc
+{
+    S t = s; // is not moved here because
+    s.x = 42; // it's reference here
+}
