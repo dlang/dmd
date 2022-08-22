33c4800b81e16ebad16d5a929b8753ef62e0c1b6
Per Nordlöw <per.nordlow@gmail.com>
2022-08-22 23:43:17 +0200

    Remove qualifiers in movetest.d

--- movetest.d
+++ movetest.d
@@ -1,42 +1,42 @@
 struct SYZ {
     int x;
-    this(int x) @safe pure nothrow @nogc { this.x = x; }
-    ~this() @safe pure nothrow @nogc {}
-    // TODO: enable when Errors are avoided @disable this(this);
+    this(int x) { this.x = x; }
+    ~this() {}
+    // @disable this(this);
 }
 
-SYZ moveOnReturn1(SYZ s) @safe pure nothrow @nogc {
+SYZ moveOnReturn1(SYZ s) {
     return s;                   // TODO: should move
 }
-// SYZ moveOnReturn2(SYZ s) @safe pure nothrow @nogc {
+// SYZ moveOnReturn2(SYZ s) {
 //     SYZ t = s;
 //     return t;                   // TODO: should move
 // }
 
-// void moveOnAssign0() @safe pure nothrow @nogc {
+// void moveOnAssign0() {
 //     SYZ s;
 //     SYZ t = s;                    // TODO: `s` should move here
 // }
-void moveOnAssign1(SYZ s) @safe pure nothrow @nogc {
+void moveOnAssign1(SYZ s) {
     SYZ t = s;                    // `s` is moved
 }
-// void moveOnAssign2(SYZ s) @safe pure nothrow @nogc {
+// void moveOnAssign2(SYZ s) {
 //     SYZ t = s;                    // `s` is moved
 //     SYZ u = t;                    // TODO: `t` should move here
 // }
 
-// void moveOnCall1(SYZ s) @safe pure nothrow @nogc {
+// void moveOnCall1(SYZ s) {
 //     static f(SYZ s) {}
 //     f(s);                       // TODO: `s` should move here
 // }
 
 // struct S2 { @disable this(this); }
 // version(none)
-// void moveOnDisabledPostblit(S2 s) @safe pure nothrow @nogc {
+// void moveOnDisabledPostblit(S2 s) {
 //     S2 t = s;                   // TODO: should not error and `s` is moved
 // }
 
-// void moveOff(SYZ s) @safe pure nothrow @nogc {
+// void moveOff(SYZ s) {
 //     SYZ t = s;                    // `s` is not moved here because
 //     s.x = 42;                   // `s` is referenced in another type of Expression here
 // }
