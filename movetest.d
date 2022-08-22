f84835cc8e5f9c2eca1cddcab380fc93451edcea
Per Nordlöw <per.nordlow@gmail.com>
2022-08-22 02:08:41 +0200

    *** empty log message ***

--- movetest.d
+++ movetest.d
@@ -1,40 +1,38 @@
-@safe pure nothrow @nogc:
-
 struct S {
     int x;
     this(int x) @safe pure nothrow @nogc { this.x = x; }
     ~this() @safe pure nothrow @nogc {}
 }
 
-void moveOnAssign1(S s) {
+void moveOnAssign1(S s) @safe pure nothrow @nogc {
     S t = s;                    // `s` is moved
 }
-void moveOnAssign2(S s) {
+void moveOnAssign2(S s) @safe pure nothrow @nogc {
     S t = s;                    // `s` is moved
     S u = t;                    // TODO: `t` should move here
 }
 
-void moveOnCall1(S s) {
+void moveOnCall1(S s) @safe pure nothrow @nogc {
     static f(S s) {}
     f(s);                       // TODO: `s` should move here
 }
 
 struct S2 { @disable this(this); }
 version(none)
-void moveOnDisabledPostblit(S2 s) {
+void moveOnDisabledPostblit(S2 s) @safe pure nothrow @nogc {
     S2 t = s;                   // TODO: should not error and `s` is moved
 }
 
-void moveOff(S s) {
+void moveOff(S s) @safe pure nothrow @nogc {
     S t = s;                    // `s` is not moved here because
     s.x = 42;                   // `s` is referenced in another type of Expression here
 }
 
-S moveOnReturn1(S s) {
+S moveOnReturn1(S s) @safe pure nothrow @nogc {
     return s;                   // TODO: should move
 }
 
-S moveOnReturn2(S s) {
+S moveOnReturn2(S s) @safe pure nothrow @nogc {
     S t = s;
     return t;                   // TODO: should move
 }
