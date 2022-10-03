31f49e705a8c08a114641eb11cf445cf3c7f8ca5
Nick Treleaven <ntrel002@gmail.com>
2022-10-03 13:52:15 +0100

    Merge error output, test mixin and remove static this for now

--- compiler/test/fail_compilation/templatethis.d
+++ compiler/test/fail_compilation/templatethis.d
@@ -1,10 +1,12 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/templatethis.d(11): Error: cannot use `this` outside an aggregate type
-fail_compilation/templatethis.d(15): Error: cannot use `this` outside an aggregate type
-fail_compilation/templatethis.d(19): Error: cannot use `this` outside an aggregate type
+fail_compilation/templatethis.d(13): Error: cannot use `this` outside an aggregate type
+fail_compilation/templatethis.d(17): Error: cannot use `this` outside an aggregate type
 fail_compilation/templatethis.d(21): Error: cannot use `this` outside an aggregate type
+fail_compilation/templatethis.d(23): Error: cannot use `this` outside an aggregate type
+fail_compilation/templatethis.d(29): Error: cannot use `this` outside an aggregate type
+fail_compilation/templatethis.d(32): Error: mixin `templatethis.t2!()` error instantiating
 ---
 */
 
@@ -22,13 +24,6 @@ void f(this T)()
 {
 }
 
-/*
-TEST_OUTPUT:
----
-fail_compilation/templatethis.d(34): Error: cannot use `this` outside an aggregate type
-fail_compilation/templatethis.d(37): Error: mixin `templatethis.t2!()` error instantiating
----
-*/
 mixin template t2()
 {
 	int i(this T) = 1;
@@ -38,8 +33,5 @@ mixin t2;
 
 class C
 {
-	static void f(this T)() {}
+	mixin t2; // OK
 }
-
-alias a = C.f!int; // FIXME error
-alias b = C.f!C; // OK
