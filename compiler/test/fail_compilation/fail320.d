c207a65fd0ee4b516b7e07c8526ca6be71eac06e
Nick Treleaven <ntrel002@gmail.com>
2022-10-31 12:15:56 +0000

    Fix Issue 21338 - Confusing error message for template overload resolution failure
    Show expression, not just identifier.
    Show candidates.

--- compiler/test/fail_compilation/fail320.d
+++ compiler/test/fail_compilation/fail320.d
@@ -2,10 +2,13 @@
 EXTRA_FILES: imports/fail320a.d imports/fail320b.d
 TEST_OUTPUT:
 ---
-fail_compilation/fail320.d(11): Error: no overload matches for `foo`
+fail_compilation/fail320.d(14): Error: no overload matches for `foo("")`
+fail_compilation/fail320.d(14):        Candidates are:
+fail_compilation/imports/fail320b.d(1):        foo(T)()
+fail_compilation/imports/fail320a.d(1):        foo
 ---
 */
 
 import imports.fail320a;
 import imports.fail320b;
-void main() { foo(); }
+void main() { foo(""); }
