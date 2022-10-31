3ec8335349ddea71a057f6ae318a8be33db3f908
Nick Treleaven <ntrel002@gmail.com>
2022-10-31 10:57:29 +0000

    Fix Issue 23447 - wrong expression in error message when template instance doesn't match any overload

--- compiler/test/fail_compilation/templateoverload.d
+++ compiler/test/fail_compilation/templateoverload.d
@@ -1,11 +1,11 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/templateoverload.d(17): Error: template `templateoverload.T` does not match any template declaration
+fail_compilation/templateoverload.d(17): Error: template instance `T!1` does not match any template declaration
 fail_compilation/templateoverload.d(17):        Candidates are:
 fail_compilation/templateoverload.d(14):        T(X)
 fail_compilation/templateoverload.d(15):        T()
-fail_compilation/templateoverload.d(22): Error: template `templateoverload.V` does not match any template declaration
+fail_compilation/templateoverload.d(22): Error: template instance `V!int` does not match any template declaration
 fail_compilation/templateoverload.d(22):        Candidates are:
 fail_compilation/templateoverload.d(19):        V(int i)
 fail_compilation/templateoverload.d(20):        V(T, alias a)
