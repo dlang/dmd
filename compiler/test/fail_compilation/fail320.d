c8077406b181315213f2baa53ac021bbdb2fcb1f
Nick Treleaven <ntrel002@gmail.com>
2022-10-31 14:54:15 +0000

    Fix missing overloads in call candidate list
    Also fix missing non-template function parameter.
    
    Part of Issue 21338 - Confusing error message for template overload resolution failure.

--- compiler/test/fail_compilation/fail320.d
+++ compiler/test/fail_compilation/fail320.d
@@ -2,10 +2,12 @@
 EXTRA_FILES: imports/fail320a.d imports/fail320b.d
 TEST_OUTPUT:
 ---
-fail_compilation/fail320.d(14): Error: no overload matches for `foo("")`
-fail_compilation/fail320.d(14):        Candidates are:
-fail_compilation/imports/fail320b.d(1):        foo(T)()
-fail_compilation/imports/fail320a.d(1):        foo
+fail_compilation/fail320.d(16): Error: no overload matches for `foo("")`
+fail_compilation/fail320.d(16):        Candidates are:
+fail_compilation/imports/fail320b.d(1):        foo(T)(string)
+fail_compilation/imports/fail320b.d(2):        foo(alias a)()
+fail_compilation/imports/fail320a.d(1):        foo(int)
+fail_compilation/imports/fail320a.d(2):        foo(bool)
 ---
 */
 
