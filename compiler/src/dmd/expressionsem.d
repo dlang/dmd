8264e81cd47cd9ada843d82b146680c718ee9fb0
RazvanN7 <razvan.nitu1305@gmail.com>
2022-11-04 15:37:39 +0200

    Fix Issue 23452 - Noncopyable variable can be silently passed to a function with variadic args

--- compiler/src/dmd/expressionsem.d
+++ compiler/src/dmd/expressionsem.d
@@ -1883,6 +1883,7 @@ private bool functionParameters(const ref Loc loc, Scope* sc,
                             else
                                 a = a.implicitCastTo(sc, tbn);
                             a = a.addDtorHook(sc);
+                            a = doCopyOrMove(sc, a);
                             (*elements)[u] = a;
                         }
                         // https://issues.dlang.org/show_bug.cgi?id=14395
