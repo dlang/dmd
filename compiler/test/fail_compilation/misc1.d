72d527eda1ce754d5c6c2bb36673e316442585df
Nick Treleaven <ntrel002@gmail.com>
2022-11-13 12:13:45 +0000

    Move test for false Increment

--- compiler/test/fail_compilation/misc1.d
+++ compiler/test/fail_compilation/misc1.d
@@ -5,6 +5,7 @@ fail_compilation/misc1.d(108): Error: `5` has no effect
 fail_compilation/misc1.d(109): Error: `1 + 2` has no effect
 fail_compilation/misc1.d(115): Error: `1 * 1` has no effect
 fail_compilation/misc1.d(116): Error: `__lambda3` has no effect
+fail_compilation/misc1.d(122): Error: `false` has no effect
 ---
 */
 
@@ -26,4 +27,11 @@ void issue23480()
     int j;
     for({} j; 1*1) {}
     for({j=2; int d = 3; } j+d<7; {j++; d++; }) {}
+    for (
+        if (true)        // (o_O)
+            assert(78);
+        else
+            assert(79);
+        false; false
+    ) {}
 }
