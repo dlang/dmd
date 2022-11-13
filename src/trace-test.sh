4fea7506522e1a39b0b814d33b7b004106ea2fa5
Per Nordlöw <per.nordlow@gmail.com>
2022-11-13 11:52:24 +0100

    *** empty log message ***

--- src/trace-test.sh
+++ src/trace-test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -euo pipefail
+
 TRACEFILE="hello"
 
-make -f posix.mak && ../generated/linux/release/64/dmd -trace="$TRACEFILE" ~/hello.d -o-
+make -f posix.mak
+
+../generated/linux/release/64/dmd -trace="$TRACEFILE" ~/hello.d -o-
 
 # dmd -i -I../compiler/src -run printTraceHeader.d "$FILE".dmd_trace Tree
 dmd -i -I../compiler/src -run printTraceHeader.d "$TRACEFILE".dmd_trace TemplateInstances
