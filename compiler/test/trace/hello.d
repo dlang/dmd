45afa9314d8ae20fd9157298d5eea0a439b2984f
Per Nordlöw <per.nordlow@gmail.com>
2022-11-13 12:26:12 +0100

    Add missing files

---
+++ compiler/test/trace/hello.d
@@ -0,0 +1,5 @@
+T f(T)(T x) { return x-1; }
+
+int main(string[] args) {
+    return f(1);
+}
--- compiler/test/trace/trace-test.sh
+++ compiler/test/trace/trace-test.sh
@@ -6,7 +6,7 @@ make -C ../../src -f posix.mak
 
 TRACEFILE="hello"
 
-../../../generated/linux/release/64/dmd -trace="$TRACEFILE" ~/hello.d -o-
+../../../generated/linux/release/64/dmd -trace="$TRACEFILE" hello.d -o-
 
 # dmd -i -I../compiler/src -run printTraceHeader.d "$FILE".dmd_trace Tree
 dmd -i -I../../src -run printTraceHeader.d "$TRACEFILE".dmd_trace TemplateInstances
