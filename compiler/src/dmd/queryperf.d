7a992af434ce3ee0c16e205f91d12eaea0386fd8
nordlow <per.nordlow@gmail.com>
2022-11-15 02:29:30 +0100

    Add LDC and AArch64 versions of QueryPerformanceCounter in queryperf.d

--- compiler/src/dmd/queryperf.d
+++ compiler/src/dmd/queryperf.d
@@ -18,6 +18,29 @@ version (Windows)
         export int QueryPerformanceCounter(ulong *);
     }
 }
+else version (AArch64)
+{
+    // We cannot use ldc.intrinsics.llvm_readcyclecounter because that is not an accurate
+    // time counter (it is a counter of CPU cycles, where here we want a time clock).
+    // Also, priviledged execution rights are needed to enable correct counting with
+    // ldc.intrinsics.llvm_readcyclecounter on AArch64.
+    extern (D) void QueryPerformanceCounter(timer_t* ctr)
+    {
+        asm { "mrs %0, cntvct_el0" : "=r" (*ctr); }
+    }
+    extern (D) void QueryPerformanceFrequency(timer_t* freq)
+    {
+        asm { "mrs %0, cntfrq_el0" : "=r" (*freq); }
+    }
+}
+else version (LDC)
+{
+    extern (D) void QueryPerformanceCounter(timer_t* ctr)
+    {
+        import ldc.intrinsics: llvm_readcyclecounter;
+        *ctr = llvm_readcyclecounter();
+    }
+}
 else version (D_InlineAsm_X86)
 {
     extern (D)
