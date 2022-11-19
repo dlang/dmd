4e293a87a8750cb3b50818f340509978edb9c96c
richard (rikki) andrew cattermole <richard@cattermole.co.nz>
2022-11-19 19:22:37 +1300

    Ughhhh

--- compiler/src/dmd/toobj.d
+++ compiler/src/dmd/toobj.d
@@ -243,7 +243,7 @@ void genModuleInfo(Module m)
     out_readonly(m.csym);
     outdata(m.csym);
 
-    if (!m.isym)
+    if (target.os == Target.OS.Windows && !m.isym)
     {
         m.isym = m.csym.toImport(m.loc);
         out_readonly(m.isym);
