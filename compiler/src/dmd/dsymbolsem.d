fecd435f695f030ac4d1de548bb00e4f690dfacd
Nick Treleaven <ntrel002@gmail.com>
2022-11-27 18:51:33 +0000

    Fix Issue 23522 - Error message when enum type is not integral and a value lacks an initializer

--- compiler/src/dmd/dsymbolsem.d
+++ compiler/src/dmd/dsymbolsem.d
@@ -2565,6 +2565,7 @@ private extern(C++) final class DsymbolSemanticVisitor : Visitor
                 return errorReturn();
 
             Expression eprev = emprev.value;
+            assert(eprev);
             // .toHeadMutable() due to https://issues.dlang.org/show_bug.cgi?id=18645
             Type tprev = eprev.type.toHeadMutable().equals(em.ed.type.toHeadMutable())
                 ? em.ed.memtype
@@ -2575,15 +2576,30 @@ private extern(C++) final class DsymbolSemanticVisitor : Visitor
                 this construct does do that and thus fixes the bug.
             */
             Expression emax = DotIdExp.create(em.ed.loc, new TypeExp(em.ed.loc, tprev), Id.max);
+            auto oldgag = global.startGagging();
             emax = emax.expressionSemantic(sc);
             emax = emax.ctfeInterpret();
+            if (global.endGagging(oldgag))
+            {
+                error(em.loc, "cannot generate value for `%s`", em.toChars());
+                errorSupplemental(eprev.loc, "Could not interpret `%s.max` for overflow check",
+                    eprev.type.toChars());
+                return errorReturn();
+            }
 
             // Set value to (eprev + 1).
             // But first check that (eprev != emax)
-            assert(eprev);
             Expression e = new EqualExp(EXP.equal, em.loc, eprev, emax);
+            oldgag = global.startGagging();
             e = e.expressionSemantic(sc);
             e = e.ctfeInterpret();
+            if (global.endGagging(oldgag))
+            {
+                error(em.loc, "cannot generate value for `%s`", em.toChars());
+                errorSupplemental(eprev.loc, "Could not interpret `%s == %s.max` for overflow check", 
+                    emprev.toChars(), eprev.type.toChars());
+                return errorReturn();
+            }
             if (e.toInteger())
             {
                 auto mt = em.ed.memtype;
@@ -2593,7 +2609,6 @@ private extern(C++) final class DsymbolSemanticVisitor : Visitor
                     emprev.ed.toChars(), emprev.toChars(), mt.toChars());
                 return errorReturn();
             }
-
             // Now set e to (eprev + 1)
             e = new AddExp(em.loc, eprev, IntegerExp.literal!1);
             e = e.expressionSemantic(sc);
