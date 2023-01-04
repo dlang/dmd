3fdd07ea1754e5fabb9b215dbbb655cb87b378aa
Nick Treleaven <ntrel002@gmail.com>
2023-01-04 12:50:08 +0000

    Fix line no

--- compiler/test/fail_compilation/diag12312.d
+++ compiler/test/fail_compilation/diag12312.d
@@ -2,7 +2,7 @@
 TEST_OUTPUT:
 ---
 fail_compilation/diag12312.d(10): Error: variable `diag12312.main.arr` of type `void[16]` does not have a default initializer
-fail_compilation/diag12312.d(11): Error: variable `diag12312.bug1176.v` of type `void[1]` does not have a default initializer
+fail_compilation/diag12312.d(15): Error: variable `diag12312.bug1176.v` of type `void[1]` does not have a default initializer
 ---
 */
 void main()
