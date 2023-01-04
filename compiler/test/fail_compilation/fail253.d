93084419f46db07bc364108050187d8febe1e0bf
Nick Treleaven <ntrel002@gmail.com>
2023-01-04 09:37:31 +0000

    Use hyphen & fix `auto Type` error too

--- compiler/test/fail_compilation/fail253.d
+++ compiler/test/fail_compilation/fail253.d
@@ -1,9 +1,9 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail253.d(13): Error: variable `fail253.main.x`: `inout` variables can only be declared inside `inout` functions
+fail_compilation/fail253.d(13): Error: variable `fail253.main.x` - `inout` variables can only be declared inside `inout` functions
 fail_compilation/fail253.d(16): Error: cannot modify `inout` expression `x`
-fail_compilation/fail253.d(19): Error: variable `fail253.main.err11`: `inout` variables can only be declared inside `inout` functions
+fail_compilation/fail253.d(19): Error: variable `fail253.main.err11` - `inout` variables can only be declared inside `inout` functions
 ---
 */
 void main()
--- compiler/test/fail_compilation/failinout2.d
+++ compiler/test/fail_compilation/failinout2.d
@@ -1,8 +1,8 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/failinout2.d(7): Error: variable `failinout2.x`: only parameters or stack-based variables can be `inout`
-fail_compilation/failinout2.d(12): Error: variable `failinout2.S3748.err8`: only parameters or stack-based variables can be `inout`
+fail_compilation/failinout2.d(8): Error: variable `failinout2.x` - only parameters or stack-based variables can be `inout`
+fail_compilation/failinout2.d(12): Error: variable `failinout2.S3748.err8` - only parameters or stack-based variables can be `inout`
 ---
 */
 inout int x;
