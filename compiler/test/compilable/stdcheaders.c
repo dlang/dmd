7204ae2bb0ebee9582f327863b61a5b01f55c2c7
Walter Bright <WalterBright@users.noreply.github.com>
2023-01-12 04:46:23 -0800

    fix Issue 23407 - ImportC: function-local struct definition as part of variable declaration doesn.t shadow global definition (#14806)

---
+++ compiler/test/compilable/stdcheaders.c
@@ -0,0 +1,32 @@
+/* Do a smoke test of the C Standard headers
+ */
+
+#include <assert.h>
+#include <complex.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fenv.h>
+#include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdalign.h>
+#include <stdarg.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <tgmath.h>
+#include <threads.h>
+#include <time.h>
+#include <uchar.h>
+#include <wchar.h>
+#include <wctype.h>
