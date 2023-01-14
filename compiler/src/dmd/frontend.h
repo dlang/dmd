815eb678c704470d190fc63579428d481bf55063
Iain Buclaw <ibuclaw@gdcproject.org>
2023-01-14 22:29:20 +0100

    fix Issue 23626 - [REG2.096] Template deduction with auto const ref Args... and delegate

--- compiler/src/dmd/frontend.h
+++ compiler/src/dmd/frontend.h
@@ -3740,7 +3740,6 @@ public:
     Type* substWildTo(uint32_t _param_0) override;
     MATCH constConv(Type* to) override;
     bool iswild() const;
-    bool attributesEqual(const TypeFunction* const other) const;
     void accept(Visitor* v) override;
 };
 
