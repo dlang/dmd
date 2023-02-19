e5e48dad895468b0cb3d6a9c0d53608f8dc393c9
Zachary Yedidia <zyedidia@gmail.com>
2023-02-18 16:00:35 -0800

    Use ulong instead of size_t

--- compiler/test/runnable/test23514.d
+++ compiler/test/runnable/test23514.d
@@ -1,10 +1,10 @@
 // REQUIRED_ARGS: -O
 // https://issues.dlang.org/show_bug.cgi?id=23514
 
-enum offset = 0xFFFF_FFFF_0000_0000UL;
+enum ulong offset = 0xFFFF_FFFF_0000_0000UL;
 
 void main()
 {
-    size_t voffset = offset;
-    assert((cast(size_t)&main + voffset) == (cast(size_t)&main + offset));
+    ulong voffset = offset;
+    assert((cast(ulong)&main + voffset) == (cast(ulong)&main + offset));
 }
