ecea5a3316782e826d71ae5cdcd91782ad96d0f7
Zachary Yedidia <zyedidia@gmail.com>
2023-02-19 13:55:07 -0800

    Restrict test to 64-bit targets

--- compiler/test/runnable/test23514.d
+++ compiler/test/runnable/test23514.d
@@ -1,4 +1,5 @@
-// REQUIRED_ARGS: -O
+// REQUIRED_ARGS: -m64 -O
+// DISABLED: win32 linux32 freebsd32 osx32 netbsd32 dragonflybsd32
 // https://issues.dlang.org/show_bug.cgi?id=23514
 
 enum ulong offset = 0xFFFF_FFFF_0000_0000UL;
