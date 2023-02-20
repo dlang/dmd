9d2112e419c2b7c477d9e9b57ef1feeb7f3fd8db
Zachary Yedidia <zyedidia@gmail.com>
2023-02-20 14:40:16 -0800

    Disable test for win64 and add comment

--- compiler/test/runnable/test23514.d
+++ compiler/test/runnable/test23514.d
@@ -1,5 +1,8 @@
-// REQUIRED_ARGS: -O
+// DISABLED: win64
 // https://issues.dlang.org/show_bug.cgi?id=23514
+
+// Note: this test is disabled on Win64 because of an issue with the Windows
+// MS-COFF backend causing it to fail.
 
 enum ulong offset = 0xFFFF_FFFF_0000_0000UL;
 
