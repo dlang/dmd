13d88731222caeaee1d31d622a1ab1c1080d7e52
Zachary Yedidia <zyedidia@gmail.com>
2023-02-20 12:42:05 -0800

    Re-enable test for 32-bit targets

--- compiler/test/runnable/test23514.d
+++ compiler/test/runnable/test23514.d
@@ -1,5 +1,4 @@
-// REQUIRED_ARGS: -m64 -O
-// DISABLED: win32 linux32 freebsd32 osx32 netbsd32 dragonflybsd32
+// REQUIRED_ARGS: -O
 // https://issues.dlang.org/show_bug.cgi?id=23514
 
 enum ulong offset = 0xFFFF_FFFF_0000_0000UL;
