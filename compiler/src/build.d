9efc250901d10ef8265f8807898a3cea9cc50c6a
Dennis <dkorpel@users.noreply.github.com>
2023-02-22 10:24:39 +0100

    Deprecate `-preview=dip25` switch (#14891)

--- compiler/src/build.d
+++ compiler/src/build.d
@@ -1366,8 +1366,6 @@ void processEnvironment()
         env.setDefault("ZIP", "zip");
 
     string[] dflags = ["-version=MARS", "-w", "-de", env["PIC_FLAG"], env["MODEL_FLAG"], "-J"~env["G"], "-I" ~ srcDir];
-    if (env["HOST_DMD_KIND"] != "gdc")
-        dflags ~= ["-dip25"]; // gdmd doesn't support -dip25
 
     // TODO: add support for dObjc
     auto dObjc = false;
