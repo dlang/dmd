872c9d668c15a8829e34a225984bf00200f633e9
Martin Kinkelin <noone@nowhere.com>
2023-02-25 02:33:52 +0100

    druntime: Reduce intra-druntime dependencies for array-appending hook
    This is important for CTFE-string-appending etc. for targets without
    proper druntime support, such as WebAssembly.
    
    See https://github.com/ldc-developers/ldc/issues/4324 for an examplary
    breakage caused by the hook addition in D v2.101, rendering dub
    totally unusable for such targets.
    
    In particular, skip all the `-profile=gc` stuff when not using that
    cmdline option.

--- druntime/src/object.d
+++ druntime/src/object.d
@@ -4622,7 +4622,8 @@ they are only intended to be instantiated by the compiler, not the user.
 public import core.internal.entrypoint : _d_cmain;
 
 public import core.internal.array.appending : _d_arrayappendT;
-public import core.internal.array.appending : _d_arrayappendTTrace;
+version (D_ProfileGC)
+    public import core.internal.array.appending : _d_arrayappendTTrace;
 public import core.internal.array.appending : _d_arrayappendcTXImpl;
 public import core.internal.array.comparison : __cmp;
 public import core.internal.array.equality : __equals;
