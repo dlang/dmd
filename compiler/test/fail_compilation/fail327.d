75027ccf47ee9539b5093ee50a8573a25a3b0628
Nick Treleaven <ntrel002@gmail.com>
2023-05-06 18:18:01 +0100

    Make it a deprecation first

--- compiler/test/fail_compilation/fail327.d
+++ compiler/test/fail_compilation/fail327.d
@@ -2,7 +2,7 @@
 TEST_OUTPUT:
 ---
 fail_compilation/fail327.d(11): Error: `asm` statement is assumed to be `@system` - mark it with `@trusted` if it is not
-fail_compilation/fail327.d(12): Error: `asm` statement cannot be marked `@safe`, use `@system` or `@trusted` instead
+fail_compilation/fail327.d(12): Deprecation: `asm` statement cannot be marked `@safe`, use `@system` or `@trusted` instead
 ---
 */
 
