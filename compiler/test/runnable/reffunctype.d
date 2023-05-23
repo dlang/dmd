6cb72d364f7e29bf63ba2446ace907d6f61f2d3c
Quirin Schroll <qschroll@mgm-edv.de>
2023-05-23 22:18:47 +0200

    Fix demo

--- compiler/test/runnable/reffunctype.d
+++ compiler/test/runnable/reffunctype.d
@@ -1,48 +1,108 @@
 // REQUIRED_ARGS: -unittest -main
 
-int i;
+// `a` takes its parameter by value;     the parameter returns by reference.
+// `b` takes its parameter by reference; the parameter returns by value.
+// The parameter storage class has priority over the value category of the parameter’s return type.
+void a( ref (int function()) ) { }
+void b((ref  int function()) ) { }
 
-void hof1((ref int function()) fptr) { fptr()++; }
-void hof2(ref (int function()) fptr) { fptr = ref () => i; }
-void hof3(ref (ref int function())[] fptrs)
+// `c` is `a` without clarifiying parentheses.
+void c( ref  int function()  ) { }
+
+static assert(!is( typeof(&a) == typeof(&b) ));
+static assert( is( typeof(&a) == typeof(&c) ));
+
+// `x` returns by reference; the return type is a function that returns an `int` by value.
+// `y` returns by vale;      the return type is a function that returns an `int` by reference.
+// The value category of the declared function has priority over the value category of the return type.
+ ref (int function()) x() { static typeof(return) fp = null; return fp; }
+(ref  int function()) y() => null;
+
+// `z` is `x` without clarifiying parentheses.
+ ref  int function()  z() => x();
+
+static assert(!is( typeof(&x) == typeof(&y) ));
+static assert( is( typeof(&x) == typeof(&z) ));
+
+@safe unittest
+{
+    static int i = 0;
+    // Congruence between function declaration and function type.
+     ref int funcName() @safe  => i;
+    (ref int delegate() @safe) fptr = &funcName;
+}
+
+// Combination of ref return and binding parameters by reference
+// as well as returning by reference and by reference returning function type.
+ref (ref int function() @safe) hof(ref (ref int function() @safe)[] fptrs) @safe
 {
     static assert(__traits(isRef, fptrs));
     fptrs[0]() = 1;
+    (ref int function() @safe)* result = &fptrs[0];
+    fptrs = [];
+    return *result;
 }
 
-ref int h       () => i;
-ref int function() fptr = &h;
-
-unittest
+@safe unittest
 {
-    hof1(&h);
-    hof1(ref() => i);
+    static int i = 0;
+    static ref int f() => i;
+    static assert(is(typeof(&f) == ref int function() nothrow @nogc @safe));
+
+    (ref int function() @safe)[] fps = [ &f, &f ];
+    auto fpp = &(hof(fps));
+    assert(fps.length == 0);
+    assert(*fpp == &f);
+    assert(i == 1);
+    static assert(is(typeof(fpp) == (ref int function() @safe)*));
+    int* p = &((*fpp)());
+    *p = 2;
     assert(i == 2);
-
-    ref int function() fp = &h;
-    fp()++;
+    (*fpp)()++;
     assert(i == 3);
-
-    fptr()++;
-    assert(i == 4);
 }
-
-alias Func = ref int function();
-static assert(is(Func == typeof(&h)));
 
 struct S
 {
     int i;
-    ref int get() => i;
+    ref int get() @safe return => i;
 }
 
-unittest
+@safe unittest
 {
     S s;
-    ref int delegate() d = &s.get;
-    d()++;
+    (ref int delegate() return @safe) dg = &s.get;
+    dg() = 1;
     assert(s.i == 1);
 }
 
-alias Del = ref int delegate();
-static assert(is(Del == typeof(&S().get)));
+static assert(is(typeof(&S().get) == ref int delegate() @safe return));
+
+@safe unittest
+{
+    static int x = 1;
+    assert(x == 1);
+    auto f = function ref int() => x;
+    static assert(is( typeof(f) : ref int function() @safe ));
+    f() = 2;
+    assert(x == 2);
+    takesFP(f);
+    assert(x == 3);
+
+    auto g = cast(ref int function()) f;
+}
+
+ref (ref int function() @safe) returnsFP() @safe { static (ref int function() @safe) fp = null; return fp; }
+void takesFP((ref int function() @safe) fp) @safe { fp() = 3; }
+
+void takesFPFP(typeof(&returnsFP) function( typeof(&returnsFP) )) { }
+
+// pretty print and actual D syntax coincide even in convoluted cases
+static assert(   typeof(&takesFPFP).stringof == "void function((ref (ref int function() @safe) function() @safe) function((ref (ref int function() @safe) function() @safe)))");
+static assert(is(typeof(&takesFPFP)          ==  void function((ref (ref int function() @safe) function() @safe) function((ref (ref int function() @safe) function() @safe))) ));
+
+// as an artifact of the type grammar, these should hold:
+static assert(is( (int) == int ));
+static assert(is( (const int) == const(int) ));
+static assert(is( (const shared int) == shared(const(int)) ));
+static assert(is( (const shared int) == shared(const int ) ));
