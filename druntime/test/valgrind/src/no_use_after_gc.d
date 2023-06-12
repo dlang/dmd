d0092f22a295febc731a4cb13db353c88dee010e
Vladimir Panteleev <git@cy.md>
2023-06-12 09:42:56 +0000

    druntime/test/valgrind: Fix no_use_after_gc test with -release
    Trick the optimizer to pretend we're doing something with the result
    of those invalid memory accesses.

--- druntime/test/valgrind/src/no_use_after_gc.d
+++ druntime/test/valgrind/src/no_use_after_gc.d
@@ -9,14 +9,18 @@ struct S
         // Dereferencing other GC-allocated values in a destructor is not allowed,
         // as the deallocation/destruction order is undefined,
         // and here even forms a loop.
-        assert(other.other is &this);
+        int dummy = other.other !is &this;
+        result += dummy;
     }
 }
 
-void main()
+__gshared int result; // Trick the optimizer
+
+int main()
 {
     auto a = new S;
     auto b = new S;
     a.other = b;
     b.other = a;
+    return result;
 }
