9d9f6391d7002fce87335024103b4c97c9f66263
Vladimir Panteleev <git@cy.md>
2023-06-12 08:34:03 +0000

    etc.valgrind.valgrind: Add @nogc
    Although the GC is the primary user, this is not a restriction of
    these Valgrind API wrappers.

--- druntime/src/etc/valgrind/valgrind.d
+++ druntime/src/etc/valgrind/valgrind.d
@@ -6,13 +6,13 @@ module etc.valgrind.valgrind;
 version (StdDdoc)
 {
     /// Mark the memory covered by `mem` as unaddressable.
-    void makeMemNoAccess (const(void)[] mem) nothrow;
+    void makeMemNoAccess (const(void)[] mem) nothrow @nogc;
 
     /// Similarly, mark memory covered by `mem` as addressable but undefined.
-    void makeMemUndefined(const(void)[] mem) nothrow;
+    void makeMemUndefined(const(void)[] mem) nothrow @nogc;
 
     /// Similarly, mark memory covered by `mem` as addressable and defined.
-    void makeMemDefined  (const(void)[] mem) nothrow;
+    void makeMemDefined  (const(void)[] mem) nothrow @nogc;
 
     /// Get the validity data for the address range covered by `mem` and copy it
     /// into the provided `bits` array.
@@ -23,7 +23,7 @@ version (StdDdoc)
     ///   - 3   if any parts of `mem`/`bits` are not addressable.
     /// The metadata is not copied in cases 0, 2 or 3 so it should be
     /// impossible to segfault your system by using this call.
-    uint getVBits(const(void)[] mem, ubyte[] bits) nothrow;
+    uint getVBits(const(void)[] mem, ubyte[] bits) nothrow @nogc;
 
     /// Set the validity data for the address range covered by `mem`, copying it
     /// from the provided `bits` array.
@@ -34,20 +34,20 @@ version (StdDdoc)
     ///   - 3   if any parts of `mem`/`bits` are not addressable.
     /// The metadata is not copied in cases 0, 2 or 3 so it should be
     /// impossible to segfault your system by using this call.
-    uint setVBits(const(void)[] mem, ubyte[] bits) nothrow;
+    uint setVBits(const(void)[] mem, ubyte[] bits) nothrow @nogc;
 
     /// Disable and re-enable reporting of addressing errors in the
     /// address range covered by `mem`.
-    void disableAddrReportingInRange(const(void)[] mem) nothrow;
+    void disableAddrReportingInRange(const(void)[] mem) nothrow @nogc;
 
     /// ditto
-    void enableAddrReportingInRange(const(void)[] mem) nothrow;
+    void enableAddrReportingInRange(const(void)[] mem) nothrow @nogc;
 }
 else:
 
 debug(VALGRIND):
 
-private extern(C) nothrow
+private extern(C) nothrow @nogc
 {
     void _d_valgrind_make_mem_noaccess (const(void)* addr, size_t len);
     void _d_valgrind_make_mem_undefined(const(void)* addr, size_t len);
@@ -58,28 +58,28 @@ private extern(C) nothrow
     void _d_valgrind_enable_addr_reporting_in_range (const(void)* addr, size_t len);
 }
 
-void makeMemNoAccess (const(void)[] mem) nothrow { _d_valgrind_make_mem_noaccess (mem.ptr, mem.length); }
-void makeMemUndefined(const(void)[] mem) nothrow { _d_valgrind_make_mem_undefined(mem.ptr, mem.length); }
-void makeMemDefined  (const(void)[] mem) nothrow { _d_valgrind_make_mem_defined  (mem.ptr, mem.length); }
+void makeMemNoAccess (const(void)[] mem) nothrow @nogc { _d_valgrind_make_mem_noaccess (mem.ptr, mem.length); }
+void makeMemUndefined(const(void)[] mem) nothrow @nogc { _d_valgrind_make_mem_undefined(mem.ptr, mem.length); }
+void makeMemDefined  (const(void)[] mem) nothrow @nogc { _d_valgrind_make_mem_defined  (mem.ptr, mem.length); }
 
-uint getVBits(const(void)[] mem, ubyte[] bits) nothrow
+uint getVBits(const(void)[] mem, ubyte[] bits) nothrow @nogc
 {
     assert(mem.length == bits.length);
     return _d_valgrind_get_vbits(mem.ptr, bits.ptr, mem.length);
 }
 
-uint setVBits(const(void)[] mem, ubyte[] bits) nothrow
+uint setVBits(const(void)[] mem, ubyte[] bits) nothrow @nogc
 {
     assert(mem.length == bits.length);
     return _d_valgrind_set_vbits(mem.ptr, bits.ptr, mem.length);
 }
 
-void disableAddrReportingInRange(const(void)[] mem) nothrow
+void disableAddrReportingInRange(const(void)[] mem) nothrow @nogc
 {
     _d_valgrind_disable_addr_reporting_in_range(mem.ptr, mem.length);
 }
 
-void enableAddrReportingInRange(const(void)[] mem) nothrow
+void enableAddrReportingInRange(const(void)[] mem) nothrow @nogc
 {
     _d_valgrind_enable_addr_reporting_in_range(mem.ptr, mem.length);
 }
