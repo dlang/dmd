cf700e6989dc4ce102ceacb78e7c7f8eef3f5a09
Nick Treleaven <ntrel002@gmail.com>
2023-06-21 13:38:46 +0100

    Update hex float test

--- compiler/test/fail_compilation/fail11751.d
+++ compiler/test/fail_compilation/fail11751.d
@@ -1,9 +1,10 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail11751.d(10): Error: missing exponent
-fail_compilation/fail11751.d(10): Error: semicolon expected following auto declaration, not `ABC`
-fail_compilation/fail11751.d(10): Error: no identifier for declarator `ABC`
+fail_compilation/fail11751.d(11): Error: missing exponent
+fail_compilation/fail11751.d(11): Error: alphanumeric character cannot follow numeric literal `0x1.FFFFFFFFFFFFFp` without whitespace
+fail_compilation/fail11751.d(11): Error: semicolon expected following auto declaration, not `ABC`
+fail_compilation/fail11751.d(11): Error: no identifier for declarator `ABC`
 ---
 */
 
