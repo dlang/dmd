9713e3482e23a3948cb675c70e5e1577a09010c4
Walter Bright <walter@walterbright.com>
2023-07-03 01:07:47 -0700

    fix Issue 24026 - ImportC: ICE on nested C initializer 2

--- compiler/test/runnable/test23786.c
+++ compiler/test/runnable/test23786.c
@@ -1,7 +1,5 @@
 // https://issues.dlang.org/show_bug.cgi?id=23768
 
-#include <assert.h>
-
 typedef struct {
   union {
     struct {
@@ -28,14 +26,41 @@ typedef struct {
 
 _Static_assert(sizeof(S) == 8, "1");
 
-int main()
+void test23768()
 {
     S data = (S) {
       {{.f = {.o = 3}}}
     };
-    assert(data.f.o == 3);
+    __check(data.f.o == 3);
     S s;
     s.f.o = 4;
-    assert(s.f.o == 4);
+    __check(s.f.o == 4);
+}
+
+/**************************/
+// https://issues.dlang.org/show_bug.cgi?id=24026
+
+struct A
+{
+    int type;
+};
+
+struct E
+{
+    struct A action;
+};
+
+void test24026()
+{
+    struct E entry = {{ .type = 1 }};
+    __check(entry.action.type == 1);
+}
+
+/**************************/
+
+int main()
+{
+    test23768();
+    test24026();
     return 0;
 }
