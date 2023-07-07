d4360f47b840bc5ee2d8d1e881437275be706740
Walter Bright <WalterBright@users.noreply.github.com>
2023-07-06 18:59:46 -0700

    add -wo switch about use of obsolete features (#15381)

--- compiler/src/dmd/globals.d
+++ compiler/src/dmd/globals.d
@@ -124,6 +124,7 @@ extern (C++) struct Param
     bool release;           // build release version
     bool preservePaths;     // true means don't strip path from source file
     DiagnosticReporting warnings = DiagnosticReporting.off;  // how compiler warnings are handled
+    bool obsolete;          // enable warnings about use of obsolete messages
     bool color;             // use ANSI colors in console output
     bool cov;               // generate code coverage data
     ubyte covPercent;       // 0..100 code coverage percentage required
