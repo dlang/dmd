dlang/dmd

commit 94e8629a204d5f9668d8d5163e97c81d3241d2c7

Fix Issue 11612 - Inconsistent error on negative new array size (#15359)
* Fix Issue 11612 - Inconsistent error on negative new array size

* Extend new array tests

compiler/src/dmd/expressionsem.d
@@ -4058,9 +4058,10 @@ private extern (C++) final class ExpressionSemanticVisitor : Visitor
                 if (arg.op == EXP.error)
                     return setError();
                 arg = arg.optimize(WANTvalue);
-                if (arg.op == EXP.int64 && cast(sinteger_t)arg.toInteger() < 0)
+                if (arg.op == EXP.int64 && (target.is64bit ?
+                    cast(sinteger_t)arg.toInteger() : cast(int)arg.toInteger()) < 0)
                 {
-                    exp.error("negative array index `%s`", arg.toChars());
+                    exp.error("negative array dimension `%s`", (*exp.arguments)[i].toChars());
                     return setError();
                 }
                 (*exp.arguments)[i] = arg;

compiler/test/fail_compilation/issue20422.d
@@ -1,12 +1,19 @@
 // https://issues.dlang.org/show_bug.cgi?id=20422
 
 /*
+REQUIRED_ARGS: -m32
 TEST_OUTPUT:
 ---
-fail_compilation/issue20422.d(11): Error: missing length argument for array
+fail_compilation/issue20422.d(15): Error: missing length argument for array
+fail_compilation/issue20422.d(16): Error: negative array dimension `-1`
+fail_compilation/issue20422.d(17): Error: negative array dimension `-2147483648`
+fail_compilation/issue20422.d(18): Error: too many arguments for array
 ---
 */
 
 void main() {
     new int[];
+    new int[-1];
+    new int[](int.min);
+    new int[](1, 2);
 }

commit bc17ab0e5673f3a1044960cd1358c306eee31535

Fix regression introduced by #15359

compiler/src/dmd/expressionsem.d
@@ -4065,7 +4065,7 @@ private extern (C++) final class ExpressionSemanticVisitor : Visitor
                 if (arg.op == EXP.error)
                     return setError();
                 arg = arg.optimize(WANTvalue);
-                if (arg.op == EXP.int64 && (target.is64bit ?
+                if (arg.op == EXP.int64 && (target.isLP64 ?
                     cast(sinteger_t)arg.toInteger() : cast(int)arg.toInteger()) < 0)
                 {
                     exp.error("negative array dimension `%s`", (*exp.arguments)[i].toChars());