1291d0558ffd913e9c2f8a37a404069f522edfc1
Razvan Nitu <razvan.nitu1305@gmail.com>
2023-08-03 17:04:38 +0300

    Fix Issue 24066 - __traits(isAbstractClass) causes a segfault when passed an opaque class (#15495)
    * Fix Issue 24066 - __traits(isAbstractClass) causes a segfault when passed an opaque class
    
    * Update comment
    
    Co-authored-by: Dennis <dkorpel@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dennis <dkorpel@users.noreply.github.com>

--- compiler/src/dmd/dclass.d
+++ compiler/src/dmd/dclass.d
@@ -877,6 +877,10 @@ extern (C++) class ClassDeclaration : AggregateDeclaration
             return 0;
         }
 
+        // opaque class is not abstract if it is not declared abstract
+        if (!members)
+            return no();
+
         for (size_t i = 0; i < members.length; i++)
         {
             auto s = (*members)[i];
