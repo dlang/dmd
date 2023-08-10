927a331e7b44fc8129cdf886199d472dc135fd27
Nick Treleaven <ntrel002@gmail.com>
2023-08-10 18:58:41 +0100

    Remove extra line diff noise

--- compiler/test/fail_compilation/ice11518.d
+++ compiler/test/fail_compilation/ice11518.d
@@ -1,13 +1,12 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/ice11518.d(18): Error: class `ice11518.B` matches more than one template declaration:
-fail_compilation/ice11518.d(13):        `B(T : A!T)`
+fail_compilation/ice11518.d(17): Error: class `ice11518.B` matches more than one template declaration:
+fail_compilation/ice11518.d(12):        `B(T : A!T)`
 and:
-fail_compilation/ice11518.d(14):        `B(T : A!T)`
+fail_compilation/ice11518.d(13):        `B(T : A!T)`
 ---
 */
-
 
 class A(T) {}
 class B(T : A!T) {}
