b67736822797741ff9a26d0000714823fcfbef84
Nick Treleaven <ntrel002@gmail.com>
2023-08-12 17:16:18 +0100

    [stable] Make enum function a deprecation, not an error (#15533)
    [stable] Make enum function a deprecation, not an error
    
    Signed-off-by: Dennis <dkorpel@users.noreply.github.com>
    Signed-off-by: Vladimir Panteleev <CyberShadow@users.noreply.github.com>
    Signed-off-by: Petar Kirov <PetarKirov@users.noreply.github.com>
    Merged-on-behalf-of: Vladimir Panteleev <CyberShadow@users.noreply.github.com>

--- compiler/src/dmd/parse.d
+++ compiler/src/dmd/parse.d
@@ -4585,8 +4585,10 @@ class Parser(AST, Lexer = dmd.lexer.Lexer) : Lexer
             }
             else if (t.ty == Tfunction)
             {
+                /* @@@DEPRECATED_2.115@@@
+                 * change to error, deprecated in 2.105.1 */
                 if (storage_class & STC.manifest)
-                    error("function cannot have enum storage class");
+                    deprecation("function cannot have enum storage class");
 
                 AST.Expression constraint = null;
                 //printf("%s funcdecl t = %s, storage_class = x%lx\n", loc.toChars(), t.toChars(), storage_class);
