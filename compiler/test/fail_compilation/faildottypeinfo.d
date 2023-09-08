5fc197c144aa3d22edda794fdbdee73faed45b00
Nick Treleaven <ntrel002@gmail.com>
2023-09-08 12:20:56 +0100

    Remove local path from test output

--- compiler/test/fail_compilation/faildottypeinfo.d
+++ compiler/test/fail_compilation/faildottypeinfo.d
@@ -3,7 +3,7 @@ TEST_OUTPUT:
 ---
 fail_compilation/faildottypeinfo.d(12): Error: no property `typeinfo` for `0` of type `int`
 fail_compilation/faildottypeinfo.d(13): Error: no property `typeinfo` for type `object.Object`
-/home/nick/git/dmd/compiler/test/../../druntime/import/object.d(110):        class `Object` defined here
+$p:druntime/import/object.d$($n$):        class `Object` defined here
 ---
 */
 
