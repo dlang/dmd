9c4dfcf16956c2d74f9c6309e4ceb3fb37298ca1
ryuukk <44361234+ryuukk@users.noreply.github.com>
2023-09-23 04:22:39 +0200

    Fix ctfe flag incorrectly checked

--- compiler/src/dmd/typinf.d
+++ compiler/src/dmd/typinf.d
@@ -43,7 +43,7 @@ extern (C++) void genTypeInfo(Expression e, const ref Loc loc, Type torig, Scope
     // Even when compiling without `useTypeInfo` (e.g. -betterC) we should
     // still be able to evaluate `TypeInfo` at compile-time, just not at runtime.
     // https://issues.dlang.org/show_bug.cgi?id=18472
-    if (!sc || !(sc.flags & SCOPE.ctfe))
+    if (!sc || (sc.flags & SCOPE.ctfe))
     {
         if (!global.params.useTypeInfo)
         {
