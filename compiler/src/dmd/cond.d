2c4003b754bc4a82a2f5c20b22368dd94318c3b7
liushuyu <liushuyu011@gmail.com>
2023-09-23 12:53:48 -0600

    compiler/test: add tests to test LoongArch-related reserved versions

--- compiler/src/dmd/cond.d
+++ compiler/src/dmd/cond.d
@@ -691,6 +691,7 @@ extern (C++) final class VersionCondition : DVCondition
             case "LDC":
             case "linux":
             case "LittleEndian":
+            case "LoongArch32":
             case "LoongArch64":
             case "LoongArch_HardFloat":
             case "LoongArch_SoftFloat":
