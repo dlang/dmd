9eded81f30b66c78d0b1729098791e0b2daa0a5f
Martin Kinkelin <noone@nowhere.com>
2023-09-29 00:13:51 +0200

    Revert "Update C++ headers (#15396)"
    This reverts commit 3a1e24922d462e5c477fd5753ff46819722310f0.

--- compiler/src/dmd/frontend.h
+++ compiler/src/dmd/frontend.h
@@ -7153,16 +7153,14 @@ public:
     ThisExp(const Loc& loc, const EXP tok);
     ThisExp* syntaxCopy() override;
     Optional<bool > toBool() override;
-    bool isLvalue() override;
-    Expression* toLvalue(Scope* sc, Expression* e) override;
+    bool isLvalue() final override;
+    Expression* toLvalue(Scope* sc, Expression* e) final override;
     void accept(Visitor* v) override;
 };
 
 class SuperExp final : public ThisExp
 {
 public:
-    bool isLvalue() override;
-    Expression* toLvalue(Scope* sc, Expression* e) override;
     void accept(Visitor* v) override;
 };
 
