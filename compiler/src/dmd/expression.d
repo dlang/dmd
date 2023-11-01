8a8d03d88a7a17082aca132f1be776f945cdda85
Martin Kinkelin <kinke@users.noreply.github.com>
2023-11-01 20:13:30 +0100

    [stable] Revert "fix Issue 24024 - cannot pass class this to ref class" (#15637)
    * Revert "fix Issue 24024 - cannot pass class this to ref class (#15389)"
    
    This reverts commit 2ee383b7908a0e2a72eb14ad3e9ca94c64d58a81.
    
    Conflicts:
    	compiler/src/dmd/expression.d
    
    * Revert "Update C++ headers (#15396)"
    
    This reverts commit 3a1e24922d462e5c477fd5753ff46819722310f0.
    
    * Add test case for Issue 24157 - [REG2.105] class `this` as lvalue leads to memory corruption

--- compiler/src/dmd/expression.d
+++ compiler/src/dmd/expression.d
@@ -2468,13 +2468,19 @@ extern (C++) class ThisExp : Expression
         return typeof(return)(true);
     }
 
-    override bool isLvalue()
+    override final bool isLvalue()
     {
-        return true;
+        // Class `this` should be an rvalue; struct `this` should be an lvalue.
+        return type.toBasetype().ty != Tclass;
     }
 
-    override Expression toLvalue(Scope* sc, Expression e)
+    override final Expression toLvalue(Scope* sc, Expression e)
     {
+        if (type.toBasetype().ty == Tclass)
+        {
+            // Class `this` is an rvalue; struct `this` is an lvalue.
+            return Expression.toLvalue(sc, e);
+        }
         return this;
     }
 
@@ -2492,18 +2498,6 @@ extern (C++) final class SuperExp : ThisExp
     extern (D) this(const ref Loc loc)
     {
         super(loc, EXP.super_);
-    }
-
-    override bool isLvalue()
-    {
-        // Class `super` should be an rvalue
-        return false;
-    }
-
-    override Expression toLvalue(Scope* sc, Expression e)
-    {
-        // Class `super` is an rvalue
-        return Expression.toLvalue(sc, e);
     }
 
     override void accept(Visitor v)
