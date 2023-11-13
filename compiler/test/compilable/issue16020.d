e72f5b458bb93eac3a94776fe338fd7b8ca08d8a
Nick Treleaven <ntrel002@gmail.com>
2023-11-13 14:37:43 +0000

    Revert "Move deprecated tests"
    This reverts commit 7dddcc9c10d7cf96896420c44ffa95de181613a1.

--- compiler/test/compilable/issue16020.d
+++ compiler/test/compilable/issue16020.d
@@ -20,6 +20,18 @@ alias void F51() @system;
 alias F52 = void() @safe;
 static assert (!is(F51 == F52));
 
+alias F61 = int() const shared;
+alias int F62() const shared ;
+alias F63 = const shared int();
+static assert (is(F61 == F62));
+static assert (is(F63 == F62));
+
+alias F71 = int() immutable inout;
+alias int F72() immutable inout;
+alias F73 = immutable inout int();
+static assert (is(F71 == F72));
+static assert (is(F73 == F72));
+
 alias FunTemplate(T) = void(T t);
 alias Specialized = FunTemplate!int;
 alias Compared = void(int);
