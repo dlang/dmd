01377d9ab406f5ddf18809d18de9b06e87ef40fc
Nick Treleaven <ntrel002@gmail.com>
2023-11-14 18:37:11 +0000

    Update tests

--- compiler/test/fail_compilation/ctor_attr.d
+++ compiler/test/fail_compilation/ctor_attr.d
@@ -1,18 +1,20 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/ctor_attr.d(24): Error: none of the overloads of `this` are callable using argument types `(int)`
-fail_compilation/ctor_attr.d(15):        Candidates are: `ctor_attr.S.this(int x) const`
-fail_compilation/ctor_attr.d(16):                        `ctor_attr.S.this(string x)`
-fail_compilation/ctor_attr.d(26): Error: none of the overloads of `foo` are callable using a mutable object
-fail_compilation/ctor_attr.d(18):        Candidates are: `ctor_attr.S.foo(int x) immutable`
-fail_compilation/ctor_attr.d(19):                        `ctor_attr.S.foo(string x)`
+fail_compilation/ctor_attr.d(26): Error: none of the overloads of `this` are callable using argument types `(int)`
+fail_compilation/ctor_attr.d(16):        Candidates are: `ctor_attr.S.this(int x) const`
+fail_compilation/ctor_attr.d(18):                        `ctor_attr.S.this(string x)`
+fail_compilation/ctor_attr.d(17):                        `this()(int x) shared`
+fail_compilation/ctor_attr.d(28): Error: none of the overloads of `foo` are callable using a mutable object
+fail_compilation/ctor_attr.d(20):        Candidates are: `ctor_attr.S.foo(int x) immutable`
+fail_compilation/ctor_attr.d(21):                        `ctor_attr.S.foo(string x)`
 ---
 */
 
 struct S
 {
     this(int x) const {}
+    this()(int x) shared {}
     this(string x) {}
 
     void foo(int x) immutable  {}
