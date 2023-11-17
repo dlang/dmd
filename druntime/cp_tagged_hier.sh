789118779e608956369589fda8ac0e1563502006
Denis Feklushkin <feklushkin.denis@gmail.com>
2023-11-17 17:51:18 +0300

    Check lines number for COPY added

--- druntime/cp_tagged_hier.sh
+++ druntime/cp_tagged_hier.sh
@@ -36,9 +36,9 @@ function applyTaggedFiles {
 
     SRC_FILES_LIST+=($(find ${SRC_TAG_DIR} -type f ))
 
-    pushd ${SRC_TAG_DIR}
+    pushd ${SRC_TAG_DIR} > /dev/null
     MAYBE_COPY_LIST+=($(find * -type f ))
-    popd
+    popd > /dev/null
 
     APPLIED+=" $TAG"
 
@@ -50,6 +50,9 @@ do
     applyTaggedFiles ${tag}
 done
 
+let LINES_TO_COPY=$(grep -v '^$' ${SRC_COPY_FILE} | wc -l)
+COPIED=0
+
 echo "TAGGED_SRCS_LIST=\\" > ${DST_FILE}
 echo "TAGGED_COPY_LIST=\\" > ${DST_COPY_FILE}
 
@@ -60,8 +63,19 @@ do
 
     maybe_copy=$(echo "${MAYBE_COPY_LIST[$i]}" | tr '/' '\\')
 
-    #FIXME: weak code:
-    grep -F "$maybe_copy" < ${SRC_COPY_FILE} && echo ${fl} >> ${DST_COPY_FILE}
+    # Adds copy entry if file mentioned in the list
+    grep -F "$maybe_copy" ${SRC_COPY_FILE} && {
+        echo ${fl} >> ${DST_COPY_FILE}
+        let "COPIED+=1"
+    }
 done
+
+if [ $COPIED -ne $LINES_TO_COPY ]; then
+    echo "File '$SRC_COPY_FILE' contains $LINES_TO_COPY meaningful line(s), but to '$DST_COPY_FILE' added $COPIED lines"
+
+    mv ${DST_FILE} "$DST_FILE.disabled"
+    echo "File '$DST_FILE' to '$DST_FILE.disabled' to avoid considering that tags parsing process was sucessfully done"
+    exit 1
+fi
 
 echo "All tags applied"
