26a57bc6e047d28783acc3071dcd8646ebd11549
Nick Treleaven <ntrel002@gmail.com>
2023-11-25 17:53:07 +0000

    Fix Issue 14128 - AliasDeclaration allows expressions, causing false code for ThisExp

---
+++ compiler/test/fail_compilation/alias_instance_member.d
@@ -0,0 +1,26 @@
+/*
+REQUIRED_ARGS: -de
+TEST_OUTPUT:
+---
+fail_compilation/alias_instance_member.d(16): Deprecation: cannot alias member of variable `that`
+fail_compilation/alias_instance_member.d(16):        Use `typeof(that)` instead to preserve behaviour
+---
+*/
+
+struct Foo
+{
+    int v;
+    void test(Foo that) const
+    {
+        alias a = this.v;
+        alias b = that.v;
+        assert(&a is &b);
+    }
+}
+
+void main ()
+{
+    Foo a = Foo(1);
+    Foo b = Foo(2);
+    a.test(b);
+}
