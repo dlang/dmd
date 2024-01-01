3843c790ffd55e273b3ec906eb0391f65839f613
Iain Buclaw <ibuclaw@gdcproject.org>
2024-01-01 18:40:37 +0100

    Update copyright years of dmd sources to 2024

--- compiler/src/dmd/backend/cgelem.d
+++ compiler/src/dmd/backend/cgelem.d
@@ -8,7 +8,7 @@
  * i.e. rewriting trees to less expensive trees.
  *
  * Copyright:   Copyright (C) 1985-1998 by Symantec
- *              Copyright (C) 2000-2023 by The D Language Foundation, All Rights Reserved
+ *              Copyright (C) 2000-2024 by The D Language Foundation, All Rights Reserved
  * Authors:     $(LINK2 https://www.digitalmars.com, Walter Bright)
  * License:     $(LINK2 https://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
  * Source:      $(LINK2 https://github.com/dlang/dmd/blob/master/src/dmd/backend/cgelem.d, backend/cgelem.d)
--- compiler/src/dmd/backend/dvarstats.d
+++ compiler/src/dmd/backend/dvarstats.d
@@ -4,7 +4,7 @@
  * Compiler implementation of the
  * $(LINK2 https://www.dlang.org, D programming language).
  *
- * Copyright:   Copyright (C) 2015-2023 by The D Language Foundation, All Rights Reserved
+ * Copyright:   Copyright (C) 2015-2024 by The D Language Foundation, All Rights Reserved
  * Authors:     Rainer Schuetze
  * License:     $(LINK2 https://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
  * Source:      $(LINK2 https://github.com/dlang/dmd/blob/master/src/dmd/backend/dvarstats.d, backend/dvarstats.d)
