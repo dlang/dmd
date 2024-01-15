14180f8bf51a0eb4947d050d4156dd647fb75587
ryuukk <ryuukk.dev@gmail.com>
2024-01-15 20:19:29 +0100

    Update more tests

--- compiler/test/fail_compilation/bug15613.d
+++ compiler/test/fail_compilation/bug15613.d
@@ -1,10 +1,12 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/bug15613.d(16): Error: function `bug15613.f(int...)` is not callable using argument types `(typeof(null))`
-fail_compilation/bug15613.d(16):        cannot pass argument `null` of type `typeof(null)` to parameter `int...`
-fail_compilation/bug15613.d(17): Error: function `bug15613.g(Object, ...)` is not callable using argument types `(int)`
-fail_compilation/bug15613.d(17):        cannot pass argument `8` of type `int` to parameter `Object`
+fail_compilation\bug15613.d(16): Error: function is not callable using argument types `(typeof(null))`
+fail_compilation\bug15613.d(16):            `bug15613.f(int...)`
+fail_compilation\bug15613.d(16):        cannot pass argument `null` of type `typeof(null)` to parameter `int...`
+fail_compilation\bug15613.d(17): Error: function is not callable using argument types `(int)`
+fail_compilation\bug15613.d(17):            `bug15613.g(Object, ...)`
+fail_compilation\bug15613.d(17):        cannot pass argument `8` of type `int` to parameter `Object`
 ---
 */
 
@@ -20,8 +22,9 @@ void main()
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/bug15613.d(32): Error: function `bug15613.h(int[]...)` is not callable using argument types `(int, void function(int[]...))`
-fail_compilation/bug15613.d(32):        cannot pass argument `& h` of type `void function(int[]...)` to parameter `int[]...`
+fail_compilation\bug15613.d(32): Error: function is not callable using argument types `(int, void function(int[]...))`
+fail_compilation\bug15613.d(32):            `bug15613.h(int[]...)`
+fail_compilation\bug15613.d(32):        cannot pass argument `& h` of type `void function(int[]...)` to parameter `int[]...`
 ---
 */
 
--- compiler/test/fail_compilation/callconst.d
+++ compiler/test/fail_compilation/callconst.d
@@ -1,9 +1,9 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/callconst.d(13): Error: function is not callable using argument types `(const(X))`
-fail_compilation/callconst.d(13):               `callconst.func(ref X)`
-fail_compilation/callconst.d(13):        cannot pass argument `x` of type `const(X)` to parameter `ref X`
+fail_compilation\callconst.d(14): Error: function is not callable using argument types `(const(X))`
+fail_compilation\callconst.d(14):           `callconst.func(ref X)`
+fail_compilation\callconst.d(14):        cannot pass argument `x` of type `const(X)` to parameter `ref X`
 ---
 */
 struct X {}
