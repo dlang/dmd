d1601c396470125aa0263c492c81f14684237c14
ryuukk <ryuukk.dev@gmail.com>
2024-01-19 15:03:59 +0100

    Attempt to fix test

--- compiler/test/fail_compilation/pr15479.d
+++ compiler/test/fail_compilation/pr15479.d
@@ -3,7 +3,7 @@ LINK:
 EXTRA_SOURCES: imports/pragma_lib_local.d
 TEST_OUTPUT:
 ---
-/usr/bin/ld: cannot find extra-files/fake.a: No such file or directory
+/usr/bin/ld: cannot find {{RESULTS_DIR}}/fail_compilation/imports/extra-files/local_lib.a: No such file or directory
 collect2: error: ld returned 1 exit status
 Error: linker exited with status 1
 ---
@@ -14,3 +14,4 @@ import imports.pragma_lib_local;
 void main() {
     printf("%i\n", lib_get_int());
 }
+
