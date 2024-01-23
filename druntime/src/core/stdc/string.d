1e5fa5e247309c78ef5a444f81c161a42e379b9c
ryuukk <ryuukk.dev@gmail.com>
2024-01-23 12:57:35 +0100

    Add strndup

--- druntime/src/core/stdc/string.d
+++ druntime/src/core/stdc/string.d
@@ -64,6 +64,8 @@ size_t strcspn(scope const char* s1, scope const char* s2) pure;
 ///
 char*  strdup(scope const char *s);
 ///
+char*  strndup(const char *str, size_t size);
+///
 char*  strerror(int errnum);
 // This `strerror_r` definition is not following the POSIX standard
 version (ReturnStrerrorR)
