8bf2b08a1b0088dcda6c21c04faaaa7240a166e4
Walter Bright <WalterBright@users.noreply.github.com>
2024-01-25 01:07:15 -0800

    move declareThis() to funcsem.d (#16093)

--- compiler/src/dmd/funcsem.d
+++ compiler/src/dmd/funcsem.d
@@ -129,25 +129,91 @@ bool functionSemantic3(FuncDeclaration fd)
 {
     if (fd.semanticRun < PASS.semantic3 && fd._scope)
     {
-	/* Forward reference - we need to run semantic3 on this function.
-	 * If errors are gagged, and it's not part of a template instance,
-	 * we need to temporarily ungag errors.
-	 */
-	TemplateInstance spec = fd.isSpeculative();
-	uint olderrs = global.errors;
-	uint oldgag = global.gag;
-	if (global.gag && !spec)
-	    global.gag = 0;
-	semantic3(fd, fd._scope);
-	global.gag = oldgag;
+        /* Forward reference - we need to run semantic3 on this function.
+         * If errors are gagged, and it's not part of a template instance,
+         * we need to temporarily ungag errors.
+         */
+        TemplateInstance spec = fd.isSpeculative();
+        uint olderrs = global.errors;
+        uint oldgag = global.gag;
+        if (global.gag && !spec)
+            global.gag = 0;
+        semantic3(fd, fd._scope);
+        global.gag = oldgag;
 
-	// If it is a speculatively-instantiated template, and errors occur,
-	// we need to mark the template as having errors.
-	if (spec && global.errors != olderrs)
-	    spec.errors = (global.errors - olderrs != 0);
-	if (olderrs != global.errors) // if errors compiling this function
-	    return false;
+        // If it is a speculatively-instantiated template, and errors occur,
+        // we need to mark the template as having errors.
+        if (spec && global.errors != olderrs)
+            spec.errors = (global.errors - olderrs != 0);
+        if (olderrs != global.errors) // if errors compiling this function
+            return false;
     }
 
     return !fd.errors && !fd.hasSemantic3Errors();
+}
+
+// called from semantic3
+/**
+ * Creates and returns the hidden parameters for this function declaration.
+ *
+ * Hidden parameters include the `this` parameter of a class, struct or
+ * nested function and the selector parameter for Objective-C methods.
+ */
+extern (D) void declareThis(FuncDeclaration fd, Scope* sc)
+{
+    const bool dualCtx = (fd.toParent2() != fd.toParentLocal());
+    if (dualCtx)
+        fd.hasDualContext = true;
+    auto ad = fd.isThis();
+    if (!dualCtx && !ad && !fd.isNested())
+    {
+        fd.vthis = null;
+        fd.objc.selectorParameter = null;
+        return;
+    }
+
+    Type addModStc(Type t)
+    {
+        return t.addMod(fd.type.mod).addStorageClass(fd.storage_class);
+    }
+
+    if (dualCtx || fd.isNested())
+    {
+        /* The 'this' for a nested function is the link to the
+         * enclosing function's stack frame.
+         * Note that nested functions and member functions are disjoint.
+         */
+        Type tthis = addModStc(dualCtx ?
+                               Type.tvoidptr.sarrayOf(2).pointerTo() :
+                               Type.tvoid.pointerTo());
+        fd.vthis = new VarDeclaration(fd.loc, tthis, dualCtx ? Id.this2 : Id.capture, null);
+        fd.vthis.storage_class |= STC.parameter | STC.nodtor;
+    }
+    else if (ad)
+    {
+        Type thandle = addModStc(ad.handleType());
+        fd.vthis = new ThisDeclaration(fd.loc, thandle);
+        fd.vthis.storage_class |= STC.parameter;
+        if (thandle.ty == Tstruct)
+        {
+            fd.vthis.storage_class |= STC.ref_;
+        }
+    }
+
+    if (auto tf = fd.type.isTypeFunction())
+    {
+        if (tf.isreturn)
+            fd.vthis.storage_class |= STC.return_;
+        if (tf.isScopeQual)
+            fd.vthis.storage_class |= STC.scope_;
+        if (tf.isreturnscope)
+            fd.vthis.storage_class |= STC.returnScope;
+    }
+
+    fd.vthis.dsymbolSemantic(sc);
+    if (!sc.insert(fd.vthis))
+        assert(0);
+    fd.vthis.parent = fd;
+    if (ad)
+        fd.objc.selectorParameter = .objc.createSelectorParameter(fd, sc);
 }
