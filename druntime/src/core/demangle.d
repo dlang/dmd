e89f319657a0b4c02ed634b3fc67b490d576c132
Denis Feklushkin <denis.feklushkin@gmail.com>
2024-01-30 07:13:02 +0700

    demangle: minor changes for clarity (#16112)
    demangle: minor changes for clarity
    
    Signed-off-by: Dennis <dkorpel@users.noreply.github.com>
    Merged-on-behalf-of: Nicholas Wilson <thewilsonator@users.noreply.github.com>

--- druntime/src/core/demangle.d
+++ druntime/src/core/demangle.d
@@ -3000,7 +3000,9 @@ private struct Buffer
 
 private struct BufSlice
 {
-    char[] dst;
+    char[] buf;
+    size_t from;
+    size_t to;
 
     @safe:
     pure:
@@ -3008,14 +3010,14 @@ private struct BufSlice
 
     @disable this();
 
-    this(return scope char[] dst) scope nothrow @nogc
+    this(return scope char[] buf) scope nothrow @nogc
     {
-        this(dst, 0, 0);
+        this(buf, 0, 0);
     }
 
-    this(return scope char[] dst, size_t from, size_t to, bool lastArgIsLen = false) scope nothrow @nogc
+    this(return scope char[] buf, size_t from, size_t to, bool lastArgIsLen = false) scope nothrow @nogc
     {
-        this.dst = dst;
+        this.buf = buf;
         this.from = from;
 
         if (lastArgIsLen)
@@ -3024,12 +3026,9 @@ private struct BufSlice
             this.to = to;
     }
 
-    size_t from;
-    size_t to;
-
     invariant()
     {
-        if (dst is null)
+        if (buf is null)
         {
             assert(from == 0);
             assert(to == 0);
@@ -3038,6 +3037,6 @@ private struct BufSlice
         assert(from <= to);
     }
 
-    auto getSlice() inout nothrow scope { return dst[from .. to]; }
+    auto getSlice() inout nothrow scope { return buf[from .. to]; }
     size_t length() const scope { return to - from; }
 }
