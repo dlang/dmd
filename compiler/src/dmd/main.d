5505b20a665feb906b9ea2972b69f01b266a762f
ryuukk <ryuukk.dev@gmail.com>
2024-02-11 12:14:54 +0100

    Don't disable exceptions in -betterC, it can still be useful to generate backtraces

--- compiler/src/dmd/main.d
+++ compiler/src/dmd/main.d
@@ -967,7 +967,6 @@ void reconcileCommands(ref Param params, ref Target target)
 
         params.useModuleInfo = false;
         params.useTypeInfo = false;
-        params.useExceptions = false;
         params.useGC = false;
     }
 }
