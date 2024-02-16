063b39bfc17e2fd4783e25fb54fa5b80bd9619ff
Teodor Dutu <teodor.dutu@gmail.com>
2024-02-16 17:27:39 +0200

    Revert the removal of `Expression.parens`
    Having this at the `Expression` level is useful to libdparse and will
    also help fix this regression
    https://issues.dlang.org/show_bug.cgi?id=24371 whose underlying issue is
    that the lowering doesn't take parentheses into account.
    
    This reverts the follwoing commits
    - 6e604e22b1e7f5f25ab2eb808a5691b2f7d3a9a0
    - e60d027582d2fa6c9236db7da5ead4b47d6e3f87
    - 71ffbe08c5fdfe91c11471b60a99a0f608cef628

--- compiler/src/dmd/expression.h
+++ compiler/src/dmd/expression.h
@@ -86,6 +86,7 @@ public:
     Type *type;                 // !=NULL means that semantic() has been run
     Loc loc;                    // file location
     EXP op;                     // to minimize use of dynamic_cast
+    d_bool parens;              // if this is a parenthesized expression
 
     size_t size() const;
     static void _init();
@@ -295,7 +296,6 @@ class IdentifierExp : public Expression
 {
 public:
     Identifier *ident;
-    d_bool parens;
 
     static IdentifierExp *create(const Loc &loc, Identifier *ident);
     bool isLvalue() override final;
