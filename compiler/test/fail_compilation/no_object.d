11a7b4b088323bd68b0aee7bb789a02b4c716eb8
Walter Bright <walter@walterbright.com>
2024-03-02 14:11:39 -0800

    consolidate references to object.d not found

--- compiler/test/fail_compilation/no_object.d
+++ compiler/test/fail_compilation/no_object.d
@@ -2,10 +2,9 @@
 DFLAGS:
 TEST_OUTPUT:
 ---
-Error: cannot find source code for runtime library file 'object.d'
+Error: `object` not found. object.d may be incorrectly installed or corrupt.
        dmd might not be correctly installed. Run 'dmd -man' for installation instructions.
        config file: not found
-import path[0] = fail_compilation
 ---
 */
 
