b5e2b1640c000a8794bff637625ccae9d42e2038
Nick Treleaven <ntrel002@gmail.com>
2024-03-20 11:14:09 +0000

    Add test for lvalue assign

--- compiler/test/fail_compilation/cast_qual.d
+++ compiler/test/fail_compilation/cast_qual.d
@@ -2,7 +2,8 @@
 REQUIRED_ARGS: -preview=dip1000 -de
 TEST_OUTPUT:
 ---
-fail_compilation/cast_qual.d(14): Deprecation: cast from `const(int)` to `int` cannot be used as an lvalue in @safe code
+fail_compilation/cast_qual.d(15): Deprecation: cast from `const(int)` to `int` cannot be used as an lvalue in @safe code
+fail_compilation/cast_qual.d(17): Deprecation: cast from `const(int)` to `int` cannot be used as an lvalue in @safe code
 ---
 */
 
@@ -13,5 +14,6 @@ void main() {
     int j = cast() i; // OK
     int* p = &cast() i; // this should not compile in @safe code
     *p = 4; // oops
+    cast() i = 5; // NG
     auto q = &cast(const) j; // OK, int* to const int*
 }
