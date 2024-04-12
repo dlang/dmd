ecbba93e7867567e435cbe0cc678e3cce21e0d3d
Harry Gillanders <contact@harrygillanders.com>
2024-04-12 22:25:38 +0100

    Add files for MSVC intrinsic implementations.

---
+++ druntime/src/__builtins_msvc.d
@@ -0,0 +1,10 @@
+/* This file contains D reimplementations of some of the intrinsics recognised
+   by the MSVC compiler, for ImportC.
+   This module is intended for only internal use, hence the leading double underscore.
+
+   Copyright: Copyright D Language Foundation 2024-2024
+   License: $(HTTP www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+   Authors: Harry Gillanders
+   Source: $(DRUNTIMESRC __builtins_msvc.d) */
+
+module __builtins_msvc;
