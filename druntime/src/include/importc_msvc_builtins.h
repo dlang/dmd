d0fcf9729e5c2c383f2092a5b6163bd759303998
Harry Gillanders <contact@harrygillanders.com>
2024-04-12 22:25:38 +0100

    Add files for MSVC intrinsic implementations.

---
+++ druntime/src/include/importc_msvc_builtins.h
@@ -0,0 +1,17 @@
+/* This file contains reimplementations of some of the intrinsics recognized
+   by the MSVC compiler, for ImportC.
+   To use it, put `#include <importc_msvc_builtins.h>` in the C source-code that intends to use it.
+
+   This header emits its declarations only when `__IMPORTC_MSVC_BUILTINS__` is not defined.
+   This header defines `__IMPORTC_MSVC_BUILTINS__` when it is included.
+
+   Copyright: Copyright D Language Foundation 2024-2024
+   License: $(HTTP www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+   Authors: Harry Gillanders
+   Source: $(DRUNTIMESRC importc_msvc_builtins.h) */
+
+#ifndef __IMPORTC_MSVC_BUILTINS__
+#define __IMPORTC_MSVC_BUILTINS__ 1
+
+__import __builtins_msvc;
+#endif
