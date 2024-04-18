d07aa342e513a0a34a6ee6ab0e29ea6c14a9bb3c
aferust <aferust@gmail.com>
2024-04-18 13:26:15 +0300

    Fix bugzilla 24509: importC cannot handle _stdcall Function Calling Convention with single heading underscore

--- compiler/test/compilable/test22727.c
+++ compiler/test/compilable/test22727.c
@@ -1,11 +1,18 @@
 // https://issues.dlang.org/show_bug.cgi?id=22727
 
+
 int fooc(int a) { return a; }
+
+#ifdef _WIN32
 
 __stdcall int foostdcall(int a) { return a; }
 
 int __stdcall foostdcall2(int a) { return a; }
 
+int _stdcall foostdcall3(int a) { return a; } // test issue 24509
+
 int __stdcall (*fp1)(int a) = &foostdcall;
 
 int (__stdcall *fp2)(int a) = &foostdcall2;
+
+#endif
