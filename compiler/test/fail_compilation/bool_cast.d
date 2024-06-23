e96efa0db1b21351197f64b670f8f5690f87c951
Nick Treleaven <ntrel002@gmail.com>
2024-06-23 13:02:09 +0100

    Tweak message for unsafe cast to bool array/pointer
    Add test for cast to `bool*`.
    See https://issues.dlang.org/show_bug.cgi?id=24582.

--- compiler/test/fail_compilation/bool_cast.d
+++ compiler/test/fail_compilation/bool_cast.d
@@ -1,9 +1,11 @@
 /*
-REQUIRED_ARGS: -de
+REQUIRED_ARGS: -de -preview=dip1000
 TEST_OUTPUT:
 ---
-fail_compilation/bool_cast.d(13): Deprecation: cast from `ubyte[]` to `bool[]` not allowed in safe code
-fail_compilation/bool_cast.d(13):        Array data may have bytes which are not 0 or 1
+fail_compilation/bool_cast.d(15): Deprecation: cast from `ubyte[]` to `bool[]` not allowed in safe code
+fail_compilation/bool_cast.d(15):        Source element may have bytes which are not 0 or 1
+fail_compilation/bool_cast.d(19): Deprecation: cast from `int*` to `bool*` not allowed in safe code
+fail_compilation/bool_cast.d(19):        Source element may have bytes which are not 0 or 1
 ---
 */
 
@@ -12,4 +14,7 @@ void main() @safe
     ubyte[] a = [2, 4];
     auto b = cast(bool[]) a; // reinterprets a's data
     auto c = cast(bool[]) [2, 4]; // literal cast applies to each element
+
+    int i = 2;
+    auto p = cast(bool*) &i;
 }
