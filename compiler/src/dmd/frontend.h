9581a91ad65b85c80e9b33a84be731271cb045c8
Martin Kinkelin <kinke@users.noreply.github.com>
2024-07-02 19:31:34 +0200

    Fix Bugzilla 24599 - Wrongly elided TypeInfo emission (#15868)
    Reverting #14844, which caused such missing TypeInfos, *and* making
    sure the special TypeInfo members are fully analyzed and ready for
    codegen (otherwise hitting an assertion for the real-world project).

--- compiler/src/dmd/frontend.h
+++ compiler/src/dmd/frontend.h
@@ -7596,7 +7596,7 @@ public:
 
 extern Target target;
 
-extern Type* getTypeInfoType(const Loc& loc, Type* t, Scope* sc, bool genObjCode = true);
+extern Type* getTypeInfoType(const Loc& loc, Type* t, Scope* sc);
 
 class SemanticTimeTransitiveVisitor : public SemanticTimePermissiveVisitor
 {
