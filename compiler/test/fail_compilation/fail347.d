9da51ae446407c29b1de633f67ca1653c2b39aaa
Nick Treleaven <ntrel002@gmail.com>
2024-07-08 13:06:17 +0100

    Combine related tests

--- compiler/test/fail_compilation/fail347.d
+++ compiler/test/fail_compilation/fail347.d
@@ -2,11 +2,13 @@
 EXTRA_FILES: imports/fail347a.d
 TEST_OUTPUT:
 ---
-fail_compilation/fail347.d(24): Error: undefined identifier `bbr`, did you mean variable `bar`?
-fail_compilation/fail347.d(25): Error: no property `ofo` for type `S`, did you mean `fail347.S.foo`?
-fail_compilation/fail347.d(27): Error: no property `fool` for `sp` of type `fail347.S*`
-fail_compilation/fail347.d(27):        did you mean variable `foo`?
-fail_compilation/fail347.d(28): Error: undefined identifier `strlenx`, did you mean function `strlen`?
+fail_compilation/fail347.d(26): Error: undefined identifier `bbr`, did you mean variable `bar`?
+fail_compilation/fail347.d(27): Error: no property `ofo` for type `S`, did you mean `fail347.S.foo`?
+fail_compilation/fail347.d(29): Error: no property `fool` for `sp` of type `fail347.S*`
+fail_compilation/fail347.d(29):        did you mean variable `foo`?
+fail_compilation/fail347.d(30): Error: undefined identifier `strlenx`, did you mean function `strlen`?
+fail_compilation/fail347.d(31): Error: no property `strlenx` for `"hello"` of type `string`
+fail_compilation/fail347.d(31):        did you mean function `strlen`?
 ---
 */
 
@@ -26,4 +28,5 @@ void main()
     auto sp = &bar;
     sp.fool = 5;
     auto s = strlenx("hello");
+    auto q = "hello".strlenx();
 }
