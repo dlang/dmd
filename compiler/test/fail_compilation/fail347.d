2da873a6313ab222721a403b4914d8f19af60fff
Nick Treleaven <ntrel002@gmail.com>
2024-07-08 13:05:30 +0100

    Fix Bugzilla 23449 - spellchecker should suggest corrections for pointer members

--- compiler/test/fail_compilation/fail347.d
+++ compiler/test/fail_compilation/fail347.d
@@ -2,9 +2,11 @@
 EXTRA_FILES: imports/fail347a.d
 TEST_OUTPUT:
 ---
-fail_compilation/fail347.d(22): Error: undefined identifier `bbr`, did you mean variable `bar`?
-fail_compilation/fail347.d(23): Error: no property `ofo` for type `S`, did you mean `fail347.S.foo`?
-fail_compilation/fail347.d(24): Error: undefined identifier `strlenx`, did you mean function `strlen`?
+fail_compilation/fail347.d(24): Error: undefined identifier `bbr`, did you mean variable `bar`?
+fail_compilation/fail347.d(25): Error: no property `ofo` for type `S`, did you mean `fail347.S.foo`?
+fail_compilation/fail347.d(27): Error: no property `fool` for `sp` of type `fail347.S*`
+fail_compilation/fail347.d(27):        did you mean variable `foo`?
+fail_compilation/fail347.d(28): Error: undefined identifier `strlenx`, did you mean function `strlen`?
 ---
 */
 
@@ -21,5 +23,7 @@ void main()
     S bar;
     bbr.foo = 3;
     bar.ofo = 4;
+    auto sp = &bar;
+    sp.fool = 5;
     auto s = strlenx("hello");
 }
