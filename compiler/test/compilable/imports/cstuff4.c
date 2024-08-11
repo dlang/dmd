23eb5b5fbe263262c202da44afc60b14909f7231
ryuukk <44361234+ryuukk@users.noreply.github.com>
2024-08-11 14:00:04 +0200

    Add test

---
+++ compiler/test/compilable/imports/cstuff4.c
@@ -0,0 +1,4 @@
+#ifdef _D_VERSION_HELLO
+void hello(void)
+{}
+#endif
---
+++ compiler/test/compilable/test16777.d
@@ -0,0 +1,8 @@
+// ARG_SETS: -i -version=HELLO
+// PERMUTE_ARGS:
+// LINK:
+import imports.cstuff4;
+void main()
+{
+    hello();
+}
