1dfc510997e830238db1708a9922b9c0327b4ea0
Nicholas Wilson <iamthewilsonator@hotmail.com>
2024-08-18 16:17:18 +0800

    [NFC] [funcsem.d] use early return

--- compiler/src/dmd/funcsem.d
+++ compiler/src/dmd/funcsem.d
@@ -1700,39 +1700,40 @@ FuncDeclaration resolveFuncCall(const ref Loc loc, Scope* sc, Dsymbol s,
            fd.kind(), fd.toPrettyChars(), parametersTypeToChars(tf.parameterList),
            tf.modToChars(), fargsBuf.peekChars());
 
+    if (global.gag && !global.params.v.showGaggedErrors)
+        return null;
+
     // re-resolve to check for supplemental message
-    if (!global.gag || global.params.v.showGaggedErrors)
+    if (tthis)
     {
-        if (tthis)
+        if (auto classType = tthis.isTypeClass())
         {
-            if (auto classType = tthis.isTypeClass())
+            if (auto baseClass = classType.sym.baseClass)
             {
-                if (auto baseClass = classType.sym.baseClass)
+                if (auto baseFunction = baseClass.search(baseClass.loc, fd.ident))
                 {
-                    if (auto baseFunction = baseClass.search(baseClass.loc, fd.ident))
+                    MatchAccumulator mErr;
+                    functionResolve(mErr, baseFunction, loc, sc, tiargs, baseClass.type, argumentList);
+                    if (mErr.last > MATCH.nomatch && mErr.lastf)
                     {
-                        MatchAccumulator mErr;
-                        functionResolve(mErr, baseFunction, loc, sc, tiargs, baseClass.type, argumentList);
-                        if (mErr.last > MATCH.nomatch && mErr.lastf)
-                        {
-                            errorSupplemental(loc, "%s `%s` hides base class function `%s`",
-                                    fd.kind, fd.toPrettyChars(), mErr.lastf.toPrettyChars());
-                            errorSupplemental(loc, "add `alias %s = %s` to `%s`'s body to merge the overload sets",
-                                    fd.toChars(), mErr.lastf.toPrettyChars(), tthis.toChars());
-                            return null;
-                        }
+                        errorSupplemental(loc, "%s `%s` hides base class function `%s`",
+                                fd.kind, fd.toPrettyChars(), mErr.lastf.toPrettyChars());
+                        errorSupplemental(loc, "add `alias %s = %s` to `%s`'s body to merge the overload sets",
+                                fd.toChars(), mErr.lastf.toPrettyChars(), tthis.toChars());
+                        return null;
                     }
                 }
             }
         }
-
-        void errorHelper2(const(char)* failMessage) scope
-        {
-            errorSupplemental(loc, failMessage);
-        }
-
-        functionResolve(m, orig_s, loc, sc, tiargs, tthis, argumentList, &errorHelper2);
     }
+
+    void errorHelper2(const(char)* failMessage) scope
+    {
+        errorSupplemental(loc, failMessage);
+    }
+
+    functionResolve(m, orig_s, loc, sc, tiargs, tthis, argumentList, &errorHelper2);
+
     return null;
 }
 
