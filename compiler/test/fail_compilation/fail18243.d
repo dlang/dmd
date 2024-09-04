913e9f537fcf88253e3caa7e4b6802f82ca9b42a
Nick Treleaven <ntrel002@gmail.com>
2024-09-04 15:19:30 +0100

    Fix listing candidates when no overloads match from different modules

--- compiler/test/fail_compilation/fail18243.d
+++ compiler/test/fail_compilation/fail18243.d
@@ -2,7 +2,9 @@
 EXTRA_FILES: imports/a18243.d
 TEST_OUTPUT:
 ---
-fail_compilation/fail18243.d(15): Error: none of the overloads of `isNaN` are callable using argument types `!()(float)`
+fail_compilation/fail18243.d(17): Error: none of the overloads of `isNaN` are callable using argument types `!()(float)`
+/home/nick/git/dmd/compiler/test/../../../phobos/std/math/traits.d(31):        Candidates are: `isNaN(X)(X x)`
+fail_compilation/imports/a18243.d(5):                        `a18243.isNaN()`
 ---
 */
 
