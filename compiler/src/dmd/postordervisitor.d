1b8e75743cb964b3e47abc01461df3f48ccf1ac9
Nicholas Wilson <iamthewilsonator@hotmail.com>
2024-09-23 15:03:36 +0800

    merge `sapply.d` into `postordervisitor.d`
    `walkPostorder` forms an overload set on `Expression`s and `Statement`s.

--- compiler/src/dmd/postordervisitor.d
+++ compiler/src/dmd/postordervisitor.d
@@ -1,5 +1,5 @@
 /**
- * A depth-first visitor for expressions.
+ * A depth-first visitor for expressions and statements.
  *
  * Copyright:   Copyright (C) 1999-2024 by The D Language Foundation, All Rights Reserved
  * Authors:     $(LINK2 https://www.digitalmars.com, Walter Bright)
@@ -14,6 +14,7 @@ module dmd.postordervisitor;
 import dmd.dtemplate;
 import dmd.expression;
 import dmd.root.array;
+import dmd.statement;
 import dmd.visitor;
 
 bool walkPostorder(Expression e, StoppableVisitor v)
@@ -23,6 +24,14 @@ bool walkPostorder(Expression e, StoppableVisitor v)
     return v.stop;
 }
 
+bool walkPostorder(Statement s, StoppableVisitor v)
+{
+    scope PostorderStatementVisitor pv = new PostorderStatementVisitor(v);
+    s.accept(pv);
+    return v.stop;
+}
+
+private:
 /**************************************
  * An Expression tree walker that will visit each Expression e in the tree,
  * in depth-first evaluation order, and call fp(e,param) on it.
@@ -33,7 +42,7 @@ bool walkPostorder(Expression e, StoppableVisitor v)
  * It's a bit slower than using virtual functions, but more encapsulated and less brittle.
  * Creating an iterator for this would be much more complex.
  */
-private extern (C++) final class PostorderExpressionVisitor : StoppableVisitor
+extern (C++) final class PostorderExpressionVisitor : StoppableVisitor
 {
     alias visit = typeof(super).visit;
 public:
@@ -148,5 +157,162 @@ public:
     override void visit(CondExp e)
     {
         doCond(e.econd) || doCond(e.e1) || doCond(e.e2) || applyTo(e);
+    }
+}
+
+/**************************************
+ * A Statement tree walker that will visit each Statement s in the tree,
+ * in depth-first evaluation order, and call fp(s,param) on it.
+ * fp() signals whether the walking continues with its return value:
+ * Returns:
+ *      0       continue
+ *      1       done
+ * It's a bit slower than using virtual functions, but more encapsulated and less brittle.
+ * Creating an iterator for this would be much more complex.
+ */
+extern (C++) final class PostorderStatementVisitor : StoppableVisitor
+{
+    alias visit = typeof(super).visit;
+public:
+    StoppableVisitor v;
+
+    extern (D) this(StoppableVisitor v) scope @safe
+    {
+        this.v = v;
+    }
+
+    bool doCond(Statement s)
+    {
+        if (!stop && s)
+            s.accept(this);
+        return stop;
+    }
+
+    bool applyTo(Statement s)
+    {
+        s.accept(v);
+        stop = v.stop;
+        return true;
+    }
+
+    override void visit(Statement s)
+    {
+        applyTo(s);
+    }
+
+    override void visit(PeelStatement s)
+    {
+        doCond(s.s) || applyTo(s);
+    }
+
+    override void visit(CompoundStatement s)
+    {
+        for (size_t i = 0; i < s.statements.length; i++)
+            if (doCond((*s.statements)[i]))
+                return;
+        applyTo(s);
+    }
+
+    override void visit(UnrolledLoopStatement s)
+    {
+        for (size_t i = 0; i < s.statements.length; i++)
+            if (doCond((*s.statements)[i]))
+                return;
+        applyTo(s);
+    }
+
+    override void visit(ScopeStatement s)
+    {
+        doCond(s.statement) || applyTo(s);
+    }
+
+    override void visit(WhileStatement s)
+    {
+        doCond(s._body) || applyTo(s);
+    }
+
+    override void visit(DoStatement s)
+    {
+        doCond(s._body) || applyTo(s);
+    }
+
+    override void visit(ForStatement s)
+    {
+        doCond(s._init) || doCond(s._body) || applyTo(s);
+    }
+
+    override void visit(ForeachStatement s)
+    {
+        doCond(s._body) || applyTo(s);
+    }
+
+    override void visit(ForeachRangeStatement s)
+    {
+        doCond(s._body) || applyTo(s);
+    }
+
+    override void visit(IfStatement s)
+    {
+        doCond(s.ifbody) || doCond(s.elsebody) || applyTo(s);
+    }
+
+    override void visit(PragmaStatement s)
+    {
+        doCond(s._body) || applyTo(s);
+    }
+
+    override void visit(SwitchStatement s)
+    {
+        doCond(s._body) || applyTo(s);
+    }
+
+    override void visit(CaseStatement s)
+    {
+        doCond(s.statement) || applyTo(s);
+    }
+
+    override void visit(DefaultStatement s)
+    {
+        doCond(s.statement) || applyTo(s);
+    }
+
+    override void visit(SynchronizedStatement s)
+    {
+        doCond(s._body) || applyTo(s);
+    }
+
+    override void visit(WithStatement s)
+    {
+        doCond(s._body) || applyTo(s);
+    }
+
+    override void visit(TryCatchStatement s)
+    {
+        if (doCond(s._body))
+            return;
+        for (size_t i = 0; i < s.catches.length; i++)
+            if (doCond((*s.catches)[i].handler))
+                return;
+        applyTo(s);
+    }
+
+    override void visit(TryFinallyStatement s)
+    {
+        doCond(s._body) || doCond(s.finalbody) || applyTo(s);
+    }
+
+    override void visit(ScopeGuardStatement s)
+    {
+        doCond(s.statement) || applyTo(s);
+    }
+
+    override void visit(DebugStatement s)
+    {
+        doCond(s.statement) || applyTo(s);
+    }
+
+    override void visit(LabelStatement s)
+    {
+        doCond(s.statement) || applyTo(s);
     }
 }
