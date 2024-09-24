da4505899743c003784b008299ca7e73b1c129ff
Nick Treleaven <ntrel002@gmail.com>
2024-09-24 16:20:05 +0100

    Fix checking first member of union after zero size field

--- compiler/test/compilable/isZeroInit.d
+++ compiler/test/compilable/isZeroInit.d
@@ -103,7 +103,6 @@ union U
     float x = 0;
     float y;
 }
-
 static assert(__traits(isZeroInit, U));
 
 union U2
@@ -111,5 +110,13 @@ union U2
     float x;
     int y;
 }
-
 static assert(!__traits(isZeroInit, U2));
+
+struct S8 {
+    int[0] dummy; // same offset as anon union, but doesn't overlap; should be ignored anyway for zero-init check
+    union {
+        float f; // is the first member of the anon union and must be checked
+        int i;
+    }
+}
+static assert(!__traits(isZeroInit, S8));
