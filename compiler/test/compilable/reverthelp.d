6a7bd45a8e103fb84e3728b01e6db12b738e2d23
Paul Backus <snarwin@gmail.com>
2024-12-08 12:40:20 -0500

    Enable -preview=fieldwise by default
    Introduced in PR #9331, this was changed from a normal bug fix to a
    -preview switch because enabling it triggered a suprious "statement is
    not reachable" warning in Phobos. That warning has since been removed
    (in PR #15568).

--- compiler/test/compilable/reverthelp.d
+++ compiler/test/compilable/reverthelp.d
@@ -9,5 +9,6 @@ Revertable language changes listed by -revert=name:
   =dip1000          revert DIP1000 changes (Scoped Pointers) (https://github.com/dlang/DIPs/blob/master/DIPs/other/DIP1000.md)
   =intpromote       revert integral promotions for unary + - ~ operators
   =dtorfields       don't destruct fields of partially constructed objects
+  =fieldwise        don't use fieldwise comparisons for struct equality (https://dlang.org/changelog/2.085.0.html#no-cmpsb)
 ----
 */
