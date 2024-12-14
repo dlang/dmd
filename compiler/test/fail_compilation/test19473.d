3b53cf8937f6ea9eda2c3a56c10620252b6c5948
Nicholas Wilson <thewilsonator@users.noreply.github.com>
2024-12-14 10:23:42 +0800

    Fix #18235
    Co-Authored-By: Nick Treleaven <ntrel002@gmail.com>

--- compiler/test/fail_compilation/test19473.d
+++ compiler/test/fail_compilation/test19473.d
@@ -1,6 +1,7 @@
 /* TEST_OUTPUT:
 ---
-fail_compilation/test19473.d(14): Error: union `test19473.P` no size because of forward reference
+fail_compilation/test19473.d(15): Error: union `test19473.P` no size because of forward reference
+fail_compilation/test19473.d(30):        error on member `test19473.P.p`
 ---
  */
 
