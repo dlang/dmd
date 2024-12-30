eed148497d6aeb9f5eaf3081a41098de4abd64ce
Nicholas Wilson <thewilsonator@users.noreply.github.com>
2024-12-30 12:50:52 +0800

    Update run.d

--- compiler/test/run.d
+++ compiler/test/run.d
@@ -199,7 +199,7 @@ Options:
             {
                 const string name = target.filename
                             ? target.normalizedTestName
-                            : "`unit` tests: " ~ unitTestRunnerCommand ~ target.args;
+                            : "`unit` tests: " ~ unitTestRunnerCommand.join(target.args, " ");
 
                 writeln(">>> TARGET FAILED: ", name);
                 synchronized failedTargets ~= name;
