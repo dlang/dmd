7ce745bfb5c0b7283e900f54796e29aa3d2ec450
bangbangsheshotmedown <apocalypse.praise@gmail.com>
2025-01-08 09:50:01 +0100

    Remove trailing space from test

--- compiler/test/runnable/switch.d
+++ compiler/test/runnable/switch.d
@@ -15,21 +15,21 @@ int main(string[] args)
     enum K = 42;
     switch (K)
     {
-        case 0  -> printf("A: no0\n");
+        case 0 -> printf("A: no0\n");
         case 42 -> printf("A: yes\n");
-        case 1  -> printf("A: no1\n");
+        case 1 -> printf("A: no1\n");
 
         default -> printf("A: default\n");
     }
     switch (K)
     {
-        case 0  -> { 
+        case 0 -> {
             printf("B: no0\n");
         }
         case 42 -> {
             printf("B: yes\n");
         }
-        case 1  -> {
+        case 1 -> {
             printf("B: no1\n");
         }
 
@@ -39,9 +39,9 @@ int main(string[] args)
     }
     switch (K)
     {
-        case 0:  printf("C: no0\n"); break;
+        case 0: printf("C: no0\n"); break;
         case 42: printf("C: yes\n"); break;
-        case 1:  printf("C: no1\n"); break;
+        case 1: printf("C: no1\n"); break;
 
         default: printf("C: default\n");
     }
