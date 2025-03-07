636ec2e18a3d5d079ef93945e8ae286b28ccc310
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-03-07 15:47:34 +0300

    RISC-V: Fiber native switch context implemented

--- druntime/src/core/thread/fiber/package.d
+++ druntime/src/core/thread/fiber/package.d
@@ -126,6 +126,16 @@ package
             version = AsmExternal;
         }
     }
+    else version (RISCV32)
+    {
+        version = RISCV_Any;
+        version = AsmExternal;
+    }
+    else version (RISCV64)
+    {
+        version = RISCV_Any;
+        version = AsmExternal;
+    }
     else version (SPARC)
     {
         // NOTE: The SPARC ABI specifies only doubleword alignment.
@@ -180,6 +190,13 @@ package
         extern (C) void fiber_switchContext( void** oldp, void* newp ) nothrow @nogc;
         version (AArch64)
             extern (C) void fiber_trampoline() nothrow;
+        version (RISCV_Any)
+        {
+            // External asm stack  initialization is used to support different register
+            // storage sizes that the D compiler does not know about
+            extern (C) void* fiber_initStack(void* stack, void* entry) nothrow @nogc;
+            extern (C) void fiber_trampoline() nothrow;
+        }
     }
     else
     extern (C) void fiber_switchContext( void** oldp, void* newp ) nothrow @nogc
@@ -362,6 +379,13 @@ package
                 pop RCX;
                 jmp RCX;
             }
+        }
+        else version (RISCV_Any)
+        {
+            version (StackGrowsDown) {}
+            else static assert(false, "RISC-V only supports decrementing stacks");
+
+            pstack = fiber_initStack(pstack, &fiber_trampoline);
         }
         else static if ( __traits( compiles, ucontext_t ) )
         {
