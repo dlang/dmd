c3a111c2c82857c983c6f59db7f54c08351fa326
Dennis Korpel <dkorpel@gmail.com>
2025-03-15 01:01:40 +0100

    WIPWIP

---
+++ compiler/src/dmd/lsp.d
@@ -0,0 +1,20 @@
+/**
+Language Server Protocol
+*/
+
+import dmd.expression;
+import dmd.location;
+import dmd.visitor;
+
+class GotoVisitor : StoppableVisitor
+{
+    override void visit(Expression e)
+    {
+
+    }
+}
+
+void check()
+{
+
+}
