ee7bf9703a35e71c419036fb59ecd0ad399fbf03
Nayaab Zameer <nybzmr02@gmail.com>
2025-03-19 09:21:09 +0530

    Remove RTLSYM for Translation PR #15299: Removed NEWARRAYT, NEWARRAYIT, TRACENEWARRAYT and TRACENEWARRAYIT

--- compiler/src/dmd/backend/drtlsym.d
+++ compiler/src/dmd/backend/drtlsym.d
@@ -105,8 +105,6 @@ Symbol* getRtlsym(RTLSYM i) @trusted
         case RTLSYM.MEMSETN:                symbolz(ps,FL.func,FREGSAVED,"_memsetn",  0, t); break;
         case RTLSYM.NEWCLASS:               symbolz(ps,FL.func,FREGSAVED,"_d_newclass", 0, t); break;
         case RTLSYM.NEWTHROW:               symbolz(ps,FL.func,FREGSAVED,"_d_newThrowable", 0, t); break;
-        case RTLSYM.NEWARRAYT:              symbolz(ps,FL.func,FREGSAVED,"_d_newarrayT", 0, t); break;
-        case RTLSYM.NEWARRAYIT:             symbolz(ps,FL.func,FREGSAVED,"_d_newarrayiT", 0, t); break;
         case RTLSYM.NEWITEMT:               symbolz(ps,FL.func,FREGSAVED,"_d_newitemT", 0, t); break;
         case RTLSYM.NEWITEMIT:              symbolz(ps,FL.func,FREGSAVED,"_d_newitemiT", 0, t); break;
         case RTLSYM.ARRAYLITERALTX:         symbolz(ps,FL.func,FREGSAVED,"_d_arrayliteralTX", 0, t); break;
@@ -173,8 +171,6 @@ Symbol* getRtlsym(RTLSYM i) @trusted
         case RTLSYM.TRACE_EPI_F:            symbolz(ps,FL.func,ALLREGS|mBP|mES,"_trace_epi_f",0,tstrace); break;
 
         case RTLSYM.TRACENEWCLASS:          symbolz(ps,FL.func,FREGSAVED,"_d_newclassTrace", 0, t); break;
-        case RTLSYM.TRACENEWARRAYT:         symbolz(ps,FL.func,FREGSAVED,"_d_newarrayTTrace", 0, t); break;
-        case RTLSYM.TRACENEWARRAYIT:        symbolz(ps,FL.func,FREGSAVED,"_d_newarrayiTTrace", 0, t); break;
         case RTLSYM.TRACENEWITEMT:          symbolz(ps,FL.func,FREGSAVED,"_d_newitemTTrace", 0, t); break;
         case RTLSYM.TRACENEWITEMIT:         symbolz(ps,FL.func,FREGSAVED,"_d_newitemiTTrace", 0, t); break;
         case RTLSYM.TRACECALLFINALIZER:     symbolz(ps,FL.func,FREGSAVED,"_d_callfinalizerTrace", 0, t); break;
