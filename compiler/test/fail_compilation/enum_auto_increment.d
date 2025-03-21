33820f21ace06209813b4ada3ad1fe451d1450d8
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2025-03-21 23:00:19 +0530

    Fix #dlang18262 - Fix expected error message for enum auto-increment diagnostic

--- compiler/test/fail_compilation/enum_auto_increment.d
+++ compiler/test/fail_compilation/enum_auto_increment.d
@@ -1,10 +1,9 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/enum_auto_increment.d(18): Error: cannot automatically assign value to enum member `d` because base type `A1` is an enum; please provide an explicit value
+fail_compilation/enum_auto_increment.d(18): Error: cannot automatically assign value to enum member `enum_auto_increment.A2.d` because base type `A1` is an enum; please provide an explicit value
 ---
 */
-
 
 enum A1 : int
 {
