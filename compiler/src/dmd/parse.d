4df0875838849d895e599e61daadb9d77f07a8fb
Abhay Pratap <abhaypratap@Abhays-MacBook-Air-4.local>
2025-03-23 02:20:43 +0530

    Refactored memcpy calls to D slices

--- compiler/src/dmd/parse.d
+++ compiler/src/dmd/parse.d
@@ -2772,7 +2772,7 @@ class Parser(AST, Lexer = dmd.lexer.Lexer) : Lexer
             if (len > 0)
             {
                 docline = cast(char*)mem.xmalloc_noscan(len + 2);
-                memcpy(docline, begPtr, len);
+                docline[0 .. len] = begPtr[0 .. len];
                 docline[len] = '\n'; // Terminate all lines by LF
                 docline[len + 1] = '\0';
             }
@@ -7104,12 +7104,12 @@ class Parser(AST, Lexer = dmd.lexer.Lexer) : Lexer
 
             case TOK.colonColon:  // treat as two separate : tokens for iasmgcc
                 *ptoklist = allocateToken();
-                memcpy(*ptoklist, &token, Token.sizeof);
+                Token* ptoken = &this.token; // Use the address-of operator (&)
                 (*ptoklist).value = TOK.colon;
                 ptoklist = &(*ptoklist).next;
 
                 *ptoklist = allocateToken();
-                memcpy(*ptoklist, &token, Token.sizeof);
+                ptoken = allocateToken();
                 (*ptoklist).value = TOK.colon;
                 ptoklist = &(*ptoklist).next;
 
@@ -8263,8 +8263,8 @@ class Parser(AST, Lexer = dmd.lexer.Lexer) : Lexer
                         const len2 = token.len;
                         len = len1 + len2;
                         auto s2 = cast(char*)mem.xmalloc_noscan(len * char.sizeof);
-                        memcpy(s2, s, len1 * char.sizeof);
-                        memcpy(s2 + len1, token.ustring, len2 * char.sizeof);
+                        s2[0 .. len1] = s[0 .. len1]; // Copy len1 elements from s to s2
+                        s2[len1 .. len1 + len2] = token.ustring[0 .. len2]; // Copy len2 elements from token.ustring to s2 starting at len1
                         s = s2;
                     }
                     else
@@ -8471,7 +8471,7 @@ class Parser(AST, Lexer = dmd.lexer.Lexer) : Lexer
                         error(loc, "unexpected `(` after `%s`, inside `is` expression. Try enclosing the contents of `is` with a `typeof` expression", token.toChars());
                         nextToken();
                         Token* tempTok = peekPastParen(&token);
-                        memcpy(&token, tempTok, Token.sizeof);
+                        token = *tempTok; // Direct assignment
                         goto Lerr;
                     }
                     targ = parseType(&ident);
