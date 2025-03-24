8a237c8371e79f63fd668da52eadc9aafae0faf6
Dennis Korpel <dkorpel@gmail.com>
2025-03-24 14:03:05 +0100

    Translate `_d_arrayliteralTX` to a template

--- compiler/src/dmd/expressionsem.d
+++ compiler/src/dmd/expressionsem.d
@@ -11755,14 +11755,13 @@ private extern (C++) final class ExpressionSemanticVisitor : Visitor
         Expression ce = new CallExp(ae.loc, id, arguments);
         res = Expression.combine(eValue2, ce).expressionSemantic(sc);
         if (isArrayAssign)
-            res = Expression.combine(res, ae.e1).expressionSemantic(sc);
+            res = Expression.combine(res, ae.e1).expressionSemantic(sc).checkGC(sc);
 
         if (global.params.v.verbose)
             message("lowered   %s =>\n          %s", ae.toChars(), res.toChars());
 
         res = new LoweredAssignExp(ae, res);
         res.type = ae.type;
-        res = res.checkGC(sc);
 
         return res;
     }
@@ -12399,7 +12398,7 @@ private extern (C++) final class ExpressionSemanticVisitor : Visitor
         tiargs.push(exp.type);
         id = new DotTemplateInstanceExp(exp.loc, id, hook, tiargs);
         id = new CallExp(exp.loc, id, arguments);
-        return id.expressionSemantic(sc);
+        return id.expressionSemantic(sc).checkGC(sc);
     }
 
     void trySetCatExpLowering(Expression exp)
