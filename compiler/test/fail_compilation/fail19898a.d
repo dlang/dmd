ec2dfbea3d3276897f51414226abf6815b7fedf3
Rainer <r.sagitario@gmx.de>
2025-03-26 09:12:29 +0100

    adjust some compiler generated ids in tests

--- compiler/test/fail_compilation/fail19898a.d
+++ compiler/test/fail_compilation/fail19898a.d
@@ -2,7 +2,7 @@
 REQUIRED_ARGS: -m64
 TEST_OUTPUT:
 ---
-fail_compilation/fail19898a.d(10): Error: expression `__key2 < __limit3` of type `__vector(int[4])` does not have a boolean value
+fail_compilation/fail19898a.d(10): Error: expression `__key$n$ < __limit$n$` of type `__vector(int[4])` does not have a boolean value
 ---
 */
 void f (__vector(int[4]) n)
