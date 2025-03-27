c853ef2a8eda414c73aeabd1a590e52d3e70e751
Rainer <r.sagitario@gmx.de>
2025-03-27 09:41:06 +0100

    tweak tests

--- compiler/test/compilable/test19266.sh
+++ compiler/test/compilable/test19266.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "${OS}" == "windows" ]; then
+if [ "${OS}" == "windows" -and -not -z WSL_DISTRO_NAME ]; then
    # break out of bash to get Windows paths
    # use cmd.exe instead of cmd to work from WSL
    cmd.exe /c $(echo $DMD | tr / \\) -c \\\\.\\%CD%\\compilable\\extra-files\\test19266.d -deps=nul:
