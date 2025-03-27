0c25609ed3345744f59a2d6b8cb2fc5a1a3cdcea
Rainer <r.sagitario@gmx.de>
2025-03-27 09:41:06 +0100

    tweak tests

--- compiler/test/compilable/test19266.sh
+++ compiler/test/compilable/test19266.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "${OS}" == "windows" ]; then
+if [ "${OS}" == "windows" -and -not -z WSL_DISTRO_NAME ]; then
    # break out of bash to get Windows paths
    # use cmd.exe instead of cmd to work from WSL
    # use MSYS_NO_PATHCONV=1 to disable /c -> c:\ translation in git bash
