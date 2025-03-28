fe3e63414ef8c2b5155f0ec607f4166b3bd6bb6c
Abul Hossain Khan <140191921+abulgit@users.noreply.github.com>
2025-03-28 14:43:12 +0530

    Improve fix: #20867 ICE on final switch forward referencing its enum. (#21097)
    * Mark C enums as semantic2done to prevent segfaults in final switch
    
    * Added corresponding test case

--- compiler/src/dmd/importc.d
+++ compiler/src/dmd/importc.d
@@ -751,6 +751,8 @@ void cEnumSemantic(Scope* sc, EnumDeclaration ed)
     }
 
     ed.memtype = commonType;
-    ed.semanticRun = PASS.semanticdone;
+    // Set semantic2done to mark C enums as fully processed
+    // Prevents issues with final switch statements that reference C enums
+    ed.semanticRun = PASS.semantic2done;
     return;
 }
