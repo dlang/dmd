24facd85387f0a69dbf9c596ad49ef6f46461e19
Rainer <r.sagitario@gmx.de>
2025-03-29 07:42:19 +0100

    fix C++ interface definitions

--- compiler/src/dmd/expression.h
+++ compiler/src/dmd/expression.h
@@ -1251,12 +1251,16 @@ public:
 class InExp final : public BinExp
 {
 public:
+    Expression lowering;        // lowered druntime hook: `_d_aaIn(aa, key)`
+
     void accept(Visitor *v) override { v->visit(this); }
 };
 
 class RemoveExp final : public BinExp
 {
 public:
+    Expression lowering;        // lowered druntime hook: `_d_aaDel(aa, key)`
+
     void accept(Visitor *v) override { v->visit(this); }
 };
 
@@ -1265,6 +1269,7 @@ public:
 class EqualExp final : public BinExp
 {
 public:
+    Expression lowering;        // lowered druntime hook: `_d_aaEqual(aa1, aa2)`
     void accept(Visitor *v) override { v->visit(this); }
 };
 
