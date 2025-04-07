a66ad4f3ce6c18763d2eecc633cc311526f98835
abul <abulkhan19175@gmail.com>
2025-04-07 10:14:28 +0530

    replace \ to / for path separators

--- compiler/test/fail_compilation/fix19613.d
+++ compiler/test/fail_compilation/fix19613.d
@@ -1,9 +1,9 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation\fix19613.d(15): Error: function `fix19613.B.a` cannot override `final` function `fix19613.A.a`
-fail_compilation\fix19613.d(15): Error: function `fix19613.B.a` does not override any function
-fail_compilation\fix19613.d(15):        Did you mean to override `void fix19613.A.a(string)`?
+fail_compilation/fix19613.d(15): Error: function `fix19613.B.a` cannot override `final` function `fix19613.A.a`
+fail_compilation/fix19613.d(15): Error: function `fix19613.B.a` does not override any function
+fail_compilation/fix19613.d(15):        Did you mean to override `void fix19613.A.a(string)`?
 ---
 */
 
