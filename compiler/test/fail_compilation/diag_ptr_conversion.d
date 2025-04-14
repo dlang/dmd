6d0441dde90fcd249fb44d6834db196a17a43e06
AbhayPratap05 <abhay.pratap.2005p@gmail.com>
2025-04-14 13:20:20 +0530

    corrected line numbers for the supplemental message

--- compiler/test/fail_compilation/diag_ptr_conversion.d
+++ compiler/test/fail_compilation/diag_ptr_conversion.d
@@ -1,10 +1,10 @@
 /*
 TEST_OUTPUT:
 ---
-dmd/compiler/test/fail_compilation/diag_ptr_conversion.d(5): Error: cannot implicitly convert `const(int)*` to `int*`
-dmd/compiler/test/fail_compilation/diag_ptr_conversion.d(5):        Note: Converting const to mutable requires an explicit cast (`cast(int*)`).
-dmd/compiler/test/fail_compilation/diag_ptr_conversion.d(6): Error: cannot implicitly convert `int*` to `float*`
-dmd/compiler/test/fail_compilation/diag_ptr_conversion.d(6):        Note: Pointer types point to different base types (`int` vs `float`)
+fail_compilation/diag_ptr_conversion.d(15): Error: cannot implicitly convert `const(int)*` to `int*`
+fail_compilation/diag_ptr_conversion.d(15):        Note: Converting const to mutable requires an explicit cast (`cast(int*)`).
+fail_compilation/diag_ptr_conversion.d(16): Error: cannot implicitly convert `int*` to `float*`
+fail_compilation/diag_ptr_conversion.d(16):        Note: Pointer types point to different base types (`int` vs `float`)
 ---
 */
 
