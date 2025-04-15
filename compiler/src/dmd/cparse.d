5fd0d29211f419cb6c92891a2b20e3c0ccf85b97
drpriver <david@davidpriver.com>
2025-04-15 16:27:33 -0700

    Fix #20502 - importc: macro conflicts with struct of same name (#21234)
    Fixes https://github.com/dlang/dmd/issues/20502
    
    Similar to VarDeclarations, don't let TemplateDeclarations from
    C defines shadow a real symbol.

--- compiler/src/dmd/cparse.d
+++ compiler/src/dmd/cparse.d
@@ -6057,6 +6057,8 @@ final class CParser(AST) : Parser!AST
             //printf("addSym() %s\n", s.toChars());
             if (auto v = s.isVarDeclaration())
                 v.isCmacro(true);       // mark it as coming from a C #define
+            if (auto td = s.isTemplateDeclaration())
+                td.isCmacro = true; // mark as coming from a C #define
             /* If it's already defined, replace the earlier
              * definition
              */
