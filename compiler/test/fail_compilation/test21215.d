7bc0051d29caff83eb48cf6fe7ffcbeb4f641a41
Ay1nDas <ayandazzz@outlook.com>
2025-04-18 21:38:02 +0530

    Fixing Error msg for not a member in struct

--- compiler/test/fail_compilation/test21215.d
+++ compiler/test/fail_compilation/test21215.d
@@ -1,13 +1,13 @@
 /* TEST_OUTPUT:
 ---
-fail_compilation/test21215.d(14): Error: named arguments not allowed here
-fail_compilation/test21215.d(19): Error: named arguments not allowed here
-fail_compilation/test21215.d(23): Error: named arguments not allowed here
-fail_compilation/test21215.d(24): Error: named arguments not allowed here
-fail_compilation/test21215.d(28): Error: named arguments not allowed here
+fail_compilation/test21215.d(14): Error: `y` is not a member of `S`
+fail_compilation/test21215.d(19): Error: `xhs` is not a member of `S`, did you mean variable `xsh`?
+fail_compilation/test21215.d(31): Error: too many initializers for `S` with 1 field
+fail_compilation/test21215.d(37): Error: `y` is not a member of `S`
+fail_compilation/test21215.d(41): Error: `yashu` is not a member of `S`
 ---
 */
-struct S { int x; }
+struct S { int xsh; }
 
 void test() {
 	auto s = S(
@@ -15,16 +15,29 @@ void test() {
     1
 	);
 
-  auto s2 = S(
-    x: 1
-  );
-
   auto s3 = S(
-    x: 1,
-    y: 2
+    xhs:
+    1
   );
 
   auto s4 = S(
+    xsh: 1
+  );
+
+  auto s5 = S(
+    xsh:
+    1,
+
+    xsh:
+    1
+  );
+
+  auto s6 = S(
+    xsh: 1,
+    y: 2
+  );
+
+  auto s7 = S(
     yashu:
     
     2
