2a8651694c93d81e5365fbe62f43e0e7ab804661
Rainer <r.sagitario@gmx.de>
2025-05-18 09:52:33 +0200

    allow running compilable/test19266.sh under git bash

--- compiler/test/compilable/test19266.sh
+++ compiler/test/compilable/test19266.sh
@@ -3,5 +3,6 @@
 if [ "${OS}" == "windows" ]; then
    # break out of bash to get Windows paths
    # use cmd.exe instead of cmd to work from WSL
-   cmd.exe /c $(echo $DMD | tr / \\) -c \\\\.\\%CD%\\compilable\\extra-files\\test19266.d -deps=nul:
+   # use MSYS_NO_PATHCONV=1 to disable /c -> c:\ translation in git bash
+   MSYS_NO_PATHCONV=1 cmd.exe /c $(echo $DMD | tr / \\) -c \\\\.\\%CD%\\compilable\\extra-files\\test19266.d -deps=nul:
 fi
