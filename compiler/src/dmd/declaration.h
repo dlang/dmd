61e493d55a5a7a443dd373ed7033c3fabbd0a22a
Richard (Rikki) Andrew Cattermole <richard@cattermole.co.nz>
2025-06-30 06:33:46 +1200

    Implement PoC DFA engine for nullability and truthiness

--- compiler/src/dmd/declaration.h
+++ compiler/src/dmd/declaration.h
@@ -619,6 +619,8 @@ public:
     AttributeViolation* pureViolation;
     AttributeViolation* nothrowViolation;
 
+    void* parametersDFAInfo;
+
     // Formerly FUNCFLAGS
     uint32_t flags;
     bool purityInprocess() const;
