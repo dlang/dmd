e92053d88f8e75cc43d46ca7da3e0488a6d2a223
Richard (Rikki) Andrew Cattermole <richard@cattermole.co.nz>
2025-06-30 06:33:46 +1200

    Implement PoC DFA engine for nullability and truthiness

---
+++ compiler/src/dmd/dfa/entry.d
@@ -0,0 +1,79 @@
+module dmd.dfa.entry;
+import dmd.dfa.common;
+import dmd.dfa.fast.expression;
+import dmd.dfa.fast.statement;
+import dmd.dfa.fast.analysis;
+import dmd.func;
+import dmd.astenums;
+import dmd.globals;
+import core.stdc.stdio;
+
+void dfaEntry(FuncDeclaration fd)
+{
+    fastDFA(fd);
+}
+
+private:
+
+void fastDFA(FuncDeclaration fd)
+{
+    if (fd.skipCodegen)
+        return; // no point running DFA on code that'll never run but has already passed CTFE
+
+    //if (fd.ident.toString != "inreal") return;
+    //if (!(fd.ident.toString == "test3632" || fd.ident.toString == "test")) return;
+    //if (fd.loc.linnum < 1380) return;
+    //if (fd.getModule.ident.toString != "start") return;
+
+    version (none)
+    {
+        auto ty = fd.type.isTypeFunction;
+        assert(ty !is null);
+        if (ty.trust == TRUST.system)
+            return;
+    }
+
+    DFACommon dfaCommon;
+    scope StatementWalker stmtWalker = new StatementWalker;
+    ExpressionWalker expWalker;
+    DFAAnalyzer analyzer;
+
+    stmtWalker.dfaCommon = &dfaCommon;
+    expWalker.dfaCommon = &dfaCommon;
+    analyzer.dfaCommon = &dfaCommon;
+
+    stmtWalker.expWalker = &expWalker;
+    expWalker.stmtWalker = stmtWalker;
+
+    stmtWalker.analyzer = &analyzer;
+    expWalker.analyzer = &analyzer;
+
+    if (dfaCommon.debugStructure)
+    {
+        printf("============================== %s : %s at %s\n",
+                fd.getModule.ident.toChars, fd.toFullSignature, fd.loc.toChars);
+        fflush(stdout);
+    }
+
+    scope (exit)
+    {
+        if (dfaCommon.debugStructure)
+        {
+            printf("------------------------------ %s : %s at %s\n",
+                    fd.getModule.ident.toChars, fd.toFullSignature, fd.loc.toChars);
+            fflush(stdout);
+        }
+    }
+
+    scope (failure)
+    {
+        if (!dfaCommon.debugStructure)
+        {
+            printf("ICE: ERROR %s : %s at %s\n", fd.getModule.ident.toChars,
+                    fd.toFullSignature, fd.loc.toChars);
+            fflush(stdout);
+        }
+    }
+
+    stmtWalker.start(fd);
+}
---
+++ compiler/src/dmd/dfa/fast/report.d
@@ -0,0 +1,246 @@
+module dmd.dfa.fast.report;
+import dmd.dfa.common;
+import dmd.location;
+import dmd.func;
+import core.stdc.stdio;
+
+alias Fact = ParameterDFAInfo.Fact;
+
+void reportDereference(DFACommon* dfaCommon, DFAConsequence* on, ref Loc loc)
+{
+    if (!dfaCommon.debugUnknownAST && on is null)
+        return;
+    assert(on !is null);
+    if (on.var is null)
+        return;
+
+    bool inferNonNull()
+    {
+        if (on.writeOnVarAtThisPoint != 0 || on.var.hasBeenAsserted)
+            return false;
+        else if (on.var.param is null || on.var.param.specifiedByUser)
+            return false;
+
+        if (dfaCommon.debugIt)
+            printf("Infer variable as non-null `%s` at %s\n", on.var.var.ident.toChars, loc.toChars);
+
+        on.nullable = Nullable.NonNull;
+        on.var.param.notNullIn = ParameterDFAInfo.Fact.Guaranteed;
+        return true;
+    }
+
+    assert(on.nullable != Nullable.NonNull);
+
+    if (on.nullable == Nullable.Unknown)
+    {
+        inferNonNull;
+        return;
+    }
+    else if (on.var is null || on.var.var is null || !on.var.isModellable)
+        return;
+
+    if (!inferNonNull)
+    {
+        if (DFAScope* loopyLabel = dfaCommon.lastLoopyLabel)
+        {
+            if (on.var.declaredAtDepth < loopyLabel.depth)
+                return;
+        }
+
+        printf("Dereference on null `%s` at %s\n", on.var.var.ident.toChars, loc.toChars);
+    }
+}
+
+void reportLoopyLabelLessNullThan(DFACommon* dfaCommon, DFAVar* var, ref Loc loc)
+{
+    if (var !is null && !var.isModellable)
+        return;
+
+    printf("See less than state at %s\n", loc.toChars);
+    printf("    Due to variable `%s` was non-null and becomes null\n", var.var.ident.toChars);
+}
+
+void reportLoopyLabelAssertAndAssign(DFACommon* dfaCommon,
+        DFAScopeVar* varThatWasAssumed, DFAScopeVar* varThatAsserted, ref Loc loc)
+{
+    printf("Variable `%s` was assumed due to assert of variable `%s` which gets invalidated at %s\n",
+            varThatWasAssumed.var.var.toChars, varThatAsserted.var.var.toChars, loc.toChars);
+}
+
+void reportEndOfScope(DFACommon* dfaCommon, FuncDeclaration fd, ref Loc loc)
+{
+    // this is where we validate escapes, for a specific location
+
+    if (!dfaCommon.currentDFAScope.haveReturned)
+        return;
+
+    // validate/infer on to function
+    foreachFunctionVariable(dfaCommon, fd, (scv) {
+        if (scv.var.unmodellable)
+            return;
+
+        ParameterDFAInfo* param = scv.var.param;
+
+        DFAConsequence* cctx = scv.lr.getContext;
+
+        assert(cctx !is null);
+        assert(cctx.var is scv.var);
+
+        Fact suggestedNotNullOut = param.notNullOut;
+
+        if (suggestedNotNullOut == Fact.Guaranteed && cctx.nullable != Nullable.NonNull)
+            suggestedNotNullOut = Fact.NotGuaranteed;
+        else if (suggestedNotNullOut == Fact.Unspecified && cctx.nullable == Nullable.NonNull)
+            suggestedNotNullOut = Fact.Guaranteed;
+
+        if (param.specifiedByUser)
+        {
+            // TODO: verify attributes
+        }
+        else
+            param.notNullOut = suggestedNotNullOut;
+    });
+}
+
+void reportEndOfFunction(DFACommon* dfaCommon, FuncDeclaration fd, ref Loc loc)
+{
+    version (none)
+    {
+        printf("End of function attributes for `%s` at %s\n", fd.ident.toChars, loc.toChars);
+    }
+
+    // validate/infer on to function
+    foreachFunctionVariable(dfaCommon, fd, (scv) {
+        ParameterDFAInfo* param = scv.var.param;
+
+        version (none)
+        {
+            if (scv.var.var !is null)
+                printf("Variable %p `%s` ", scv.var, scv.var.var.ident.toChars);
+            else
+                printf("Variable %p ", scv.var);
+
+            printf("%d %d:%d %d:%d %d\n", scv.var.unmodellable, scv.var.isTruthy,
+                scv.var.isNullable, scv.var.isByRef, scv.var.writeCount, param.specifiedByUser);
+            printf("    notNullIn=%d, notNullOut=%d\n", param.notNullIn, param.notNullOut);
+        }
+
+        if (scv.var.unmodellable)
+        {
+            if (!param.specifiedByUser)
+            {
+                param.specifiedByUser = false;
+
+                if (scv.var.isNullable)
+                {
+                    param.notNullIn = Fact.Unspecified;
+                    param.notNullOut = Fact.Unspecified;
+                }
+            }
+        }
+        else if (!param.specifiedByUser)
+        {
+            if (scv.var.isNullable)
+            {
+                if (param.notNullIn == Fact.Unspecified)
+                    param.notNullIn = Fact.NotGuaranteed;
+
+                if (scv.var.isByRef)
+                {
+                    if (scv.var.writeCount > 0)
+                    {
+                        if (param.notNullOut == Fact.Unspecified)
+                            param.notNullOut = Fact.NotGuaranteed;
+                    }
+                    else
+                        param.notNullOut = param.notNullIn;
+                }
+                else
+                {
+                    param.notNullOut = Fact.Unspecified;
+                }
+            }
+            else
+            {
+                param.notNullIn = Fact.Unspecified;
+                param.notNullOut = Fact.Unspecified;
+            }
+        }
+
+        version (none)
+        {
+            printf("    notNullIn=%d, notNullOut=%d\n", cast(int) param.notNullIn, param.notNullOut);
+        }
+    });
+}
+
+void reportAssertIsFalse(DFACommon* dfaCommon, ref DFALatticeRef lr, ref const Loc loc)
+{
+    if (!lr.haveNonContext)
+        return; // ignore literals, too false positive heavy
+
+    DFAConsequence* cctx = lr.getContext;
+    if (cctx.var !is null || !lr.isModellable)
+        return;
+
+    if (DFAScope* loopyLabel = dfaCommon.lastLoopyLabel)
+    {
+        bool couldBeChanged;
+
+        lr.walkMaybeTops((c) {
+            if (c.var !is null && c.var.declaredAtDepth < loopyLabel.depth)
+            {
+                couldBeChanged = true;
+                return false;
+            }
+
+            return true;
+        });
+
+        if (couldBeChanged)
+            return;
+    }
+
+    printf("Assert is provably false at %s\n", loc.toChars);
+}
+
+void reportFunctionCallArgumentLessThan(DFACommon* dfaCommon, DFAConsequence* c,
+        ParameterDFAInfo* paramInfo, FuncDeclaration calling, ref Loc loc)
+{
+    if (!c.var.isModellable)
+        return;
+
+    printf("See less than state at %s\n", loc.toChars);
+    printf("    Due to argument expected to be non-null and was null\n");
+    printf("    Calling function `%s` at %s\n", calling.ident.toChars, calling.loc.toChars);
+}
+
+private:
+
+void foreachFunctionVariable(DFACommon* dfaCommon, FuncDeclaration fd,
+        scope void delegate(DFAScopeVar* scv) del)
+{
+    DFAVar* var;
+    DFAScopeVar* scv;
+
+    if (fd.vthis !is null)
+    {
+        var = dfaCommon.findVariable(fd.vthis);
+        scv = dfaCommon.acquireScopeVar(var);
+        del(scv);
+    }
+
+    var = dfaCommon.getReturnVariable();
+    scv = dfaCommon.acquireScopeVar(var);
+    del(scv);
+
+    if (fd.parameters !is null)
+    {
+        foreach (i, param; *fd.parameters)
+        {
+            var = dfaCommon.findVariable(param);
+            scv = dfaCommon.acquireScopeVar(var);
+            del(scv);
+        }
+    }
+}
