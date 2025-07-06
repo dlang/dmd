075f022b164289533f631071c8d5a169b33fc9de
Albert24GG <guiman.albert09@gmail.com>
2025-07-07 00:02:19 +0300

    Remove unused templates

--- druntime/src/core/internal/array/equality.d
+++ druntime/src/core/internal/array/equality.d
@@ -126,69 +126,6 @@ ref at(T)(scope T[] r, size_t i) @trusted
     assert(a1 != a2);
 }
 
-
-private:
-
-// - Recursively folds static array types to their element type,
-// - maps void to ubyte, and
-// - pointers to size_t.
-template BaseType(T)
-{
-    static if (__traits(isStaticArray, T))
-        alias BaseType = BaseType!(typeof(T.init[0]));
-    else static if (is(immutable T == immutable void))
-        alias BaseType = ubyte;
-    else static if (is(T == E*, E))
-        alias BaseType = size_t;
-    else
-        alias BaseType = T;
-}
-
-// Use memcmp if the element sizes match and both base element types are integral.
-// Due to int promotion, disallow small integers of diverging signed-ness though.
-template useMemcmp(T1, T2)
-{
-    static if (T1.sizeof != T2.sizeof)
-        enum useMemcmp = false;
-    else
-    {
-        alias B1 = BaseType!T1;
-        alias B2 = BaseType!T2;
-        enum useMemcmp = __traits(isIntegral, B1) && __traits(isIntegral, B2)
-           && !( (B1.sizeof < 4 || B2.sizeof < 4) && __traits(isUnsigned, B1) != __traits(isUnsigned, B2) );
-    }
-}
-
-unittest
-{
-    enum E { foo, bar }
-
-    static assert(useMemcmp!(byte, byte));
-    static assert(useMemcmp!(ubyte, ubyte));
-    static assert(useMemcmp!(void, const void));
-    static assert(useMemcmp!(void, immutable bool));
-    static assert(useMemcmp!(void, inout char));
-    static assert(useMemcmp!(void, shared ubyte));
-    static assert(!useMemcmp!(void, byte));       // differing signed-ness
-    static assert(!useMemcmp!(char[8], byte[8])); // ditto
-
-    static assert(useMemcmp!(short, short));
-    static assert(useMemcmp!(wchar, ushort));
-    static assert(!useMemcmp!(wchar, short)); // differing signed-ness
-
-    static assert(useMemcmp!(int, uint)); // no promotion, ignoring signed-ness
-    static assert(useMemcmp!(dchar, E));
-
-    static assert(useMemcmp!(immutable void*, size_t));
-    static assert(useMemcmp!(double*, ptrdiff_t));
-    static assert(useMemcmp!(long[2][3], const(ulong)[2][3]));
-
-    static assert(!useMemcmp!(float, float));
-    static assert(!useMemcmp!(double[2], double[2]));
-    static assert(!useMemcmp!(Object, Object));
-    static assert(!useMemcmp!(int[], int[]));
-}
-
 // https://issues.dlang.org/show_bug.cgi?id=21094
 unittest
 {
