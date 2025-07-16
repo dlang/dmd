ac7e45033f7706e55450c981ee2c318a3126ad24
Nicholas Wilson <thewilsonator@users.noreply.github.com>
2025-07-16 14:23:20 +0800

    [glue.d] make `Module.doppelganger` private (#21557)

--- compiler/src/dmd/dmodule.d
+++ compiler/src/dmd/dmodule.d
@@ -1173,7 +1173,6 @@ extern (C++) final class Module : Package
     }
 
     // Back end
-    int doppelganger; // sub-module
     Symbol* cov; // private uint[] __coverage;
     uint[] covb; // bit array of valid code line numbers
     Symbol* sictor; // module order independent constructor
