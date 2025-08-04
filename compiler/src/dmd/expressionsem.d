2742293b72bb25655bff751b2484edd90dd56857
Sergii Kuzko <gorsing444@gmail.com>
2025-08-04 17:09:43 +0300

    move-deducetype-functions-to-templatesem.d

--- compiler/src/dmd/expressionsem.d
+++ compiler/src/dmd/expressionsem.d
@@ -83,7 +83,7 @@ import dmd.semantic3;
 import dmd.sideeffect;
 import dmd.safe;
 import dmd.target;
-import dmd.templatesem : matchWithInstance;
+import dmd.templatesem : matchWithInstance, deduceType;
 import dmd.tokens;
 import dmd.traits;
 import dmd.typesem;
