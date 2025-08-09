51ea2c63e63b58c328a9b90c5b28e223ba8a29c7
Iain Buclaw <ibuclaw@gdcproject.org>
2025-08-09 11:17:05 +0200

    dmd.dmodule: Remove unused Module.find function (#21687)

--- compiler/src/dmd/frontend.h
+++ compiler/src/dmd/frontend.h
@@ -7221,7 +7221,6 @@ public:
     size_t nameoffset;
     size_t namelen;
     static Module* create(const char* filename, Identifier* ident, int32_t doDocComment, int32_t doHdrGen);
-    static const char* find(const char* filename);
     static Module* load(Loc loc, Array<Identifier* >* packages, Identifier* ident);
     const char* kind() const override;
     bool read(Loc loc);
