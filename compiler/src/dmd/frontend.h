10feb25d7781031557f26c00337ff6841b30b80c
Nicholas Wilson <iamthewilsonator@hotmail.com>
2025-08-14 08:37:51 +0800

    [func.d] move `getLevel`, `needsClosure` and `checkEscapingSiblings`
    to `funcsem.d`

--- compiler/src/dmd/frontend.h
+++ compiler/src/dmd/frontend.h
@@ -4066,7 +4066,6 @@ public:
     bool overloadInsert(Dsymbol* s) override;
     bool inUnittest();
     LabelDsymbol* searchLabel(Identifier* ident, Loc loc);
-    enum : int32_t { LevelError = -2 };
 
     const char* toPrettyChars(bool QualifyTypes = false) override;
     const char* toFullSignature();
@@ -4089,7 +4088,6 @@ public:
     virtual bool addPreInvariant();
     virtual bool addPostInvariant();
     const char* kind() const override;
-    bool needsClosure();
     bool hasNestedFrameRefs();
     ParameterList getParameterList();
     virtual FuncDeclaration* toAliasFunc();
