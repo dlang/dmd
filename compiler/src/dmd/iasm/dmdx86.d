ee09195ab24804cc37e5a37ca3cba0e9dacc6620
Dennis Korpel <dkorpel@gmail.com>
2025-09-05 14:12:36 +0200

    Shrink oversized static arrays

--- compiler/src/dmd/iasm/dmdx86.d
+++ compiler/src/dmd/iasm/dmdx86.d
@@ -539,7 +539,7 @@ enum // 64 bit only registers
     _RIP = 0xFF,   // some unique value
 }
 
-immutable REG[65] regtab64 =
+immutable REG[62] regtab64 =
 [
     {"RAX",  _RAX,   _r64 | _rax},
     {"RBX",  _RBX,   _r64},
