b2de805ee63b9fbae4fc4a2f78ec06a5055e1cbe
Mohamed El Shorbagy <mohrizq895@gmail.com>
2025-09-18 05:17:52 +0300

    Revert "Try dmd::equals"
    This reverts commit af8bf8cdda5723d80ebbbb2438051858d5bf1a56.

--- compiler/src/tests/cxxfrontend.cc
+++ compiler/src/tests/cxxfrontend.cc
@@ -1332,7 +1332,7 @@ public:
                 if (j == i)
                     continue;
                 FuncDeclaration *fd2 = d->vtbl[j]->isFuncDeclaration();
-                if (!dmd::equals((const Dsymbol*) fd2->ident, (const Dsymbol*) fd->ident))
+                if (!fd2->ident->equals(fd->ident))
                     continue;
                 if (fd2->isFuture())
                     continue;
