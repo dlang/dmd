6fa81a1db0aa9dff6747d8b3636293b8ca6f715b
Mohamed El Shorbagy <mohrizq895@gmail.com>
2025-09-20 01:59:27 +0300

    Expose funcs to dmd namespace

--- compiler/src/dmd/dsymbol.h
+++ compiler/src/dmd/dsymbol.h
@@ -428,4 +428,11 @@ namespace dmd
     bool hasPointers(Dsymbol *d);
     Type *getType(Dsymbol *d);
     uinteger_t size(Dsymbol *ds, Loc loc);
+    void semantic3OnDependencies(Module* m);
+    void addDeferredSemantic(Dsymbol* s);
+    void addDeferredSemantic2(Dsymbol* s);
+    void addDeferredSemantic3(Dsymbol* s);
+    void runDeferredSemantic();
+    void runDeferredSemantic2();
+    void runDeferredSemantic3();
 }
