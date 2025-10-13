601745068e6497378670126a3c55f747a92b76df
Richard (Rikki) Andrew Cattermole <richard@cattermole.co.nz>
2025-10-13 17:45:00 +1300

    Fix null dereference in expressionsem (#21964)

--- compiler/src/dmd/expressionsem.d
+++ compiler/src/dmd/expressionsem.d
@@ -4528,7 +4528,11 @@ private extern (C++) final class ExpressionSemanticVisitor : Visitor
                 setError();
             }
         }
-        scope (exit) result.rvalue = exp.rvalue;
+        scope (exit)
+        {
+            if (result !is null)
+                result.rvalue = exp.rvalue;
+        }
 
         Dsymbol scopesym;
         Dsymbol s = sc.search(exp.loc, exp.ident, scopesym);
@@ -6620,7 +6624,7 @@ private extern (C++) final class ExpressionSemanticVisitor : Visitor
                     {
                         setError();
                     }
-                    else
+                    else if (result !is null)
                     {
                         result.rvalue = true;
                     }
