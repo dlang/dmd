bee5c7d414348d86d8185a05b78f4cddc66f4d32
Richard (Rikki) Andrew Cattermole <richard@cattermole.co.nz>
2025-10-13 16:56:20 +1300

    PoC DFA engine for nullability and truthiness

--- compiler/src/dmd/globals.d
+++ compiler/src/dmd/globals.d
@@ -216,6 +216,7 @@ extern (C++) struct Param
     FeatureState dtorFields;     // destruct fields of partially constructed objects
                                  // https://issues.dlang.org/show_bug.cgi?id=14246
     FeatureState systemVariables; // limit access to variables marked @system from @safe code
+    bool useFastDFA;                 // Use fast data flow analysis engine
 
     CHECKENABLE useInvariants  = CHECKENABLE._default;  // generate class invariant checks
     CHECKENABLE useIn          = CHECKENABLE._default;  // generate precondition checks
