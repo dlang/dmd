cccfcefb9b6ac20ab898f18e13a02d87b00c59e0
Richard (Rikki) Andrew Cattermole <richard@cattermole.co.nz>
2025-10-14 21:12:05 +1300

    Disable rewriting of finally statements to sequences unless requested

--- compiler/src/dmd/globals.h
+++ compiler/src/dmd/globals.h
@@ -198,6 +198,7 @@ struct Param
     d_bool addMain;       // add a default main() function
     d_bool allInst;       // generate code for all template instantiations
     d_bool bitfields;         // support C style bit fields
+    d_bool rewriteNoExceptionToSeq;
     CppStdRevision cplusplus;  // version of C++ name mangling to support
 
     Help help;
