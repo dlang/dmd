a313d4f885e042465f7fac968c218cd18aa57890
Emmankoko <emmankoko519@gmail.com>
2025-10-17 11:09:40 +0000

    best possible way to test now. this currently errs on
    duplicate symbol '_a' in:
        file.o
    duplicate symbol '_a' in:
        file.o

--- compiler/test/runnable/fix21925.c
+++ compiler/test/runnable/fix21925.c
@@ -1,12 +1,11 @@
 //importC: symbol duplication in symbol table for global redeclared symbols #21925
 #include <assert.h>
 int a;
-/*int a;
 int a;
 int a;
 int a;
 int a;
-*/
+int a;
 
 int main()
 {
