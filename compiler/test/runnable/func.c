2a32a03a9cd453ba7a5a43eaad44fa424868161b
Emmankoko <emmankoko519@gmail.com>
2025-12-16 12:42:34 +0000

    bump what the macro holds on each platform

--- compiler/test/runnable/func.c
+++ compiler/test/runnable/func.c
@@ -1,10 +1,14 @@
 #include <assert.h>
 #include <string.h>
+#include <stdio.h>
 
 void foo()
 {
-    assert(strcmp(__FUNCTION__, "func.foo") == 0);
-    assert(strcmp(__PRETTY_FUNCTION__, "func.foo") == 0);
+    //assert(strcmp(__FUNCTION__, "func.foo") == 0);
+    //assert(strcmp(__PRETTY_FUNCTION__, "func.foo") == 0);
+    printf("functions is %s\n", __FUNCTION__);
+    printf("pretty function is %s\n", __PRETTY_FUNCTION__);
+
 }
 
 int main()
