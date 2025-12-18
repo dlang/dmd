f4f29432cc05d6b534675cd52b5b1bb639fa17bc
Emmankoko <emmankoko519@gmail.com>
2025-12-18 20:56:12 +0000

    check platform differences

--- compiler/test/runnable/func.c
+++ compiler/test/runnable/func.c
@@ -5,14 +5,23 @@
 void foo()
 {
     assert(strcmp(__FUNCTION__, "foo") == 0);
+
+#ifndef __linux__
     assert(strcmp(__PRETTY_FUNCTION__, "void foo()") == 0);
+#else
+    assert(strcmp(__PRETTY_FUNCTION__, "foo") == 0);
+#endif
 
 }
 
 int bar(int a, int b)
 {
     assert(strcmp(__FUNCTION__, "bar") == 0);
+#ifndef __linux__
     assert(strcmp(__PRETTY_FUNCTION__, "int bar(int, int)") == 0);
+#else
+    assert(strcmp(__PRETTY_FUNCTION__, "bar") == 0);
+#endif
     return 0;
 }
 
