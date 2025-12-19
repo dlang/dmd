33da719aee2109b9b691aa20efc4571f140b856b
Emmankoko <emmankoko519@gmail.com>
2025-12-19 12:29:03 +0000

    check issue #22259.c

---
+++ compiler/test/runnable/testc22259.c
@@ -0,0 +1,20 @@
+// https://github.com/dlang/dmd/issues/22259
+
+#include <complex.h>
+#include <stdio.h>
+#include <assert.h>
+
+void foo()
+{
+    _Complex double x = 1 + 1.0if;
+    assert(creal(x) == 1.000000);
+    assert(cimag(x) == 1.000000);
+    return;
+}
+
+
+int main()
+{
+    foo();
+    return 0;
+}
