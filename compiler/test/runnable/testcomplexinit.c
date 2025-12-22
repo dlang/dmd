30ed42cdd45646b2eb6942720b3fc27c6bd47ca7
Emmankoko <emmankoko519@gmail.com>
2025-12-22 21:04:03 +0000

    make new complex structure work with Cinitializer
    using the fundamental complex interferes with this a little when
    compiling s7 library.this fixes it.

---
+++ compiler/test/runnable/testcomplexinit.c
@@ -0,0 +1,27 @@
+// DISABLED: win32 win64
+
+#include <complex.h>
+#include <stdio.h>
+
+struct com
+{
+    int r;
+    int c;
+};
+
+void foo()
+{
+    struct com obj = { 1, 3};
+    _Complex double x = { obj.r, obj.c}; //real + im;
+
+    assert(creal(x) == 1.000000);
+    assert(cimag(x) == 3.000000);
+    return;
+}
+
+
+int main()
+{
+    foo();
+    return 0;
+}
