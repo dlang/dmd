f28104c351c076eb2694e229d38a975a0eb4b2ec
Iskaban10 <janasourish197@gmail.com>
2025-12-23 15:57:06 +0530

    fix #21381 for correct location in error message

--- compiler/src/dmd/typesem.d
+++ compiler/src/dmd/typesem.d
@@ -961,12 +961,12 @@ private void resolveHelper(TypeQualified mt, Loc loc, Scope* sc, Dsymbol s, Dsym
             if (const n = importHint(id.toString()))
                 error(loc, "`%s` is not defined, perhaps `import %.*s;` ?", p, cast(int)n.length, n.ptr);
             else if (auto s2 = sc.search_correct(id))
-                error(loc, "undefined identifier `%s`, did you mean %s `%s`?", p, s2.kind(), s2.toChars());
+                error(mt.loc, "undefined identifier `%s`, did you mean %s `%s`?", p, s2.kind(), s2.toChars());
             else if (const q = Scope.search_correct_C(id))
-                error(loc, "undefined identifier `%s`, did you mean `%s`?", p, q);
+                error(mt.loc, "undefined identifier `%s`, did you mean `%s`?", p, q);
             else if ((id == Id.This   && sc.getStructClassScope()) ||
                      (id == Id._super && sc.getClassScope()))
-                error(loc, "undefined identifier `%s`, did you mean `typeof(%s)`?", p, p);
+                error(mt.loc, "undefined identifier `%s`, did you mean `typeof(%s)`?", p, p);
             else
                 error(mt.loc, "undefined identifier `%s`", p);
         }
---
+++ compiler/test/fail_compilation/diag21381.d
@@ -0,0 +1,16 @@
+// https://github.com/dlang/dmd/issues/21381
+
+/*
+TEST_OUTPUT:
+---
+fail_compilation/diag21381.d(16): Error: undefined identifier `xyx`, did you mean struct `xyz`?
+---
+*/
+
+enum plusOne(int x) = x + 1;
+struct xyz{}
+
+alias PlusOne =
+  plusOne
+  !
+  xyx;
