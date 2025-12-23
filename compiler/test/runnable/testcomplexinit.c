250556f18ee13a2b9ec7ffeeb412bff10ac4828b
Emmankoko <emmankoko519@gmail.com>
2025-12-23 08:37:58 +0000

    no functional change intended

--- compiler/test/runnable/testcomplexinit.c
+++ compiler/test/runnable/testcomplexinit.c
@@ -13,7 +13,7 @@ struct com
 void foo()
 {
     struct com obj = { 1, 3};
-    _Complex double x = { obj.r, obj.c}; //real + im;
+    _Complex double x = { obj.r, obj.c}; //real + im*i;
 
     assert(creal(x) == 1.000000);
     assert(cimag(x) == 3.000000);
