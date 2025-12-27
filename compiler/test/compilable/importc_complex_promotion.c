5f4fde8c70cebd2111275b42c66876372fb9c69e
Aryan Dadwal <aryandadwal2006@gmail.com>
2025-12-27 15:42:59 +0530

    Update importc_complex_promotion.c

--- compiler/test/compilable/importc_complex_promotion.c
+++ compiler/test/compilable/importc_complex_promotion.c
@@ -1,23 +1,17 @@
 // Ensure complex float can be implicitly promoted to complex double
 // Fixes issue #22259
-
 #include <complex.h>
-
 void testComplexPromotion()
 {
     // Basic case from issue #22259
     _Complex double x = 1 + 1.0if;
-    
     // Float imaginary with int
     _Complex double y = 2 + 3.0if;
-    
     // Two float complex values
     _Complex double z = 1.0if + 2.0if;
-    
     // Mixed real and float imaginary
     _Complex double w = 5.0 + 1.0if;
 }
-
 void testComplexPreservation()
 {
     // These should still work (no change)
@@ -26,7 +20,6 @@ void testComplexPreservation()
     _Complex double d1 = 1.0 + 2.0i;
     _Complex double d2 = 2.0 + 3.0i;
 }
-
 int main()
 {
     testComplexPromotion();
