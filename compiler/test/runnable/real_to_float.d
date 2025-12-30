ff8e431ad1327e187569d6bee10d9735223495e4
Eugene 'Vindex' Stulin <tech.vindex@gmail.com>
2025-12-30 07:41:25 -0600

    Change test by Rainer Schuetze

--- compiler/test/runnable/real_to_float.d
+++ compiler/test/runnable/real_to_float.d
@@ -3,15 +3,26 @@
  * "converting real to float uses double rounding for 64-bit code
  * causing unexpected results"
  */
+pragma(inline, false)
+void test(real r)
+{
+    assert(r == 0x1.000002fffffffcp-1);
+    double d = r;
+    assert(d == 0x1.000003p-1);
+    float f = r;
+    assert(f == 0x1.000002p-1);
+    float fd = d;
+    assert(fd == 0x1.000004p-1);
+    real rd = d;
+    assert(rd == 0x1.000003p-1);
+    float frd = rd;
+    assert(frd == 0x1.000004p-1);
+}
+
 void main()
 {
     static if (real.sizeof > 8)
     {
-        real r = 0.5000000894069671353303618843710864894092082977294921875;
-        assert(r == 0x1.000002fffffffcp-1);
-        double d = r;
-        assert(d == 0x1.000003p-1);
-        float f = r;
-        assert(f == 0x1.000002p-1);
+        test(0x1.000002fffffffcp-1);
     }
 }
