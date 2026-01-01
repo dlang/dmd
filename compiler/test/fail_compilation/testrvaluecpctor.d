2d075f00ef0d31d9f44e45d5d04bf7078e7d6056
Iskaban10 <janasourish197@gmail.com>
2026-01-01 18:56:59 +0530

    Updated test

--- compiler/test/fail_compilation/testrvaluecpctor.d
+++ compiler/test/fail_compilation/testrvaluecpctor.d
@@ -3,14 +3,11 @@
 /*
 TEST_OUTPUT:
 ---
-/*
-TEST_OUTPUT:
----
-fail_compilation/testrvaluecpctor.d(19): Error: cannot define both an rvalue constructor and a copy constructor for `struct Foo`
-fail_compilation/testrvaluecpctor.d(27):        Template instance `testrvaluecpctor.Foo!int.Foo.this!(immutable(Foo!int), immutable(Foo!int))` creates an rvalue constructor for `struct Foo`
-fail_compilation/testrvaluecpctor.d(27): Error: none of the overloads of `this` can construct an immutable object with argument types `(immutable(Foo!int))`. Expected `immutable(immutable(Foo!int))`
-fail_compilation/testrvaluecpctor.d(21):        Candidate 1 is: `testrvaluecpctor.Foo!int.Foo.this(ref scope Foo!int rhs)`
-fail_compilation/testrvaluecpctor.d(19):        Candidate 2 is: `this(Rhs, this This)(scope Rhs rhs)`
+fail_compilation/testrvaluecpctor.d(16): Error: cannot define both an rvalue constructor and a copy constructor for `struct Foo`
+fail_compilation/testrvaluecpctor.d(24):        Template instance `testrvaluecpctor.Foo!int.Foo.this!(immutable(Foo!int), immutable(Foo!int))` creates an rvalue constructor for `struct Foo`
+fail_compilation/testrvaluecpctor.d(24): Error: none of the overloads of `this` can construct an immutable object with argument types `(immutable(Foo!int))`. Expected `immutable(immutable(Foo!int))`
+fail_compilation/testrvaluecpctor.d(18):        Candidate 1 is: `testrvaluecpctor.Foo!int.Foo.this(ref scope Foo!int rhs)`
+fail_compilation/testrvaluecpctor.d(16):        Candidate 2 is: `this(Rhs, this This)(scope Rhs rhs)`
 ---
 */
 
@@ -32,8 +29,8 @@ void fail22593()
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/testrvaluecpctor.d(43): Error: cannot define both an rvalue constructor and a copy constructor for `struct Test`
-fail_compilation/testrvaluecpctor.d(49):        Template instance `testrvaluecpctor.Test.this!()` creates an rvalue constructor for `struct Test`
+fail_compilation/testrvaluecpctor.d(40): Error: cannot define both an rvalue constructor and a copy constructor for `struct Test`
+fail_compilation/testrvaluecpctor.d(46):        Template instance `testrvaluecpctor.Test.this!()` creates an rvalue constructor for `struct Test`
 ---
 */
 
