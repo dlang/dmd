f8151af0b57d24524825ec26866b036d2689f38b
Iskaban10 <janasourish197@gmail.com>
2026-01-02 17:00:29 +0530

    loc changed to mt.loc

--- compiler/src/dmd/typesem.d
+++ compiler/src/dmd/typesem.d
@@ -1514,24 +1514,24 @@ private void resolveHelper(TypeQualified mt, Loc loc, Scope* sc, Dsymbol s, Dsym
         if (!(sc && sc.inCfile))
         {
             if (const n = importHint(id.toString()))
-                error(loc, "`%s` is not defined, perhaps `import %.*s;` ?", p, cast(int)n.length, n.ptr);
+                error(mt.loc, "`%s` is not defined, perhaps `import %.*s;` ?", p, cast(int)n.length, n.ptr);
             else if (auto s2 = sc.search_correct(id))
                 error(mt.loc, "undefined identifier `%s`, did you mean %s `%s`?", p, s2.kind(), s2.toChars());
             else if (const q = search_correct_C(id))
-                error(loc, "undefined identifier `%s`, did you mean `%s`?", p, q);
+                error(mt.loc, "undefined identifier `%s`, did you mean `%s`?", p, q);
             else if ((id == Id.This   && sc.getStructClassScope()) ||
                      (id == Id._super && sc.getClassScope()))
-                error(loc, "undefined identifier `%s`, did you mean `typeof(%s)`?", p, p);
+                error(mt.loc, "undefined identifier `%s`, did you mean `typeof(%s)`?", p, p);
             else
                 error(mt.loc, "undefined identifier `%s`", p);
         }
         else {
             if (const n = cIncludeHint(id.toString()))
-                error(loc, "`%s` is not defined, perhaps `#include %.*s` ?", p, cast(int)n.length, n.ptr);
+                error(mt.loc, "`%s` is not defined, perhaps `#include %.*s` ?", p, cast(int)n.length, n.ptr);
             else if (auto s2 = sc.search_correct(id))
-                error(loc, "undefined identifier `%s`, did you mean %s `%s`?", p, s2.kind(), s2.toChars());
+                error(mt.loc, "undefined identifier `%s`, did you mean %s `%s`?", p, s2.kind(), s2.toChars());
             else
-                error(loc, "undefined identifier `%s`", p);
+                error(mt.loc, "undefined identifier `%s`", p);
         }
 
         pt = Type.terror;
