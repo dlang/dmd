175b8f65c2c2a15d007e71b65b20e128942593bc
Mohamed El Shorbagy <mohrizq895@gmail.com>
2026-01-13 01:05:10 +0200

    Update headers

--- compiler/src/dmd/frontend.h
+++ compiler/src/dmd/frontend.h
@@ -7045,7 +7045,7 @@ public:
     bool explicitVisibility(bool v);
     Previews previews;
     UserAttributeDeclaration* userAttribDecl;
-    DocComment* lastdc;
+    void* lastdc;
     void* anchorCounts;
     Identifier* prevAnchor;
     AliasDeclaration* aliasAsg;
