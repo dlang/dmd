0043394218ab577f49b600b92c15e6ce31373a32
Sergii Kuzko <gorsing444@gmail.com>
2026-01-23 19:09:44 +0300

    fix

--- compiler/test/fail_compilation/array_literal_assign.d
+++ compiler/test/fail_compilation/array_literal_assign.d
@@ -1,17 +1,14 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/array_literal_assign.d(20): Error: cannot modify the content of array literal `[1, 2, 3]`
-fail_compilation/array_literal_assign.d(20): Error: cannot modify the content of array literal `[1, 2, 3]`
-fail_compilation/array_literal_assign.d(20): Error: discarded assignment to indexed array literal
-fail_compilation/array_literal_assign.d(22): Error: cannot modify the content of array literal `[1, 2]`
-fail_compilation/array_literal_assign.d(22): Error: discarded assignment to indexed array literal
+fail_compilation/array_literal_assign.d(17): Error: cannot modify the content of array literal `[1, 2, 3]`
+fail_compilation/array_literal_assign.d(17): Error: cannot modify the content of array literal `[1, 2, 3]`
+fail_compilation/array_literal_assign.d(19): Error: cannot modify the content of array literal `[1, 2]`
+fail_compilation/array_literal_assign.d(20): Error: cannot modify the content of array literal `[1, 2]`
+fail_compilation/array_literal_assign.d(21): Error: cannot modify the content of array literal `[1, 2]`
 fail_compilation/array_literal_assign.d(23): Error: cannot modify the content of array literal `[1, 2]`
-fail_compilation/array_literal_assign.d(23): Error: discarded assignment to indexed array literal
-fail_compilation/array_literal_assign.d(24): Error: cannot modify the content of array literal `[1, 2]`
-fail_compilation/array_literal_assign.d(24): Error: discarded assignment to indexed array literal
-fail_compilation/array_literal_assign.d(26): Error: cannot modify the content of array literal `[1, 2]`
-fail_compilation/array_literal_assign.d(26): Error: cannot modify the content of array literal `[1, 2]`
+fail_compilation/array_literal_assign.d(23): Error: cannot modify the content of array literal `[1, 2]`
+fail_compilation/array_literal_assign.d(23): Error: rvalue `__error` cannot be assigned to `ref x`
 ---
 */
 
--- compiler/test/fail_compilation/fail22328.d
+++ compiler/test/fail_compilation/fail22328.d
@@ -1,13 +1,11 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail22328.d(16): Error: cannot modify the content of array literal `[1, 2, 3]`
-fail_compilation/fail22328.d(16): Error: cannot modify the content of array literal `[1, 2, 3]`
-fail_compilation/fail22328.d(16): Error: discarded assignment to indexed array literal
-fail_compilation/fail22328.d(17): Error: cannot modify the content of array literal `[10, 20]`
-fail_compilation/fail22328.d(17): Error: cannot modify the content of array literal `[10, 20]`
-fail_compilation/fail22328.d(17): Error: discarded assignment to indexed array literal
-fail_compilation/fail22328.d(19): Error: cannot modify the content of array literal `[1, 2, 3]`
+fail_compilation/fail22328.d(14): Error: cannot modify the content of array literal `[1, 2, 3]`
+fail_compilation/fail22328.d(14): Error: cannot modify the content of array literal `[1, 2, 3]`
+fail_compilation/fail22328.d(15): Error: cannot modify the content of array literal `[10, 20]`
+fail_compilation/fail22328.d(15): Error: cannot modify the content of array literal `[10, 20]`
+fail_compilation/fail22328.d(17): Error: cannot modify the content of array literal `[1, 2, 3]`
 ---
 */
 
