2e113d691dbfdfd033ad6a0d87b638bf1028ce0d
CHIKI Badreddine <boderdin@github.com>
2026-03-24 01:05:26 +0100

    Add runnable test for valid static array parameters

---
+++ compiler/test/runnable/test22790.c
@@ -0,0 +1,12 @@
+﻿// https://issues.dlang.org/show_bug.cgi?id=22790
+
+void test_static(int array[static 4]) {
+    _Static_assert(sizeof(array) == sizeof(int*), "array must decay to pointer");
+}
+
+int main()
+{
+    int a[4];
+    test_static(a);
+    return 0;
+}
