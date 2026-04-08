f407cc975e9158d850b3ba6d00602d056997fabc
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2026-04-08 17:22:29 +0200

    C++ headers: Remove removed Scope::search() member function
    That function was converted to a free-standing function in v2.113.

--- compiler/src/dmd/scope.h
+++ compiler/src/dmd/scope.h
@@ -138,6 +138,4 @@ struct Scope final
     AliasDeclaration *aliasAsg; // if set, then aliasAsg is being assigned a new value,
                                 // do not set wasRead for it
     StructDeclaration *argStruct; // elimiate recursion when looking for rvalue construction
-
-    Dsymbol *search(Loc loc, Identifier *ident, Dsymbol *&pscopesym, SearchOptFlags flags = (SearchOptFlags)SearchOpt::all);
 };
