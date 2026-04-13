d065ed93cb501763f4ed711f15b97547841e73ce
Dennis Korpel <dkorpel@gmail.com>
2026-04-13 15:45:57 +0200

    Add expected output file to TEST_OUTPUT

--- compiler/test/compilable/test21707.d
+++ compiler/test/compilable/test21707.d
@@ -1,15 +1,35 @@
 // https://github.com/dlang/dmd/issues/21707
 // Segfault with -vcg-ast and foreach on a tuple sequence
 
-/*
+/+
 REQUIRED_ARGS: -vcg-ast -o-
 OUTPUT_FILES: compilable/test21707.d.cg
 TEST_OUTPUT:
 ---
 foobar(T)
 test()
+=== compilable/test21707.d.cg
+import object;
+template TypeTuple(T...)
+{
+	alias TypeTuple = T;
+}
+enum foobar(T) = 2;
+void test()
+{
+	/*unrolled*/ {
+		{
+			alias sym = foobar;
+		}
+	}
+	/*unrolled*/ {
+		{
+			alias sym = test;
+		}
+	}
+}
 ---
-*/
++/
 
 template TypeTuple(T...)
 {
