b4c9474a59d90d64d16d2e2118ab96892e19b324
Dennis Korpel <dkorpel@gmail.com>
2026-04-13 14:10:58 +0200

    Clarify semantics in spec

--- compiler/test/fail_compilation/template_alias_param.d
+++ compiler/test/fail_compilation/template_alias_param.d
@@ -4,19 +4,25 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/template_alias_param.d(17): Error: template instance `Foo!1` does not match template declaration `Foo(alias s : S)`
-fail_compilation/template_alias_param.d(18): Error: template instance `Foo!(T)` does not match template declaration `Foo(alias s : S)`
+fail_compilation/template_alias_param.d(19): Error: template instance `Foo!1` does not match template declaration `Foo(alias s : S)`
+fail_compilation/template_alias_param.d(20): Error: template instance `Foo!(T)` does not match template declaration `Foo(alias s : S)`
 ---
 */
+
 
 struct S {}
 struct T {}
 template Foo(alias s : S) {}
 
-alias F = Foo!S; // valid: matching type specialization (issue 20997)
-alias G = Foo!1; // invalid: expression should not match type specialization (issue 20998)
-alias H = Foo!T; // invalid: wrong type should not match (issue 20998)
+alias F  = Foo!S; // valid: matching type specialization (issue 20997)
+alias FC = Foo!(const(S));
+alias G  = Foo!1; // invalid: expression should not match type specialization (issue 20998)
+alias H  = Foo!T; // invalid: wrong type should not match (issue 20998)
 
 template Bar(alias s : 3) {}
 alias BG = Bar!3;
 alias BH = Bar!(1 + 2);
+
+template SpecConstS(alias s : const(S)) {}
+alias CS3 = SpecConstS!S;
+alias CS4 = SpecConstS!(const(S));
