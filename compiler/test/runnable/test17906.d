dlang/dmd

commit 0be1f3dfa6751262712069f62aa44659cd682517

Improve diagnostic messages by the integration with "kind 'fully.qualified.name'"

src/canthrow.d
@@ -65,32 +65,28 @@ extern (C++) bool canThrow(Expression e, FuncDeclaration func, bool mustNotThrow
              */
             Type t = ce.e1.type.toBasetype();
             if (ce.f && ce.f == func)
+                return;
+            if (t.ty == Tfunction && (cast(TypeFunction)t).isnothrow)
+                return;
+            if (t.ty == Tdelegate && (cast(TypeFunction)(cast(TypeDelegate)t).next).isnothrow)
+                return;
+
+            if (mustNotThrow)
             {
-            }
-            else if (t.ty == Tfunction && (cast(TypeFunction)t).isnothrow)
-            {
-            }
-            else if (t.ty == Tdelegate && (cast(TypeFunction)(cast(TypeDelegate)t).next).isnothrow)
-            {
-            }
-            else
-            {
-                if (mustNotThrow)
+                if (ce.f)
                 {
-                    const(char)* s;
-                    if (ce.f)
-                        s = ce.f.toPrettyChars();
-                    else if (ce.e1.op == TOKstar)
-                    {
-                        // print 'fp' if ce->e1 is (*fp)
-                        s = (cast(PtrExp)ce.e1).e1.toChars();
-                    }
-                    else
-                        s = ce.e1.toChars();
-                    ce.error("'%s' is not nothrow", s);
+                    ce.error("%s '%s' is not nothrow",
+                        ce.f.kind(), ce.f.toPrettyChars());
                 }
-                stop = true;
+                else
+                {
+                    auto e1 = ce.e1;
+                    if (e1.op == TOKstar)   // print 'fp' if e1 is (*fp)
+                        e1 = (cast(PtrExp)e1).e1;
+                    ce.error("'%s' is not nothrow", e1.toChars());
+                }
             }
+            stop = true;
         }
 
         override void visit(NewExp ne)
@@ -104,7 +100,10 @@ extern (C++) bool canThrow(Expression e, FuncDeclaration func, bool mustNotThrow
                     if (t.ty == Tfunction && !(cast(TypeFunction)t).isnothrow)
                     {
                         if (mustNotThrow)
-                            ne.error("allocator %s is not nothrow", ne.allocator.toChars());
+                        {
+                            ne.error("%s '%s' is not nothrow",
+                                ne.allocator.kind(), ne.allocator.toPrettyChars());
+                        }
                         stop = true;
                     }
                 }
@@ -113,7 +112,10 @@ extern (C++) bool canThrow(Expression e, FuncDeclaration func, bool mustNotThrow
                 if (t.ty == Tfunction && !(cast(TypeFunction)t).isnothrow)
                 {
                     if (mustNotThrow)
-                        ne.error("constructor %s is not nothrow", ne.member.toChars());
+                    {
+                        ne.error("%s '%s' is not nothrow",
+                            ne.member.kind(), ne.member.toPrettyChars());
+                    }
                     stop = true;
                 }
             }
@@ -156,7 +158,10 @@ extern (C++) bool canThrow(Expression e, FuncDeclaration func, bool mustNotThrow
                 if (t.ty == Tfunction && !(cast(TypeFunction)t).isnothrow)
                 {
                     if (mustNotThrow)
-                        de.error("destructor %s is not nothrow", ad.dtor.toChars());
+                    {
+                        de.error("%s '%s' is not nothrow",
+                            ad.dtor.kind(), ad.dtor.toPrettyChars());
+                    }
                     stop = true;
                 }
             }
@@ -166,7 +171,10 @@ extern (C++) bool canThrow(Expression e, FuncDeclaration func, bool mustNotThrow
                 if (t.ty == Tfunction && !(cast(TypeFunction)t).isnothrow)
                 {
                     if (mustNotThrow)
-                        de.error("deallocator %s is not nothrow", ad.aggDelete.toChars());
+                    {
+                        de.error("%s '%s' is not nothrow",
+                            ad.aggDelete.kind(), ad.aggDelete.toPrettyChars());
+                    }
                     stop = true;
                 }
             }
@@ -202,7 +210,10 @@ extern (C++) bool canThrow(Expression e, FuncDeclaration func, bool mustNotThrow
             else
             {
                 if (mustNotThrow)
-                    ae.error("'%s' is not nothrow", sd.postblit.toPrettyChars());
+                {
+                    ae.error("%s '%s' is not nothrow",
+                        sd.postblit.kind(), sd.postblit.toPrettyChars());
+                }
                 stop = true;
             }
         }

src/expression.d
@@ -2714,7 +2714,8 @@ public:
             FuncDeclaration ff = outerfunc;
             if (sc.flags & SCOPEcompile ? ff.isPureBypassingInference() >= PUREweak : ff.setImpure())
             {
-                error("pure function '%s' cannot call impure function '%s'", ff.toPrettyChars(), f.toPrettyChars());
+                error("pure %s '%s' cannot call impure %s '%s'",
+                    ff.kind(), ff.toPrettyChars(), f.kind(), f.toPrettyChars());
                 return true;
             }
         }
@@ -2774,7 +2775,8 @@ public:
                     break;
                 if (sc.flags & SCOPEcompile ? ff.isPureBypassingInference() >= PUREweak : ff.setImpure())
                 {
-                    error("pure function '%s' cannot access mutable static data '%s'", ff.toPrettyChars(), v.toChars());
+                    error("pure %s '%s' cannot access mutable static data '%s'",
+                        ff.kind(), ff.toPrettyChars(), v.toChars());
                     err = true;
                     break;
                 }
@@ -2820,7 +2822,8 @@ public:
                 {
                     if (ff.type.isImmutable())
                     {
-                        error("pure immutable nested function '%s' cannot access mutable data '%s'", ff.toPrettyChars(), v.toChars());
+                        error("pure immutable %s '%s' cannot access mutable data '%s'",
+                            ff.kind(), ff.toPrettyChars(), v.toChars());
                         err = true;
                         break;
                     }
@@ -2830,7 +2833,8 @@ public:
                 {
                     if (ff.type.isImmutable())
                     {
-                        error("pure immutable member function '%s' cannot access mutable data '%s'", ff.toPrettyChars(), v.toChars());
+                        error("pure immutable %s '%s' cannot access mutable data '%s'",
+                            ff.kind(), ff.toPrettyChars(), v.toChars());
                         err = true;
                         break;
                     }
@@ -2845,7 +2849,8 @@ public:
         {
             if (sc.func.setUnsafe())
             {
-                error("safe function '%s' cannot access __gshared data '%s'", sc.func.toChars(), v.toChars());
+                error("safe %s '%s' cannot access __gshared data '%s'",
+                    sc.func.kind(), sc.func.toChars(), v.toChars());
                 err = true;
             }
         }
@@ -2874,7 +2879,8 @@ public:
             {
                 if (loc.linnum == 0) // e.g. implicitly generated dtor
                     loc = sc.func.loc;
-                error("safe function '%s' cannot call system function '%s'", sc.func.toPrettyChars(), f.toPrettyChars());
+                error("safe %s '%s' cannot call system %s '%s'",
+                    sc.func.kind(), sc.func.toPrettyChars(), f.kind(), f.toPrettyChars());
                 return true;
             }
         }
@@ -2903,7 +2909,8 @@ public:
             {
                 if (loc.linnum == 0) // e.g. implicitly generated dtor
                     loc = sc.func.loc;
-                error("@nogc function '%s' cannot call non-@nogc function '%s'", sc.func.toPrettyChars(), f.toPrettyChars());
+                error("@nogc %s '%s' cannot call non-@nogc %s '%s'",
+                    sc.func.kind(), sc.func.toPrettyChars(), f.kind(), f.toPrettyChars());
                 return true;
             }
         }
@@ -9540,17 +9547,20 @@ public:
                 bool err = false;
                 if (!tf.purity && !(sc.flags & SCOPEdebug) && sc.func.setImpure())
                 {
-                    error("pure function '%s' cannot call impure %s '%s'", sc.func.toPrettyChars(), p, e1.toChars());
+                    error("pure %s '%s' cannot call impure %s '%s'",
+                        sc.func.kind(), sc.func.toPrettyChars(), p, e1.toChars());
                     err = true;
                 }
                 if (!tf.isnogc && sc.func.setGC())
                 {
-                    error("@nogc function '%s' cannot call non-@nogc %s '%s'", sc.func.toPrettyChars(), p, e1.toChars());
+                    error("@nogc %s '%s' cannot call non-@nogc %s '%s'",
+                        sc.func.kind(), sc.func.toPrettyChars(), p, e1.toChars());
                     err = true;
                 }
                 if (tf.trust <= TRUSTsystem && sc.func.setUnsafe())
                 {
-                    error("safe function '%s' cannot call system %s '%s'", sc.func.toPrettyChars(), p, e1.toChars());
+                    error("safe %s '%s' cannot call system %s '%s'",
+                        sc.func.kind(), sc.func.toPrettyChars(), p, e1.toChars());
                     err = true;
                 }
                 if (err)

src/nogc.d
@@ -72,7 +72,8 @@ public:
             return;
         if (f.setGC())
         {
-            e.error("array literal in @nogc function %s may cause GC allocation", f.toChars());
+            e.error("array literal in @nogc %s '%s' may cause GC allocation",
+                f.kind(), f.toPrettyChars());
             err = true;
             return;
         }
@@ -85,7 +86,8 @@ public:
             return;
         if (f.setGC())
         {
-            e.error("associative array literal in @nogc function %s may cause GC allocation", f.toChars());
+            e.error("associative array literal in @nogc %s '%s' may cause GC allocation",
+                f.kind(), f.toPrettyChars());
             err = true;
             return;
         }
@@ -105,7 +107,8 @@ public:
             return;
         if (f.setGC())
         {
-            e.error("cannot use 'new' in @nogc function %s", f.toChars());
+            e.error("cannot use 'new' in @nogc %s '%s'",
+                f.kind(), f.toPrettyChars());
             err = true;
             return;
         }
@@ -143,7 +146,8 @@ public:
 
         if (f.setGC())
         {
-            e.error("cannot use 'delete' in @nogc function %s", f.toChars());
+            e.error("cannot use 'delete' in @nogc %s '%s'",
+                f.kind(), f.toPrettyChars());
             err = true;
             return;
         }
@@ -157,7 +161,8 @@ public:
         {
             if (f.setGC())
             {
-                e.error("indexing an associative array in @nogc function %s may cause GC allocation", f.toChars());
+                e.error("indexing an associative array in @nogc %s '%s' may cause GC allocation",
+                    f.kind(), f.toPrettyChars());
                 err = true;
                 return;
             }
@@ -171,7 +176,8 @@ public:
         {
             if (f.setGC())
             {
-                e.error("setting 'length' in @nogc function %s may cause GC allocation", f.toChars());
+                e.error("setting 'length' in @nogc %s '%s' may cause GC allocation",
+                    f.kind(), f.toPrettyChars());
                 err = true;
                 return;
             }
@@ -183,7 +189,8 @@ public:
     {
         if (f.setGC())
         {
-            e.error("cannot use operator ~= in @nogc function %s", f.toChars());
+            e.error("cannot use operator ~= in @nogc %s '%s'",
+                f.kind(), f.toPrettyChars());
             err = true;
             return;
         }
@@ -194,7 +201,8 @@ public:
     {
         if (f.setGC())
         {
-            e.error("cannot use operator ~ in @nogc function %s", f.toChars());
+            e.error("cannot use operator ~ in @nogc %s '%s'",
+                f.kind(), f.toPrettyChars());
             err = true;
             return;
         }

test/fail_compilation/diag10319.d
@@ -5,8 +5,8 @@ fail_compilation/diag10319.d(25): Error: pure function 'D main' cannot call impu
 fail_compilation/diag10319.d(25): Error: safe function 'D main' cannot call system function 'diag10319.foo'
 fail_compilation/diag10319.d(26): Error: pure function 'D main' cannot call impure function 'diag10319.bar!int.bar'
 fail_compilation/diag10319.d(26): Error: safe function 'D main' cannot call system function 'diag10319.bar!int.bar'
-fail_compilation/diag10319.d(25): Error: 'diag10319.foo' is not nothrow
-fail_compilation/diag10319.d(26): Error: 'diag10319.bar!int.bar' is not nothrow
+fail_compilation/diag10319.d(25): Error: function 'diag10319.foo' is not nothrow
+fail_compilation/diag10319.d(26): Error: function 'diag10319.bar!int.bar' is not nothrow
 fail_compilation/diag10319.d(23): Error: function 'D main' is nothrow yet may throw
 ---
 */

test/fail_compilation/diag7050a.d
@@ -1,7 +1,7 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/diag7050a.d(5): Error: safe function 'diag7050a.foo' cannot call system function 'diag7050a.Foo.this'
+fail_compilation/diag7050a.d(5): Error: safe function 'diag7050a.foo' cannot call system constructor 'diag7050a.Foo.this'
 ---
 */
 

test/fail_compilation/diag7050c.d
@@ -1,7 +1,7 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/diag7050c.d(6): Error: safe function 'diag7050c.B.~this' cannot call system function 'diag7050c.A.~this'
+fail_compilation/diag7050c.d(6): Error: safe destructor 'diag7050c.B.~this' cannot call system destructor 'diag7050c.A.~this'
 ---
 */
 

test/fail_compilation/diag9148.d
@@ -3,10 +3,10 @@ TEST_OUTPUT:
 ---
 fail_compilation/diag9148.d(19): Error: pure function 'diag9148.test9148a.foo' cannot access mutable static data 'g'
 fail_compilation/diag9148.d(23): Error: pure function 'diag9148.test9148a.bar' cannot access mutable static data 'g'
-fail_compilation/diag9148.d(24): Error: pure immutable nested function 'diag9148.test9148a.bar' cannot access mutable data 'x'
+fail_compilation/diag9148.d(24): Error: pure immutable function 'diag9148.test9148a.bar' cannot access mutable data 'x'
 fail_compilation/diag9148.d(31): Error: pure function 'diag9148.test9148a.S.foo' cannot access mutable static data 'g'
 fail_compilation/diag9148.d(35): Error: pure function 'diag9148.test9148a.S.bar' cannot access mutable static data 'g'
-fail_compilation/diag9148.d(36): Error: pure immutable member function 'diag9148.test9148a.S.bar' cannot access mutable data 'x'
+fail_compilation/diag9148.d(36): Error: pure immutable function 'diag9148.test9148a.S.bar' cannot access mutable data 'x'
 ---
 */
 void test9148a() pure

test/fail_compilation/fail10254.d
@@ -1,10 +1,10 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/fail10254.d(18): Error: pure function 'fail10254.foo' cannot call impure function 'fail10254.C.this'
-fail_compilation/fail10254.d(18): Error: safe function 'fail10254.foo' cannot call system function 'fail10254.C.this'
-fail_compilation/fail10254.d(19): Error: pure function 'fail10254.foo' cannot call impure function 'fail10254.S.this'
-fail_compilation/fail10254.d(19): Error: safe function 'fail10254.foo' cannot call system function 'fail10254.S.this'
+fail_compilation/fail10254.d(18): Error: pure function 'fail10254.foo' cannot call impure constructor 'fail10254.C.this'
+fail_compilation/fail10254.d(18): Error: safe function 'fail10254.foo' cannot call system constructor 'fail10254.C.this'
+fail_compilation/fail10254.d(19): Error: pure function 'fail10254.foo' cannot call impure constructor 'fail10254.S.this'
+fail_compilation/fail10254.d(19): Error: safe function 'fail10254.foo' cannot call system constructor 'fail10254.S.this'
 ---
 */
 

commit decfa380fae18d699fc5362e9939239f690401e9

Fix regression with deleted catch(Exception) block
I think it was introduced in 0be1f3dfa67

when you have a recursive function, it stops trying to see if it throws
and thus incorrectly marks it as fallthrough. Then later, in statement
semantic you get into this block:

        /* If the try body never throws, we can eliminate any catches
         * of recoverable exceptions.
         */

and it elides the whole catch block, which leads to bizarre runtime
results.

See https://forum.dlang.org/thread/qohwofoqnooswcbgyzja@forum.dlang.org

compiler/include/dmd/declaration.h
@@ -675,6 +675,8 @@ public:
     bool hasInlineAsm(bool v);
     bool hasMultipleReturnExp() const;
     bool hasMultipleReturnExp(bool v);
+    bool hasInferredAttributes() const;
+    bool hasInferredAttributes(bool v);
 
     // Data for a function declaration that is needed for the Objective-C
     // integration.

compiler/src/dmd/canthrow.d
@@ -130,7 +130,7 @@ CT canThrow(Expression e, FuncDeclaration func, ErrorSink eSink)
              * then this expression cannot throw.
              * Note that pure functions can throw.
              */
-            if (ce.f && ce.f == func)
+            if (ce.f && ce.f == func && ce.f.hasInferredAttributes) // if recursive and inferred, leave it to inferrence
                 return;
             const tf = ce.calledFunctionType();
             if (tf && tf.isNothrow)

compiler/src/dmd/frontend.h
@@ -3835,6 +3835,8 @@ public:
     bool hasInlineAsm(bool v);
     bool hasMultipleReturnExp() const;
     bool hasMultipleReturnExp(bool v);
+    bool hasInferredAttributes() const;
+    bool hasInferredAttributes(bool v);
 private:
     uint32_t bitFields;
 public:

compiler/src/dmd/func.d
@@ -125,6 +125,8 @@ private struct FUNCFLAG
     bool hasReturnExp;         /// Has return exp; statement
     bool hasInlineAsm;         /// Has asm{} statement
     bool hasMultipleReturnExp; /// Has multiple return exp; statements
+
+    bool hasInferredAttributes; /// If it attempted to infer attributes
 }
 
 /***********************************************************

compiler/src/dmd/funcsem.d
@@ -1546,6 +1546,8 @@ private void initInferAttributes(FuncDeclaration fd)
 
     // Initialize for inferring STC.scope_
     fd.scopeInprocess = true;
+
+    fd.hasInferredAttributes = true;
 }
 
 /****************************************************

compiler/test/runnable/test17906.d
@@ -1,0 +1,20 @@
+void foo(bool shouldthrow)
+{
+    if(shouldthrow) {
+        throw new Exception("here");
+    }
+    try {
+        foo(true);
+    }
+    catch(Exception e) {
+        // happy
+    }
+    catch(Throwable t) {
+        assert(0, "should not reach here");
+    }
+}
+
+void main()
+{
+    foo(false);
+}