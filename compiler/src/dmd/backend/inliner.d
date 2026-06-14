8f403dc6687d48a2d22d9c1d80bbf95d0f9c2903
Walter Bright <WalterBright@users.noreply.github.com>
2026-06-14 13:34:23 -0700

    merge elem.d into el.d (#23261)

--- compiler/src/dmd/backend/inliner.d
+++ compiler/src/dmd/backend/inliner.d
@@ -39,7 +39,6 @@ import dmd.backend.el;
 import dmd.backend.global : REGSIZE;
 import dmd.backend.debugprint : tym_str;
 import dmd.backend.ee : eecontext_convs;
-import dmd.backend.elem : exp2_copytotemp;
 import dmd.backend.symbol : symbol_add, symbol_copy, symbol_print, globsym, SYMIDX;
 import dmd.backend.oper;
 import dmd.backend.ty;
