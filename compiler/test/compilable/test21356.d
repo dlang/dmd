8ce6d114ebdb48fbda560d970f0760e154c23b70
0-v-0 <kduice@qq.com>
2026-06-29 10:38:05 +0800

    Support mixin templates of traits and string mixin

--- compiler/test/compilable/test21356.d
+++ compiler/test/compilable/test21356.d
@@ -8,3 +8,8 @@ alias AliasSeq(A...) = A;
 alias thing = AliasSeq!(A);
 
 mixin thing[0].Foo;
+mixin typeof(thing)[0].Foo;
+
+mixin template M() {}
+mixin mixin("M");
+mixin __traits(getMember, A, "Foo");
