aff4e2700b714c4c994fab5bac1cf4c85433dae7
Nick Treleaven <ntrel002@gmail.com>
2026-07-17 10:15:35 +0100

    Rename `main`

--- compiler/test/compilable/class_struct_disable_default.d
+++ compiler/test/compilable/class_struct_disable_default.d
@@ -2,7 +2,8 @@
 struct Parser { @disable this(); this(int) {} }
 class Client { Parser parser; this() { parser = Parser(0); } }
 void sink(out Client r) {}
-void main() {
+
+void test() {
     Client c;
     // passing c just sets it to null, so no @disable error needed
     sink(c);
