400f45d2fe780328d69e2999d0fca27d9c9893ff
Demetrius Kanios <demetrius@kanios.net>
2026-07-18 11:44:37 -0700

    druntime: Move `accumulatePure` from `core.internal.array` to `rt.profilegc` (#23408)

--- druntime/src/core/internal/array/utils.d
+++ druntime/src/core/internal/array/utils.d
@@ -21,23 +21,15 @@ auto gcStatsPure() nothrow pure
     return impureBypass();
 }
 
-ulong accumulatePure(string file, int line, string funcname, string name, ulong size) nothrow pure
-{
-    static ulong impureBypass(string file, int line, string funcname, string name, ulong size) @nogc nothrow
-    {
-        import core.internal.traits : externDFunc;
-
-        alias accumulate = externDFunc!("rt.profilegc.accumulate", void function(string file, uint line, string funcname, string type, ulong sz) @nogc nothrow);
-        accumulate(file, line, funcname, name, size);
-        return size;
-    }
-
-    auto func = cast(ulong function(string file, int line, string funcname, string name, ulong size) @nogc nothrow pure)&impureBypass;
-    return func(file, line, funcname, name, size);
-}
-
 version (D_ProfileGC)
 {
+    // Cannot use `externD` template, because then it will cause
+    // cyclic dependencies (and error out) as CTFE string appending
+    // expands runtime hooks, which on -profilegc call this.
+    pragma(mangle, "_D2rt9profilegc14accumulatePureFNaNbNiAyaiQeQgmZm")
+    ulong accumulatePure(string file, int line, string funcname, string name, ulong size) @nogc nothrow pure;
+
+
     /**
      * TraceGC wrapper generator around the runtime hook `Hook`.
      * Params:
