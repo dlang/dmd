6779da5ffb75cfca256726d4f8d62310fc9fadb0
Rainer <r.sagitario@gmx.de>
2026-07-21 20:30:00 +0200

    fix #23431 - Windows: libraries built with debug symbols often emit warnings about multiple objects of the same name
    emit fully qualified object name into library

---
+++ druntime/test/link/test23431.d
@@ -0,0 +1,7 @@
+// https://github.com/dlang/dmd/issues/23431
+// test linking against debug build of druntime.lib producing
+// druntime.lib(package.obj) : warning LNK4255: library contain multiple objects of the same name; linking object as if no debug info
+
+void main()
+{
+}
