90237dccf9588c9d7e8917fb3089124ceea7203d
Denis Feklushkin <feklushkin.denis@gmail.com>
2026-07-31 13:14:58 +0300

    Mem.allocateBlank() added

--- druntime/src/core/system.d
+++ druntime/src/core/system.d
@@ -3,11 +3,18 @@ module core.system;
 
 package:
 
-static struct Mem
+struct Mem
 {
     import core.stdc.stdlib;
 
     alias allocate = malloc;
     alias reallocate = realloc;
     alias free = core.stdc.stdlib.free;
+
+    static void* allocateBlank(size_t size) nothrow @nogc
+    {
+        ubyte* p = cast(ubyte*) allocate(size);
+        p[0 .. size] = 0;
+        return cast(void*) p;
+    }
 }
