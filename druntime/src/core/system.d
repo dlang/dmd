8ae2a8f879f93b7c9a0ca31e0d3c36ea6c349f11
Denis Feklushkin <feklushkin.denis@gmail.com>
2026-08-01 12:08:31 +0300

    allocateBlank() commented-out code removed

--- druntime/src/core/system.d
+++ druntime/src/core/system.d
@@ -11,17 +11,7 @@ struct Mem
     alias allocate = malloc;
     alias reallocate = realloc;
     alias free = core.stdc.stdlib.free;
-
     static void* allocateBlank(size_t size) nothrow @nogc => calloc(1, size);
-
-    /+
-    static void* allocateBlank(size_t size) nothrow @nogc
-    {
-        ubyte* p = cast(ubyte*) allocate(size);
-        p[0 .. size] = 0;
-        return cast(void*) p;
-    }
-    +/
 
     alias allocateOnStack = alloca;
 }
