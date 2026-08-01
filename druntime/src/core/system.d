85de307380045005e59b49e2875a7dfb747486c9
Denis Feklushkin <feklushkin.denis@gmail.com>
2026-08-01 12:29:34 +0300

    allocateFew() and allocateFewBlank() added

--- druntime/src/core/system.d
+++ druntime/src/core/system.d
@@ -9,9 +9,11 @@ struct Mem
     import core.stdc.stdlib;
 
     alias allocateOne = malloc;
+    alias allocateFew = (size_t num, size_t size) => allocateOne(num * size);
+    static void* allocateOneBlank(size_t size) nothrow @nogc => allocateFewBlank(1, size);
+    alias allocateFewBlank = calloc;
     alias reallocate = realloc;
     alias free = core.stdc.stdlib.free;
-    static void* allocateOneBlank(size_t size) nothrow @nogc => calloc(1, size);
 
     alias allocateOnStack = alloca;
 }
