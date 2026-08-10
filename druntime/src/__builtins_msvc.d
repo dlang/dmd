71e2773e903ae0d7e8a81015d69d42e6d8ef1adb
Harry Gillanders <contact@harrygillanders.com>
2026-08-10 20:05:10 +0100

    Prevent LDC from emitting `'+prfchw' is not a recognized feature for this target (ignoring feature)` when targeting AArch64 in `__builtins_msvc.d`.

--- druntime/src/__builtins_msvc.d
+++ druntime/src/__builtins_msvc.d
@@ -1357,9 +1357,18 @@ version (MSVCIntrinsics)
         }
     }
 
+    version (X86_64_Or_X86)
+    {
+        /* prfchw forces prefetchw to be emitted on x86 (which is how MSVC behaves). */
+        private enum prefetchWriteTarget = "prfchw";
+    }
+    else
+    {
+        private enum prefetchWriteTarget = "";
+    }
+
     /* This is trusted so that it's @safe without DIP1000 enabled. */
-    /* prfchw forces prefetchw to be emitted on x86 (which is how MSVC behaves). */
-    @llvm_target("prfchw")
+    @llvm_target(prefetchWriteTarget)
     extern(C)
     pragma(inline, true)
     private void prefetchData(bool write, ubyte level)(scope const(void)* address) @trusted pure nothrow @nogc
