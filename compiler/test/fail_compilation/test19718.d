8e44852fb5f20bca196d1bb01a5f2805ad069bf6
usefahmed07 <youssef.ahmed.hamaza@gmail.com>
2026-08-26 15:00:42 +0300

    Simplify regression test for issue 19718 (remove main)

--- compiler/test/fail_compilation/test19718.d
+++ compiler/test/fail_compilation/test19718.d
@@ -1,7 +1,7 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/test19718.d(29): Error: none of the overloads of template `test19718.execute` are callable using argument types `!()(int function(ref Struct rng) @system)`
+fail_compilation/test19718.d(27): Error: none of the overloads of template `test19718.execute` are callable using argument types `!()(int function(ref Struct rng) @system)`
 fail_compilation/test19718.d(15):        Candidates are: `execute(T)(T function(ref Struct) @safe dg)`
 fail_compilation/test19718.d(21):                        `execute(T)(T delegate(ref Struct) @safe dg)`
 ---
@@ -24,7 +24,4 @@ public T execute (T)(T delegate(ref Struct) @safe dg)
     return dg(rng);
 }
 
-void main (string[] args)
-{
-    execute((ref Struct rng) { return rng.get(); });
-}
+auto x = execute((ref Struct rng) { return rng.get(); });
