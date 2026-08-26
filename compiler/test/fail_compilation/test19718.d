11d583ffb88a462f1fedadc19556a2636e46e4de
usefahmed07 <youssef.ahmed.hamaza@gmail.com>
2026-08-26 14:58:43 +0300

    Add regression test for issue 19718

---
+++ compiler/test/fail_compilation/test19718.d
@@ -0,0 +1,30 @@
+/*
+TEST_OUTPUT:
+---
+fail_compilation/test19718.d(29): Error: none of the overloads of template `test19718.execute` are callable using argument types `!()(int function(ref Struct rng) @system)`
+fail_compilation/test19718.d(15):        Candidates are: `execute(T)(T function(ref Struct) @safe dg)`
+fail_compilation/test19718.d(21):                        `execute(T)(T delegate(ref Struct) @safe dg)`
+---
+*/
+
+struct Struct
+{
+    int get() { return 1; }
+}
+
+public T execute (T)(T function(ref Struct) @safe dg)
+{
+    Struct rng;
+    return dg(rng);
+}
+
+public T execute (T)(T delegate(ref Struct) @safe dg)
+{
+    Struct rng;
+    return dg(rng);
+}
+
+void main (string[] args)
+{
+    execute((ref Struct rng) { return rng.get(); });
+}
