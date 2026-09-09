de29b06543236fcaeb9e9a946fcd220a16b74a59
usefahmed07 <youssef.ahmed.hamaza@gmail.com>
2026-09-09 20:24:47 +0300

    test: fix line numbers in fail20012.d after adding DISABLED directive
    Adding the DISABLED: linux32 win32 line shifted the source code
    down by one line, so the expected error line numbers (10, 11)
    no longer matched the actual output (11, 12). Update the
    expected TEST_OUTPUT line numbers accordingly.

--- compiler/test/fail_compilation/fail20012.d
+++ compiler/test/fail_compilation/fail20012.d
@@ -2,10 +2,10 @@
 DISABLED: linux32 win32
 TEST_OUTPUT:
 ---
-fail_compilation/fail20012.d(10): Error: array cast from `string` to `dstring` is not supported at compile time
+fail_compilation/fail20012.d(11): Error: array cast from `string` to `dstring` is not supported at compile time
 $p:druntime/import/core/internal/array/casting.d$($n$): Error: `immutable(char)[]` of length 3 cannot be cast to `immutable(dchar)[]` as its length in bytes (3) is not a multiple of `immutable(dchar).sizeof` (4).
 $p:druntime/import/core/internal/array/casting.d$($n$):        called from here: `onArrayCastError("immutable(char)", fromSize, from.length, "immutable(dchar)", 4LU)`
-fail_compilation/fail20012.d(11):        called from here: `__ArrayCast("huh"c)`
+fail_compilation/fail20012.d(12):        called from here: `__ArrayCast("huh"c)`
 ---
 */
 enum a = cast(dstring) "what"c;
