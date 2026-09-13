6dfc0e5e8e2a68595967c9bd3fa1c5c3ddd2c002
usefahmed07 <youssef.ahmed.hamaza@gmail.com>
2026-09-14 00:56:21 +0300

    Turn 'assert/static assert condition cannot be a string literal' into an error
    This was deprecated since 2.107 (boolean evaluation of a string
    literal, e.g. assert("message") silently having no effect when
    0 was meant). The deprecation period is long over; make it a hard
    error, following the pattern used in dmd#23596.

--- compiler/test/fail_compilation/array_bool.d
+++ compiler/test/fail_compilation/array_bool.d
@@ -1,11 +1,10 @@
 /*
-REQUIRED_ARGS: -de
 TEST_OUTPUT:
 ---
-fail_compilation/array_bool.d(13): Deprecation: assert condition cannot be a string literal
+fail_compilation/array_bool.d(12): Error: assert condition cannot be a string literal
+fail_compilation/array_bool.d(12):        If intentional, use `"foo" !is null` instead to preserve behaviour
+fail_compilation/array_bool.d(13): Error: static assert condition cannot be a string literal
 fail_compilation/array_bool.d(13):        If intentional, use `"foo" !is null` instead to preserve behaviour
-fail_compilation/array_bool.d(14): Deprecation: static assert condition cannot be a string literal
-fail_compilation/array_bool.d(14):        If intentional, use `"foo" !is null` instead to preserve behaviour
 ---
 */
 void main()
