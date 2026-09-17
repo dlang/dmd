420a61e4161e6657f253c61584a495ae27530fd9
Martin Kinkelin <noone@nowhere.com>
2026-09-18 00:57:11 +0200

    Windows: Skip 3 .sh 'compilable' tests depending on nm, if nm isn't in PATH
    Seems like the Windows CI runners have a compatible `nm` tool in PATH,
    so that we haven't seen any failures yet. But users might not have it,
    so just skip these tests (but only on Windows) checking object-file
    symbols.

--- compiler/test/compilable/extra-files/minimal/verify_symbols.sh
+++ compiler/test/compilable/extra-files/minimal/verify_symbols.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+if [[ $OS = "win"* ]]; then
+    for tool in nm objdump; do
+        if ! which -s $tool; then
+            echo "No '$tool' tool found in PATH, skipping symbols verification on Windows."
+            exit 0
+        fi
+    done
+fi
+
+obj_file="${RESULTS_TEST_DIR}/d/${TEST_NAME}_0${OBJ}"
+
 # ensure no ModuleInfo or TypeInfo related code was generated
-if ! nm "$1" | grep -q 'ModuleInfo\|_d_dso_registry\__start_minfo\|__stop_minfo\|TypeInfo' ; then
+if nm "${obj_file}" | (! grep -q 'ModuleInfo\|_d_dso_registry\__start_minfo\|__stop_minfo\|TypeInfo') ; then
     # ensure no exception handling code was generated
-    if ! objdump -h "$1" | grep -q ".eh_frame" ; then
+    if objdump -h "${obj_file}" | (! grep -q ".eh_frame") ; then
         exit 0
     fi
 fi
--- compiler/test/compilable/test19463.sh
+++ compiler/test/compilable/test19463.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+if [[ $OS = "win"* ]] && ! which -s nm; then
+    echo 'No `nm` tool found in PATH, skipping test on Windows.'
+    exit 0
+fi
 
 $DMD -c -preview=dip1008 -m${MODEL} -of${OUTPUT_BASE}${OBJ} -I${EXTRA_FILES} ${EXTRA_FILES}/${TEST_NAME}.d
-!(nm ${OUTPUT_BASE}${OBJ} | grep _d_newclass)
+nm ${OUTPUT_BASE}${OBJ} | (! grep _d_newclass)
 
 rm_retry ${OUTPUT_BASE}${OBJ}
