638c5016d07407a98acfa4460d0c21ee8d3819c0
Martin Kinkelin <noone@nowhere.com>
2026-09-18 18:11:09 +0200

    [alright then, brace groups FTW]

--- compiler/test/compilable/test19463.sh
+++ compiler/test/compilable/test19463.sh
@@ -8,4 +8,4 @@ fi
 $DMD -c -preview=dip1008 -m${MODEL} -of${OUTPUT_BASE}${OBJ} -I${EXTRA_FILES} ${EXTRA_FILES}/${TEST_NAME}.d
 nm ${OUTPUT_BASE}${OBJ} | { ! grep _d_newclass; }
 
-rm_retry ${OUTPUT_BASE}${OBJ} ${OUTPUT_BASE}.symbols
+rm_retry ${OUTPUT_BASE}${OBJ}
