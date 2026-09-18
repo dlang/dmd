0b4102adf9cfedcdd6cc4ec236de009cffd1c94d
Martin Kinkelin <noone@nowhere.com>
2026-09-18 15:58:06 +0200

    [troubleshoot problem on Mac]

--- compiler/test/compilable/test19463.sh
+++ compiler/test/compilable/test19463.sh
@@ -6,6 +6,9 @@ if [[ $OS = "win"* ]] && ! which -s nm; then
 fi
 
 $DMD -c -preview=dip1008 -m${MODEL} -of${OUTPUT_BASE}${OBJ} -I${EXTRA_FILES} ${EXTRA_FILES}/${TEST_NAME}.d
+echo ".: ================================================="
+nm ${OUTPUT_BASE}${OBJ}
+echo ".: ================================================="
 nm ${OUTPUT_BASE}${OBJ} | (! grep _d_newclass)
 
 rm_retry ${OUTPUT_BASE}${OBJ}
